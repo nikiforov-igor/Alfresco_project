9410b8214d29ac852f2a1f1594ed330b8705e1ff
Махмуд Шафеев <MShafeev@it.ru>
2012-09-26 12:05:47 +0000

    Json для получения значений таблицы
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@71 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-dictionary/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/dictionary/items.get.js
@@ -0,0 +1,33 @@
+var query = "";
+if (args["nodeRef"] == null || args["nodeRef"] == "") {
+	query = "PATH: \"/app:company_home/cm:Dictionary/cm:Organization/*\"";
+} else {
+	query = "PARENT: \"" + args["nodeRef"] + "\"";
+}
+
+var sort = {
+	column: "@{http://www.alfresco.org/model/content/1.0}name",
+	ascending: true
+}
+
+var def = {
+	query: query + " AND TYPE: \"lecm-dic:dictionary_values\"",
+	sort: [sort]
+}
+var dictionary = search.query(def);
+
+var items = [];
+
+model.items = items;
+
+for each(var item in dictionary) {
+	name = item.properties["lecm-dic:name_values"];print(name)
+	description = item.properties["lecm-dic:description_values"]; print(description);
+	active = item.properties["lecm-dic:active_values"]; print(active);
+
+	items.push({
+		name:name,
+		description:description,
+		active:active
+	})
+}
