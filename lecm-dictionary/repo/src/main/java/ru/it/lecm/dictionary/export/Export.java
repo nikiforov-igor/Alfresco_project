14866fb381b82dfa86c5cf0aa995eabf82a62ea9
Зиновин Александр <AZinovin@it.ru>
2012-11-02 13:03:24 +0000

    экспорт в XML - рефакторинг
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@299 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-dictionary/repo/src/main/java/ru/it/lecm/dictionary/export/Export.java
+++ lecm-dictionary/repo/src/main/java/ru/it/lecm/dictionary/export/Export.java
@@ -1,26 +1,19 @@
 package ru.it.lecm.dictionary.export;
 
-import org.alfresco.model.ContentModel;
-import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.namespace.QName;
+import org.alfresco.service.namespace.NamespaceService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.extensions.webscripts.AbstractWebScript;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
 
-import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-//import javax.xml.stream.*;
 
 /**
  * User: mShafeev
@@ -30,50 +23,15 @@ import java.util.Set;
 public class Export extends AbstractWebScript {
 
 	private static final Log log = LogFactory.getLog(Export.class);
-
-	protected NodeService nodeService;
+	private NodeService nodeService;
+	private NamespaceService namespaceService;
 
 	public void setNodeService(NodeService nodeService) {
 		this.nodeService = nodeService;
 	}
 
-	public void collectNodes(NodeRef node, XMLStreamWriter xmlw, ArrayList<String> namespace,
-	                         String[] fields
-	) throws XMLStreamException {
-//        list.add(node);
-		List<ChildAssociationRef> childAssocs = nodeService.getChildAssocs(node);
-//        xmlw.writeStartElement("items");
-		if (!childAssocs.isEmpty()) {
-			xmlw.writeStartElement("items");
-			{
-				for (ChildAssociationRef subNodeAss : childAssocs) {
-					xmlw.writeStartElement("item");
-					{
-
-
-						String name = nodeService.getProperty(subNodeAss.getChildRef(), ContentModel.PROP_NAME).toString();
-						xmlw.writeAttribute("name", name);
-						Set set = nodeService.getProperties(subNodeAss.getChildRef()).entrySet();
-						for (Object aSet : set) {
-							Map.Entry m = (Map.Entry) aSet;
-							QName key = (QName) m.getKey();
-							String value = m.getValue().toString();
-							for (int i = 0; i < namespace.size(); i++) {
-								if (namespace.get(i).equals(key.getLocalName())) {
-									xmlw.writeStartElement("property");
-									xmlw.writeAttribute("name", fields[i]);
-									xmlw.writeCharacters(value);
-									xmlw.writeEndElement();
-								}
-							}
-						}
-					}
-					collectNodes(subNodeAss.getChildRef(), xmlw, namespace, fields);
-					xmlw.writeEndElement();
-				}
-			}
-			xmlw.writeEndElement();
-		}
+	public void setNamespaceService(NamespaceService namespaceService) {
+		this.namespaceService = namespaceService;
 	}
 
 	@Override
@@ -81,50 +39,19 @@ public class Export extends AbstractWebScript {
 
 		OutputStream resOutputStream = null;
 		try {
-			ArrayList<String> namespace = new ArrayList<String>();
-			String[] fields = req.getParameterValues("field");
-            String[] selectItems = req.getParameterValues("selectedItems");
+			List<String> fields = Arrays.asList(req.getParameterValues("field"));
 			String nodeRefStr = req.getParameter("nodeRef");
-			for (String field : fields) {
-				namespace.add(field.split(":")[1]);
-			}
 			NodeRef nodeRef = new NodeRef(nodeRefStr);
-
-			String name = nodeService.getProperty(nodeRef, ContentModel.PROP_NAME).toString();
-
-
-			// Create an output factory
-			XMLOutputFactory xmlof = XMLOutputFactory.newInstance();
 
 			res.setContentEncoding("UTF-8");
 			res.setContentType("text/xml");
 			res.addHeader("Content-Disposition", "attachment; filename=dictionary.xml");
 			// Create an XML stream writer
 			resOutputStream = res.getOutputStream();
-			XMLStreamWriter xmlw = xmlof.createXMLStreamWriter(resOutputStream);
-			xmlw.writeStartDocument("1.0");
-			{
-				xmlw.writeStartElement("dictionary");
-				{
-					xmlw.writeAttribute("name", name);
-					xmlw.writeStartElement("namespaceURI");
-					{
-						xmlw.writeAttribute("name", nodeService.getType(nodeService.getChildAssocs(nodeRef).get(0).getChildRef()).getNamespaceURI());
-					}
-					xmlw.writeEndElement();
-					xmlw.writeStartElement("type");
-					{
-						xmlw.writeAttribute("name", nodeService.getProperty(nodeRef, QName.createQName(nodeService.getType(nodeRef).getNamespaceURI(),"type")).toString().split(":")[1]);
-					}
-					xmlw.writeEndElement();
-					//Обход по дереву
-					collectNodes(nodeRef, xmlw, namespace, fields);
-				}
-				xmlw.writeEndElement();
-			}
-			xmlw.writeEndDocument();
-			// Close the writer to flush the output
-			xmlw.close();
+
+			XmlDictionaryExporter xmlDictionaryExporter = new XmlDictionaryExporter(resOutputStream, fields, nodeService, namespaceService);
+			xmlDictionaryExporter.writeDictionary(nodeRef);
+			xmlDictionaryExporter.close();
 			resOutputStream.flush();
 		} catch (XMLStreamException e) {
 			e.printStackTrace();
@@ -137,5 +64,4 @@ public class Export extends AbstractWebScript {
 		}
 		log.info("Export complete");
 	}
-
 }
---
+++ lecm-dictionary/repo/src/main/java/ru/it/lecm/dictionary/export/XmlDictionaryExporter.java
@@ -0,0 +1,120 @@
+package ru.it.lecm.dictionary.export;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.NamespacePrefixResolver;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class XmlDictionaryExporter {
+
+	private XMLStreamWriter xmlw;
+	private List<String> fields;
+	private NodeService nodeService;
+	private NamespacePrefixResolver namespaceService;
+	private static final Set<String> ignoredNamespaces = new HashSet<String>();
+
+	static {
+		ignoredNamespaces.add(NamespaceService.CONTENT_MODEL_1_0_URI);
+		ignoredNamespaces.add(NamespaceService.SYSTEM_MODEL_1_0_URI);
+	}
+
+	public XmlDictionaryExporter(OutputStream resOutputStream, List<String> fields, NodeService nodeService, NamespaceService namespaceService) throws XMLStreamException {
+		// Create an output factory
+		XMLOutputFactory xmlof = XMLOutputFactory.newInstance();
+		xmlw = xmlof.createXMLStreamWriter(resOutputStream);
+		this.fields = fields;
+		this.nodeService = nodeService;
+		this.namespaceService = namespaceService;
+		xmlw.writeStartDocument("1.0");
+	}
+
+	public void writeDictionary(NodeRef nodeRef) throws XMLStreamException {
+		xmlw.writeStartElement("dictionary");
+		{
+			String name = nodeService.getProperty(nodeRef, ContentModel.PROP_NAME).toString();
+			xmlw.writeAttribute("name", name);
+			xmlw.writeStartElement("namespaceURI");
+			{
+				xmlw.writeAttribute("name", nodeService.getType(nodeService.getChildAssocs(nodeRef).get(0).getChildRef()).getNamespaceURI());
+			}
+			xmlw.writeEndElement();
+			xmlw.writeStartElement("type");
+			{
+				xmlw.writeAttribute("name", nodeService.getProperty(nodeRef, QName.createQName(nodeService.getType(nodeRef).getNamespaceURI(), "type")).toString().split(":")[1]);
+			}
+			xmlw.writeEndElement();
+			writeProperties(nodeRef, true);
+			//Обход по дереву
+			writeItems(nodeRef);
+		}
+		xmlw.writeEndElement();
+
+	}
+
+	private void writeItems(NodeRef node) throws XMLStreamException {
+		List<ChildAssociationRef> childAssocs = nodeService.getChildAssocs(node);
+		if (!childAssocs.isEmpty()) {
+			xmlw.writeStartElement("items");
+			{
+				for (ChildAssociationRef subNodeAss : childAssocs) {
+					NodeRef childRef = subNodeAss.getChildRef();
+					writeItem(childRef);
+				}
+			}
+			xmlw.writeEndElement();
+		}
+	}
+
+	private void writeItem(NodeRef childRef) throws XMLStreamException {
+		xmlw.writeStartElement("item");
+		{
+			String name = nodeService.getPrimaryParent(childRef).getQName().getLocalName();
+			xmlw.writeAttribute("name", name);
+			writeProperties(childRef);
+		}
+		writeItems(childRef);
+		xmlw.writeEndElement();
+	}
+
+	private void writeProperties(NodeRef childRef) throws XMLStreamException {
+		writeProperties(childRef, false);
+	}
+
+	private void writeProperties(NodeRef childRef, boolean doNotFilterFields) throws XMLStreamException {
+		//экспорт свойств справочника
+		for (Map.Entry<QName, Serializable> entry : nodeService.getProperties(childRef).entrySet()) {
+			QName qName = entry.getKey().getPrefixedQName(namespaceService);
+			String value = entry.getValue().toString();
+			if ((doNotFilterFields && !ignoredNamespaces.contains(qName.getNamespaceURI()))
+				|| (!doNotFilterFields && fields != null && fields.contains(qName.getPrefixString()))) {
+				writeProperty(qName, value);
+			}
+		}
+	}
+
+	private void writeProperty(QName name, String value) throws XMLStreamException {
+		xmlw.writeStartElement("property");
+		xmlw.writeAttribute("name", name.toPrefixString());
+		xmlw.writeCharacters(value);
+		xmlw.writeEndElement();
+	}
+
+	public void close() throws XMLStreamException {
+		xmlw.writeEndDocument();
+		// Close the writer to flush the output
+		xmlw.close();
+	}
+}
