5481a40509b2c52f0523b3965fc70f4aa3036275
Катаманов Андрей <AKatamanov@it.ru>
2012-11-06 06:31:52 +0000

    Всё работает!!!
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@308 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-theme/share/src/main/webapp/scripts/jsButton.js
+++ lecm-theme/share/src/main/webapp/scripts/jsButton.js
@@ -38,22 +38,91 @@ LogicECM.module.Header = LogicECM.module.Header || {};
             this,
             "LogicECM.module.Header.JsButton",
             htmlId,
-            ["button", "container", "connection", "json", "selector"]);
+            ["button", "menu", "container", "connection", "json", "selector"]);
     };
 
     YAHOO.lang.extend(LogicECM.module.Header.JsButton, Alfresco.component.Base, {
 
-        self:null,
+        context:null,
+
+        location: {href:"#"},
 
         onReady: function () {
-            self = this;
+            context = this;
 
             Alfresco.logger.info ("A new LogicECM.module.Header.JsButton has been created");
 
+
+
+            context.createNotifyer();
+            context.startFakeTimer();
+
+            context.initMenu();
+
+
+
+        },
+
+        createNotifyer: function(){
             var btn = YAHOO.util.Dom.get(this.id);
             var div=document.createElement("div");
             div.innerHTML='<div id="myElem">1<div/>';
             btn.appendChild(div);
+        },
+
+        startFakeTimer : function(){
+            var counter = 0;
+
+            function increaseTimer(){
+                var elem = YAHOO.util.Dom.get("myElem");
+                counter++;
+                if (counter > 25){
+                    counter =0;
+
+                }
+                elem.innerHTML = counter;
+            }
+
+            setInterval(increaseTimer, 1500);
+
+
+        },
+
+        go : function(){
+
+            var container = YAHOO.util.Dom.get('myPassword');
+            alert(container.value);
+
+        },
+
+        initMenu : function(){
+
+            // Inject the template from the XHR request into a new DIV element
+            var containerDiv = document.createElement("div");
+            containerDiv.innerHTML =
+                '<div id="jsButton-menu" class="yuimenu menu-with-icons yui-overlay yui-overlay-hidden">' +
+                    '   <div class="bd"> '+
+                    '       <ul> ' +
+                    '           <li> '+
+                    '               <a href="#" onclick=\'alert("hello"); return false;\'>Hello!</a>' +
+                    '           </li>' +
+                    '       </ul>' +
+                    '       <div> <input id="myPassword" type="password"/></div>' +
+                    '       <div> <div  id="' + context.id +'-goBtn"></div></div>' +
+                    '   </div>'+
+                    '</div>';
+            document.body.insertBefore(containerDiv, document.body.firstChild);
+
+            this.widgets.myButton = new YAHOO.widget.Button(this.id,
+                {
+                    type: "menu",
+                    //menu: this.id + "-sites-menu",
+                    menu: "jsButton-menu",
+                    lazyloadmenu: false
+                });
+
+            var container = YAHOO.util.Dom.get(context.id);
+            Alfresco.util.createYUIButton(container, "goBtn", context.go, {label: "создать доверенность"});
 
         }
     });
