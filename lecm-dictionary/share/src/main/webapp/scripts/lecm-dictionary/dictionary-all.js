b7038cc582ce7ea4412cb33afcd8c0dd7d3b1c1c
Махмуд Шафеев <MShafeev@it.ru>
2012-11-20 13:46:50 +0000

    DataGrid добавлена сортировка по умолчанию. В моделях добавлены настроки индексации для правильной сортировки в DataGrid по требуемым столбцам.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@465 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/dictionary-all.js
+++ lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/dictionary-all.js
@@ -7,7 +7,19 @@
 	YAHOO.Bubbling.fire("activeGridChanged",
 		{
 			datagridMeta:{
-				itemType:"lecm-dic:dictionary"
+				itemType:"lecm-dic:dictionary",
+                searchConfig:{ //настройки поиска (необязателен)
+                    filter:"",
+                    /** Настройки полнотекстового поиска. Пример объекта:
+                     {
+                     parentNodeRef - относительно какой директории искать (чаще всего совпадает с datagridMeta.nodeRef
+                     fields - какие свойства объекта следует заполнить и вернуть
+                     searchTerm - строка для поиска
+                     }
+                     */
+                    fullTextSearch: null,
+                    sort: "cm:name|true" // сортировка. Указываем по какому полю и порядок (true - asc), например, cm:name|true
+                }
 			}
 		});
 	}
