ed8945eada8af3535078020a5718604c96a729ac
Ивкин Алексей <AIvkin@it.ru>
2012-11-22 08:47:55 +0000

    В перечень справочников выводятся справочники, расположенные в папке /app:company_home/lecm-dic:Dictionary
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@487 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/dictionary-all.js
+++ lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/dictionary-all.js
@@ -8,17 +8,10 @@
 		{
 			datagridMeta:{
 				itemType:"lecm-dic:dictionary",
-                searchConfig:{ //настройки поиска (необязателен)
-                    filter:"",
-                    /** Настройки полнотекстового поиска. Пример объекта:
-                     {
-                     parentNodeRef - относительно какой директории искать (чаще всего совпадает с datagridMeta.nodeRef
-                     fields - какие свойства объекта следует заполнить и вернуть
-                     searchTerm - строка для поиска
-                     }
-                     */
+                searchConfig:{
+                    filter:'+PATH:"/app:company_home/lecm-dic:Dictionary/*" AND (NOT (ASPECT:"lecm-dic:aspect_active") OR lecm\\-dic:active:true)',
                     fullTextSearch: null,
-                    sort: "cm:name|true" // сортировка. Указываем по какому полю и порядок (true - asc), например, cm:name|true
+                    sort: "cm:name|true"
                 }
 			}
 		});
