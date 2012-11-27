6f1655feaf370f9ee91d339947a4b22e190f6908
Денис Башмаков <DBashmakov@it.ru>
2012-11-27 11:13:50 +0000

    Доработки в api
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@545 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-orgstructure/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/orgstructure/api/employee.positions.get.js
+++ lecm-orgstructure/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/orgstructure/api/employee.positions.get.js
@@ -1,4 +1,4 @@
-var employee = search.findNode("workspace://SpacesStore/041663d9-9ad7-435e-8e46-52eabf30e060");
+var employee = search.findNode(args["nodeRef"]);
 
 var results = [];
 
@@ -6,13 +6,9 @@ var results = [];
 var links = employee.sourceAssocs["lecm-orgstr:employee-link-employee-assoc"];
 for (var index in links){
     var employeeLink = links[index];
-    // получаем список объектов, ссылающихся на данную связь (Штатные расписания и Участники Рабочих групп)
-    var positions = employeeLink.sourceAssocs["lecm-orgstr:element-member-employee-assoc"];
-    for (var i in positions){
-        var position = positions[i];
-        if (position.getTypeShort() == "lecm-orgstr:staff-list") {// выбираем только Штатные расписания
-            results.push(position);
-        }
+    var position = employeeLink.getParent();
+    if (position.getTypeShort() == "lecm-orgstr:staff-list") {// выбираем только Штатные расписания
+        results.push(position);
     }
 }
 
