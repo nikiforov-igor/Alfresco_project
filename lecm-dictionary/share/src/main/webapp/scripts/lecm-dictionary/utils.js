7057de2e6c50ab21628220968b0be03b13b83799
Махмуд Шафеев <MShafeev@it.ru>
2012-11-27 07:33:26 +0000

    Рефакторинг. Добавленный коментарии. Подправлен код.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@541 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/utils.js
+++ lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/utils.js
@@ -13,15 +13,20 @@
 
         var h = parseInt(Dom.getStyle(wrapper, 'height')) - Dom.getY(block)
             - parseInt(Dom.getStyle(block, 'margin-bottom')) - parseInt(Dom.getStyle(bd, 'margin-bottom'));
-        var footerHeight = parseInt(Dom.getStyle(Dom.get('lecm-content-ft'),'height'))*2;
+        // Высота верхнего footer-a
+        var footerDivTopHeight = parseInt(Dom.getStyle(Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2', 'div'), 'height'));
+        // Высота нижнего footer-a
+        var footerDivBottomHeight = parseInt(Dom.getStyle(Dom.get('lecm-content-ft'), 'height'));
 
         block = Dom.get('dictionary');
-        Dom.setStyle(block, 'height', h-footerHeight - 16 +'px');
+        // Выставляем высоту, чтобы scrollbar был внизу перед нижним footer-ом
+        Dom.setStyle(block, 'height', h - footerDivTopHeight - footerDivBottomHeight - 16 +'px');
     }
 
     function setScroll(){
         var w = Dom.getElementsByClassName('datalists tree')[0].scrollWidth;
         var footer = Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2')[0];
+        // Удлиняем верхний footer при перемещении ползунка scrollbar-а
         Dom.setStyle(footer,'width',w+'px');
     }
 
@@ -33,7 +38,6 @@
 
     Event.onDOMReady(function() {
         setHeight();
-//        setScroll();
         Event.on(Dom.getElementsByClassName("datalists tree"),"scroll",function(e){
             setScroll();
         },this,true);
