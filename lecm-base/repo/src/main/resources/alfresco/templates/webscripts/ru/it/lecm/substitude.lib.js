1c90153ac07ddcdc21617f33625e041d37ce73dd
Ивкин Алексей <AIvkin@it.ru>
2012-11-27 08:47:12 +0000

    Контролы оргструктуры: Добавлена возможность задания в форматной строке контрола ассоциаций, и условий
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@543 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/substitude.lib.js
+++ lecm-base/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/substitude.lib.js
@@ -1,18 +1,22 @@
 var parentSymbol = "..";
-var splitSymbol = "/";
+var splitTransitionsSymbol = "/";
+var equalsSymbol = "=";
+var splitExpressionSymbol = ",";
+var openExpressionsSymbol = "(";
+var closeExpressionsSymbol = ")";
 
 function getSubstitudeField(node, field) {
 	var showNode = node;
 	var fieldName = null;
 	var transitions = [];
 
-	if (field.indexOf(splitSymbol) != -1) {
-		var firstIndex = field.indexOf(splitSymbol);
+	if (field.indexOf(splitTransitionsSymbol) != -1) {
+		var firstIndex = field.indexOf(splitTransitionsSymbol);
 		transitions.push(field.substring(0, firstIndex));
-		var lastIndex = field.lastIndexOf(splitSymbol);
+		var lastIndex = field.lastIndexOf(splitTransitionsSymbol);
 		while (firstIndex != lastIndex) {
 			var oldFirstIndex = firstIndex;
-			var firstIndex = field.indexOf(splitSymbol, firstIndex + 1);
+			firstIndex = field.indexOf(splitTransitionsSymbol, firstIndex + 1);
 			transitions.push(field.substring(oldFirstIndex + 1, firstIndex));
 		}
 		fieldName = field.substring(lastIndex + 1, ("" + field).length);
@@ -22,13 +26,15 @@ function getSubstitudeField(node, field) {
 
 	for (var i = 0; i < transitions.length; i++) {
 		var el = transitions[i];
+		var expressions = getExpression(el);
+		if (expressions.length > 0) {
+			el = el.substring(0, el.indexOf(openExpressionsSymbol));
+		}
 		if (el.indexOf(parentSymbol) == 0) {
 			var assocType = el.replace(parentSymbol, "");
 			if (("" + assocType).length > 0) {
 				showNode = showNode.sourceAssocs[assocType];
-				if (showNode != null) {
-					showNode = showNode[0];
-				} else {
+				if (showNode == null) {
 					break;
 				}
 			} else {
@@ -38,38 +44,101 @@ function getSubstitudeField(node, field) {
 			var temp = showNode.getAssocs()[el];
 			if (temp == null) {
 				showNode = showNode.getChildAssocs()[el];
-				if (showNode != null) {
-					showNode = showNode[0];
-				} else {
+				if (showNode == null) {
 					break;
 				}
 			} else {
-				showNode = temp[0];
+				showNode = temp;
+			}
+		}
+		if (showNode != null) {
+			if (expressions.length > 0) {
+				var exist = false;
+				for (var i = 0; i < showNode.length; i++) {
+					var node = showNode[i];
+					var expressionsFalse = false;
+					for (var j = 0; j < expressions.length; j++) {
+						var expression = expressions[j];
+						if (("" + node.properties[expression.field]) != expression.value) {
+							expressionsFalse = true;
+							break;
+						}
+					}
+					if (!expressionsFalse) {
+						showNode = node;
+						exist = true;
+						break;
+					}
+				}
+				if (!exist) {
+					showNode = null;
+					break;
+				}
+			} else if (showNode.length > 0) {
+				showNode = showNode[0];
 			}
 		}
 	}
 
 	var result = "";
-	if (showNode != null) {
+	if (showNode != null ) {
 		result = showNode.properties[fieldName];
+		if (result != null) {
+			result = trim(result);
+		} else {
+			result = "";
+		}
 	}
 
 	return result;
 }
 
-function splitSubstitudeFieldsString(string, openSymbol, closeSymbol) {
+function splitSubstitudeFieldsString(str, openSymbol, closeSymbol) {
 	var result = [];
-	if (string.indexOf(openSymbol) != -1 && string.indexOf(closeSymbol) != -1) {
-		var openIndex = string.indexOf(openSymbol);
-		var closeIndex = string.indexOf(closeSymbol);
-		result.push(string.substring(openIndex + 1, closeIndex));
-		var lastOpenIndex = string.lastIndexOf(openSymbol);
-		var lastCloseIndex = string.lastIndexOf(closeSymbol);
+	if (str.indexOf(openSymbol) != -1 && str.indexOf(closeSymbol) != -1) {
+		var openIndex = str.indexOf(openSymbol);
+		var closeIndex = str.indexOf(closeSymbol);
+		result.push(str.substring(openIndex + 1, closeIndex));
+		var lastOpenIndex = str.lastIndexOf(openSymbol);
+		var lastCloseIndex = str.lastIndexOf(closeSymbol);
 		while (openIndex != lastOpenIndex && closeIndex != lastCloseIndex) {
-			var openIndex = string.indexOf(openSymbol, openIndex + 1);
-			var closeIndex = string.indexOf(closeSymbol, closeIndex + 1);
-			result.push(string.substring(openIndex + 1, closeIndex));
+			openIndex = str.indexOf(openSymbol, openIndex + 1);
+			closeIndex = str.indexOf(closeSymbol, closeIndex + 1);
+			result.push(str.substring(openIndex + 1, closeIndex));
 		}
 	}
 	return result;
+}
+
+function getExpression(str) {
+	var expressions = [];
+
+	var openIndex = str.indexOf(openExpressionsSymbol);
+	var closeIndex = str.indexOf(closeExpressionsSymbol);
+	if (openIndex > -1 && closeIndex > -1) {
+		var expressionsStr = str.substring(openIndex + 1, closeIndex);
+		if (expressionsStr.length > 0 && expressionsStr.indexOf(equalsSymbol) > -1) {
+			var equalsIndex = -1;
+			var endIndex = -1;
+			var lastEqualsIndex = expressionsStr.lastIndexOf(equalsSymbol);
+			while (equalsIndex != lastEqualsIndex) {
+				var oldEndIndex = endIndex;
+				equalsIndex = expressionsStr.indexOf(equalsSymbol, equalsIndex + 1);
+				endIndex = expressionsStr.indexOf(splitExpressionSymbol, endIndex + 1);
+				if (endIndex == -1) {
+					endIndex = expressionsStr.length;
+				}
+				var expression = {
+					field: trim(expressionsStr.substring(oldEndIndex + 1, equalsIndex)),
+					value: trim(expressionsStr.substring(equalsIndex + 1, endIndex))
+				};
+				expressions.push(expression);
+			}
+		}
+	}
+	return expressions;
+}
+
+function trim(str) {
+	return str.replace(/^\s+|\s+$/g, "");
 }
