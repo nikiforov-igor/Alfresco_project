660a489b490bf3fba767dc30753a6455b215940b
Катаманов Андрей <AKatamanov@it.ru>
2012-12-07 11:56:40 +0000

    заготовка под IM
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@650 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-im/share/src/main/java/ru/it/lecm/im/client/LecmIM.java
@@ -0,0 +1,33 @@
+package ru.it.lecm.im.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Akatamanov
+ * Date: 07.12.12
+ * Time: 16:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class LecmIM implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        //To change body of implemented methods use File | Settings | File Templates.
+
+        Button button = new Button("Btn");
+        RootPanel.get().add(button);
+
+        button.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent clickEvent) {
+                Window.alert("Hello!");
+            }
+        });
+
+    }
+}
