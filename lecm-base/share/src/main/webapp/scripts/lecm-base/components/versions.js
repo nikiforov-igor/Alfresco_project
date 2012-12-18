8b58ef24716239f70e43f618d5ebf033bda248e6
Салахова Ольга <ORakovskaya@it.ru>
2012-12-18 10:58:35 +0000

    баг с прилипанием окна версий к краю страницы - fixed
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@790 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/share/src/main/webapp/scripts/lecm-base/components/versions.js
+++ lecm-base/share/src/main/webapp/scripts/lecm-base/components/versions.js
@@ -210,7 +210,9 @@
             var dialogDiv = YAHOO.util.Dom.getFirstChild(containerDiv);
 
             // Create the panel from the HTML returned in the server reponse
-            this.widgets.panel = Alfresco.util.createYUIPanel(dialogDiv);
+            this.widgets.panel = Alfresco.util.createYUIPanel(dialogDiv, {
+                width: "600px"
+            });
 
             // Create menu button:
             this.createMenu(dialogDiv);
