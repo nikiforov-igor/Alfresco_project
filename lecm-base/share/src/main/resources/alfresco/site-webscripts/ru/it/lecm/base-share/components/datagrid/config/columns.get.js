da5d09d977f55021009615b7d56d56e027c6279b
Зиновин Александр <AZinovin@it.ru>
2013-01-09 11:24:48 +0000

    поправлен скрипт - теперь есть возможность указывать label и label-id для поля в конфиге формы
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@929 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/base-share/components/datagrid/config/columns.get.js
+++ lecm-base/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/base-share/components/datagrid/config/columns.get.js
@@ -27,15 +27,15 @@
  */
 function getArgument(argName, defValue)
 {
-   var result = args[argName];
-   
-   // if we don't have a result and a default has been defined, return that instead
-   if (result === null && typeof defValue !== "undefined")
-   {
-      result = defValue;
-   }
-   
-   return result;
+	var result = args[argName];
+
+	// if we don't have a result and a default has been defined, return that instead
+	if (result === null && typeof defValue !== "undefined")
+	{
+		result = defValue;
+	}
+
+	return result;
 }
 
 /**
@@ -51,40 +51,40 @@ function getArgument(argName, defValue)
  */
 function getFormConfig(itemId, formId)
 {
-   var formConfig = null;
-   
-   // query for configuration for item
-   var nodeConfig = config.scoped[itemId];
-   
-   if (nodeConfig !== null)
-   {
-      // get the forms configuration
-      var formsConfig = nodeConfig.forms;
+	var formConfig = null;
 
-      if (formsConfig !== null)
-      {
-         if (formId !== null && formId.length > 0)
-         {
-            // look up the specific form
-            formConfig = formsConfig.getForm(formId);
-         }
-         
-         // drop back to default form if formId config missing
-         if (formConfig === null)
-         {
-            // look up the default form
-            formConfig = formsConfig.defaultForm;
-         }
-      }
-   }
-   
-   return formConfig;
+	// query for configuration for item
+	var nodeConfig = config.scoped[itemId];
+
+	if (nodeConfig !== null)
+	{
+		// get the forms configuration
+		var formsConfig = nodeConfig.forms;
+
+		if (formsConfig !== null)
+		{
+			if (formId !== null && formId.length > 0)
+			{
+				// look up the specific form
+				formConfig = formsConfig.getForm(formId);
+			}
+
+			// drop back to default form if formId config missing
+			if (formConfig === null)
+			{
+				// look up the default form
+				formConfig = formsConfig.defaultForm;
+			}
+		}
+	}
+
+	return formConfig;
 }
 
 /**
- * Returns the list of fields configured to be visible for the 
+ * Returns the list of fields configured to be visible for the
  * given mode. If this method returns null or an empty list the
- * component should attempt to display ALL known data for the item, 
+ * component should attempt to display ALL known data for the item,
  * unless there are fields configured to be hidden.
  *
  * @method getVisibleFields
@@ -94,39 +94,39 @@ function getFormConfig(itemId, formId)
  */
 function getVisibleFields(mode, formConfig)
 {
-   var visibleFields = null;
-   
-   if (formConfig !== null)
-   {
-      // get visible fields for the current mode
-      switch (mode)
-      {
-         case "view":
-            visibleFields = formConfig.visibleViewFieldNames;
-            break;
-         case "edit":
-            visibleFields = formConfig.visibleEditFieldNames;
-            break;
-         case "create":
-            visibleFields = formConfig.visibleCreateFieldNames;
-            break;
-         default:
-            visibleFields = formConfig.visibleViewFieldNames;
-            break;
-      }
-   }
-   
-   if (logger.isLoggingEnabled())
-   {
-      var listOfVisibleFields = visibleFields;
-      if (visibleFields !== null)
-      {
-         listOfVisibleFields = "[" + visibleFields.join(",") + "]";
-      }
-      logger.log("Fields configured to be visible for " + mode + " mode = " + listOfVisibleFields);
-   }
-         
-   return visibleFields;
+	var visibleFields = null;
+
+	if (formConfig !== null)
+	{
+		// get visible fields for the current mode
+		switch (mode)
+		{
+			case "view":
+				visibleFields = formConfig.visibleViewFieldNames;
+				break;
+			case "edit":
+				visibleFields = formConfig.visibleEditFieldNames;
+				break;
+			case "create":
+				visibleFields = formConfig.visibleCreateFieldNames;
+				break;
+			default:
+				visibleFields = formConfig.visibleViewFieldNames;
+				break;
+		}
+	}
+
+	if (logger.isLoggingEnabled())
+	{
+		var listOfVisibleFields = visibleFields;
+		if (visibleFields !== null)
+		{
+			listOfVisibleFields = "[" + visibleFields.join(",") + "]";
+		}
+		logger.log("Fields configured to be visible for " + mode + " mode = " + listOfVisibleFields);
+	}
+
+	return visibleFields;
 }
 
 /**
@@ -142,43 +142,43 @@ function getVisibleFields(mode, formConfig)
  */
 function createPostBody(itemKind, itemId, visibleFields, formConfig)
 {
-   var postBody = {};
-   
-   postBody.itemKind = itemKind;
-   postBody.itemId = itemId.replace(":/", "");
-   
-   if (visibleFields !== null)
-   {
-      // create list of fields to show and a list of
-      // those fields to 'force'
-      var postBodyFields = [];
-      var postBodyForcedFields = [];
-      var fieldId = null;
-      for (var f = 0; f < visibleFields.length; f++)
-      {
-         fieldId = visibleFields[f];
-	      var indexOf = fieldId.indexOf("|");
-	      var parsedFieldsId = indexOf == -1 ? fieldId : fieldId.substring(0, indexOf);
-	      postBodyFields.push(parsedFieldsId);
-         if (formConfig.isFieldForced(fieldId))
-         {
-            postBodyForcedFields.push(parsedFieldsId);
-         }
-      }
-      
-      postBody.fields = postBodyFields;
-      if (postBodyForcedFields.length > 0)
-      {
-         postBody.force = postBodyForcedFields;
-      }
-   }
-   
-   if (logger.isLoggingEnabled())
-   {
-      logger.log("postBody = " + jsonUtils.toJSONString(postBody));
-   }
-      
-   return postBody;
+	var postBody = {};
+
+	postBody.itemKind = itemKind;
+	postBody.itemId = itemId.replace(":/", "");
+
+	if (visibleFields !== null)
+	{
+		// create list of fields to show and a list of
+		// those fields to 'force'
+		var postBodyFields = [];
+		var postBodyForcedFields = [];
+		var fieldId = null;
+		for (var f = 0; f < visibleFields.length; f++)
+		{
+			fieldId = visibleFields[f];
+			var indexOf = fieldId.indexOf("|");
+			var parsedFieldsId = indexOf == -1 ? fieldId : fieldId.substring(0, indexOf);
+			postBodyFields.push(parsedFieldsId);
+			if (formConfig.isFieldForced(fieldId))
+			{
+				postBodyForcedFields.push(parsedFieldsId);
+			}
+		}
+
+		postBody.fields = postBodyFields;
+		if (postBodyForcedFields.length > 0)
+		{
+			postBody.force = postBodyForcedFields;
+		}
+	}
+
+	if (logger.isLoggingEnabled())
+	{
+		logger.log("postBody = " + jsonUtils.toJSONString(postBody));
+	}
+
+	return postBody;
 }
 
 /**
@@ -188,65 +188,65 @@ function createPostBody(itemKind, itemId, visibleFields, formConfig)
  */
 function main()
 {
-   var itemType = getArgument("itemType"),
-       formId = getArgument("formId", "datagrid"),
-       columns = [],
-	   columnDefs = [];
-   
-   if (itemType !== null && itemType.length > 0)
-   {
-      // get the config for the form
-      var formConfig = getFormConfig(itemType, formId);
-      
-      // может содержать форматную строку для колонки послке "|"
-      var visibleFields = getVisibleFields("view", formConfig);
-	   var substituteStrings = [];
-	   for (var i = 0; i < visibleFields.length; i++) {
-		   var vf = visibleFields[i];
-		   var indexOf = vf.indexOf("|");
-		   if (indexOf != -1) {
-			   substituteStrings[vf.substring(0, indexOf)] = vf.substring(indexOf + 1);
-		   }
-	   }
-      // build the JSON object to send to the server
-      var postBody = createPostBody("type", itemType, visibleFields, formConfig);
-         
-      // make remote call to service
-      var connector = remote.connect("alfresco");
-      var json = connector.post("/api/formdefinitions", jsonUtils.toJSONString(postBody), "application/json");
-      
-      if (logger.isLoggingEnabled())
-      {
-         logger.log("json = " + json);
-      }
-      
-      if (json.status == 401)
-      {
-         status.setCode(json.status, "Not authenticated");
-         return;
-      }
-      else
-      {
-         var formModel = eval('(' + json + ')');
-         
-         // if we got a successful response attempt to render the form
-         if (json.status == 200)
-         {
-            columns = formModel.data.definition.fields;
-	         for (var j = 0; j < columns.length; j++) {
-		         var col = columns[j];
-		         columnDefs[col.name] = col;
-	         }
-         }
-         else
-         {
-            model.error = formModel.message;
-         }
-      }
-   }
-   
-   // pass form ui model to FTL
-   model.columns = [];
+	var itemType = getArgument("itemType"),
+		formId = getArgument("formId", "datagrid"),
+		columns = [],
+		columnDefs = [];
+
+	if (itemType !== null && itemType.length > 0)
+	{
+		// get the config for the form
+		var formConfig = getFormConfig(itemType, formId);
+
+		// может содержать форматную строку для колонки послке "|"
+		var visibleFields = getVisibleFields("view", formConfig);
+		var substituteStrings = [];
+		for (var i = 0; i < visibleFields.length; i++) {
+			var vf = visibleFields[i];
+			var indexOf = vf.indexOf("|");
+			if (indexOf != -1) {
+				substituteStrings[vf.substring(0, indexOf)] = vf.substring(indexOf + 1);
+			}
+		}
+		// build the JSON object to send to the server
+		var postBody = createPostBody("type", itemType, visibleFields, formConfig);
+
+		// make remote call to service
+		var connector = remote.connect("alfresco");
+		var json = connector.post("/api/formdefinitions", jsonUtils.toJSONString(postBody), "application/json");
+
+		if (logger.isLoggingEnabled())
+		{
+			logger.log("json = " + json);
+		}
+
+		if (json.status == 401)
+		{
+			status.setCode(json.status, "Not authenticated");
+			return;
+		}
+		else
+		{
+			var formModel = eval('(' + json + ')');
+
+			// if we got a successful response attempt to render the form
+			if (json.status == 200)
+			{
+				columns = formModel.data.definition.fields;
+				for (var j = 0; j < columns.length; j++) {
+					var col = columns[j];
+					columnDefs[col.name] = col;
+				}
+			}
+			else
+			{
+				model.error = formModel.message;
+			}
+		}
+	}
+
+	// pass form ui model to FTL
+	model.columns = [];
 	for (var k = 0; k < visibleFields.length; k++) {
 		var obj = visibleFields[k];
 		if (obj.indexOf("|") != -1) {
@@ -259,11 +259,23 @@ function main()
 				name:obj,
 				label:"",
 				dataType:"",
-                sortable: false
+				sortable: false
 			}
 		}
 		if (substituteStrings[obj] != null) {
 			colDef.nameSubstituteString = substituteStrings[obj];
+		}
+		var label = null;
+		var formField = formConfig.fields[obj];
+		if (formField != null) {
+			if (formField.labelId != null && formField.labelId != "") {
+				label = msg.get(formField.labelId);
+			}else if (formField.label != null && formField.label != "") {
+				label = formField.label;
+			}
+		}
+		if (label != null) {
+			colDef.label = label;
 		}
 		model.columns.push(colDef);
 	}
