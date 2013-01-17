f465ed2f3a1738b33f365a3bc8431dd84ba169f5
Денис Башмаков <DBashmakov@it.ru>
2013-01-17 12:50:14 +0000

    [fix] поиск в РГ
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1036 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-orgstructure/share/src/main/webapp/scripts/lecm-orgstructure/workgroup-datagrid.js
+++ lecm-orgstructure/share/src/main/webapp/scripts/lecm-orgstructure/workgroup-datagrid.js
@@ -198,7 +198,7 @@ LogicECM.module.Orgstructure = LogicECM.module.Orgstructure || {};
                 }
             }
             // initialize Search
-            this.search = new LogicECM.AdvancedSearch(this.id, this.datagridMeta, this.widgets.dataTable, this.datagridColumns, this.widgets.dataSource, this.options.bubblingLabel).setOptions({
+            this.search = new LogicECM.AdvancedSearch(this.id, this).setOptions({
                 showExtendSearchBlock: this.options.showExtendSearchBlock
             });
 
