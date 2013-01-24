80acc8c0e3101525b10d5200d73b44243abb8406
Малыгин Владимир <VMalygin@it.ru>
2013-01-24 12:51:41 +0000

    параметризовал скрипт который проверяет права для участника делегирования
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1129 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-delegation/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/delegation/delegation.description.post.json.js
+++ lecm-delegation/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/delegation/delegation.description.post.json.js
@@ -1,13 +1,35 @@
-var container = delegation.getDelegationOptsContainer ();
-var itemType = delegation.getItemType ();
-var isEngineer = delegation.isEngineer (orgstructure.getCurrentEmployee ().nodeRef.toString ());
-var isBoss = delegation.isBoss (orgstructure.getCurrentEmployee ().nodeRef.toString ());
+var page = url.templateArgs.page;
 
-model.nodeRef = container.nodeRef.toString ();
-logger.log ("model.nodeRef = " + model.nodeRef);
-model.itemType = itemType;
-logger.log ("model.itemType = " + model.itemType);
-model.isEngineer = isEngineer;
-logger.log ("model.isEngineer = " + model.isEngineer);
-model.isBoss = isBoss;
-logger.log ("model.isBoss = " + model.isBoss);
+function getDescriptionList () {
+	var container = delegation.getDelegationOptsContainer ();
+	var itemType = delegation.getItemType ();
+	var isEngineer = delegation.isEngineer (orgstructure.getCurrentEmployee ().nodeRef.toString ());
+	var isBoss = delegation.isBoss (orgstructure.getCurrentEmployee ().nodeRef.toString ());
+
+	model.nodeRef = container.nodeRef.toString ();
+	logger.log ("model.nodeRef = " + model.nodeRef);
+	model.itemType = itemType;
+	logger.log ("model.itemType = " + model.itemType);
+	model.isEngineer = isEngineer;
+	logger.log ("model.isEngineer = " + model.isEngineer);
+	model.isBoss = isBoss;
+	logger.log ("model.isBoss = " + model.isBoss);
+}
+
+function getDescriptionOpts () {
+
+}
+
+switch (page) {
+	case "list":
+		getDescriptionList ();
+		break;
+	case "opts":
+		getDescriptionOpts ();
+		break;
+	default:
+		status.code = 400;
+		status.message = '"' + page + '" is unknown page. Valid page is "list" or "opts"';
+		status.redirect = true;
+		break;
+}
