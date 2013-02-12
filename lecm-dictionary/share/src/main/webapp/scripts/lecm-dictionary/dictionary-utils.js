7ad596d3a9c9731290263d53f75ab402f2ba3b9a
Махмуд Шафеев <MShafeev@it.ru>
2013-02-12 12:10:16 +0000

    Справочники. Рефакторинг. Веб скрипты вынесены в отдельные папки
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1360 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/dictionary-utils.js
+++ lecm-dictionary/share/src/main/webapp/scripts/lecm-dictionary/dictionary-utils.js
@@ -2,9 +2,9 @@
     /**
      * YUI Library aliases
      */
-    var Event = YAHOO.util.Event;
-    var Dom = YAHOO.util.Dom;
-    var Bubbling = YAHOO.Bubbling;
+    var Event = YAHOO.util.Event,
+        Dom = YAHOO.util.Dom,
+        Bubbling = YAHOO.Bubbling;
 
     //Set block height
     function setTreeHeight() {
@@ -15,36 +15,39 @@
 
         var h = parseInt(Dom.getStyle(wrapper, 'height')) - Dom.getY(block)
             - parseInt(Dom.getStyle(block, 'margin-bottom')) - parseInt(Dom.getStyle(bd, 'margin-bottom'));
-        // Высота верхнего footer-a
-        var footerDivTopHeight = parseInt(Dom.getStyle(Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2', 'div'), 'height'));
-        // Высота нижнего footer-a
+        // Высота header-а
+        var headerDivTopHeight = parseInt(Dom.getStyle(Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2', 'div'), 'height'));
+        // Высота footer-a
         var footerDivBottomHeight = parseInt(Dom.getStyle(Dom.get('lecm-content-ft'), 'height'));
         block = Dom.get('dictionary');
-        var height = h - footerDivTopHeight - footerDivBottomHeight - 16;
-        // Выставляем высоту, чтобы scrollbar был внизу перед нижним footer-ом
+        var height = h - headerDivTopHeight - footerDivBottomHeight - 16;
+        // Выставляем высоту, чтобы scrollbar был внизу перед footer-ом
         Dom.setStyle(block, 'height', height +'px');
         Dom.setStyle(block, "position", "inherit");
         Dom.setStyle(block, 'width', 'auto');
     }
 
     function setHeaderWidth(){
-        var headerWidth = parseInt(Dom.getStyle(Dom.getElementsByClassName('datalists tree')[0], 'width'));
-        var footer = Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2')[0];
+        // ширина дерева
+        var treeWidth = parseInt(Dom.getStyle(Dom.getElementsByClassName('datalists tree')[0], 'width'));
+        // ширина ползунка
         var resizer = parseInt(Dom.getStyle(Dom.getElementsByClassName("yui-resize-handle yui-resize-handle-r"),"width"));
-        // Удлиняем верхний footer при перемещении ползунка resize-ра
-        Dom.setStyle(footer,'width',headerWidth+resizer+'px');
+        var header = Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2')[0];
+        // Удлиняем header при перемещении ползунка resize-ра
+        Dom.setStyle(header, 'width', treeWidth+resizer+'px');
     }
 
     Event.onDOMReady(function() {
         Bubbling.on("GridRendered", setTreeHeight, this);
         Bubbling.on("HeightSetted", setTreeHeight, this);
         Bubbling.on("SetHeaderWidth",setHeaderWidth, this);
+        // Собитие на перемещение ползунка scrollbar-а
         Event.on(Dom.getElementsByClassName("datalists tree"),"scroll",function(e){
-            var headerWidth = Dom.getElementsByClassName('datalists tree')[0].scrollWidth;
-            var footer = Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2')[0];
+            var treeWidth = Dom.getElementsByClassName('datalists tree')[0].scrollWidth;
+            var header = Dom.getElementsByClassName('header-bar toolbar flat-button theme-bg-2')[0];
             var resizer = parseInt(Dom.getStyle(Dom.getElementsByClassName("yui-resize-handle yui-resize-handle-r"),"width"));
-            // Удлиняем верхний footer при перемещении ползунка scrollbar-а
-            Dom.setStyle(footer,'width',headerWidth+resizer+'px');
+            // Удлиняем header при перемещении ползунка scrollbar-а
+            Dom.setStyle(header, 'width', treeWidth + resizer + 'px');
         },this,true);
     });
 
