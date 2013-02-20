842cbf25bf3301afe7c690550787b2643f678b50
Ивкин Алексей <AIvkin@it.ru>
2013-02-20 08:26:55 +0000

    ALF-440: При создании свзяи добавляется аспект о присутствии связи
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1391 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/api/src/main/java/ru/it/lecm/base/beans/DocumentConnectionService.java
+++ lecm-base/api/src/main/java/ru/it/lecm/base/beans/DocumentConnectionService.java
@@ -12,7 +12,14 @@ public interface DocumentConnectionService {
 	public static final String DOCUMENT_CONNECTIONS_ROOT_NAME = "Связи";
 
 	public static final String DOCUMENT_CONNECTIONS_NAMESPACE_URI = "http://www.it.ru/lecm/org/connection/1.0";
+	public static final String DOCUMENT_CONNECTIONS_ASPECT_NAMESPACE_URI = "http://www.it.ru/lecm/connect/aspects/1.0";
+
 	public static final QName TYPE_CONNECTION = QName.createQName(DOCUMENT_CONNECTIONS_NAMESPACE_URI, "connection");
+
+	public static final QName ASSOC_PRIMARY_DOCUMENT = QName.createQName(DOCUMENT_CONNECTIONS_NAMESPACE_URI, "primary-document-assoc");
+	public static final QName ASSOC_CONNECTED_DOCUMENT = QName.createQName(DOCUMENT_CONNECTIONS_NAMESPACE_URI, "connected-document-assoc");
+
+	public static final QName ASPECT_HAS_CONNECTED_DOCUMENTS = QName.createQName(DOCUMENT_CONNECTIONS_ASPECT_NAMESPACE_URI, "has-connected-documents");
 
 	/**
 	 * Получение директории подписки.
---
+++ lecm-base/repo/src/main/java/ru/it/lecm/base/policy/DocumentConnectionPolicy.java
@@ -0,0 +1,66 @@
+package ru.it.lecm.base.policy;
+
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.JavaBehaviour;
+import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.StoreRef;
+import ru.it.lecm.base.beans.DocumentConnectionService;
+
+import java.util.List;
+
+/**
+ * User: AIvkin
+ * Date: 20.02.13
+ * Time: 9:52
+ */
+public class DocumentConnectionPolicy implements NodeServicePolicies.OnCreateAssociationPolicy, NodeServicePolicies.BeforeDeleteNodePolicy {
+	private PolicyComponent policyComponent;
+	private NodeService nodeService;
+
+	public void setPolicyComponent(PolicyComponent policyComponent) {
+		this.policyComponent = policyComponent;
+	}
+
+	public void setNodeService(NodeService nodeService) {
+		this.nodeService = nodeService;
+	}
+
+	public final void init() {
+		policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME,
+				DocumentConnectionService.TYPE_CONNECTION, DocumentConnectionService.ASSOC_CONNECTED_DOCUMENT,
+				new JavaBehaviour(this, "onCreateAssociation"));
+
+		policyComponent.bindClassBehaviour(NodeServicePolicies.BeforeDeleteNodePolicy.QNAME,
+				DocumentConnectionService.TYPE_CONNECTION, new JavaBehaviour(this, "beforeDeleteNode"));
+	}
+
+	@Override
+	public void onCreateAssociation(AssociationRef nodeAssocRef) {
+		NodeRef documentRef = nodeAssocRef.getTargetRef();
+		if (!nodeService.hasAspect(documentRef, DocumentConnectionService.ASPECT_HAS_CONNECTED_DOCUMENTS)){
+			nodeService.addAspect(documentRef, DocumentConnectionService.ASPECT_HAS_CONNECTED_DOCUMENTS, null);
+		}
+	}
+
+	@Override
+	public void beforeDeleteNode(NodeRef nodeRef) {
+		List<AssociationRef> connectedDocumentList = nodeService.getTargetAssocs(nodeRef, DocumentConnectionService.ASSOC_CONNECTED_DOCUMENT);
+		if (connectedDocumentList.size() == 1) {
+			NodeRef documentRef = connectedDocumentList.get(0).getTargetRef();
+
+			List<AssociationRef> assocs = nodeService.getSourceAssocs(documentRef, DocumentConnectionService.ASSOC_CONNECTED_DOCUMENT);
+			int size = 0;
+			for (AssociationRef assocRef: assocs) {
+				if (!assocRef.getSourceRef().getStoreRef().equals(StoreRef.STORE_REF_ARCHIVE_SPACESSTORE)) {
+					size++;
+				}
+			}
+			if (size == 1) {
+				nodeService.removeAspect(documentRef, DocumentConnectionService.ASPECT_HAS_CONNECTED_DOCUMENTS);
+			}
+		}
+	}
+}
