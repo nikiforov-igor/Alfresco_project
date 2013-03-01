d41d0eafab8ef802fb90bed0ac95214656aa37d5
Ивкин Алексей <AIvkin@it.ru>
2013-03-01 06:53:13 +0000

    Изменено отображение заголовка документа на форме просмотра
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1451 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/header/document.header.get.head.ftl
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/header/document.header.get.head.ftl
@@ -1,4 +1,3 @@
 <#include "/org/alfresco/components/component.head.inc">
-<!-- Node Details Header -->
-<@link rel="stylesheet" type="text/css" href="${page.url.context}/res/components/node-details/node-header.css" />
-<@script type="text/javascript" src="${page.url.context}/res/components/node-details/node-header.js"></@script>
+
+<link rel="stylesheet" type="text/css" href="${page.url.context}/css/components/document-header.css" />
--- lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/header/document.header.get.js
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/header/document.header.get.js
@@ -3,25 +3,11 @@
 function main()
 {
    AlfrescoUtil.param("nodeRef");
-   AlfrescoUtil.param("site", null);
-   AlfrescoUtil.param("rootPage", "documentlibrary");
-   AlfrescoUtil.param("rootLabelId", "path.documents");
-   AlfrescoUtil.param("showFavourite", "true");
-   AlfrescoUtil.param("showLikes", "true");
-   AlfrescoUtil.param("showComments", "true");
-   AlfrescoUtil.param("showDownload", "true");
-   AlfrescoUtil.param("showPath", "true");
-   var nodeDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site);
+   var nodeDetails = AlfrescoUtil.getNodeDetails(model.nodeRef);
    if (nodeDetails)
    {
       model.item = nodeDetails.item;
-      model.node = nodeDetails.item.node;
-      model.isContainer = nodeDetails.item.node.isContainer;
-      model.paths = AlfrescoUtil.getPaths(nodeDetails, model.rootPage, model.rootLabelId);
-      model.showComments = (nodeDetails.item.node.permissions.user["CreateChildren"] && model.showComments).toString();
-      model.showDownload = (!model.isContainer && model.showDownload).toString();
-      var count = nodeDetails.item.node.properties["fm:commentCount"];
-      model.commentCount = (count != undefined ? count : null);
+      model.documentName = nodeDetails.item.node.properties["cm:name"];
    }
 }
 
