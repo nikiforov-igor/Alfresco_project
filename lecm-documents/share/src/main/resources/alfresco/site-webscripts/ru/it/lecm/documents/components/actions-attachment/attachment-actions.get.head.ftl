9e10aa4340b2cb45622b31e1d313c8df36e99d00
Ивкин Алексей <AIvkin@it.ru>
2013-03-12 08:02:44 +0000

    ALF-487: действия с вложением
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1552 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/actions-attachment/attachment-actions.get.head.ftl
@@ -0,0 +1,4 @@
+<#include "/org/alfresco/components/component.head.inc">
+<!-- Document Details Actions -->
+<@link rel="stylesheet" type="text/css" href="${page.url.context}/res/components/document-details/document-actions.css" />
+<@script type="text/javascript" src="${page.url.context}/res/components/document-details/document-actions.js"></@script>
---
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/actions-attachment/attachment-actions.get.js
@@ -0,0 +1,52 @@
+<import resource="classpath:/alfresco/templates/org/alfresco/import/alfresco-util.js">
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/components/documentlibrary/include/documentlist.lib.js">
+
+function main()
+{
+   AlfrescoUtil.param('nodeRef');
+   AlfrescoUtil.param('site', null);
+   AlfrescoUtil.param('container', 'documentLibrary');
+	args.view = "attachment";
+
+   var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site,
+   {
+      actions: true
+   });
+   if (documentDetails)
+   {
+	  documentDetails.item.actions = getShowAction(documentDetails.item.actions);
+	  model.documentDetailsJSON = jsonUtils.toJSONString(documentDetails);
+      doclibCommon();
+   }
+}
+
+function getShowAction(actions) {
+	var result = [];
+	for (var i = 0; i < actions.length; i++) {
+		if (showAction(actions[i].id)) {
+			result.push(actions[i]);
+		}
+	}
+	return result;
+}
+
+function showAction(action) {
+	var showActions = [
+		"document-download",
+		"document-view-content",
+		"document-edit-metadata",
+		"document-upload-new-version",
+		"document-inline-edit",
+		"document-edit-offline",
+		"document-copy-to",
+		"document-delete"
+	];
+	for (var i = 0; i < showActions.length; i++) {
+		if (action == showActions[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+main();
