808c6f91f445fae050a8144c0ad69a723473dcbc
Зиновин Александр <AZinovin@it.ru>
2013-03-15 11:10:51 +0000

    заготовка скрипта редеплоя сервера
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1615 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- server/redeploy.sh
+++ server/redeploy.sh
@@ -7,8 +7,8 @@ echo "ALF_HOME: $ALF_HOME"
 echo "CATALINA_HOME: $CATALINA_HOME"
 
 echo "Shutting down server"
-.$ALF_HOME/alfresco.sh stop
+$ALF_HOME/./alfresco.sh stop
 
 
 echo "Starting server"
-.$ALF_HOME/alfresco.sh start
+$ALF_HOME/./alfresco.sh start
