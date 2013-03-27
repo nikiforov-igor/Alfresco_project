d7de02a486ca520b7411cc1311e8b2a827fca070
Ивкин Алексей <AIvkin@it.ru>
2013-03-27 12:53:47 +0000

    Добавлены записи в бизнес журнал для вложений
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1787 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/repo/src/main/java/ru/it/lecm/documents/policy/DocumentAttachmentsPolicy.java
+++ lecm-documents/repo/src/main/java/ru/it/lecm/documents/policy/DocumentAttachmentsPolicy.java
@@ -1,19 +1,24 @@
 package ru.it.lecm.documents.policy;
 
 import org.alfresco.model.ContentModel;
+import org.alfresco.model.ForumModel;
 import org.alfresco.repo.node.NodeServicePolicies;
 import org.alfresco.repo.policy.Behaviour;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.repo.version.VersionServicePolicies;
 import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.version.Version;
 import org.alfresco.service.namespace.QName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.it.lecm.base.beans.SubstitudeBean;
 import ru.it.lecm.businessjournal.beans.BusinessJournalService;
+import ru.it.lecm.businessjournal.beans.EventCategory;
+import ru.it.lecm.documents.beans.DocumentConnectionService;
 import ru.it.lecm.documents.beans.DocumentMembersService;
 import ru.it.lecm.documents.beans.DocumentService;
 import ru.it.lecm.orgstructure.beans.OrgstructureBean;
@@ -30,7 +35,10 @@ import java.util.Map;
  * Date: 15.03.13
  * Time: 15:38
  */
-public class DocumentAttachmentsPolicy implements NodeServicePolicies.OnDeleteNodePolicy, NodeServicePolicies.OnUpdatePropertiesPolicy,
+public class DocumentAttachmentsPolicy implements
+		NodeServicePolicies.BeforeDeleteNodePolicy,
+		NodeServicePolicies.OnUpdatePropertiesPolicy,
+		VersionServicePolicies.AfterCreateVersionPolicy,
         NodeServicePolicies.OnCreateNodePolicy {
 
     final protected Logger logger = LoggerFactory.getLogger(DocumentAttachmentsPolicy.class);
@@ -71,11 +79,13 @@ public class DocumentAttachmentsPolicy implements NodeServicePolicies.OnDeleteNo
 
     public final void init() {
         policyComponent.bindClassBehaviour(NodeServicePolicies.OnCreateNodePolicy.QNAME,
-                ContentModel.TYPE_CONTENT, new JavaBehaviour(this, "onCreateNode", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
+                ContentModel.TYPE_CONTENT, new JavaBehaviour(this, "onCreateNode"));
         policyComponent.bindClassBehaviour(NodeServicePolicies.OnUpdatePropertiesPolicy.QNAME,
-                ContentModel.TYPE_CONTENT, new JavaBehaviour(this, "onUpdateProperties"));
-        policyComponent.bindClassBehaviour(NodeServicePolicies.OnDeleteNodePolicy.QNAME,
-                ContentModel.TYPE_CONTENT, new JavaBehaviour(this, "onDeleteNode"));
+                ContentModel.TYPE_CONTENT, new JavaBehaviour(this, "onUpdateProperties", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
+        policyComponent.bindClassBehaviour(NodeServicePolicies.BeforeDeleteNodePolicy.QNAME,
+                ContentModel.TYPE_CONTENT, new JavaBehaviour(this, "beforeDeleteNode"));
+        policyComponent.bindClassBehaviour(VersionServicePolicies.AfterCreateVersionPolicy.QNAME,
+			    ContentModel.TYPE_CONTENT, new JavaBehaviour(this, "afterCreateVersion", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
     }
 
     @Override
@@ -84,15 +94,28 @@ public class DocumentAttachmentsPolicy implements NodeServicePolicies.OnDeleteNo
         if (document != null) {
             // добавляем пользователя добавившего вложение как участника
             documentMembersService.addMember(document, orgstructureService.getCurrentEmployee(), null);
+
+	        if (!childAssocRef.getQName().getLocalName().contains("(Рабочая копия)")) {
+
+		        List<String> objects = new ArrayList<String>(1);
+		        objects.add(childAssocRef.getChildRef().toString());
+		        businessJournalService.log(document, EventCategory.ADD_DOCUMENT_ATTACHMENT, "Сотрудник #initiator добавил вложение #object1 к документу #mainobject", objects);
+	        }
         }
     }
 
     @Override
-    public void onDeleteNode(ChildAssociationRef childAssocRef, boolean isNodeArchived) {
-        NodeRef document = getDocumentByAttachment(childAssocRef);
+    public void beforeDeleteNode(NodeRef nodeRef) {
+        NodeRef document = getDocumentByAttachment(nodeRef);
         if (document != null) {
             // добавляем пользователя удалившего вложения как участника
             documentMembersService.addMember(document, orgstructureService.getCurrentEmployee(), null);
+
+	        if (!nodeService.hasAspect(nodeRef, ContentModel.ASPECT_WORKING_COPY)) {
+				List<String> objects = new ArrayList<String>(1);
+				objects.add(nodeRef.toString());
+				businessJournalService.log(document, EventCategory.DELETE_DOCUMENT_ATTACHMENT, "Сотрудник #initiator удалил вложение #object1 в документе #mainobject", objects);
+	        }
         }
     }
 
@@ -122,8 +145,9 @@ public class DocumentAttachmentsPolicy implements NodeServicePolicies.OnDeleteNo
 
     @Override
     public void onUpdateProperties(NodeRef nodeRef, Map<QName, Serializable> before, Map<QName, Serializable> after) {
-        List<QName> changedProps = getAffectedProperties(before, after);
-        if (!changedProps.isEmpty() && getDocumentByAttachment(nodeRef) != null) { // обновляем только автора и изменившего ноду
+	    NodeRef document = getDocumentByAttachment(nodeRef);
+	    List<QName> changedProps = getAffectedProperties(before, after);
+        if (!changedProps.isEmpty() && document != null) { // обновляем только автора и изменившего ноду
             for (QName changedProp : changedProps) {
                 QName propName = QName.createQName(DocumentService.DOCUMENT_NAMESPACE_URI, changedProp.getLocalName());
                 QName propRef = QName.createQName(DocumentService.DOCUMENT_NAMESPACE_URI, changedProp.getLocalName() + "-ref");
@@ -132,6 +156,20 @@ public class DocumentAttachmentsPolicy implements NodeServicePolicies.OnDeleteNo
                 nodeService.setProperty(nodeRef, propRef, employeeRef.toString());
             }
         }
+
+	    if (document != null && !nodeService.hasAspect(nodeRef, ContentModel.ASPECT_WORKING_COPY)) {
+		    Serializable commentCountBefore = before.get(ForumModel.PROP_COMMENT_COUNT);
+		    Serializable commentCountAfter = after.get(ForumModel.PROP_COMMENT_COUNT);
+		    if (commentCountAfter != null && (commentCountBefore == null || !commentCountAfter.equals(commentCountBefore))) {
+			    List<String> objects = new ArrayList<String>(1);
+			    objects.add(nodeRef.toString());
+			    businessJournalService.log(document, EventCategory.COMMENT_DOCUMENT_ATTACHMENT, "Сотрудник #initiator прокомментировал вложение #object1 в документе #mainobject", objects);
+		    } else if (before.size() == after.size()) {
+			    List<String> objects = new ArrayList<String>(1);
+			    objects.add(nodeRef.toString());
+		        businessJournalService.log(document, EventCategory.EDIT_DOCUMENT_ATTACHMENT_PROPERTIES, "Сотрудник #initiator изменил свойства вложения #object1 в документе #mainobject", objects);
+		    }
+	    }
     }
 
     private List<QName> getAffectedProperties(Map<QName, Serializable> before, Map<QName, Serializable> after) {
@@ -149,4 +187,14 @@ public class DocumentAttachmentsPolicy implements NodeServicePolicies.OnDeleteNo
     public void setSubstituteService(SubstitudeBean substitudeService) {
         this.substituteService = substitudeService;
     }
+
+	@Override
+	public void afterCreateVersion(NodeRef nodeRef, Version version) {
+		NodeRef document = getDocumentByAttachment(nodeRef);
+		if (document != null && !nodeService.hasAspect(nodeRef, ContentModel.ASPECT_WORKING_COPY)) {
+			List<String> objects = new ArrayList<String>(1);
+			objects.add(nodeRef.toString());
+			businessJournalService.log(document, EventCategory.ADD_DOCUMENT_ATTACHMENT_NEW_VERSION, "Сотрудник #initiator обновил версию вложения #object1 в документе #mainobject", objects);
+		}
+	}
 }
