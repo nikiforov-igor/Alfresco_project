2c6abab27f1616cbc76b9a6c37f0d4008304db08
Денис Башмаков <DBashmakov@it.ru>
2013-03-28 06:29:57 +0000

    [+]проверка executionId на Null
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1797 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-statemachine/repo/src/main/java/ru/it/lecm/statemachine/StateMachineHelper.java
+++ lecm-statemachine/repo/src/main/java/ru/it/lecm/statemachine/StateMachineHelper.java
@@ -282,19 +282,19 @@ public class StateMachineHelper implements StateMachineServiceBean {
     @Override
     public StateFields getStateFields(NodeRef document) {
         String executionId = (String) serviceRegistry.getNodeService().getProperty(document, StatemachineModel.PROP_STATEMACHINE_ID);
-        String taskId = getCurrentTaskId(executionId);
-
-        if (taskId != null) {
-            List<StateMachineAction> actions = getStatusChangeActions(document);
-            Set<StateField> result = new HashSet<StateField>();
-            for (StateMachineAction action : actions) {
-                StatusChangeAction statusChangeAction = (StatusChangeAction) action;
-                result.addAll(statusChangeAction.getFields());
+        if (executionId != null) {
+            String taskId = getCurrentTaskId(executionId);
+            if (taskId != null) {
+                List<StateMachineAction> actions = getStatusChangeActions(document);
+                Set<StateField> result = new HashSet<StateField>();
+                for (StateMachineAction action : actions) {
+                    StatusChangeAction statusChangeAction = (StatusChangeAction) action;
+                    result.addAll(statusChangeAction.getFields());
+                }
+                return new StateFields(true, result);
             }
-            return new StateFields(true, result);
-        } else {
-            return new StateFields(false);
         }
+        return new StateFields(false);
     }
 
     @Override
