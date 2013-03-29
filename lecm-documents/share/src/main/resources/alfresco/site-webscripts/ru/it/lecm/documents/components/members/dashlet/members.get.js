f284a98ba8dc4944f24f5f2b214fac506bcd54b5
Денис Башмаков <DBashmakov@it.ru>
2013-03-29 05:55:20 +0000

    [+] ALF-626 Права. Просмотр перечня участников.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@1822 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/members/dashlet/members.get.js
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/members/dashlet/members.get.js
@@ -1,10 +1,23 @@
 function main() {
-    var url = "/lecm/document/api/getMembersFolder?nodeRef=" + args["nodeRef"];
-    var json = remote.connect("alfresco").get(url);
-    if (json.status == 200) {
-        var obj = eval("(" + json + ")");
-        model.folderRef = obj.nodeRef;
+    var hasPerm = hasPermission(args["nodeRef"]);
+    if(hasPerm){
+        var url = "/lecm/document/api/getMembersFolder?nodeRef=" + args["nodeRef"];
+        var json = remote.connect("alfresco").get(url);
+        if (json.status == 200) {
+            var obj = eval("(" + json + ")");
+            model.folderRef = obj.nodeRef;
+        }
     }
+}
+
+function hasPermission(nodeRef) {
+    var url = '/lecm/security/api/getPermission?nodeRef=' + nodeRef + '&permission=lecmPerm_MemberList';
+    var result = remote.connect("alfresco").get(url);
+    if (result.status != 200) {
+        return false;
+    }
+    var permObj = eval('(' + result + ')');
+    return (("" + permObj.hasPermission) ==  "true");
 }
 
 main();
