b6ce001a4eea65a6c504269285d3c24da99b0f60
Махмуд Шафеев <MShafeev@it.ru>
2013-04-10 12:54:58 +0000

    Рефакторинг. Удален повторяющийся код.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2023 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/data/surf-doclist.get.js
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/data/surf-doclist.get.js
@@ -1,3 +1,4 @@
 <import resource="classpath:/alfresco/site-webscripts/ru/it/lecm/documents/data/surf-doclist.lib.js">
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/components/documentlibrary/data/surf-doclist.lib.js">
 
-surfDoclist_main();
+lecmDoclist_main();
