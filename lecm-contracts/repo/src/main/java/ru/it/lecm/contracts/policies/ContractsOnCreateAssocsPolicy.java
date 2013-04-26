d13456f5c3263fd702a198ad9c0c327d636cff24
Денис Башмаков <DBashmakov@it.ru>
2013-04-26 10:37:05 +0000

    [~] включены css для формы для дашлета и страницы с договорами [~] сохранение данных ассоциаций для сортировки по ним в гриде
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2270 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-contracts/repo/src/main/java/ru/it/lecm/contracts/policies/ContractsOnCreateAssocsPolicy.java
@@ -0,0 +1,23 @@
+package ru.it.lecm.contracts.policies;
+
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.JavaBehaviour;
+import ru.it.lecm.base.policies.LogicECMAssociationPolicy;
+import ru.it.lecm.contracts.beans.ContractsBeanImpl;
+
+/**
+ * User: dbashmakov
+ * Date: 23.04.13
+ * Time: 17:30
+ */
+public class ContractsOnCreateAssocsPolicy extends LogicECMAssociationPolicy {
+    @Override
+    public final void init() {
+        super.init();
+        policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnDeleteAssociationPolicy.QNAME,
+                ContractsBeanImpl.TYPE_CONTRACTS_DOCUMENT, new JavaBehaviour(this, "onDeleteAssociation"));
+
+        policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME,
+                ContractsBeanImpl.TYPE_CONTRACTS_DOCUMENT, new JavaBehaviour(this, "onCreateAssociation"));
+    }
+}
