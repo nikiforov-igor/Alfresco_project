c92c854e74b0147ee16940b729bc768c60424b57
Махмуд Шафеев <MShafeev@it.ru>
2013-04-29 14:36:51 +0000

    [+]Значение аттрибута по ассоциации для машины состояний
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2303 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-statemachine/repo/src/main/java/ru/it/lecm/statemachine/expression/ExpressionDocument.java
+++ lecm-statemachine/repo/src/main/java/ru/it/lecm/statemachine/expression/ExpressionDocument.java
@@ -1,9 +1,13 @@
 package ru.it.lecm.statemachine.expression;
 
 import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.AssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
 import ru.it.lecm.documents.beans.DocumentAttachmentsService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * User: PMelnikov
@@ -30,6 +34,37 @@ public class ExpressionDocument {
 		QName attribute = QName.createQName(attributeName,serviceRegistry.getNamespaceService());
 		return serviceRegistry.getNodeService().getProperty(nodeRef, attribute);
 	}
+
+    public Object assocAttr(String assocName, String attributeName) {
+        QName assocTypaName = QName.createQName(assocName, serviceRegistry.getNamespaceService());
+        QName attributeTypeName = QName.createQName(attributeName, serviceRegistry.getNamespaceService());
+
+        List<AssociationRef> associationRefs = serviceRegistry.getNodeService().getTargetAssocs(nodeRef,assocTypaName);
+        for (AssociationRef associationRef : associationRefs) {
+            NodeRef assocNodeRef = associationRef.getTargetRef();
+            if (assocNodeRef != null) {
+                return serviceRegistry.getNodeService().getProperty(assocNodeRef, attributeTypeName);
+            }
+        }
+        return null;
+    }
+
+    public List<Object> assocAttrs(String assocName, String attributeName) {
+        QName assocTypaName = QName.createQName(assocName, serviceRegistry.getNamespaceService());
+        QName attributeTypeName = QName.createQName(attributeName, serviceRegistry.getNamespaceService());
+        List<Object> attributs = new ArrayList<Object>();
+
+        List<AssociationRef> associationRefs = serviceRegistry.getNodeService().getTargetAssocs(nodeRef,assocTypaName);
+        for (AssociationRef associationRef : associationRefs) {
+            NodeRef assocNodeRef = associationRef.getTargetRef();
+            if (assocNodeRef != null) {
+                if (serviceRegistry.getNodeService().getProperty(assocNodeRef, attributeTypeName) != null) {
+                    attributs.add(serviceRegistry.getNodeService().getProperty(assocNodeRef, attributeTypeName));
+                }
+            }
+        }
+        return attributs;
+    }
 
 	//Наличие вложения с определенным типом
 	public boolean hasCategoryAttachment(String attachmentCategory) {
