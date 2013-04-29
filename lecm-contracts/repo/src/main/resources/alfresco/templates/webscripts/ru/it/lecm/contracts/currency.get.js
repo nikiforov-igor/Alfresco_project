f63c87d32c92bbc641f3e00fe7e18abae5cf0bf6
Салахова Ольга <ORakovskaya@it.ru>
2013-04-29 06:57:43 +0000

    Договор: Основные сведения: Currency - not mandatory
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2289 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-contracts/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/contracts/currency.get.js
+++ lecm-contracts/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/contracts/currency.get.js
@@ -1,4 +1,9 @@
 var nodeRef = args["nodeRef"];
 var node = search.findNode(nodeRef);
-var currency = node.assocs["lecm-contract:currency-assoc"][0].properties["lecm-currency:alphabetic-code"];
+var currency;
+if (node.assocs["lecm-contract:currency-assoc"]) {
+    currency = node.assocs["lecm-contract:currency-assoc"][0].properties["lecm-currency:alphabetic-code"];
+} else {
+    currency = "";
+}
 model.currency = currency;
