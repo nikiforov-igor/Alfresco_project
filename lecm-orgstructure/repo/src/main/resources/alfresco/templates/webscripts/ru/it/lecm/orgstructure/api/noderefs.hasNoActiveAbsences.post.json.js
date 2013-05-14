d03683ac3631a235b031869e124b240874459015
Малыгин Владимир <VMalygin@it.ru>
2013-05-14 05:26:51 +0000

    comment deleted
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2408 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-orgstructure/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/orgstructure/api/noderefs.hasNoActiveAbsences.post.json.js
+++ lecm-orgstructure/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/orgstructure/api/noderefs.hasNoActiveAbsences.post.json.js
@@ -1,8 +1,3 @@
-﻿/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 logger.log("nodeRefs.hasNoActiveAbsences!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
 model.reason = '';
