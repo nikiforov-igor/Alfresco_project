e17d4093e6e91a134b2dd8be917d6d8a57a0bc71
Денис Башмаков <DBashmakov@it.ru>
2013-05-22 12:39:56 +0000

    [+]ALF-527 Отрисовка документов в стандартном репозитории
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2601 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-documents/repo/src/main/java/ru/it/lecm/documents/beans/CustomBaseDocNameProperty.java
@@ -0,0 +1,39 @@
+package ru.it.lecm.documents.beans;
+
+import org.alfresco.repo.jscript.app.PropertyDecorator;
+import org.alfresco.service.cmr.dictionary.DictionaryService;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import ru.it.lecm.documents.beans.DocumentService;
+
+import java.io.Serializable;
+
+/**
+ * User: dbashmakov
+ * Date: 21.05.13
+ * Time: 12:20
+ */
+public class CustomBaseDocNameProperty implements PropertyDecorator {
+
+    private NodeService nodeService = null;
+    private DictionaryService dictionaryService;
+
+    @Override
+    public Serializable decorate(NodeRef nodeRef, String propertyName, Serializable value) {
+        if (dictionaryService.isSubClass(nodeService.getType(nodeRef), DocumentService.TYPE_BASE_DOCUMENT)) {
+            //Получаем представление
+            Serializable replaceValue = nodeService.getProperty(nodeRef, DocumentService.PROP_PRESENT_STRING);
+            //Замещаем имя на представление или возвращаем обычное
+            return replaceValue != null ? replaceValue.toString().replaceAll("<.*?>","") : value;
+        }
+        return value;
+    }
+
+    public void setNodeService(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    public void setDictionaryService(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
+    }
+}
---
+++ lecm-documents/repo/src/main/java/ru/it/lecm/documents/beans/EmployeeNamePropertyDecorator.java
@@ -0,0 +1,71 @@
+package ru.it.lecm.documents.beans;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.jscript.app.PropertyDecorator;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.security.PersonService;
+import org.alfresco.service.namespace.QName;
+import ru.it.lecm.orgstructure.beans.OrgstructureBean;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * User: dbashmakov
+ * Date: 22.05.13
+ * Time: 15:09
+ */
+public class EmployeeNamePropertyDecorator implements PropertyDecorator {
+    private ServiceRegistry services;
+    private NodeService nodeService = null;
+    private PersonService personService = null;
+
+    private OrgstructureBean orgstructureService;
+
+    public void setOrgstructureService(OrgstructureBean orgstructureService) {
+        this.orgstructureService = orgstructureService;
+    }
+
+    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+        this.services = serviceRegistry;
+        this.nodeService = serviceRegistry.getNodeService();
+        this.personService = serviceRegistry.getPersonService();
+    }
+
+    public Serializable decorate(NodeRef nodeRef, String propertyName, Serializable value) {
+        String username = value.toString();
+        String firstName;
+        String lastName;
+        Map<String, Serializable> map = new LinkedHashMap<String, Serializable>(4);
+        map.put("userName", username);
+
+        if (this.personService.personExists(username)) {
+            NodeRef employeeRef = orgstructureService.getEmployeeByPerson(username);
+            if (employeeRef != null) {
+                Map<QName, Serializable> properties = this.nodeService.getProperties(employeeRef);
+                firstName = (String) properties.get(OrgstructureBean.PROP_EMPLOYEE_FIRST_NAME);
+                lastName = (String) properties.get(OrgstructureBean.PROP_EMPLOYEE_LAST_NAME);
+            } else {
+                NodeRef personRef = this.personService.getPerson(username);
+                Map<QName, Serializable> properties = this.nodeService.getProperties(personRef);
+                firstName = (String) properties.get(ContentModel.PROP_FIRSTNAME);
+                lastName = (String) properties.get(ContentModel.PROP_LASTNAME);
+            }
+        } else if (username.equals("System") || username.startsWith("System@")) {
+            firstName = "System";
+            lastName = "User";
+        } else {
+            map.put("isDeleted", true);
+            return (Serializable) map;
+        }
+
+        map.put("firstName", firstName);
+        map.put("lastName", lastName);
+        map.put("displayName", (firstName != null ? firstName + " " : "" + (lastName != null ? lastName : "")).replaceAll("^\\s+|\\s+$", ""));
+        return (Serializable) map;
+    }
+}
+
