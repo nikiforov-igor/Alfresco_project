433a248299101bb61165f1555a64ffd3f5819797
Ивкин Алексей <AIvkin@it.ru>
2013-06-17 12:45:03 +0000

    ALF-1065: Исключение участника из Рабочей группы
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2903 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-orgstructure/share/src/main/webapp/scripts/lecm-orgstructure/workforce-datagrid.js
+++ lecm-orgstructure/share/src/main/webapp/scripts/lecm-orgstructure/workforce-datagrid.js
@@ -59,83 +59,101 @@ LogicECM.module.Orgstructure = LogicECM.module.Orgstructure || {};
         },
         onActionEmployeeDelete: function DataGridActions_onActionEmployeeDelete(p_item, owner, actionsConfig, fnDeleteComplete) {
             var me = this;
-            var staffRow = p_item;
-            // Получаем для трудового ресурса (участника раб. группы) ссылку на сотрудника
-            var sUrl = Alfresco.constants.PROXY_URI + "/lecm/orgstructure/api/getStaffEmployeeLink?nodeRef=" + staffRow.nodeRef;
-            var callback = {
-                success: function (oResponse) {
-                    var oResult = eval("(" + oResponse.responseText + ")");
-                    if (oResult) {
-                        var onPrompt = function (fnAfterPrompt) {
-                            Alfresco.util.PopupManager.displayPrompt(
-                                {
-                                    title: this.msg("message.employee.role.delete.title"),
-                                    text: this.msg("message.employee.role.delete.prompt",
-                                        staffRow.itemData["assoc_lecm-orgstr_element-member-employee-assoc"].displayValue,
-                                        staffRow.itemData["assoc_lecm-orgstr_element-member-position-assoc"].displayValue),
-                                    buttons: [
-                                        {
-                                            text: this.msg("button.employee.remove"),
-                                            handler: function DataGridActions__onActionDelete_delete() {
-                                                this.destroy();
-                                                fnAfterPrompt.call(me, [oResult]);
-                                            }
-                                        },
-                                        {
-                                            text: this.msg("button.cancel"),
-                                            handler: function DataGridActions__onActionDelete_cancel() {
-                                                this.destroy();
-                                            },
-                                            isDefault: true
-                                        }
-                                    ]
-                                });
-                        };
-                        var fnDeleteComplete = function () {
-                            // Reload the node's metadata
-                            Alfresco.util.Ajax.jsonPost(
-                                {
-                                    url: Alfresco.constants.PROXY_URI + "lecm/base/item/node/" + new Alfresco.util.NodeRef(p_item.nodeRef).uri,
-                                    dataObj: this._buildDataGridParams(),
-                                    successCallback: {
-                                        fn: function DataGrid_onActionEdit_refreshSuccess(response) {
-                                            // Fire "itemUpdated" event
-                                            YAHOO.Bubbling.fire("dataItemUpdated",
-                                                {
-                                                    item: response.json.item,
-                                                    bubblingLabel: me.options.bubblingLabel
-                                                });
-                                        },
-                                        scope: this
-                                    },
-                                    failureCallback: {
-                                        fn: function DataGrid_onActionEdit_refreshFailure(response) {
-                                            Alfresco.util.PopupManager.displayMessage(
-                                                {
-                                                    text: this.msg("message.details.failure")
-                                                });
-                                        },
-                                        scope: this
-                                    }
-                                });
-                        }.bind(me);
 
-                        me.onDelete([oResult], owner, {fullDelete: true, targetDelete: true, successMessage: "message.employee.role.delete.success"}, fnDeleteComplete, onPrompt);
-                    } else {
-                        Alfresco.util.PopupManager.displayMessage(
-                            {
-                                text: this.msg("message.employee.role.delete.failure")
-                            });
-                    }
-                },
-                failure: function (oResponse) {
-                    Alfresco.util.PopupManager.displayMessage(
-                        {
-                            text: this.msg("message.employee.role.delete.failure")
-                        });
-                }
-            };
-            YAHOO.util.Connect.asyncRequest('GET', sUrl, callback);
+	        var sUrlWorkGroup = Alfresco.constants.PROXY_URI + "/lecm/orgstructure/api/getWorkGroupProperties?nodeRef=" + this.datagridMeta.nodeRef;
+	        var callbackWorkGroup = {
+		        success: function (oResponse) {
+			        var oResultWorkGroup = eval("(" + oResponse.responseText + ")");
+			        if (oResultWorkGroup) {
+						var workgroupShortName = oResultWorkGroup.shortName;
+
+				        var staffRow = p_item;
+				        // Получаем для трудового ресурса (участника раб. группы) ссылку на сотрудника
+				        var sUrl = Alfresco.constants.PROXY_URI + "/lecm/orgstructure/api/getStaffEmployeeLink?nodeRef=" + staffRow.nodeRef;
+				        var callback = {
+					        success: function (oResponse) {
+						        var oResult = eval("(" + oResponse.responseText + ")");
+						        if (oResult) {
+							        var onPrompt = function (fnAfterPrompt) {
+								        Alfresco.util.PopupManager.displayPrompt(
+									        {
+										        title: this.msg("message.employee.role.delete.title"),
+										        text: this.msg("message.employee.role.delete.prompt",
+											        staffRow.itemData["assoc_lecm-orgstr_element-member-employee-assoc"].displayValue,
+											        workgroupShortName),
+										        buttons: [
+											        {
+												        text: this.msg("button.employee.remove"),
+												        handler: function DataGridActions__onActionDelete_delete() {
+													        this.destroy();
+													        fnAfterPrompt.call(me, [oResult]);
+												        }
+											        },
+											        {
+												        text: this.msg("button.cancel"),
+												        handler: function DataGridActions__onActionDelete_cancel() {
+													        this.destroy();
+												        },
+												        isDefault: true
+											        }
+										        ]
+									        });
+							        };
+							        var fnDeleteComplete = function () {
+								        // Reload the node's metadata
+								        Alfresco.util.Ajax.jsonPost(
+									        {
+										        url: Alfresco.constants.PROXY_URI + "lecm/base/item/node/" + new Alfresco.util.NodeRef(p_item.nodeRef).uri,
+										        dataObj: this._buildDataGridParams(),
+										        successCallback: {
+											        fn: function DataGrid_onActionEdit_refreshSuccess(response) {
+												        // Fire "itemUpdated" event
+												        YAHOO.Bubbling.fire("dataItemUpdated",
+													        {
+														        item: response.json.item,
+														        bubblingLabel: me.options.bubblingLabel
+													        });
+											        },
+											        scope: this
+										        },
+										        failureCallback: {
+											        fn: function DataGrid_onActionEdit_refreshFailure(response) {
+												        Alfresco.util.PopupManager.displayMessage(
+													        {
+														        text: this.msg("message.details.failure")
+													        });
+											        },
+											        scope: this
+										        }
+									        });
+							        }.bind(me);
+
+							        me.onDelete([oResult], owner, {fullDelete: true, targetDelete: true, successMessage: "message.employee.role.delete.success"}, fnDeleteComplete, onPrompt);
+						        } else {
+							        Alfresco.util.PopupManager.displayMessage(
+								        {
+									        text: this.msg("message.employee.role.delete.failure")
+								        });
+						        }
+					        },
+					        failure: function (oResponse) {
+						        Alfresco.util.PopupManager.displayMessage(
+							        {
+								        text: this.msg("message.employee.role.delete.failure")
+							        });
+					        }
+				        };
+				        YAHOO.util.Connect.asyncRequest('GET', sUrl, callback);
+			        }
+		        },
+		        failure: function (oResponse) {
+			        Alfresco.util.PopupManager.displayMessage(
+				        {
+					        text: this.msg("message.employee.role.delete.failure")
+				        });
+		        }
+	        };
+	        YAHOO.util.Connect.asyncRequest('GET', sUrlWorkGroup, callbackWorkGroup);
         }
     }, true);
 })();
