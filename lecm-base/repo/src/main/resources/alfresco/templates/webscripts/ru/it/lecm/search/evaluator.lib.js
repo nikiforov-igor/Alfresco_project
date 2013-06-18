7c9fd88427d72cad4b2ddb5073fc90cf55493d93
Котельникова Полина <PKotelnikova@it.ru>
2013-06-18 11:49:11 +0000

    ALF-1071 При нажатии "Исключить сотрудника" и подтверждении в появляющемся сообщении, сотрудник не снимается с роли.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@2923 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/search/evaluator.lib.js
+++ lecm-base/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/search/evaluator.lib.js
@@ -133,7 +133,7 @@ var Evaluator =
        }
        else if (type.indexOf(":") > 0 && node.isSubType("cm:cmobject")) {
            obj = Evaluator.getContentObject(value);
-           if (obj == null) {
+           if (obj == null || obj.nodeRef.toString().startsWith("archive")) {
                return false;
            }
            if (substituteNode == null) {
