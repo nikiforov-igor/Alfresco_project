802b3cfa085f358dea293471f299ba80c2d026a1
Салахова Ольга <ORakovskaya@it.ru>
2013-06-24 13:49:59 +0000

    Исправлен текст
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3031 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-statemachine/share/src/main/webapp/scripts/statemachine/form.js
+++ lecm-statemachine/share/src/main/webapp/scripts/statemachine/form.js
@@ -258,7 +258,7 @@ LogicECM.module = LogicECM.module || {};
             var form = '<div id="confirm-edit-fields-form-container" class="yui-panel">' +
                 '<div id="confirm-edit-fields-head" class="hd">Ошибка действия "' + label + '"</div>' +
                 '<div id="confirm-edit-fields-body" class="bd">' +
-                '<div id="confirm-edit-fields-content" class="form-container"><div class="form-fields" style="padding: 1em">Выполнение действия не возможно.<br/>';
+                '<div id="confirm-edit-fields-content" class="form-container"><div class="form-fields" style="padding: 1em">Выполнение действия невозможно.<br/>';
                 for (var i = 0; i < errors.length; i++) {
                     form += errors[i];
                     form += "<br/>";
