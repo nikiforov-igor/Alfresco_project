3c2e9002847e805bd3bc3f7cea4fdd71a6214c07
Денис Гончар <DGonchar@it.ru>
2013-06-28 10:05:14 +0000

    [~] ALF-1162: "Наследование" ИНН от материнской компании для филиалов.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3095 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-contractors/api/src/main/java/ru/it/lecm/contractors/api/Contractors.java
+++ lecm-contractors/api/src/main/java/ru/it/lecm/contractors/api/Contractors.java
@@ -1,10 +1,18 @@
 package ru.it.lecm.contractors.api;
 
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+
 import java.util.List;
 import java.util.Map;
 
 public interface Contractors {
+
+    String CONTRACTOR_NAMESPACE = "http://www.it.ru/lecm/contractors/model/contractor/1.0";
+    QName TYPE_CONTRACTOR = QName.createQName(CONTRACTOR_NAMESPACE, "contractor-type");
+    QName PROP_CONTRACTOR_SHORTNAME = QName.createQName(CONTRACTOR_NAMESPACE, "shortname");
+    QName PROP_CONTRACTOR_INN = QName.createQName(CONTRACTOR_NAMESPACE, "INN");
+
     void assignAsPrimaryRepresentative(NodeRef representativeToAssignAsPrimary);
     Map<String, String> getParentContractor(NodeRef childContractor);
     List<Object> getRepresentatives(NodeRef targetContractor);
--- lecm-contractors/repo/src/main/java/ru/it/lecm/contractors/policies/ContractorsContractorPolicy.java
+++ lecm-contractors/repo/src/main/java/ru/it/lecm/contractors/policies/ContractorsContractorPolicy.java
@@ -4,10 +4,15 @@ import org.alfresco.model.ContentModel;
 import org.alfresco.repo.node.NodeServicePolicies;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.util.PropertyCheck;
+import ru.it.lecm.contractors.api.Contractors;
+import ru.it.lecm.dictionary.beans.DictionaryBean;
+
+import java.io.Serializable;
 
 /**
  * @author dgonchar
@@ -30,17 +35,49 @@ public class ContractorsContractorPolicy implements NodeServicePolicies.OnUpdate
         PropertyCheck.mandatory(this, "nodeService", nodeService);
 
         policyComponent.bindClassBehaviour(NodeServicePolicies.OnUpdateNodePolicy.QNAME,
-                QName.createQName("http://www.it.ru/lecm/contractors/model/contractor/1.0", "contractor-type"),
+                Contractors.TYPE_CONTRACTOR,
                 new JavaBehaviour(this, "onUpdateNode"));
     }
 
     @Override
     public void onUpdateNode(NodeRef contractor) {
-        if(nodeService.getProperty(contractor, QName.createQName("http://www.it.ru/lecm/contractors/model/contractor/1.0", "shortname")) == null) {
-            return;
+
+        /* Наследуем ИНН от материнской компании */
+        Serializable currentContractorINN = nodeService.getProperty(contractor, Contractors.PROP_CONTRACTOR_INN);
+
+        // Если есть такое свойство
+        if(currentContractorINN != null) {
+
+            // Если у текущего контрагента нет ИНН
+            if(currentContractorINN.toString().isEmpty()) {
+
+                NodeRef currentContractorParentRef = nodeService.getPrimaryParent(contractor).getParentRef();
+                QName currentContractorParentType = nodeService.getType(currentContractorParentRef);
+
+                // Если текущий контрагент является представителем
+                if(currentContractorParentType.equals(Contractors.TYPE_CONTRACTOR)) {
+
+                    Serializable currentContractorParentINN = nodeService.getProperty(currentContractorParentRef, Contractors.PROP_CONTRACTOR_INN);
+
+                    // Если у материнской компании есть ИНН
+                    if(!currentContractorParentINN.toString().isEmpty()) {
+
+                        // То "унаследуем" ИНН от материнской компании
+                        nodeService.setProperty(contractor, Contractors.PROP_CONTRACTOR_INN, currentContractorParentINN);
+                    }
+                }
+            }
+
         }
 
-        String shortname = nodeService.getProperty(contractor, QName.createQName("http://www.it.ru/lecm/contractors/model/contractor/1.0", "shortname")).toString();
-        nodeService.setProperty(contractor, ContentModel.PROP_NAME, shortname.replaceAll("[^_\\-\\dA-Za-zА-Яа-я ]", ""));
+        /* Формируем имя "файла" в репозитории на основе сокращённого наименования, убрав запрещённые символы */
+        Serializable currentContractorShortName = nodeService.getProperty(contractor, Contractors.PROP_CONTRACTOR_SHORTNAME);
+
+        // Если есть такое свойство
+        if(currentContractorShortName != null) {
+            String shortname = nodeService.getProperty(contractor, Contractors.PROP_CONTRACTOR_SHORTNAME).toString();
+            String filename = shortname.replaceAll("[^_\\-\\dA-Za-zА-Яа-я ]", "");
+            nodeService.setProperty(contractor, ContentModel.PROP_NAME, filename);
+        }
     }
 }
