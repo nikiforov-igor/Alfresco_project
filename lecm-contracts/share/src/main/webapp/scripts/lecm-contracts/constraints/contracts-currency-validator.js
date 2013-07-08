734b6c07d7fb08dc9badbaec6f1470f53220f160
Ивкин Алексей <AIvkin@it.ru>
2013-07-08 05:53:46 +0000

    ALF-1275: Валюта договора должна быть обязательным атрибутом только при непустом значении атрибута Сумма договора.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3164 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-contracts/share/src/main/webapp/scripts/lecm-contracts/constraints/contracts-currency-validator.js
@@ -0,0 +1,24 @@
+if (typeof LogicECM == "undefined" || !LogicECM) {
+	var LogicECM = {};
+}
+
+LogicECM.module = LogicECM.module || {};
+LogicECM.module.Contracts = LogicECM.module.Contracts || {};
+
+LogicECM.module.Contracts.currencyValidation =
+	function Contracts_currencyValidation(field, args,  event, form, silent, message) {
+		var totalAmount = field.form["prop_lecm-contract_totalAmount"];
+		var currency = field.form["assoc_lecm-contract_currency-assoc"];
+
+		if (totalAmount != null && currency != null && totalAmount.value.length > 0) {
+			var totalAmountFloat = parseFloat(totalAmount.value);
+			if (isNaN(totalAmountFloat)) {
+				return false;
+			} else {
+				if (totalAmountFloat != 0) {
+					return currency.value.length > 0;
+				}
+			}
+		}
+		return true;
+	};
