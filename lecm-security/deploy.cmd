47808c249d8723908253fcf02c1a11b1cb6860b3
Зиновин Александр <AZinovin@it.ru>
2013-07-08 13:19:36 +0000

    смена версии платформы и всех модулей на 0.5.0
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3174 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-security/deploy.cmd
+++ lecm-security/deploy.cmd
@@ -8,9 +8,9 @@ IF %TOMCAT%x==x SET TOMCAT=C:\Alfresco\tomcat
 SET DEST_REPO=%TOMCAT%\webapps\alfresco
 SET DEST_SHARE=%TOMCAT%\webapps\share
 
-SET SRC_API=.\api\target\%MODULE%-api-0.1.0
-SET SRC_REPO=.\repo\target\%MODULE%-repo-0.1.0
-SET SRC_SHARE=.\share\target\%MODULE%-share-0.1.0
+SET SRC_API=.\api\target\%MODULE%-api-0.5.0
+SET SRC_REPO=.\repo\target\%MODULE%-repo-0.5.0
+SET SRC_SHARE=.\share\target\%MODULE%-share-0.5.0
 
 xcopy /Y /E %SRC_API%\config\*.*   %DEST_REPO%\WEB-INF\classes\
 xcopy /Y %SRC_API%\lib\*.*   %DEST_REPO%\WEB-INF\lib\
