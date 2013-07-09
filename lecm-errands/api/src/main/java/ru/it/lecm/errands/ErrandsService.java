48666e0e09c8f7a1a44775c13d93abf2efd96c04
Ивкин Алексей <AIvkin@it.ru>
2013-07-09 08:20:35 +0000

    ALF-1210: В модель данных для договоров добавлены служебные поля для поиска и сортировки по ассоциациям
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3188 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-errands/api/src/main/java/ru/it/lecm/errands/ErrandsService.java
@@ -0,0 +1,14 @@
+package ru.it.lecm.errands;
+
+import org.alfresco.service.namespace.QName;
+
+/**
+ * User: AIvkin
+ * Date: 09.07.13
+ * Time: 12:09
+ */
+public interface ErrandsService {
+	public static final String ERRANDS_NAMESPACE_URI = "http://www.it.ru/logicECM/errands/1.0";
+
+	public static final QName TYPE_ERRANDS = QName.createQName(ERRANDS_NAMESPACE_URI, "document");
+}
---
+++ lecm-errands/repo/src/main/java/ru/it/lecm/errands/policies/ErrandsOnCreateAssocsPolicy.java
@@ -0,0 +1,23 @@
+package ru.it.lecm.errands.policies;
+
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.JavaBehaviour;
+import ru.it.lecm.base.policies.LogicECMAssociationPolicy;
+import ru.it.lecm.errands.ErrandsService;
+
+/**
+ * User: dbashmakov
+ * Date: 23.04.13
+ * Time: 17:30
+ */
+public class ErrandsOnCreateAssocsPolicy extends LogicECMAssociationPolicy {
+    @Override
+    public final void init() {
+        super.init();
+        policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnDeleteAssociationPolicy.QNAME,
+		        ErrandsService.TYPE_ERRANDS, new JavaBehaviour(this, "onDeleteAssociation"));
+
+        policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME,
+		        ErrandsService.TYPE_ERRANDS, new JavaBehaviour(this, "onCreateAssociation"));
+    }
+}
