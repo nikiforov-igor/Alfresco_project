b600a90f227edece4f98821eef4261e7ccd39064
Ивкин Алексей <AIvkin@it.ru>
2013-07-12 07:10:08 +0000

    ALF-1239: Добавлен контрол для выбора заголовка поручений
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3233 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-base/share/src/main/webapp/scripts/lecm-base/components/lecm-association-autocomplete-text.js
@@ -0,0 +1,280 @@
+/**
+ * LogicECM root namespace.
+ *
+ * @namespace LogicECM
+ */
+// Ensure LogicECM root object exists
+if (typeof LogicECM == "undefined" || !LogicECM) {
+	var LogicECM = {};
+}
+
+/**
+ * LogicECM top-level module namespace.
+ *
+ * @namespace LogicECM
+ * @class LogicECM.module
+ */
+LogicECM.module = LogicECM.module || {};
+
+(function () {
+	var Dom = YAHOO.util.Dom;
+
+	var $combine = Alfresco.util.combinePaths;
+
+	LogicECM.module.AssociationAutoCompleteText = function (fieldHtmlId) {
+		LogicECM.module.AssociationAutoCompleteText.superclass.constructor.call(this, "LogicECM.module.AssociationAutoCompleteText", fieldHtmlId, [ "container", "datasource"]);
+		YAHOO.Bubbling.on("refreshAutocompleteItemList_" + fieldHtmlId, this.onRefreshAutocompleteItemList, this);
+
+		this.controlId = fieldHtmlId + "-cntrl";
+		this.currentValueHtmlId = fieldHtmlId;
+		this.dataArray = [];
+
+		return this;
+	};
+
+	YAHOO.extend(LogicECM.module.AssociationAutoCompleteText, Alfresco.component.Base,
+		{
+			options:{
+				disabled: false,
+
+				mandatory:false,
+
+				startLocation: null,
+
+				parentNodeRef:"",
+
+				itemType:"cm:content",
+
+				itemFamily:"node",
+
+				maxSearchResults:1000,
+
+				nameSubstituteString:"{cm:name}",
+
+				additionalFilter: "",
+
+				ignoreNodes: [],
+
+				childrenDataSource: "lecm/forms/picker"
+			},
+
+			dataArray: null,
+
+			controlId:"",
+
+			currentValueHtmlId: "",
+
+			dataSource:null,
+
+			onReady:function () {
+				if (!this.options.disabled) {
+					this.populateData();
+				}
+			},
+
+			onRefreshAutocompleteItemList: function (layer, args)
+			{
+//				this.selectedItems = args[1].selectedItems;
+//
+//				if (!this.options.disabled) {
+//					this.updateSelectedItems();
+//					this.updateInputUI();
+//				} else {
+//					this.updateCurrentDisplayValue();
+//				}
+			},
+
+			makeAutocomplete: function() {
+				var oDS = new YAHOO.util.LocalDataSource(this.dataArray);
+				oDS.responseSchema = {fields:["name", "nodeRef"]};
+				var oAC = new YAHOO.widget.AutoComplete(this.currentValueHtmlId, this.controlId + "-autocomplete-container", oDS);
+				oAC.prehighlightClassName = "yui-ac-prehighlight";
+				oAC.useShadow = true;
+				oAC.forceSelection = true;
+				oAC._bFocused = true;
+
+				var selectItemHandler = function (sType, aArgs) {
+					if (this.options.mandatory) {
+						YAHOO.Bubbling.fire("mandatoryControlValueUpdated", this);
+					}
+				}.bind(this);
+				oAC.itemSelectEvent.subscribe(selectItemHandler);
+			},
+
+			populateData: function () {
+				this._createDataSource();
+
+				var successHandler = function (sRequest, oResponse, oPayload)
+				{
+					var results = oResponse.results;
+					for (var i = 0; i < results.length; i++) {
+						var node = results[i];
+						if (node.selectable) {
+							this.dataArray.push({
+								name: node.name,
+								nodeRef: node.nodeRef
+							});
+						}
+					}
+					this.makeAutocomplete();
+				}.bind(this);
+
+				var failureHandler = function (sRequest, oResponse)
+				{
+					if (oResponse.status == 401)
+					{
+						// Our session has likely timed-out, so refresh to offer the login page
+						window.location.reload();
+					}
+					else
+					{
+						//todo show failure message
+					}
+				}.bind(this);
+
+				var url = this._generateChildrenUrlPath(this.options.parentNodeRef) + this._generateChildrenUrlParams("");
+
+				this.dataSource.sendRequest(url,
+					{
+						success: successHandler,
+						failure: failureHandler,
+						scope: this
+					});
+			},
+
+			_createDataSource: function () {
+				var me = this;
+
+				var pickerChildrenUrl = Alfresco.constants.PROXY_URI + this.options.childrenDataSource + "/" + this.options.itemFamily;
+				this.dataSource = new YAHOO.util.DataSource(pickerChildrenUrl,
+					{
+						responseType: YAHOO.util.DataSource.TYPE_JSON,
+						connXhrMode: "queueRequests",
+						responseSchema:
+						{
+							resultsList: "items",
+							metaFields:
+							{
+								parent: "parent"
+							}
+						}
+					});
+
+				this.dataSource.doBeforeParseData = function (oRequest, oFullResponse)
+				{
+					var updatedResponse = oFullResponse;
+
+					if (oFullResponse)
+					{
+						var items = oFullResponse.data.items;
+
+						if (me.options.maxSearchResults > -1 && items.length > me.options.maxSearchResults)
+						{
+							items = items.slice(0, me.options.maxSearchResults-1);
+						}
+
+						var index, item;
+						for (index in items)
+						{
+							if (items.hasOwnProperty(index))
+							{
+								item = items[index];
+								if (item.type == "cm:category" && item.displayPath.indexOf("/categories/Tags") !== -1)
+								{
+									item.type = "tag";
+									oFullResponse.data.parent.type = "tag";
+								}
+							}
+						}
+
+						var ignoreItems = me.options.ignoreNodes;
+						if (ignoreItems != null) {
+							var tempItems = [];
+							var k = 0;
+							for (index in items) {
+								item = items[index];
+								var ignore = false;
+								for (var i = 0; i < ignoreItems.length; i++) {
+									if (ignoreItems[i] == item.nodeRef) {
+										ignore = true;
+									}
+								}
+								if (!ignore) {
+									tempItems[k] = item;
+									k++;
+								}
+							}
+							items = tempItems;
+						}
+
+						updatedResponse =
+						{
+							parent: oFullResponse.data.parent,
+							items: items
+						};
+					}
+
+					return updatedResponse;
+				};
+			},
+
+			_generateChildrenUrlPath: function (nodeRef)
+			{
+				return $combine("/", nodeRef.replace("://", "/"), "children");
+			},
+
+			_generateChildrenUrlParams: function (searchTerm)
+			{
+				var params = "?selectableType=" + this.options.itemType + "&searchTerm=" + encodeURIComponent(searchTerm) +
+					"&size=" + this.options.maxSearchResults + "&nameSubstituteString=" + encodeURIComponent(this.options.nameSubstituteString) +
+					"&additionalFilter=" + encodeURIComponent(this.options.additionalFilter);
+
+				if (this.options.startLocation && this.options.startLocation.charAt(0) == "/")
+				{
+					params += "&xpath=" + encodeURIComponent(this.options.startLocation);
+				} else if (this.options.xPathLocation)
+				{
+					params += "&xPathLocation=" + encodeURIComponent(this.options.xPathLocation);
+					if (this.options.xPathLocationRoot != null) {
+						params += "&xPathRoot=" + encodeURIComponent(this.options.xPathLocationRoot);
+					}
+				}
+				// has a rootNode been specified?
+				if (this.options.rootNode)
+				{
+					var rootNode = null;
+
+					if (this.options.rootNode.charAt(0) == "{")
+					{
+						if (this.options.rootNode == "{companyhome}")
+						{
+							rootNode = "alfresco://company/home";
+						}
+						else if (this.options.rootNode == "{userhome}")
+						{
+							rootNode = "alfresco://user/home";
+						}
+						else if (this.options.rootNode == "{siteshome}")
+						{
+							rootNode = "alfresco://sites/home";
+						}
+					}
+					else
+					{
+						// rootNode is either an xPath expression or a nodeRef
+						rootNode = this.options.rootNode;
+					}
+					if (rootNode !== null)
+					{
+						params += "&rootNode=" + encodeURIComponent(rootNode);
+					}
+				}
+
+				return params;
+			},
+
+			destroy:function () {
+				LogicECM.module.AssociationAutoCompleteText.superclass.destroy.call(this);
+			}
+		});
+})();
