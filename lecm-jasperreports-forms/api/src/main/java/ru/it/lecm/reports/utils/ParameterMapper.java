890b46da58b3f8e43e308c8b6e30d421d9ff4852
Денис Башмаков <DBashmakov@it.ru>
2013-07-18 05:19:15 +0000

    дополнение по ALF-1117 Генерация web-формы задания параметров отчета
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3351 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-jasperreports-forms/api/src/main/java/ru/it/lecm/reports/utils/ParameterMapper.java
+++ lecm-jasperreports-forms/api/src/main/java/ru/it/lecm/reports/utils/ParameterMapper.java
@@ -1,84 +1,79 @@
 package ru.it.lecm.reports.utils;
 
-import java.util.Arrays;
-import java.util.Map;
-
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import ru.it.lecm.reports.api.model.ColumnDescriptor;
 import ru.it.lecm.reports.api.model.ParameterType.Type;
 import ru.it.lecm.reports.api.model.ReportDescriptor;
+
+import java.util.Map;
 
 /**
  * Распределитель request-аргументов по параметрам описателя отчёта.
  * Название аргумента для колонки данных выбирается в getArgRootName.
  */
 public class ParameterMapper {
+    static final transient Logger log = LoggerFactory.getLogger(ParameterMapper.class);
+    public static final String DATE_RANGE = "-date-range";
+    public static final String NUMBER_RANGE = "-range";
 
-	static final transient Logger log = LoggerFactory.getLogger(ParameterMapper.class);
-
-	/**
+    /**
 	 * Задать параметры из списка. Подразумевается, что параметры имеют названия 
 	 * совпадающие с названиями параметра колонки данных из описателя reportDesc.getDsDescriptor.
 	 * @param reportDesc
 	 * @param args
-	 */
-	public static void assignParameters( ReportDescriptor reportDesc, Map<String, String[]> args) {
+     */
+    public static void assignParameters(ReportDescriptor reportDesc, Map<String, String[]> args) {
+        if (args == null || reportDesc == null || reportDesc.getDsDescriptor() == null)
+            return;
 
-		if (args == null || reportDesc == null || reportDesc.getDsDescriptor() == null)
-			return;
+        for (ColumnDescriptor colDesc : reportDesc.getDsDescriptor().getColumns()) {
+            if (colDesc.getParameterValue() == null) // колонка не описана как параметр ...
+                continue;
 
-		for ( ColumnDescriptor colDesc: reportDesc.getDsDescriptor().getColumns()) {
+            // если колонка параметризована ...
+            String argRootName = colDesc.getColumnName();
+            switch (colDesc.getParameterValue().getType()) {
+                case VALUE:
+                case LIST:
+                    if (args.containsKey(argRootName)) {
+                        final String[] paramValue = args.get(argRootName);
+                        if ((colDesc.getParameterValue().getType() == Type.VALUE) && (paramValue != null) && (paramValue.length > 0)){
+                            // для простых значений явно зададим один элемент
+                            colDesc.getParameterValue().setBound1(paramValue[0]);
+                        } else {
+                            colDesc.getParameterValue().setBound1(paramValue);
+                        }
+                    }
+                    break;
+                case RANGE:
+                    // проверяем диапозон дат
+                    String dateRangeParam = argRootName + DATE_RANGE;
+                    if (args.containsKey(dateRangeParam)) {
+                        argRootName = dateRangeParam;
+                    } else {
+                        // не нашли параметра - пробуем получить диапозон для чисел
+                        String numberRangeParam = argRootName + NUMBER_RANGE;
+                        if (args.containsKey(numberRangeParam)) {
+                            argRootName = numberRangeParam;
+                        }
+                    }
+                    if (args.containsKey(argRootName)) {
+                        final String[] paramValue = args.get(argRootName)[0].split("\\|");
+                        colDesc.getParameterValue().setBound1((paramValue[0] != null && paramValue[0].length() > 0) ? paramValue[0] : null);
+                        if (paramValue.length == 2) {
+                            colDesc.getParameterValue().setBound2((paramValue[1] != null && paramValue[1].length() > 0) ? paramValue[1] : null);
+                        }
+                    }
 
-			if ( colDesc.getParameterValue() == null) // колонка не описана как параметр ...
-				continue;
-
-			// если колонка параметризована ...
-			final String argRootName = getArgRootName(colDesc);
-			switch (colDesc.getParameterValue().getType()) {
-				case VALUE:
-				case LIST:
-					if (args.containsKey(argRootName)) {
-						final String[] paramValue = args.get(argRootName);
-						log.debug(String.format( "assigning for column '%s' parameter '%s': value '%s'"
-								, colDesc.getColumnName(), argRootName, Arrays.toString(paramValue)) );
-						if ( (colDesc.getParameterValue().getType() == Type.VALUE)
-								&& (paramValue != null) && (paramValue.length > 0)
-								) // для простых значений явно зададим один элемент
-							colDesc.getParameterValue().setBound1(paramValue[0]);
-						else
-							colDesc.getParameterValue().setBound1(paramValue);
-					}
-					break;
-				case RANGE:
-					// диапазон задаётся параметрами "{mnem}_lo" и "{mnem}_hi" ...
-					final String argNameLo = argRootName + "_lo";
-					final String argNameHi = argRootName + "_hi";
-
-					if (args.containsKey(argNameLo)) {
-						final String[] paramValue = args.get(argNameLo);
-						log.debug(String.format( "assigning for column '%s' low-bound from arg '%s' with value '%s'"
-								, colDesc.getColumnName(), argNameLo, Arrays.toString(paramValue)) );
-						colDesc.getParameterValue().setBound1( (paramValue != null && paramValue.length > 0) ? paramValue[0] : null);
-					}
-
-					if (args.containsKey(argNameHi)) {
-						final String[] paramValue = args.get(argNameHi);
-						log.debug(String.format( "assigning for column '%s' high-bound from arg '%s' with value '%s'"
-								, colDesc.getColumnName(), argNameHi, Arrays.toString(paramValue)) );
-						colDesc.getParameterValue().setBound2( (paramValue != null && paramValue.length > 0) ? paramValue[0] : null);
-					}
-					break;
-
-				default: // непонятный тип - сообщение об ошибке и игнор ...
-					log.error( String.format("Unsupported parameter type '%s' skipped", Utils.coalesce(colDesc.getParameterValue().getType(), "NULL") ));
-					break;
-			}
-	
-		}
-	}
+                    break;
+                default: // непонятный тип - сообщение об ошибке и игнор ...
+                    log.error(String.format("Unsupported parameter type '%s' skipped", Utils.coalesce(colDesc.getParameterValue().getType(), "NULL")));
+                    break;
+            }
+        }
+    }
 
 
 	/**
