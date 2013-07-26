e0d00abe178f5461f43c6ceb7b76225a2db959f4
Котельникова Полина <PKotelnikova@it.ru>
2013-07-26 14:46:44 +0000

    ALF-1245, ALF-1246
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3481 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/tasks/dashlet/my-tasks.get.js
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/tasks/dashlet/my-tasks.get.js
@@ -13,14 +13,16 @@ function main() {
 
     var statuses = "В работе,На доработке,На утверждении контролером,На утверждении инициатором";
 
-    var url = "/lecm/errands/api/documentMyErrands?nodeRef=" + args["nodeRef"] + "&statuses=" + statuses;
+//    var url = "/lecm/errands/api/documentMyErrands?nodeRef=" + args["nodeRef"] + "&statuses=" + statuses;
+    var url = "/lecm/errands/api/documentMyErrands?nodeRef=" + args["nodeRef"] + "&statuses=";
     var json = remote.connect("alfresco").get(url);
     if (json.status == 200) {
         var obj = eval("(" + json + ")");
         model.myErrandsData = obj;
     }
 
-    var url = "/lecm/errands/api/documentErrandsIssuedByMe?nodeRef=" + args["nodeRef"] + "&statuses=" + statuses;
+//    var url = "/lecm/errands/api/documentErrandsIssuedByMe?nodeRef=" + args["nodeRef"] + "&statuses=" + statuses;
+    var url = "/lecm/errands/api/documentErrandsIssuedByMe?nodeRef=" + args["nodeRef"] + "&statuses=";
     var json = remote.connect("alfresco").get(url);
     if (json.status == 200) {
         var obj = eval("(" + json + ")");
