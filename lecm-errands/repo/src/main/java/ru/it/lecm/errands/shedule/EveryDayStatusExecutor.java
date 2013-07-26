2b35255d0ded82ed808534181e271f2e59abbf9f
Махмуд Шафеев <MShafeev@it.ru>
2013-07-26 06:23:49 +0000

    6.2 Автоматическая установка атрибута «Просрочен».
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3466 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-errands/repo/src/main/java/ru/it/lecm/errands/shedule/EveryDayStatusExecutor.java
@@ -0,0 +1,96 @@
+package ru.it.lecm.errands.shedule;
+
+import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import ru.it.lecm.base.beans.BaseBean;
+import ru.it.lecm.documents.beans.DocumentService;
+import ru.it.lecm.errands.ErrandsService;
+import ru.it.lecm.errands.beans.ErrandsServiceImpl;
+import ru.it.lecm.notifications.beans.Notification;
+import ru.it.lecm.notifications.beans.NotificationsService;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * User: mshafeev
+ * Date: 24.07.13
+ * Time: 15:48
+ */
+public class EveryDayStatusExecutor extends ActionExecuterAbstractBase {
+    private ErrandsServiceImpl errandsService;
+    private NotificationsService notificationsService;
+    private NodeService nodeService;
+
+    public void setErrandsService(ErrandsServiceImpl errandsService) {
+        this.errandsService = errandsService;
+    }
+
+    public void setNotificationsService(NotificationsService notificationsService) {
+        this.notificationsService = notificationsService;
+    }
+
+    public void setNodeService(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    @Override
+    protected void executeImpl(Action action, NodeRef nodeRef) {
+        // выставляем атрибут "Просрочен" в значение true
+        nodeService.setProperty(nodeRef, errandsService.TYPE_ERRANDS_IS_EXPIRED, true);
+
+        // формируем уведомление Исполнителю, Инициатору и Контроллеру:
+        Notification notification = new Notification();
+
+        List<NodeRef> employeeList = getEmployeeList(nodeRef);
+
+        notification.setRecipientEmployeeRefs(employeeList); // значения должны быть уникальны
+        notification.setAuthor(AuthenticationUtil.getSystemUserName());
+        notification.setDescription(getNotificationDescription(nodeRef));
+        notification.setObjectRef(nodeRef);
+        notificationsService.sendNotification(notification);
+    }
+
+    @Override
+    protected void addParameterDefinitions(List<ParameterDefinition> paramList) {
+    }
+
+    private String getNotificationDescription(NodeRef document) {
+        String docDesc = errandsService.wrapperLink(document, nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING).toString(), BaseBean.DOCUMENT_LINK_URL);
+        String desc = docDesc + " не исполнено в установленный срок! Дальнейшая работа с ним невозможна!";
+        return desc;
+    }
+
+    /**
+     * Получаем список участников в подписке: Исполнителей, Инициатора и Контроллера
+     *
+     * @param document
+     * @return
+     */
+    private ArrayList<NodeRef> getEmployeeList(NodeRef document) {
+        Set<NodeRef> employee = new HashSet<NodeRef>();
+
+        List<AssociationRef> employeeAssocs = new ArrayList<AssociationRef>();
+
+        employeeAssocs.addAll(nodeService.getTargetAssocs(document, ErrandsService.ASSOC_ERRANDS_EXECUTOR));
+        employeeAssocs.addAll(nodeService.getTargetAssocs(document, ErrandsService.ASSOC_ERRANDS_INITIATOR));
+        employeeAssocs.addAll(nodeService.getTargetAssocs(document, ErrandsService.ASSOC_ERRANDS_CONTROLLER));
+
+        if (employeeAssocs != null && !employeeAssocs.isEmpty()) {
+            for (AssociationRef employeeAssoc : employeeAssocs) {
+                employee.add(employeeAssoc.getTargetRef());
+            }
+        }
+
+        return new ArrayList<NodeRef>(employee);
+    }
+
+
+}
