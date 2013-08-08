c15ce0ead5c7560af0063f746b57816af19a1299
Салахова Ольга <ORakovskaya@it.ru>
2013-08-08 11:37:03 +0000

    ALF-1271 : Форма поручения (главная форма) : bugfix & refactoring
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@3657 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-errands/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/errands/api/document-errands/errand.additional-document.get.json.ftl
+++ lecm-errands/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/errands/api/document-errands/errand.additional-document.get.json.ftl
@@ -1,6 +1,8 @@
 <#escape x as x?js_string>
 {
-"nodeRef": "${additionalDoc.nodeRef}",
-"name": "${additionalDoc.properties["lecm-document:present-string"]!additionalDoc.getName()}"
+    <#if additionalDoc??>
+        "nodeRef": "${additionalDoc.nodeRef}",
+        "name": "${additionalDoc.properties["lecm-document:present-string"]!additionalDoc.getName()}"
+    </#if>
 }
 </#escape>
--- lecm-errands/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/errands/components/form.get.js
+++ lecm-errands/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/errands/components/form.get.js
@@ -10,30 +10,29 @@ function main() {
         model.node = nodeDetails.item.node;
     }
 
-    var atts = getAttachments(model.nodeRef, "Поручение");
-    if (atts != null) {
-        model.attachments = atts;
-    }
-    atts = getAttachments(model.nodeRef, "Исполнение");
-    if (atts != null) {
-        model.attachmentsExec = atts;
-    }
-    atts = getAttachments(model.nodeRef, "Контроль");
-    if (atts != null) {
-        model.attachmentsControl = atts;
-    }
-
-    var coexecs = getCoexecutors(model.nodeRef);
-    if (coexecs != null) {
-        model.coexecs = coexecs;
-    }
-
-    var additionalDoc = getAdditonalDoc(model.nodeRef);
-    if (additionalDoc != null) {
-        model.additionalDoc = additionalDoc;
-    }
-
     var uri = '';
+
+    uri = addParamToUrl('/lecm/document/attachments/api/getAttachmentsByCategory', 'documentNodeRef', model.nodeRef);
+    uri = addParamToUrl(uri, 'category', encodeURIComponent("Поручение"));
+    model.attachments  = doGetCall(uri);
+
+    uri = addParamToUrl('/lecm/document/attachments/api/getAttachmentsByCategory', 'documentNodeRef', model.nodeRef);
+    uri = addParamToUrl(uri, 'category', encodeURIComponent("Исполнение"));
+    model.attachmentsExec  = doGetCall(uri);
+
+    uri = addParamToUrl('/lecm/document/attachments/api/getAttachmentsByCategory', 'documentNodeRef', model.nodeRef);
+    uri = addParamToUrl(uri, 'category', encodeURIComponent("Контроль"));
+    model.attachmentsControl  = doGetCall(uri);
+
+    //todo: change members to coexecutors
+    uri = addParamToUrl('/lecm/document/api/getMembers', 'nodeRef', model.nodeRef);
+    uri = addParamToUrl(uri, 'skipCount', 0);
+    uri = addParamToUrl(uri, 'loadCount', 10);
+    model.coexecs  = doGetCall(uri);
+
+    uri = addParamToUrl('/lecm/errands/api/getAdditionalDoc', 'nodeRef', model.nodeRef);
+    model.additionalDoc  = doGetCall(uri);
+
     uri = addParamToUrl('/lecm/errands/api/getLinks', 'nodeRef', model.nodeRef);
     uri = addParamToUrl(uri, 'assocType', 'lecm-errands:links-assoc');
     model.links  = doGetCall(uri);
@@ -42,45 +41,7 @@ function main() {
     uri = addParamToUrl(uri, 'assocType', 'lecm-errands:execution-links-assoc');
     model.executeLinks  = doGetCall(uri);
 
-
     model.limitDate = new Date(nodeDetails.item.node.properties["lecm-errands:limitation-date"]["value"]);
-}
-
-function getAttachments(nodeRef, categoryName) {
-    var url = '/lecm/document/attachments/api/getAttachmentsByCategory?documentNodeRef=' + nodeRef + '&category=' + encodeURIComponent(categoryName);
-    var result = remote.connect("alfresco").get(url);
-    if (result.status != 200) {
-        return null;
-    }
-    return eval('(' + result + ')');
-}
-function getCoexecutors(nodeRef) {
-    //todo: change members to coexecutors
-    var url = '/lecm/document/api/getMembers?nodeRef=' + nodeRef + "&skipCount=0&loadCount=10";
-    var result = remote.connect("alfresco").get(url);
-
-    if (result.status != 200) {
-        return null;
-    }
-    return eval('(' + result + ')');
-}
-function getAdditonalDoc(nodeRef) {
-    var url = '/lecm/errands/api/getAdditionalDoc?nodeRef=' + nodeRef;
-    var result = remote.connect("alfresco").get(url);
-
-    if (result.status != 200) {
-        return null;
-    }
-    return eval('(' + result + ')');
-}
-
-function getLinks(nodeRef, assocType) {
-    var url = '/lecm/errands/api/getLinks?nodeRef=' + nodeRef + "&assocType" + assocType;
-    var result = remote.connect("alfresco").get(url);
-    if (result.status != 200) {
-    return null;
-    }
-    return eval('(' + result + ')');
 }
 
 main();
