220f4547a442896b12a8aa07cb7b5c800c1248f2
Халиков Искандер <IKhalikov@it.ru>
2013-09-13 06:43:25 +0000

    [ЮЗД] [ALF-1558] Добавлено сообщение о разблокировке функций создания версий
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@4325 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-signed-docflow/share/src/main/webapp/scripts/signed-docflow/document-unlock.js
+++ lecm-signed-docflow/share/src/main/webapp/scripts/signed-docflow/document-unlock.js
@@ -3,35 +3,65 @@ YAHOO.util.Event.onDOMReady((function() {
     {
         actionName: "onUnlockAction",
         fn: function onUnlock(file) {
-            this.modules.actions.genericAction(
-            {
-                failure:
-                {
-                    message: this.msg("Произошла ошибка при разблокировании документа {0}", file.displayName, Alfresco.constants.USERNAME)
-                },
-                webscript:
-                {
-                    name: "lecm/signed-docflow/unlock?nodeRef={nodeRef}",
-                    stem: Alfresco.constants.PROXY_URI,
-                    method: Alfresco.util.Ajax.GET,
-                    params:
-                    {
-                        nodeRef: file.nodeRef,
-                    }
-                },
-                success:{
-                	callback:{
-                		fn: function (){
-                			window.location.reload();                			
-                		},
-                		scope: this
-                	}
-                },
-                config:
-                {
-                }
 
-            });
+            var actionScope = this;
+
+            function handleNo() {
+                this.hide();
+            };
+            
+            Alfresco.util.PopupManager.displayPrompt(
+                { 
+                    title: "Подтверждение разблокировки", // the title of the dialog, default is null 
+                    text: "В случае создания новых версий документа, загруженные Электронные подписи будут более недоступны. Вы уверены, что хотите разблокировать функции создания версий?", // the text to display for the user, mandatory 
+                    modal: true, // if a grey transparent overlay should be displayed in the background 
+                    buttons: [
+                        { 
+                            text: "Да", 
+                            handler: handleYes
+                        },
+                        {
+                            text: "Нет",
+                            handler: handleNo
+                        }
+                    ]
+                }
+                );
+
+            function handleYes(scope){
+                actionScope.modules.actions.genericAction(
+                {
+                    failure:
+                    {
+                        message: actionScope.msg("Произошла ошибка при разблокировании документа {0}", file.displayName, Alfresco.constants.USERNAME)
+                    },
+                    webscript:
+                    {
+                        name: "lecm/signed-docflow/unlock?nodeRef={nodeRef}",
+                        stem: Alfresco.constants.PROXY_URI,
+                        method: Alfresco.util.Ajax.GET,
+                        params:
+                        {
+                            nodeRef: file.nodeRef,
+                        }
+                    },
+                    success:{
+                    	callback:{
+                    		fn: function (){
+                    			window.location.reload();                			
+                    		},
+                    		scope: actionScope
+                    	}
+                    },
+                    config:
+                    {
+    					
+                    }
+
+                });
+
+                this.hide();
+            }
         }
     });
 }));
