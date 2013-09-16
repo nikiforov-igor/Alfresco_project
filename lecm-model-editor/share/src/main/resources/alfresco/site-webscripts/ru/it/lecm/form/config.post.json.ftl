b8c6b5a385f2658cc44b54dd06d37dd12a966398
Дмитрий Дмитриев <DDmitriev@it.ru>
2013-09-16 11:02:42 +0000

    Добавил базовую функциональность по деплою описания формы.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@4356 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-model-editor/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/form/config.post.json.ftl
@@ -0,0 +1,4 @@
+{
+	body: ${jsonBody}
+	result: ${configResult}
+}
--- lecm-model-editor/share/src/main/webapp/components/model-editor/model-editor.js
+++ lecm-model-editor/share/src/main/webapp/components/model-editor/model-editor.js
@@ -469,6 +469,22 @@ IT.component = IT.component || {};
 			Dom.get(field.id).value = xval;
 			//validation conditions
 			//if(Dom.get(field.id).value.length > 0) return true;
+			
+			Alfresco.util.Ajax.request({
+				url : Alfresco.constants.URL_SERVICECONTEXT + "config",
+				method : "POST",
+				requestContentType : "application/json",
+				dataObj: args.modelObject,
+				successCallback : { fn : function GenericFormTool_onLoad_onFormLoaded(response)
+	               {
+						
+	               } , scope : this },
+				failureCallback : { fn : function GenericFormTool_onLoad_onFormLoaded(response)
+	               {
+						
+	               }, scope : this }
+			});// request
+			
 			if(args.widgets.categoriesDataTable.getRecordSet().getLength()>0) return true;
 			if(args.widgets.attributesDataTable.getRecordSet().getLength()>0) return true;
 			if(args.widgets.associationsDataTable.getRecordSet().getLength()>0) return true;
