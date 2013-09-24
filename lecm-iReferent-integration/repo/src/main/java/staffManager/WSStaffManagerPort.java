4caa561002d2f106ad4102fc88778135a60b1cfe
Денис Башмаков <DBashmakov@it.ru>
2013-09-24 13:57:07 +0000

    [+]заглушки для тестов
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@4498 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-iReferent-integration/repo/src/main/java/staffManager/WSStaffManagerPort.java
+++ lecm-iReferent-integration/repo/src/main/java/staffManager/WSStaffManagerPort.java
@@ -10,38 +10,53 @@ import javax.jws.WebParam;
 
 @javax.jws.WebService(name = "StaffManagerServicePort", serviceName = "StaffManager", portName = "StaffManagerServicePort", targetNamespace = "urn:DefaultNamespace", endpointInterface = "staffManager.WSStaffManager")
 public class WSStaffManagerPort implements staffManager.WSStaffManager{
+    private ObjectFactory objectFactory;
+
+    public void setObjectFactory(ObjectFactory objectFactory) {
+        this.objectFactory = objectFactory;
+    }
+
     @Override
     public WSOGROUP getGroup(@WebParam(name = "ID", partName = "ID") String id, @WebParam(name = "INCLUDEATTACHMENTS", partName = "INCLUDEATTACHMENTS") boolean includeattachments, @WebParam(name = "CONTEXT", partName = "CONTEXT") WSOCONTEXT context) {
-        WSOGROUP wsogroup = new WSOGROUP();
-        wsogroup.setID("TEST");
-        wsogroup.setTITLE("TITLE-TEST");
-        wsogroup.setTYPE("ONLY TEST TYPE");
-        return wsogroup;
+        WSOGROUP group =  objectFactory.createWSOGROUP();
+        group.setTYPE("TYPE");
+        group.setTITLE("TITLE");
+        group.setID("ID");
+        group.setLEADER(objectFactory.createWSOPERSON());
+        return group;
     }
 
     @Override
     public WSOCOLLECTION getOrganizations(@WebParam(name = "ISMOBJECT", partName = "ISMOBJECT") boolean ismobject, @WebParam(name = "INCLUDEATTACHMENTS", partName = "INCLUDEATTACHMENTS") boolean includeattachments, @WebParam(name = "CONTEXT", partName = "CONTEXT") WSOCONTEXT context) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return objectFactory.createWSOCOLLECTION();
     }
 
     @Override
     public WSOPERSON getPerson(@WebParam(name = "ID", partName = "ID") String id, @WebParam(name = "INCLUDEATTACHMENTS", partName = "INCLUDEATTACHMENTS") boolean includeattachments, @WebParam(name = "CONTEXT", partName = "CONTEXT") WSOCONTEXT context) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        WSOPERSON person =  objectFactory.createWSOPERSON();
+        person.setTITLE("PERSON_TITLE");
+        person.setID("PERSON_ID");
+        person.setFIRSTNAME("IVAN");
+        person.setMIDDLENAME("IVANOVICH");
+        person.setLASTNAME("IVANOV");
+        return person;
     }
 
     @Override
     public WSOBJECT getPersonId(@WebParam(name = "PERSONLOGIN", partName = "PERSONLOGIN") String personlogin) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        WSOBJECT id = objectFactory.createWSOBJECT();
+        id.setID("ID");
+        return id;
     }
 
     @Override
     public WSOCOLLECTION getPersons(@WebParam(name = "GROUPID", partName = "GROUPID") String groupid, @WebParam(name = "ISMOBJECT", partName = "ISMOBJECT") boolean ismobject, @WebParam(name = "CHILDSLEVEL", partName = "CHILDSLEVEL") int childslevel, @WebParam(name = "INCLUDEATTACHMENTS", partName = "INCLUDEATTACHMENTS") boolean includeattachments, @WebParam(name = "CONTEXT", partName = "CONTEXT") WSOCONTEXT context) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return objectFactory.createWSOCOLLECTION();
     }
 
     @Override
     public WSOGROUP getStructure(@WebParam(name = "GROUPID", partName = "GROUPID") String groupid, @WebParam(name = "ISMOBJECT", partName = "ISMOBJECT") boolean ismobject, @WebParam(name = "CHILDSLEVEL", partName = "CHILDSLEVEL") int childslevel, @WebParam(name = "INCLUDEATTACHMENTS", partName = "INCLUDEATTACHMENTS") boolean includeattachments, @WebParam(name = "CONTEXT", partName = "CONTEXT") WSOCONTEXT context) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return getGroup(null,true,null);
     }
 
     @Override
