d00adcad39ee20f14b3ed6277656d83c69697bfd
Денис Башмаков <DBashmakov@it.ru>
2013-10-08 06:31:10 +0000

    рефаторинг обретки для Сотрудника
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@4772 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-jasperreports-forms/repo/src/main/java/ru/it/lecm/reports/jasper/containers/BasicEmployeeInfo.java
+++ lecm-jasperreports-forms/repo/src/main/java/ru/it/lecm/reports/jasper/containers/BasicEmployeeInfo.java
@@ -1,189 +1,194 @@
 package ru.it.lecm.reports.jasper.containers;
 
-import java.util.List;
-
 import org.alfresco.model.ContentModel;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.QName;
-
 import ru.it.lecm.orgstructure.beans.OrgstructureBean;
 import ru.it.lecm.reports.utils.Utils;
 
 /**
  * Контейнер для типизирования данных по Сотрудникам
- * 
+ * <p/>
  * Уникальность/Сравнение строго по employeeId.
  *
  * @author rabdullin
  */
 public class BasicEmployeeInfo {
-	// название Должностной Позиции
-	// "lecm-orgstr:staffPosition"::"lecm-orgstr:staffPosition-code"
-	public static final QName PROP_DP_INFO = ContentModel.PROP_NAME; // QName.createQName(OrgstructureBean.ORGSTRUCTURE_NAMESPACE_URI, "staffPosition-code");
+    // название Должностной Позиции
+    public static final QName PROP_DP_INFO = ContentModel.PROP_NAME;
 
-	// "lecm-orgstr:organization-element"::"element-short-name"
-	public static final QName PROP_ORGUNIT_NAME = QName.createQName(OrgstructureBean.ORGSTRUCTURE_NAMESPACE_URI, "element-short-name");
+    public static final QName PROP_ORGUNIT_NAME = OrgstructureBean.PROP_ORG_ELEMENT_SHORT_NAME;
 
-	// "lecm-orgstr:organization-unit"::"unit-code"
-	public static final QName PROP_ORGUNIT_CODE = QName.createQName(OrgstructureBean.ORGSTRUCTURE_NAMESPACE_URI, "unit-code");
+    // <!-- Сотрудник организации -->
+    public static final QName PROP_EMPLOYEE_NAME_FIRST = OrgstructureBean.PROP_EMPLOYEE_FIRST_NAME;
+    public static final QName PROP_EMPLOYEE_NAME_MIDDLE = OrgstructureBean.PROP_EMPLOYEE_MIDDLE_NAME;
+    public static final QName PROP_EMPLOYEE_NAME_LAST = OrgstructureBean.PROP_EMPLOYEE_LAST_NAME;
 
-	// <!-- Сотрудник организации -->
- 	public static final QName PROP_EMPLOYEE_NAME_FIRST = QName.createQName(OrgstructureBean.ORGSTRUCTURE_NAMESPACE_URI, "employee-first-name");
- 	public static final QName PROP_EMPLOYEE_NAME_MIDDLE = QName.createQName(OrgstructureBean.ORGSTRUCTURE_NAMESPACE_URI, "employee-middle-name");
- 	public static final QName PROP_EMPLOYEE_NAME_LAST = QName.createQName(OrgstructureBean.ORGSTRUCTURE_NAMESPACE_URI, "employee-last-name");
+    public NodeRef employeeId = null;
 
- 	public static final QName PROP_STAFF_NAME = QName.createQName(OrgstructureBean.ORGSTRUCTURE_NAMESPACE_URI, "element-member-position-assoc-text-content");
+    /**
+     * ФИО
+     */
+    public String firstName = null;
+    public String middleName = null;
+    public String lastName = null;
 
-	final public NodeRef employeeId;
+    /**
+     * Название основной должности
+     */
+    public String staffName = null;
 
-	/** ФИО */
-	public String firstName, middleName, lastName;
+    /**
+     * id основной должности
+     */
+    public NodeRef staffId = null;
 
-	/** Название основной должности */
-	public String staffName;
+    /**
+     * Название подразделения по основной должности
+     */
+    public String unitName = null;
 
-	/** id основной должности */
-	public NodeRef staffId;
+    /**
+     * id подразделения по основной должности
+     */
+    public NodeRef unitId = null;
 
-	/** Название подразделения по основной должности */
-	public String unitName;
+    /**
+     * характеристики связанного системного пользователя
+     */
+    public String userLogin = null;
 
-	/** id подразделения по основной должности */
-	public NodeRef unitId;
+    public BasicEmployeeInfo(NodeRef employeeId) {
+        super();
+        this.employeeId = employeeId;
+    }
 
-	/** характеристики связанного системного пользователя */ 
-	public String userLogin;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((employeeId == null) ? 0 : employeeId.hashCode());
+        return result;
+    }
 
-	public BasicEmployeeInfo(NodeRef employeeId) {
-		super();
-		this.employeeId = employeeId;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final BasicEmployeeInfo other = (BasicEmployeeInfo) obj;
+        if (employeeId == null) {
+            if (other.employeeId != null)
+                return false;
+        } else if (!employeeId.equals(other.employeeId))
+            return false;
+        return true;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((employeeId == null) ? 0 : employeeId.hashCode());
-		return result;
-	}
+    @Override
+    public String toString() {
+        return "BasicEmployeeInfo [employeeId=" + employeeId + ", firstName="
+                + firstName + ", middleName=" + middleName + ", lastName="
+                + lastName + ", staffName=" + staffName + ", unitName="
+                + unitName + "]";
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final BasicEmployeeInfo other = (BasicEmployeeInfo) obj;
-		if (employeeId == null) {
-			if (other.employeeId != null)
-				return false;
-		} else if (!employeeId.equals(other.employeeId))
-			return false;
-		return true;
-	}
+    /**
+     * @return Фамилия И.О.
+     */
+    public String ФамилияИО() {
+        final StringBuilder result = new StringBuilder();
+        result.append(Фамилия());
+        // Далее первые буквы Имени и отчества в верхнем регистре
+        if (!Utils.isStringEmpty(firstName)) {
+            result.append(String.format(" %s.", ИмяИнициал()));
+        }
+        if (!Utils.isStringEmpty(middleName)) {
+            result.append(String.format("%s.", ОтчествоИнициал()));
+        }
+        return result.toString();
+    }
 
-	@Override
-	public String toString() {
-		return "BasicEmployeeInfo [employeeId=" + employeeId + ", firstName="
-				+ firstName + ", middleName=" + middleName + ", lastName="
-				+ lastName + ", staffName=" + staffName + ", unitName="
-				+ unitName + "]";
-	}
+    /**
+     * @return (none null) Фамилия или пустая строка
+     */
+    public String Фамилия() {
+        return Utils.coalesce(lastName, "");
+    }
 
-	/**
-	 * @return Фамилия И.О.
-	 */
-	public String ФамилияИО() {
-		/// JASPER jrxml format: $F{col_Employee.LastName}+ " "+ ($F{col_Employee.FirstName}+ " ").substring( 0, 1 )+ "."+ ($F{col_Employee.MiddleName}+ " ").substring(0,1) + "."
-		final StringBuilder result = new StringBuilder();
-		result.append( Фамилия());
-		// Далее первые буквы Имени и отчества в верхнем регистре
-		if (!Utils.isStringEmpty(this.firstName))
-			result.append( String.format(" %s.", ИмяИнициал()) );
-		if (!Utils.isStringEmpty(this.middleName))
-			result.append( String.format("%s.", ОтчествоИнициал()) );
-		return result.toString();
-	}
+    /**
+     * @return (none null) Первая буква фамилии в верхнем регистре или пусто
+     */
+    public String ФамилияИнициал() {
+        return (Utils.isStringEmpty(lastName))
+                ? ""
+                : lastName.toUpperCase().substring(0, 1);
+    }
 
-	/**
-	 * @return (none null) Фамилия или пустая строка 
-	 */
-	public String Фамилия() {
-		return Utils.coalesce(this.lastName, "");
-	}
+    /**
+     * @return (none null) Имя или пустая строка
+     */
+    public String Имя() {
+        return Utils.coalesce(firstName, "");
+    }
 
-	/**
-	 * @return (none null) Первая буква фамилии в верхнем регистре или пусто 
-	 */
-	public String ФамилияИнициал() {
-		return (Utils.isStringEmpty(this.lastName))
-				? ""
-				: this.lastName.toUpperCase().substring(0,1);
-	}
+    /**
+     * @return (none null) Первая буква имени в верхнем регистре или пусто
+     */
+    public String ИмяИнициал() {
+        return (Utils.isStringEmpty(firstName))
+                ? ""
+                : firstName.toUpperCase().substring(0, 1);
+    }
 
-	/**
-	 * @return (none null) Имя или пустая строка 
-	 */
-	public String Имя() {
-		return Utils.coalesce( this.firstName, "");
-	}
+    /**
+     * @return (none null) Отчество или пустая строка
+     */
+    public String Отчество() {
+        return Utils.coalesce(middleName, "");
+    }
 
-	/**
-	 * @return (none null) Первая буква имени в верхнем регистре или пусто 
-	 */
-	public String ИмяИнициал() {
-		return (Utils.isStringEmpty(this.firstName))
-				? ""
-				: this.firstName.toUpperCase().substring(0,1);
-	}
+    /**
+     * @return (none null) Первая буква отчества в верхнем регистре или пусто
+     */
+    public String ОтчествоИнициал() {
+        return (Utils.isStringEmpty(middleName))
+                ? ""
+                : middleName.toUpperCase().substring(0, 1);
+    }
 
-	/**
-	 * @return (none null) Отчество или пустая строка 
-	 */
-	public String Отчество() {
-		return Utils.coalesce(this.middleName, "");
-	}
-
-	/**
-	 * @return (none null) Первая буква отчества в верхнем регистре или пусто 
-	 */
-	public String ОтчествоИнициал() {
-		return (Utils.isStringEmpty(this.middleName))
-				? ""
-				: this.middleName.toUpperCase().substring(0,1);
-	}
-
-	/**
-	 * Подгрузить данные для employeeId.
-	 * @param nodeSrv обязательный параметр
-	 * @param orgSrv необязательный, задаётся если требуются параметры об Организации и Должности Сотрудника
-	 */
+    /**
+     * Подгрузить данные для employeeId.
+     *
+     * @param nodeSrv обязательный параметр
+     * @param orgSrv  необязательный, задаётся если требуются параметры об Организации и Должности Сотрудника
+     */
     public void loadProps(NodeService nodeSrv, OrgstructureBean orgSrv) {
         if (employeeId != null) {
-            this.firstName = Utils.coalesce(nodeSrv.getProperty(employeeId, PROP_EMPLOYEE_NAME_FIRST), "");
-            this.middleName = Utils.coalesce(nodeSrv.getProperty(employeeId, PROP_EMPLOYEE_NAME_MIDDLE), "");
-            this.lastName = Utils.coalesce(nodeSrv.getProperty(employeeId, PROP_EMPLOYEE_NAME_LAST), "");
+            firstName = Utils.coalesce(nodeSrv.getProperty(employeeId, PROP_EMPLOYEE_NAME_FIRST), "");
+            middleName = Utils.coalesce(nodeSrv.getProperty(employeeId, PROP_EMPLOYEE_NAME_MIDDLE), "");
+            lastName = Utils.coalesce(nodeSrv.getProperty(employeeId, PROP_EMPLOYEE_NAME_LAST), "");
 
-            this.unitName = "";
-            this.staffName = "";
+            unitName = "";
+            staffName = "";
             if (orgSrv != null) {
                 final NodeRef staffList = orgSrv.getEmployeePrimaryStaff(employeeId);
                 if (staffList != null) {
                     // занимаемая Должность
-                    this.staffId = staffList;
-                    this.unitId = orgSrv.getUnitByStaff(staffList);
+                    staffId = staffList;
+                    // получить словарное значение Должности по штатной позиции
+                    NodeRef dpId = orgSrv.getPositionByStaff(staffList);
+                    staffName = (dpId == null) ? "" : Utils.coalesce(nodeSrv.getProperty(dpId, PROP_DP_INFO), "");
+
+                    unitId = orgSrv.getUnitByStaff(staffList);
 
                     // название Подразделения ...
-                    this.unitName = (this.unitId != null) ? Utils.coalesce(nodeSrv.getProperty(this.unitId, PROP_ORGUNIT_NAME), "") : "";
-
-                    // получить словарное значение Должности по штатной позиции
-                    final NodeRef dpId = orgSrv.getPositionByStaff(this.staffId);
-                    this.staffName = (dpId == null) ? ""
-                            : Utils.coalesce(nodeSrv.getProperty(dpId, PROP_DP_INFO), nodeSrv.getProperty(this.staffId, PROP_STAFF_NAME), "");
+                    unitName = (unitId != null) ? Utils.coalesce(nodeSrv.getProperty(unitId, PROP_ORGUNIT_NAME), "") : "";
                 }
             }
         }
