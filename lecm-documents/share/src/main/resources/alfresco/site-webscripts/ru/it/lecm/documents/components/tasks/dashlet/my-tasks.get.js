dc00ce5bee9d4fff66dcc879e4e28b5b9852c033
Котельникова Полина <PKotelnikova@it.ru>
2013-10-28 12:39:20 +0000

    ALF-1618) Асинхронная загрузка задач, поручений, бизнес-процессов в базовом документе
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@5111 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/tasks/dashlet/my-tasks.get.js
+++ lecm-documents/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documents/components/tasks/dashlet/my-tasks.get.js
@@ -1,3 +1,5 @@
+<import resource="classpath:/alfresco/site-webscripts/ru/it/lecm/documents/utils/permission-utils.js">
+
 function main() {
     model.nodeRef = args["nodeRef"];
     model.hasStatemachine = hasStatemachine(args["nodeRef"]);
