e03e8282154e294b997b1dfd809ed5074856788c
Мельников Петр <PMelnikov@it.ru>
2013-11-08 12:00:02 +0000

    [~] ALF-1617. Контрольно-диагностическая утилита
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@5374 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-diagnostic-utility/diagnostic.sh
+++ lecm-diagnostic-utility/diagnostic.sh
@@ -1,1 +1,9 @@
-java -cp ".;./lecm-diagnostic-utility-1.0.0.jar;./lib/*" -Dfile.encoding=UTF-8 ru.it.lecm.platform.DiagnosticUtility
+#!/bin/sh
+file="./utility-properties.cfg"
+while read line
+do
+    var=$(echo $line | sed -e 's/.$//' | awk -F"=" '{a = $1 "=\"" $2 "\""; print a}')
+    eval $var
+done < $file
+
+$alf_java_home/bin/java -cp ".;./lecm-diagnostic-utility-1.0.0.jar;./lib/*" -Dfile.encoding=UTF-8 ru.it.lecm.platform.DiagnosticUtility
