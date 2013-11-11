646ef406b012eb88b127ddeab60b7b7d8d052928
Мельников Петр <PMelnikov@it.ru>
2013-11-11 12:25:45 +0000

    [~] ALF-1617. Контрольно-диагностическая утилита. В логирование бизнес-журнала добавлена дополнительная информация.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@5392 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-business-journal/repo/src/main/java/ru/it/lecm/businessjournal/script/ExportLastBJRecords.java
+++ lecm-business-journal/repo/src/main/java/ru/it/lecm/businessjournal/script/ExportLastBJRecords.java
@@ -3,6 +3,7 @@ package ru.it.lecm.businessjournal.script;
 import com.csvreader.CsvWriter;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.QName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.extensions.webscripts.AbstractWebScript;
@@ -13,6 +14,8 @@ import ru.it.lecm.businessjournal.beans.BusinessJournalService;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -64,13 +67,52 @@ public class ExportLastBJRecords extends AbstractWebScript {
             // По умолчанию charset в UTF-8
             Charset charset = Charset.defaultCharset();
             CsvWriter wr = new CsvWriter(resOutputStream, ';', charset);
-            wr.write("\ufeff");
+            wr.write("\ufeffДата");
+            wr.write("Описание");
+            wr.write("Категория");
+            wr.write("Тип");
+            wr.write("Осн. объект");
+            wr.write("Доп. объект 1");
+            wr.write("Доп. объект 2");
+            wr.write("Доп. объект 3");
+            wr.write("Доп. объект 4");
+            wr.write("Доп. объект 5");
+            wr.endRecord();
             List<NodeRef> bjRecordbjRecords = businessJournalService.getLastRecords(recordsCount, includeArchiveRecords);
+            SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
             for (NodeRef bjRecord : bjRecordbjRecords) {
+                Date reportDate = (Date) nodeService.getProperty(bjRecord, BusinessJournalService.PROP_BR_RECORD_DATE);
+                wr.write(format.format(reportDate));
+
                 String reportDescription = (String) nodeService.getProperty(bjRecord, BusinessJournalService.PROP_BR_RECORD_DESC);
                 reportDescription = reportDescription.replaceAll("<a[^>]*>", "");
                 reportDescription = reportDescription.replaceAll("</a>", "");
                 wr.write(reportDescription);
+
+                String category = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-evCategory-assoc-text-content"));
+                wr.write(category);
+
+                String type = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-objType-assoc-text-content"));
+                wr.write(type);
+
+                String mainObject = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-mainObject-assoc-ref"));
+                wr.write(mainObject);
+
+                String obj1 = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-secondaryObj1-assoc-ref"));
+                wr.write(obj1);
+
+                String obj2 = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-secondaryObj2-assoc-ref"));
+                wr.write(obj2);
+
+                String obj3 = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-secondaryObj3-assoc-ref"));
+                wr.write(obj3);
+
+                String obj4 = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-secondaryObj4-assoc-ref"));
+                wr.write(obj4);
+
+                String obj5 = (String) nodeService.getProperty(bjRecord, QName.createQName(BusinessJournalService.BJ_NAMESPACE_URI, "bjRecord-secondaryObj5-assoc-ref"));
+                wr.write(obj5);
+
                 wr.endRecord();
             }
             wr.close();
