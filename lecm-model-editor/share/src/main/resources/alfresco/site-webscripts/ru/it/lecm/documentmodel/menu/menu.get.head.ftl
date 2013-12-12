a0b911dda5d8f59b250675a2f9a6ffbf517a9d95
Ивкин Алексей <AIvkin@it.ru>
2013-12-12 11:29:40 +0000

    ALF-1722: Сделать левую панель меню.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@5849 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-model-editor/share/src/main/resources/alfresco/site-webscripts/ru/it/lecm/documentmodel/menu/menu.get.head.ftl
@@ -0,0 +1,3 @@
+<#include "/org/alfresco/components/form/form.get.head.ftl">
+<@link rel="stylesheet" type="text/css" href="${page.url.context}/css/lecm-base/components/base-menu/base-menu.css" />
+<@script type="text/javascript" src="${page.url.context}/scripts/lecm-forms-editor/lecm-modeleditor-menu.js"></@script>
---
+++ lecm-model-editor/share/src/main/webapp/scripts/lecm-forms-editor/lecm-modeleditor-menu.js
@@ -0,0 +1,52 @@
+/**
+ * LogicECM root namespace.
+ *
+ * @namespace LogicECM
+ */
+// Ensure LogicECM root object exists
+if (typeof LogicECM == "undefined" || !LogicECM) {
+	var LogicECM = {};
+}
+
+/**
+ * LogicECM top-level module namespace.
+ *
+ * @namespace LogicECM
+ * @class LogicECM.module
+ */
+LogicECM.module = LogicECM.module || {};
+
+
+/**
+ * LogicECM Dictionary module namespace.
+ *
+ * @namespace LogicECM
+ * @class LogicECM.module.AllDictionary.AllDictionary
+ */
+LogicECM.module.ModelEditor = LogicECM.module.ModelEditor || {};
+
+/**
+ * AllDictionary module.
+ *
+ * @namespace LogicECM.module
+ * @class LogicECM.module.AllDictionary
+ */
+(function () {
+
+	LogicECM.module.ModelEditor.Menu = function (htmlId) {
+		return LogicECM.module.ModelEditor.Menu.superclass.constructor.call(
+			this,
+			"LogicECM.module.AllDictionary.Menu",
+			htmlId,
+			["button"]);
+	};
+
+	YAHOO.extend(LogicECM.module.ModelEditor.Menu, Alfresco.component.Base, {
+		onReady:function Menu_onReady () {
+			var onHomeClick = function (e) {
+				window.location.href = Alfresco.constants.URL_PAGECONTEXT + "doc-model-list";
+			};
+			this.widgets.dictionariesButton = Alfresco.util.createYUIButton(this, "modelEditorHomeBtn", onHomeClick, {});
+		}
+	});
+})();
