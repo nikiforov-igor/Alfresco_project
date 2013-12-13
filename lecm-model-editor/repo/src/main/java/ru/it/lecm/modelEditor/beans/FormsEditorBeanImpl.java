73fed0e0fa42bb31aa93cdb791468e2a156f732c
Ивкин Алексей <AIvkin@it.ru>
2013-12-13 06:33:11 +0000

    Редактор форм: 1) Добавлен replase=true 2) Удалено автоматическое создание формы с дашлетами
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@5858 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-model-editor/repo/src/main/java/ru/it/lecm/modelEditor/beans/FormsEditorBeanImpl.java
+++ lecm-model-editor/repo/src/main/java/ru/it/lecm/modelEditor/beans/FormsEditorBeanImpl.java
@@ -57,10 +57,6 @@ public class FormsEditorBeanImpl extends BaseBean {
 
 	public static final String FAKE_ATTRIBUTE_TYPE = "fake";
 
-	public static final String DASHLETS_EVALUATOR = "node-type";
-	public static final String DASHLETS_FORM_ID = "main-page-dashlets";
-	public static final String DEFAULT_DASHLETS_TEMPLATE= "/ru/it/lecm/documents/controls/document-dashlet-control.ftl";
-
 	private NamespaceService namespaceService;
 	private DictionaryService dictionaryService;
 	private Repository repository;
@@ -383,19 +379,8 @@ public class FormsEditorBeanImpl extends BaseBean {
 				xmlw = XMLOutputFactory.newInstance().createXMLStreamWriter(out);
 				xmlw.writeStartElement("alfresco-config");
 
-				boolean existDashletsForm = false;
 				for (NodeRef form : forms) {
 					writeForm(xmlw, form, modelName);
-
-					String evaluator = (String) nodeService.getProperty(form, PROP_FORM_EVALUATOR);
-					String formId = (String) nodeService.getProperty(form, PROP_FORM_ID);
-					if (DASHLETS_EVALUATOR.equals(evaluator) && DASHLETS_FORM_ID.equals(formId)) {
-						existDashletsForm = true;
-					}
-				}
-
-				if (!existDashletsForm) {
-					writeDefaultDashletsForm(xmlw, modelName);
 				}
 
 				xmlw.writeEndElement();
@@ -428,6 +413,7 @@ public class FormsEditorBeanImpl extends BaseBean {
 		    xmlw.writeStartElement("config");
 		    xmlw.writeAttribute("evaluator", evaluator);
 		    xmlw.writeAttribute("condition", modelName);
+		    xmlw.writeAttribute("replace", "true");
 
 		    xmlw.writeStartElement("forms");
 		    xmlw.writeStartElement("form");
@@ -584,44 +570,6 @@ public class FormsEditorBeanImpl extends BaseBean {
 				tabIndex++;
 			}
 		}
-	}
-
-	private void writeDefaultDashletsForm(XMLStreamWriter xmlw, String modelName) throws XMLStreamException {
-		xmlw.writeStartElement("config");
-		xmlw.writeAttribute("evaluator", DASHLETS_EVALUATOR);
-		xmlw.writeAttribute("condition", modelName);
-
-		xmlw.writeStartElement("forms");
-		xmlw.writeStartElement("form");
-		xmlw.writeAttribute("id", DASHLETS_FORM_ID);
-
-		xmlw.writeStartElement("field-visibility");
-		xmlw.writeStartElement("show");
-		xmlw.writeAttribute("id", "line1");
-		xmlw.writeEndElement();
-		xmlw.writeEndElement();
-
-		xmlw.writeStartElement("appearance");
-		xmlw.writeStartElement("field");
-		xmlw.writeAttribute("id", "line1");
-		xmlw.writeStartElement("control");
-		xmlw.writeAttribute("template", DEFAULT_DASHLETS_TEMPLATE);
-		xmlw.writeStartElement("control-param");
-		xmlw.writeAttribute("name", "first");
-		xmlw.writeCharacters("/lecm/components/dashlets/document-main;/lecm/components/dashlets/document-members");
-		xmlw.writeEndElement();
-		xmlw.writeStartElement("control-param");
-		xmlw.writeAttribute("name", "second");
-		xmlw.writeCharacters("/lecm/components/dashlets/document-attachments;/lecm/components/dashlets/document-my-tasks");
-		xmlw.writeEndElement();
-		xmlw.writeEndElement();
-		xmlw.writeEndElement();
-		xmlw.writeEndElement();
-
-		xmlw.writeEndElement();
-		xmlw.writeEndElement();
-
-		xmlw.writeEndElement();
 	}
 
 	private Map<String, List<String>> getSets(List<NodeRef> fields) {
