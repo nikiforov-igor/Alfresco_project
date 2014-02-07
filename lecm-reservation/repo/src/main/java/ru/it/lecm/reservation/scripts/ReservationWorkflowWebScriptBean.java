45cae7f9ed4f167a5e06189d169351003cdc170a
Малыгин Владимир <VMalygin@it.ru>
2014-02-07 05:28:41 +0000

    резервирование регномеров, чистка
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@6366 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-reservation/repo/src/main/java/ru/it/lecm/reservation/scripts/ReservationWorkflowWebScriptBean.java
+++ lecm-reservation/repo/src/main/java/ru/it/lecm/reservation/scripts/ReservationWorkflowWebScriptBean.java
@@ -1,15 +1,11 @@
 package ru.it.lecm.reservation.scripts;
 
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import org.alfresco.repo.jscript.ScriptNode;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.repo.workflow.activiti.ActivitiScriptNode;
 import org.alfresco.repo.workflow.activiti.ActivitiScriptNodeList;
-import org.alfresco.repo.workflow.jscript.JscriptWorkflowInstance;
-import org.alfresco.service.cmr.dictionary.DictionaryService;
-import org.alfresco.service.cmr.notification.NotificationService;
 import org.alfresco.service.cmr.repository.AssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -31,155 +27,138 @@ import ru.it.lecm.regnumbers.template.TemplateRunException;
  * @author snovikov
  */
 public class ReservationWorkflowWebScriptBean extends BaseWebScript {
+
 	private static final Logger logger = LoggerFactory.getLogger(ReservationWorkflowWebScriptBean.class);
 
 	private OrgstructureBean orgstructureService;
-	private DocumentService documentService;
-	private DictionaryService dictionaryService;
 	private NodeService nodeService;
 	private RegNumbersService regNumbersService;
 	private NotificationsService notificationsService;
 	private BusinessJournalService businessJournalService;
 
-
 	public void setOrgstructureService(OrgstructureBean orgstructureService) {
-        this.orgstructureService = orgstructureService;
-    }
-
-	public void setDocumentService(DocumentService documentService) {
-        this.documentService = documentService;
-    }
-
-	public void setDictionaryService(DictionaryService dictionaryService) {
-		this.dictionaryService = dictionaryService;
+		this.orgstructureService = orgstructureService;
 	}
 
 	public void setNodeService(NodeService nodeService) {
-        this.nodeService = nodeService;
-    }
+		this.nodeService = nodeService;
+	}
 
 	public void setRegNumbersService(RegNumbersService regNumbersService) {
-        this.regNumbersService = regNumbersService;
-    }
+		this.regNumbersService = regNumbersService;
+	}
 
 	public void setNotificationsService(NotificationsService notificationsService) {
-        this.notificationsService = notificationsService;
-    }
+		this.notificationsService = notificationsService;
+	}
 
 	public void setBusinessJournalService(BusinessJournalService businessJournalService) {
-        this.businessJournalService = businessJournalService;
-    }
+		this.businessJournalService = businessJournalService;
+	}
 
-	public ActivitiScriptNodeList getReservationDocumentRegistrators(ScriptNode documentNode){
+	public ActivitiScriptNodeList getReservationDocumentRegistrators(ScriptNode documentNode) {
 		NodeRef document = documentNode.getNodeRef();
+		//TODO: получать регистраторов через API которое напишет Дима
 		List<NodeRef> registrars = orgstructureService.getEmployeesByBusinessRole("OUTGOING_REGISTRAR", true);
 		ActivitiScriptNodeList registrarsScriptList = new ActivitiScriptNodeList();
-		for (NodeRef registrar:registrars){
+		for (NodeRef registrar : registrars) {
 			NodeRef userRef = orgstructureService.getPersonForEmployee(registrar);
 			ActivitiScriptNode userScriptNode = new ActivitiScriptNode(userRef, serviceRegistry);
 			registrarsScriptList.add(userScriptNode);
 		}
-		notifyRegistrarsAboutStartReservate(document,registrars);
+		notifyRegistrarsAboutStartReservate(document, registrars);
 		return registrarsScriptList;
 	}
 
-	public NodeRef getCurrentEmployee(){
+	public NodeRef getCurrentEmployee() {
 		return orgstructureService.getCurrentEmployee();
 	}
 
-	public void regnumReservate(ScriptNode documentNode, String isReservate, NodeRef reservateInitiator, String comment) throws TemplateParseException, TemplateRunException{
-		NodeRef document  = documentNode.getNodeRef();
-		if (isReservate.equals("Зарезервировать")){
+	public void regnumReservate(ScriptNode documentNode, String isReservate, NodeRef reservateInitiator, String comment) throws TemplateParseException, TemplateRunException {
+		NodeRef document = documentNode.getNodeRef();
+		if ("Зарезервировать".equals(isReservate)) {
 			regNumbersService.registerDocument(document, "OUTGOING_DOC_NUMBER", true);
-			notifyReservateInitiatorAboutSuccess(document,reservateInitiator);
-		}else{
-			notifyReservateInitiatorAboutReject(document,reservateInitiator,comment);
+			notifyReservateInitiatorAboutSuccess(document, reservateInitiator);
+		} else {
+			notifyReservateInitiatorAboutReject(document, reservateInitiator, comment);
 		}
 	}
 
-	public void regnumReservateEmpty(ScriptNode documentNode, NodeRef reservateInitiator) throws TemplateParseException, TemplateRunException{
-		NodeRef document  = documentNode.getNodeRef();
+	public void regnumReservateEmpty(ScriptNode documentNode, NodeRef reservateInitiator) throws TemplateParseException, TemplateRunException {
+		NodeRef document = documentNode.getNodeRef();
 		regNumbersService.registerDocument(document, "OUTGOING_DOC_NUMBER", true);
 		List<AssociationRef> regDocDataList = nodeService.getTargetAssocs(document, DocumentService.ASSOC_REG_DOCUMENT_DATA);
-		if (null!=regDocDataList && regDocDataList.size()>0){
+		if (null != regDocDataList && regDocDataList.size() > 0) {
 			NodeRef regDocData = regDocDataList.get(0).getTargetRef();
 			nodeService.setProperty(regDocData, DocumentService.PROP_REG_DATA_NUMBER, "\"нет доступных регистраторов\"");
 		}
-		notifyReserveInitiatorAboutEmptyRegistrars(document,reservateInitiator);
+		notifyReserveInitiatorAboutEmptyRegistrars(document, reservateInitiator);
 
-		String doumentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
-		logger.warn("Нет доступных регистраторов для документа "+doumentString);
+		String documentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
+		logger.warn("Нет доступных регистраторов для документа {}", documentString);
 	}
 
-	private void notifyRegistrarsAboutStartReservate(NodeRef document, List<NodeRef> registrars){
+	private void notifyRegistrarsAboutStartReservate(NodeRef document, List<NodeRef> registrars) {
 		NodeRef currentEmp = orgstructureService.getCurrentEmployee();
-		String employeeName = java.lang.String.format(
-					"%s %s %s",
-					nodeService.getProperty(currentEmp, orgstructureService.PROP_EMPLOYEE_LAST_NAME).toString(),
-					nodeService.getProperty(currentEmp, orgstructureService.PROP_EMPLOYEE_FIRST_NAME).toString(),
-					nodeService.getProperty(currentEmp, orgstructureService.PROP_EMPLOYEE_MIDDLE_NAME).toString());
-		String doumentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
-		String notificationMessage = java.lang.String.format(
-					"%s запросил резервирование номера для документа %s",
-					this.wrapperLink(currentEmp.toString(), employeeName),
-					this.wrapperLink(document.toString(), doumentString, "/share/page/document"));
+		String employeeName = (String) nodeService.getProperty(currentEmp, orgstructureService.PROP_EMPLOYEE_SHORT_NAME);
+		String documentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
+		String template = "%s запросил резервирование номера для документа %s";
+		String employeeUrl = wrapperLink(currentEmp.toString(), employeeName);
+		String documentUrl = wrapperLink(document.toString(), documentString, BaseBean.DOCUMENT_LINK_URL);
+		String notificationMessage = String.format(template, employeeUrl, documentUrl);
 
 		Notification notification = new Notification();
 		notification.setRecipientEmployeeRefs(registrars);
 		notification.setAuthor(AuthenticationUtil.getSystemUserName());
 		notification.setDescription(notificationMessage);
 		notification.setObjectRef(document);
-		notification.setInitiatorRef(null);
 		notificationsService.sendNotification(notification);
 	}
 
-	private void notifyReservateInitiatorAboutSuccess(NodeRef document, NodeRef initiator){
-		String doumentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
-		String notificationMessage = java.lang.String.format(
-			"Ваш запрос на резервирование регистрационного номера для документа %s выполнен",
-			this.wrapperLink(document.toString(), doumentString, "/share/page/document"));
+	private void notifyReservateInitiatorAboutSuccess(NodeRef document, NodeRef initiator) {
+		String template = "Ваш запрос на резервирование регистрационного номера для документа %s выполнен";
+		String documentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
+		String documentUrl = wrapperLink(document.toString(), documentString, BaseBean.DOCUMENT_LINK_URL);
+		String notificationMessage = String.format(template, documentUrl);
 
 		Notification notification = new Notification();
 		notification.setRecipientEmployeeRefs(Arrays.asList(initiator));
 		notification.setAuthor(AuthenticationUtil.getSystemUserName());
 		notification.setDescription(notificationMessage);
 		notification.setObjectRef(document);
-		notification.setInitiatorRef(null);
 		notificationsService.sendNotification(notification);
 	}
 
-	private void notifyReservateInitiatorAboutReject(NodeRef document, NodeRef initiator, String comment){
-		String doumentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
-		String notificationMessage = java.lang.String.format(
-			"Ваш запрос на резервирование регистрационного номера для документа %s <a href=\"#\" title=\""+comment+"\">отклонен</a>",
-			this.wrapperLink(document.toString(), doumentString, "/share/page/document"));
+	private void notifyReservateInitiatorAboutReject(NodeRef document, NodeRef initiator, String comment) {
+		String template = "Ваш запрос на резервирование регистрационного номера для документа %s %s";
+		String documentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
+		String documentUrl = wrapperLink(document.toString(), documentString, BaseBean.DOCUMENT_LINK_URL);
+		String commentUrl = String.format("<a href='#' title='%s'>отклонен</a>", comment);
+		String notificationMessage = String.format(template, documentUrl, commentUrl);
 
 		Notification notification = new Notification();
 		notification.setRecipientEmployeeRefs(Arrays.asList(initiator));
 		notification.setAuthor(AuthenticationUtil.getSystemUserName());
 		notification.setDescription(notificationMessage);
 		notification.setObjectRef(document);
-		notification.setInitiatorRef(null);
 		notificationsService.sendNotification(notification);
 
-		String bjMessage = "#initiator <a href=\"#\" title=\""+comment+"\">отклонил запрос</a> в резервировании номера документа #mainobject";
+		String bjMessage = "#initiator <a href=\"#\" title=\"" + comment + "\">отклонил запрос</a> в резервировании номера документа #mainobject";
 		String registrarLogin = orgstructureService.getEmployeeLogin(orgstructureService.getCurrentEmployee());
 		businessJournalService.log(registrarLogin, document, "RESERVATION", bjMessage, null);
 	}
 
-	private void notifyReserveInitiatorAboutEmptyRegistrars(NodeRef document, NodeRef initiator){
+	private void notifyReserveInitiatorAboutEmptyRegistrars(NodeRef document, NodeRef initiator) {
+		String template = "Нет доступных регистраторов для регистрации документа %s";
 		String documentString = (String) nodeService.getProperty(document, DocumentService.PROP_PRESENT_STRING);
-		String notificationMessage = java.lang.String.format(
-			"Нет доступных регистраторов для регистрации документа %s",
-			this.wrapperLink(document.toString(), documentString, "/share/page/document"));
+		String documentUrl = wrapperLink(document.toString(), documentString, BaseBean.DOCUMENT_LINK_URL);
+		String notificationMessage = String.format(template, documentUrl);
 
 		Notification notification = new Notification();
 		notification.setRecipientEmployeeRefs(Arrays.asList(initiator));
 		notification.setAuthor(AuthenticationUtil.getSystemUserName());
 		notification.setDescription(notificationMessage);
 		notification.setObjectRef(document);
-		notification.setInitiatorRef(null);
 		notificationsService.sendNotification(notification);
 	}
-
 }
