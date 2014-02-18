6239dae7280118c26d58372d5408c59eeb747676
Ивкин Алексей <AIvkin@it.ru>
2014-02-18 11:54:47 +0000

    ALF-1858: Структура данных для хранения настроек АРМов - добавлено правило построения узлов по статусам
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@6517 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-arm/share/src/main/webapp/scripts/lecm-arm/constraints/arm-statuses-child-rule-validator.js
@@ -0,0 +1,17 @@
+if (typeof LogicECM == "undefined" || !LogicECM) {
+	var LogicECM = {};
+}
+
+LogicECM.module = LogicECM.module || {};
+LogicECM.module.ARM = LogicECM.module.ARM || {};
+
+LogicECM.module.ARM.statusesChildRuleValidation =
+	function (field, args,  event, form, silent, message) {
+		if (field.form != null) {
+			var statusesTextField = field.form["prop_lecm-arm_selected-statuses"];
+			if (statusesTextField != null) {
+				statusesTextField.disabled = field.value != "SELECTED" && field.value != "EXCEPT_SELECTED";
+			}
+		}
+		return true;
+	};
