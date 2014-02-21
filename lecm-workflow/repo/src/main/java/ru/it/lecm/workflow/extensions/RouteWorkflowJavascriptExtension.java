85dc56abaeee8c3684882336b785e313333a6740
Малыгин Владимир <VMalygin@it.ru>
2014-02-21 12:55:23 +0000

    заготовка под сервисный бизнес-процесс через который будет запускаться документ по маршруту
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@6562 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-workflow/repo/src/main/java/ru/it/lecm/workflow/extensions/RouteWorkflowJavascriptExtension.java
@@ -0,0 +1,11 @@
+package ru.it.lecm.workflow.extensions;
+
+import ru.it.lecm.base.beans.BaseWebScript;
+
+/**
+ *
+ * @author vmalygin
+ */
+public class RouteWorkflowJavascriptExtension extends BaseWebScript {
+
+}
