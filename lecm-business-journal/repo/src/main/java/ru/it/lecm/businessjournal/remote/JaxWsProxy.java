c226cd2676d3ccff9162310cd5dae556ad8612b5
Малыгин Владимир <VMalygin@it.ru>
2014-02-21 08:20:12 +0000

    избавился от надоедливого stacktrace при старте сервера. Stacktrace там не сильно важен, поэтому спрятал его в TRACE
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@6550 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-business-journal/repo/src/main/java/ru/it/lecm/businessjournal/remote/JaxWsProxy.java
+++ lecm-business-journal/repo/src/main/java/ru/it/lecm/businessjournal/remote/JaxWsProxy.java
@@ -8,35 +8,33 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * User: pmelnikov
- * Date: 10.12.13
- * Time: 17:10
+ * User: pmelnikov Date: 10.12.13 Time: 17:10
  */
 public class JaxWsProxy extends JaxWsPortProxyFactoryBean {
 
-    Logger logger = LoggerFactory.getLogger(JaxWsProxy.class);
+	private final static Logger logger = LoggerFactory.getLogger(JaxWsProxy.class);
 
-    private String useRemoteService;
-    private URL wsdlUrl;
+	private String useRemoteService;
+	private URL wsdlUrl;
 
-    @Override
-    public void afterPropertiesSet() {
-        if (Boolean.valueOf(useRemoteService) && wsdlUrl != null) {
-            super.afterPropertiesSet();
-        }
-    }
+	@Override
+	public void afterPropertiesSet() {
+		if (Boolean.valueOf(useRemoteService) && wsdlUrl != null) {
+			super.afterPropertiesSet();
+		}
+	}
 
-    public void setUseRemoteService(String useRemoteService) {
-        this.useRemoteService = useRemoteService;
-    }
+	public void setUseRemoteService(String useRemoteService) {
+		this.useRemoteService = useRemoteService;
+	}
 
-    public void setWsdlUrl(String wsdlUrl) {
-        try {
-            this.wsdlUrl = new URL(wsdlUrl);
-            super.setWsdlDocumentUrl(this.wsdlUrl);
-        } catch (MalformedURLException e) {
-           logger.warn("WSDL URL was not set for Remote BJ Service (" + wsdlUrl + ")", e);
-        }
-    }
-
+	public void setWsdlUrl(String wsdlUrl) {
+		try {
+			this.wsdlUrl = new URL(wsdlUrl);
+			super.setWsdlDocumentUrl(this.wsdlUrl);
+		} catch (MalformedURLException ex) {
+			logger.warn("WSDL URL was not set for Remote BJ Service ({})", wsdlUrl);
+			logger.trace(ex.getMessage(), ex);
+		}
+	}
 }
