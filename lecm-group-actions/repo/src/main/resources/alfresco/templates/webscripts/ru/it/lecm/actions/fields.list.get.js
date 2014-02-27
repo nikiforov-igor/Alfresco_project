c0952520b51edbc790600c3f55fe2506d6da0ad6
Мельников Петр <PMelnikov@it.ru>
2014-02-27 11:10:34 +0000

    [+] ALF-1907. Групповые операции. Добавлена сортировка для полей формы ввода данных операции
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@6619 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-group-actions/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/actions/fields.list.get.js
+++ lecm-group-actions/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/actions/fields.list.get.js
@@ -7,9 +7,11 @@ for each (var action in actionNode.children) {
     result.push({
         name: action.properties["cm:name"],
         id: action.properties["lecm-group-actions:field-id"],
-        type: action.properties["lecm-group-actions:field-type"]
+        type: action.properties["lecm-group-actions:field-type"],
+        order: action.properties["lecm-group-actions:field-priority"]
     });
 }
 
+result.sort(function(a, b) {return a.order - b.order});
 
 model.result = jsonUtils.toJSONString(result);
