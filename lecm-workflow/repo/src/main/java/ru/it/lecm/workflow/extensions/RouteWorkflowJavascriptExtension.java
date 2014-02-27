bc46d815ae737e06c158769c11390957013fb76f
Малыгин Владимир <VMalygin@it.ru>
2014-02-27 15:08:51 +0000

    маршруты: запуск служебного регламента
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@6630 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-workflow/repo/src/main/java/ru/it/lecm/workflow/extensions/RouteWorkflowJavascriptExtension.java
+++ lecm-workflow/repo/src/main/java/ru/it/lecm/workflow/extensions/RouteWorkflowJavascriptExtension.java
@@ -1,5 +1,10 @@
 package ru.it.lecm.workflow.extensions;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.it.lecm.base.beans.BaseWebScript;
 
 /**
@@ -8,4 +13,17 @@ import ru.it.lecm.base.beans.BaseWebScript;
  */
 public class RouteWorkflowJavascriptExtension extends BaseWebScript {
 
+	private final static Logger logger = LoggerFactory.getLogger(RouteWorkflowJavascriptExtension.class);
+
+	public void exploreExecutionVariables(final DelegateExecution execution) {
+
+		Map<String, Object> variables = execution.getVariables();
+		for(Entry<String, Object> entry : variables.entrySet()) {
+			String key = entry.getKey();
+			Object value = entry.getValue();
+			String simpleName = (value == null) ? "null":value.getClass().getSimpleName();
+			Object args[] = {key, simpleName, value};
+			logger.debug("variable {}\t\tsimpleName {}\t\tvalue {}", args);
+		}
+	}
 }
