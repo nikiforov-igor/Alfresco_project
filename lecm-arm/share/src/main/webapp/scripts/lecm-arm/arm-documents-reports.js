d8a70abbc99734fb832abb6276d0b26c6c2aca6d
Ивкин Алексей <AIvkin@it.ru>
2014-03-19 08:30:05 +0000

    ALF-1984: Вид страницы отчетов
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@6971 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-arm/share/src/main/webapp/scripts/lecm-arm/arm-documents-reports.js
+++ lecm-arm/share/src/main/webapp/scripts/lecm-arm/arm-documents-reports.js
@@ -21,30 +21,22 @@ LogicECM.module.ARM = LogicECM.module.ARM|| {};
 			onUpdateArmReports: function(layer, args) {
 				var types = args[1].types;
 				if (types !== null) {
-					var container = Dom.get(this.id);
+					var container = Dom.get(this.id + "-data");
 
 					if (container != null) {
 						container.innerHTML = "";
-
 						var sUrl = Alfresco.constants.PROXY_URI + "/lecm/reports/rptmanager/registeredReports?forCollection=true&docType=" + types;
 						var callback = {
 							success: function (oResponse) {
-                                container.innerHTML = "";
                                 var oResults = eval("(" + oResponse.responseText + ")");
                                 if (oResults != null && oResults.list != null) {
+	                                var resultLength = oResults.list.length;
+
+	                                Dom.setStyle(this.id + "-data", "display", resultLength > 0 ? "" : "none");
+	                                Dom.setStyle(this.id + "-empty", "display", resultLength == 0 ? "" : "none");
+
 									for (var i = 0; i < oResults.list.length; i++) {
-										var report = oResults.list[i];
-
-										var reportContent = "";
-										reportContent += "<div><h3>";
-										reportContent += '<a href="#" class="theme-color-1" style="font-weight: bold;" onClick=\'LogicECM.module.Documents.Reports.reportLinkClicked(null, {"reportCode": "' + report.code + '"});\'>' + report.name + '</a>';
-										reportContent += "</h3>";
-										if (report.description != null) {
-											reportContent += report.description;
-										}
-										reportContent += "</div><br/>";
-
-										container.innerHTML += reportContent;
+										container.innerHTML += this.getReportTr(oResults.list[i], i, oResults.list.length);
 									}
 								}
 							},
@@ -54,6 +46,32 @@ LogicECM.module.ARM = LogicECM.module.ARM|| {};
 						YAHOO.util.Connect.asyncRequest('GET', sUrl, callback);
 					}
 				}
+			},
+
+			getReportTr: function(report, index, allCount) {
+				var trClasses = "yui-dt-rec";
+				if (index == 0) {
+					trClasses += " yui-dt-first";
+				}
+				if (index == allCount - 1) {
+					trClasses += " yui-dt-last";
+				}
+				if (index % 2 == 0) {
+					trClasses += " yui-dt-even";
+				} else {
+					trClasses += " yui-dt-odd";
+				}
+
+				var result = "<tr class=\"" + trClasses + "\">";
+				result += "<td>";
+				result += "<div class=\"yui-dt-liner\">";
+
+				result += '<a href="#" onClick=\'LogicECM.module.Documents.Reports.reportLinkClicked(null, {"reportCode": "' + report.code + '"});\'>' + report.name + '</a>';
+
+				result += "</div>";
+				result += "</td>";
+				result += "</tr>";
+				return result;
 			}
 		}, true);
 })();
