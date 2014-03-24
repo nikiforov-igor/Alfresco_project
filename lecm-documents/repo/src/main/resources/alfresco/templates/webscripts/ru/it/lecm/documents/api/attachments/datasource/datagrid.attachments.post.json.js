7db813fb6051516348fc01f14531c82016e05985
Ивкин Алексей <AIvkin@it.ru>
2014-03-24 12:11:47 +0000

    ALF-2181: Столбец "Имя файла" в дашлете "Вложения" не сортируется
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@7105 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/documents/api/attachments/datasource/datagrid.attachments.post.json.js
+++ lecm-documents/repo/src/main/resources/alfresco/templates/webscripts/ru/it/lecm/documents/api/attachments/datasource/datagrid.attachments.post.json.js
@@ -3,18 +3,49 @@
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/datalists/parse-args.lib.js">
 
 function main() {
-    var attachments = [];
-    var fields;
-    var nameSubstituteStrings;
+	var attachments = [];
+	var fields;
+	var nameSubstituteStrings;
+	var sort = null;
 
-    if (typeof json !== "undefined" && json.has("params")) {
-        var pars = json.get("params");
-        var categoryRef = (pars.get("parent").length() > 0)  ? pars.get("parent") : null;
+	if (typeof json !== "undefined" && json.has("params")) {
+		var pars = json.get("params");
+		var categoryRef = (pars.get("parent").length() > 0) ? pars.get("parent") : null;
 
-        attachments = documentAttachments.getAttachmentsByCategory(categoryRef);
-        fields = (pars.get("fields").length() > 0) ? pars.get("fields") : null;
-        nameSubstituteStrings = (pars.get("nameSubstituteStrings") !== null) ? pars.get("nameSubstituteStrings") : null;
-    }
+		attachments = documentAttachments.getAttachmentsByCategory(categoryRef);
+		fields = (pars.get("fields").length() > 0) ? pars.get("fields") : null;
+		nameSubstituteStrings = (pars.get("nameSubstituteStrings") !== null) ? pars.get("nameSubstituteStrings") : null;
+		sort = (pars.get("sort").length() > 0) ? pars.get("sort") : null;
+	}
+
+	var sortField = {column: "cm:name", ascending: true};
+
+	if (sort != null && ("" + sort).length > 0) {
+		var asc = true;
+		var separator = sort.indexOf("|");
+		if (separator != -1) {
+			asc = (sort.substring(separator + 1) == "true");
+			sort = sort.substring(0, separator);
+		}
+		sortField.column = sort;
+		sortField.ascending = asc;
+	}
+
+	attachments.sort(function (a, b) {
+		var value1 = a.properties[sortField.column];
+		var value2 = b.properties[sortField.column];
+
+		if (value1 < value2) {
+			return -1;
+		} else if (value1 > value2) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+	if (!sortField.ascending) {
+		attachments.reverse();
+	}
 
 	var result = processResults(attachments, fields, nameSubstituteStrings, 0, attachments.length); // call method from search.lib.js
 	if (result != null && result.items != null) {
@@ -23,7 +54,7 @@ function main() {
 		}
 	}
 
-    model.data = result;
+	model.data = result;
 }
 
 main();
