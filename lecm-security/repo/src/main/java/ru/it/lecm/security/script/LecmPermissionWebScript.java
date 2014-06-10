a165550707e56707aeb47794455c9ba86df0c516
Денис Башмаков <DBashmakov@it.ru>
2014-06-10 09:57:42 +0000

    [+] javaDoc для Пермиссий
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@8299 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-security/repo/src/main/java/ru/it/lecm/security/script/LecmPermissionWebScript.java
+++ lecm-security/repo/src/main/java/ru/it/lecm/security/script/LecmPermissionWebScript.java
@@ -28,6 +28,13 @@ public class LecmPermissionWebScript extends BaseScopableProcessorExtension {
         this.authService = authService;
     }
 
+    /**
+     * Определение наличия конкретной привилегии у текущего Сотрудника относительно узла (документа, папки)
+     * @param permission группа ("LECM_BASIC_PG_Initiator") или атомарная привилегия (например, "_lecmPerm_ViewTag")
+     * @param node проверяемый узел
+     * @return true, если указанная привилегия permission имеется у пользователя
+     * userName для объекта node, иначе false.
+     */
     public boolean hasPermission(ScriptNode node, String permission) {
         return lecmPermissionService.hasPermission(permission, node.getNodeRef(), authService.getCurrentUserName());
     }
@@ -36,7 +43,11 @@ public class LecmPermissionWebScript extends BaseScopableProcessorExtension {
         return lecmPermissionService.hasPermission(permission, new NodeRef(nodeRef), authService.getCurrentUserName());
     }
 
-	public boolean hasReadAccess(ScriptNode node) {
+    /**
+     * Проверяет элемент на доступность
+     * @param node элемент
+     */
+    public boolean hasReadAccess(ScriptNode node) {
         return lecmPermissionService.hasReadAccess(node.getNodeRef());
     }
 
@@ -44,15 +55,35 @@ public class LecmPermissionWebScript extends BaseScopableProcessorExtension {
         return lecmPermissionService.hasPermission(permission, new NodeRef(nodeRef), userLogin);
     }
 
+    /**
+     * Предоставить Динамическую Роль на документ/папку указанному пользователю.
+     * @param roleCode id Динамической Роли
+     * @param docRef документа или папки
+     * @param employeeRef id Сотрудника
+     * @param roleCode предоставляемый доступ, если null, то будет присвоено право по-умолчанию (конфигурируется бинами)
+     */
     public void grantDynamicRole(String docRef, String employeeRef, String roleCode) {
         lecmPermissionService.grantDynamicRole(roleCode, new NodeRef(docRef), new NodeRef(employeeRef).getId(), LecmPermissionService.LecmPermissionGroup.PGROLE_Reader);
     }
 
-	public void revokeDynamicRole(String docRef, String employeeRef, String roleCode) {
+    /**
+     * Отобрать у Сотрудника динамическую роль в документе/папке
+     * @param roleCode id Динамической Роли
+     * @param docRef документа или папки
+     * @param employeeRef id Сотрудника
+     */
+
+    public void revokeDynamicRole(String docRef, String employeeRef, String roleCode) {
         lecmPermissionService.revokeDynamicRole(roleCode, new NodeRef(docRef), new NodeRef(employeeRef).getId());
     }
+    /**
+     * Отобрать у Сотрудника динамическую роль в документе/папке
+     * @param roleCode id Динамической Роли
+     * @param document документа
+     * @param employee Сотрудник
+     */
 
-	public void revokeDynamicRole(ScriptNode document, ScriptNode employee, String roleCode) {
+    public void revokeDynamicRole(ScriptNode document, ScriptNode employee, String roleCode) {
         lecmPermissionService.revokeDynamicRole(roleCode, document.getNodeRef(), employee.getNodeRef().getId());
     }
 
@@ -60,7 +91,11 @@ public class LecmPermissionWebScript extends BaseScopableProcessorExtension {
         lecmPermissionService.grantDynamicRole(roleCode, document.getNodeRef(), employee.getNodeRef().getId(), lecmPermissionService.findPermissionGroup(permission));
     }
 
-	public boolean isAdmin(String login) {
+    /**
+     * Проверка, что пользователь является админом
+     * @param login логин пользователя
+     */
+    public boolean isAdmin(String login) {
 		return lecmPermissionService.isAdmin(login);
 	}
 
@@ -69,7 +104,6 @@ public class LecmPermissionWebScript extends BaseScopableProcessorExtension {
      * @param document документ
      * @param employee сотрудник
      * @param roleName имя роли
-     * @return
      */
     public boolean hasEmployeeDynamicRole(ScriptNode document, ScriptNode employee, String roleName) {
         return lecmPermissionService.hasEmployeeDynamicRole(document.getNodeRef(), employee.getNodeRef(), roleName);
@@ -79,7 +113,6 @@ public class LecmPermissionWebScript extends BaseScopableProcessorExtension {
      * Список существующих прав для пользователя по отношению к документу
      * @param document документ
      * @param employee сотрудник
-     * @return
      */
     public Scriptable getEmployeeRoles(ScriptNode document, ScriptNode employee) {
         List<String> results = lecmPermissionService.getEmployeeRoles(document.getNodeRef(), employee.getNodeRef());
