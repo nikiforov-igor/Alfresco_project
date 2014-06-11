c40a3a6764d91d0f214bcafabbc95961491ffa7c
Ивкин Алексей <AIvkin@it.ru>
2014-06-11 12:35:19 +0000

    ALF-3010: Создать страницу создания документа -договора и поручения
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@8328 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/amp/web/scripts/lecm-documents/documents-list-toolbar.js
+++ lecm-documents/share/src/main/amp/web/scripts/lecm-documents/documents-list-toolbar.js
@@ -8,12 +8,6 @@ LogicECM.module.Documents = LogicECM.module.Documents|| {};
 
 (function () {
     /**
-     * YUI Library aliases
-     */
-    var Dom = YAHOO.util.Dom,
-        Event = YAHOO.util.Event;
-
-    /**
      * Toolbar constructor.
      *
      * @param htmlId {String} The HTML id of the parent element
@@ -57,84 +51,9 @@ LogicECM.module.Documents = LogicECM.module.Documents|| {};
                 this.toolbarButtons[this.options.searchButtonsType].exSearchButton = Alfresco.util.createYUIButton(this, "extendSearchButton", this.onExSearchClick);
             },
 
-            onNewRow: function (e, p_obj) {
-                var destination = this.options.destination,
-                    itemType = this.options.itemType;
-                this.showCreateDialog({itemType: itemType, nodeRef: destination});
-            },
-
-            showCreateDialog: function (meta) {
-                if (this.doubleClickLock) return;
-                this.doubleClickLock = true;
-                // Intercept before dialog show
-                var me = this;
-                var doBeforeDialogShow = function (p_form, p_dialog) {
-                    var contId = p_dialog.id + "-form-container";
-                    var addMsg = meta.addMessage;
-                    var defaultMsg = this.msg("label.create-row.title");
-                    var testMsg = this.msg(me.options.newRowDialogTitle);
-                    if (testMsg != me.options.newRowDialogTitle){
-                        defaultMsg = testMsg;
-                    }
-                    Alfresco.util.populateHTML(
-                        [contId + "_h", addMsg ? addMsg : defaultMsg ]
-                    );
-
-                    Dom.addClass(contId, "metadata-form-edit");
-                    if (me.options.createDialogClass != "") {
-                        Dom.addClass(contId, me.options.createDialogClass);
-                    }
-                    this.doubleClickLock = false;
-	                p_dialog.dialog.subscribe('destroy', LogicECM.module.Base.Util.formDestructor, {moduleId: p_dialog.id}, this);
-                };
-
-                var templateUrl = Alfresco.constants.URL_SERVICECONTEXT + "lecm/components/form";
-	            var templateRequestParams = {
-                        itemKind: "type",
-                        itemId: meta.itemType,
-                        destination: meta.nodeRef,
-                        mode: "create",
-                        formId: meta.createFormId != null ? meta.createFormId : "",
-		            submitType: "json",
-		            showCancelButton: true
-	            };
-
-                // Using Forms Service, so always create new instance
-                var createDetails = new Alfresco.module.SimpleDialog(this.id + "-createDetails");
-                createDetails.setOptions(
-                    {
-                        width: this.options.createDialogWidth,
-                        templateUrl: templateUrl,
-	                    templateRequestParams: templateRequestParams,
-                        actionUrl: null,
-                        destroyOnHide: true,
-                        doBeforeDialogShow: {
-                            fn: doBeforeDialogShow,
-                            scope: this
-                        },
-                        onSuccess: {
-                            fn: function DataGrid_onActionCreate_success(response) {
-                                Alfresco.util.PopupManager.displayMessage(
-                                    {
-                                        text: this.msg("message.save.success")
-                                    });
-                                window.location.href = window.location.protocol + "//" + window.location.host +
-                                    Alfresco.constants.URL_PAGECONTEXT + "document?nodeRef=" + response.json.persistedObject;
-                                this.doubleClickLock = false;
-                            },
-                            scope: this
-                        },
-                        onFailure: {
-                            fn: function DataGrid_onActionCreate_failure(response) {
-                                Alfresco.util.PopupManager.displayMessage(
-                                    {
-                                        text: this.msg("message.save.failure")
-                                    });
-                                this.doubleClickLock = false;
-                            },
-                            scope: this
-                        }
-                    }).show();
+            onNewRow: function () {
+	            window.location.href =
+		            Alfresco.constants.URL_PAGECONTEXT + "document-create?documentType=" + this.options.itemType;
             }
         }, true);
 })();
