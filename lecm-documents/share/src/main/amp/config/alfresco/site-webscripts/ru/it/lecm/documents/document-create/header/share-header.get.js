6d770a1636615feab9415258764933b362f0e313
Ивкин Алексей <AIvkin@it.ru>
2014-06-17 07:35:22 +0000

    ALF-3012: Создать страницу редактирования документа - заголовок
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@8340 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/documents/document-create/header/share-header.get.js
+++ lecm-documents/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/documents/document-create/header/share-header.get.js
@@ -2,22 +2,34 @@
 <import resource="classpath:/alfresco/site-webscripts/org/alfresco/callutils.js">
 <import resource="classpath:/alfresco/site-webscripts/ru/it/lecm/documents/utils/document-utils.js">
 
-var getDocumentTitleName = function(type) {
-    if (type) {
-        var uri = addParamToUrl('/lecm/document-type/settings', 'docType', type);
-        var arm = doGetCall(uri);
-        if (arm && arm.title) {
-            return "Создать " + arm.title;
-        }
-    }
-    return null;
+var getDocumentTitleName = function (type) {
+	if (type) {
+		var uri = addParamToUrl('/lecm/document-type/settings', 'docType', type);
+		var arm = doGetCall(uri);
+		if (arm && arm.title) {
+			return "Создать " + arm.title;
+		}
+	}
+	return null;
+};
+
+var getDocumentPresentString = function (nodeRef) {
+	if (nodeRef) {
+		var nodeDetails = DocumentUtils.getNodeDetails(nodeRef);
+		if (nodeDetails) {
+			return nodeDetails.item.node.properties["lecm-document:ext-present-string"];
+		}
+	}
+	return null;
 };
 
 var headerTitle = widgetUtils.findObject(model.jsonModel, "id", "HEADER_TITLE");
 if (headerTitle != null) {
-    if (page.url.args) {
-        if (page.id == "document-create" && page.url.args.documentType) {
-            headerTitle.config.label = getDocumentTitleName(page.url.args.documentType) || headerTitle.config.label;
-        }
-    }
+	if (page.url.args) {
+		if (page.id == "document-create" && page.url.args.documentType) {
+			headerTitle.config.label = getDocumentTitleName(page.url.args.documentType) || headerTitle.config.label;
+		} else if (page.id == "document-edit" && page.url.args.nodeRef) {
+			headerTitle.config.label = getDocumentPresentString(page.url.args.nodeRef) || headerTitle.config.label;
+		}
+	}
 }
