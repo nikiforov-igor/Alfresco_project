90fa33521ea6f499ae40aa4cb42812bb42161e0f
Зиновин Александр <AZinovin@it.ru>
2014-06-17 11:00:23 +0000

    ALF-2494 - Последние обработанные документы LastDocumentsProcessor
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@8345 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-documents/repo/src/main/java/ru/it/lecm/documents/processors/LastDocumentsProcessor.java
@@ -0,0 +1,35 @@
+package ru.it.lecm.documents.processors;
+
+import ru.it.lecm.base.beans.SearchQueryProcessor;
+import ru.it.lecm.documents.beans.DocumentFrequencyAnalysisService;
+
+import java.util.Map;
+
+/**
+ * User: azinovin
+ * Date: 17.06.2014
+ * Time: 14:20
+ */
+public class LastDocumentsProcessor extends SearchQueryProcessor {
+
+    private DocumentFrequencyAnalysisService documentFrequencyAnalysisService;
+
+    public void setDocumentFrequencyAnalysisService(DocumentFrequencyAnalysisService documentFrequencyAnalysisService) {
+        this.documentFrequencyAnalysisService = documentFrequencyAnalysisService;
+    }
+
+    @Override
+    public String getQuery(Map<String, Object> params) {
+        StringBuilder sbQuery = new StringBuilder();
+        String lastDocuments = documentFrequencyAnalysisService.getLastDocuments();
+            if (lastDocuments != null && lastDocuments.length() > 0) {
+                String[] docsRefs = lastDocuments.split(";");
+                for (String docsRef : docsRefs) {
+                    sbQuery.append("ID:").append(docsRef.replace(":", "\\:")).append(" OR ");
+                }
+
+            }
+        sbQuery.append("ID:\"NOT_REF\""); // выключать поиск, если документы не найдены
+        return sbQuery.toString();
+    }
+}
