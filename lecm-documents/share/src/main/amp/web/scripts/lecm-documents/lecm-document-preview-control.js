1724d0f64b2470c0992f39771e93480665e62d3e
Малыгин Владимир <VMalygin@it.ru>
2014-07-11 12:28:24 +0000

    модифицировал контрол document-preview-control суть в том что в основе этого контроле лежит компонент Alfresco.WebPreview в силу причин известных лишь разработчикам этого компонента, в нем есть определенный хардкод расчета высоты контейнера (web-preview-extend.js). Этот хардкод завязан на верстку страницы /share/page/document-details. В результате, при использовании Alfresco.WebPreview в любом другом месте, высота контейнера получается равна высоте окна браузера.
    Чтобы это обойти пришлось добавить еще один хардкод, потому-что все изменения высоты перехватываются событием onResize и решить проблему с помощью css не удалось.
    
    В общем, у контрола теперь появилось 2 параметра: height и viewerHeight. Их можно задавать через share-config-custom и таким образом ограничивать высоту Alfresco.WebPreview
    
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@8749 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/amp/web/scripts/lecm-documents/lecm-document-preview-control.js
+++ lecm-documents/share/src/main/amp/web/scripts/lecm-documents/lecm-document-preview-control.js
@@ -4,7 +4,7 @@
  * @namespace LogicECM
  */
 // Ensure LogicECM root object exists
-if (typeof LogicECM == "undefined" || !LogicECM) {
+if (typeof LogicECM == 'undefined' || !LogicECM) {
 	var LogicECM = {};
 }
 
@@ -18,134 +18,176 @@ LogicECM.module = LogicECM.module || {};
 
 LogicECM.module.Documents = LogicECM.module.Documents || {};
 
-(function () {
+(function() {
 	var Dom = YAHOO.util.Dom,
 		Event = YAHOO.util.Event;
 
-	LogicECM.module.Documents.DocumentPreviewControl = function (fieldHtmlId) {
-		LogicECM.module.Documents.DocumentPreviewControl.superclass.constructor.call(this, "LogicECM.module.Documents.DocumentPreviewControl", fieldHtmlId, [ "container"]);
+	LogicECM.module.Documents.DocumentPreviewControl = function(fieldHtmlId) {
+		LogicECM.module.Documents.DocumentPreviewControl.superclass.constructor.call(this, 'LogicECM.module.Documents.DocumentPreviewControl', fieldHtmlId, ['container']);
 
 		return this;
 	};
 
-	YAHOO.extend(LogicECM.module.Documents.DocumentPreviewControl, Alfresco.component.Base,
-		{
-			options:{
-				taskId: ""
-			},
+	YAHOO.extend(LogicECM.module.Documents.DocumentPreviewControl, Alfresco.component.Base, {
+		options: {
+			taskId: '',
+			height: '',
+			viewerHeight: ''
+		},
 
-			documentNodeRef: null,
+		documentNodeRef: null,
 
-			attachmentsSelect: null,
+		attachmentsSelect: null,
 
-			onReady:function () {
-				this.attachmentsSelect = Dom.get(this.id + "-attachment-select");
+		onReady: function() {
+			this.attachmentsSelect = Dom.get(this.id + '-attachment-select');
 
-				if (this.attachmentsSelect != null) {
-					this.loadDocument();
-				}
+			if (this.attachmentsSelect != null) {
+				this.loadDocument();
+			}
 
-				Event.on(this.attachmentsSelect, "change", this.reloadAttachmentPreview, this, true);
-			},
+			Event.on(this.attachmentsSelect, 'change', this.reloadAttachmentPreview, null, this);
+		},
 
-			loadDocument: function() {
-				if (this.options.taskId != null) {
-					Alfresco.util.Ajax.request({
-						method: "GET",
-						url: Alfresco.constants.PROXY_URI_RELATIVE + "lecm/workflow/GetDocumentDataByTaskId?taskID=" + this.options.taskId,
-						successCallback: {
-							fn: function (response) {
-								var result = response.json;
-								if (result != null && result.nodeRef != null) {
-									this.documentNodeRef = result.nodeRef;
-									this.loadDocumentAttachments();
-								}
-							},
-							scope: this
+		loadDocument: function() {
+			if (this.options.taskId != null) {
+				Alfresco.util.Ajax.request({
+					method: 'GET',
+					url: Alfresco.constants.PROXY_URI_RELATIVE + 'lecm/workflow/GetDocumentDataByTaskId?taskID=' + this.options.taskId,
+					successCallback: {
+						scope: this,
+						fn: function(response) {
+							var result = response.json;
+							if (result != null && result.nodeRef != null) {
+								this.documentNodeRef = result.nodeRef;
+								this.loadDocumentAttachments();
+							}
 						}
-					});
-				}
-			},
+					}
+				});
+			}
+		},
 
-			loadDocumentAttachments: function() {
-				if (this.options.taskId != null) {
-					Alfresco.util.Ajax.request({
-						method: "GET",
-						url: Alfresco.constants.PROXY_URI_RELATIVE + "lecm/document/attachments/api/get?documentNodeRef=" + this.documentNodeRef + "&showEmptyCategory=true",
-						successCallback: {
-							fn: function (response) {
-								var result = response.json;
-								if (result != null) {
-									this.attachmentsSelect.innerHTML = "";
+		loadDocumentAttachments: function() {
+			if (this.options.taskId != null) {
+				Alfresco.util.Ajax.request({
+					method: 'GET',
+					url: Alfresco.constants.PROXY_URI_RELATIVE + 'lecm/document/attachments/api/get?documentNodeRef=' + this.documentNodeRef + '&showEmptyCategory=true',
+					successCallback: {
+						scope: this,
+						fn: function(response) {
+							var result = response.json;
+							if (result != null) {
+								this.attachmentsSelect.innerHTML = '';
 
-									if (result.items != null && result.items.length > 0) {
-										for (var i = 0; i < result.items.length; i++) {
-											var item = result.items[i];
+								if (result.items != null && result.items.length > 0) {
+									for (var i = 0; i < result.items.length; i++) {
+										var item = result.items[i];
 
-											var optionGroup = document.createElement("optgroup");
-											if (item.category != null && item.category.name != null) {
-												optionGroup.label = item.category.name;
-											}
+										var optionGroup = document.createElement('optgroup');
+										if (item.category != null && item.category.name != null) {
+											optionGroup.label = item.category.name;
+										}
 
-											if (item.attachments != null && item.attachments.length > 0) {
-												for (var j = 0; j < item.attachments.length; j++) {
-													var attachment = item.attachments[j];
+										if (item.attachments != null && item.attachments.length > 0) {
+											for (var j = 0; j < item.attachments.length; j++) {
+												var attachment = item.attachments[j];
 
-													if (attachment.nodeRef != null) {
-														var option = document.createElement("option");
-														option.value = attachment.nodeRef;
-														if (attachment.name != null) {
-															option.innerHTML = attachment.name;
-														}
-
-														optionGroup.appendChild(option);
+												if (attachment.nodeRef != null) {
+													var option = document.createElement('option');
+													option.value = attachment.nodeRef;
+													if (attachment.name != null) {
+														option.innerHTML = attachment.name;
 													}
+													optionGroup.appendChild(option);
 												}
 											}
-
-											this.attachmentsSelect.appendChild(optionGroup);
 										}
+										this.attachmentsSelect.appendChild(optionGroup);
 									}
-
-									this.reloadAttachmentPreview();
 								}
-							},
-							scope: this
+								this.reloadAttachmentPreview();
+							}
 						}
-					});
-				}
-			},
+					}
+				});
+			}
+		},
 
-			reloadAttachmentPreview: function() {
-                if (this.attachmentsSelect.value) {
-				Alfresco.util.Ajax.request(
-					{
-						url: Alfresco.constants.URL_SERVICECONTEXT + "components/preview/web-preview",
-						dataObj: {
-							nodeRef: this.attachmentsSelect.value,
-							htmlid: this.id + "-preview-container"
-						},
-						successCallback: {
-							fn:function(response){
-								Dom.get(this.id + "-preview-container").innerHTML = response.serverResponse.responseText;
-                                var previewId = this.id + "-preview-container-full-window-div";
-
-								Event.onAvailable(previewId, function() {
-									var preview = Dom.get(previewId);
-									var container = Dom.get(this.id + "-preview-container-previewer-div");
-
-									container.innerHTML = "";
-                                    preview.setAttribute("style", "");
-									container.appendChild(preview);
-								}, {}, this);
-							},
-							scope: this
-						},
-						failureMessage: this.msg("message.failure"),
+		reloadAttachmentPreview: function() {
+			if (this.attachmentsSelect.value) {
+				Alfresco.util.Ajax.request({
+					url: Alfresco.constants.URL_SERVICECONTEXT + 'components/preview/web-preview',
+					dataObj: {
+						nodeRef: this.attachmentsSelect.value,
+						htmlid: this.id + '-preview-container'
+					},
+					successCallback: {
 						scope: this,
-						execScripts: true
-					});
+						fn: this.reloadAttachmentPreviewSuccessCallback
+					},
+					failureMessage: this.msg('message.failure'),
+					scope: this,
+					execScripts: true
+				});
 			}
+		},
+
+		reloadAttachmentPreviewSuccessCallback: function(response) {
+			var options = {
+				fullWindowDiv: this.id + '-preview-container-full-window-div',
+				previewerDiv: this.id + '-preview-container-previewer-div',
+				viewer: this.id + '-preview-container-viewer'
 			}
-		});
+
+			Dom.get(this.id + '-preview-container').innerHTML = response.serverResponse.responseText;
+
+			Event.onAvailable(options.fullWindowDiv, this.onFullWindowDivAvaliable, options, this);
+			if (this.options.height) {
+				Event.onAvailable(options.previewerDiv, this.onPreviewerDivAvaliable, options, this);
+				if (this.options.viewerHeight) {
+					Event.onContentReady(options.viewer, this.onViewerReady, options, this);
+				}
+			}
+		},
+
+		onFullWindowDivAvaliable: function(obj) {
+			var preview = Dom.get(obj.fullWindowDiv);
+			var container = Dom.get(obj.previewerDiv);
+
+			container.innerHTML = '';
+			preview.setAttribute('style', '');
+			container.appendChild(preview);
+		},
+
+		onPreviewerDivAvaliable: function(obj) {
+			Dom.setStyle(obj.previewerDiv, 'height', this.options.height);
+		},
+
+		onViewerReady: function(obj) {
+
+			function onWebPreviewResize(obj) {
+				var subscribers = webPreview.plugin.onResize.subscribers;
+				var oldSubscribers = YAHOO.lang.merge({subscribers: subscribers});
+
+				webPreview.plugin.onResize.unsubscribeAll();
+				Dom.setStyle(obj.previewerDiv, 'height', this.options.height);
+				Dom.setStyle(obj.viewer, 'height', this.options.viewerHeight);
+				webPreview.plugin.onResize.subscribers = oldSubscribers.subscribers;
+				return false;
+			}
+
+			var webPreview, components = Alfresco.util.ComponentManager.find({name: 'Alfresco.WebPreview'});
+
+				Dom.setStyle(obj.previewerDiv, 'height', this.options.height);
+				Dom.setStyle(obj.viewer, 'height', this.options.viewerHeight);
+
+			if (components && components.length) {
+				webPreview = components[0];
+				if (webPreview.plugin && webPreview.plugin.onResize) {
+					webPreview.plugin.onResize.subscribe(onWebPreviewResize, obj, this);
+				}
+			}
+		}
+	});
 })();
