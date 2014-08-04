5700a4feafdc1f03fd7403f597c9d0a3b3de6737
Дмитрий Дмитриев <DDmitriev@it.ru>
2014-08-04 13:15:50 +0000

    [-] Удалил ненужные классы
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@9094 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/api/src/main/java/ru/it/lecm/base/utils/IBaseAlgorithmUtils.java
+++
@@ -1,9 +0,0 @@
-package ru.it.lecm.base.utils;
-
-/**
- *
- * @author snovikov
- */
-public interface IBaseAlgorithmUtils {
-	public <T,E> T getDocumentsOnStatusAmmount(AtomicWork<T> worker, E context);
-}
