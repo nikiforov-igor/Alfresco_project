676cf550f32846e904eed8f287088a5b9a87765f
Денис Башмаков <DBashmakov@it.ru>
2014-08-07 13:07:34 +0000

    FMS-422 - Элементы управления для выбора из оргструктуры (фильтрация в дереве)
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@9138 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/association-tree-view/asssociation-tree-children.get.js
+++ lecm-base/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/association-tree-view/asssociation-tree-children.get.js
@@ -2,15 +2,20 @@ var nodeRef = url.templateArgs.store_type + "://" + url.templateArgs.store_id + 
 var nodeSubstituteString = args['nodeSubstituteString'];
 var nodeTitleSubstituteString = args['nodeTitleSubstituteString'];
 var selectableType = args['selectableType'];
+var useOnlyInSameOrg = args['onlyInSameOrg'];
 
 var parentNode = search.findNode(nodeRef);
 var branch = [];
+
+// Если условие строгое - то принимаем только те, к которым есть доступ. Если не строгое - проверяем есть ли вообще поле
+var useStrictFilterByOrg = (useOnlyInSameOrg != null && ("" + useOnlyInSameOrg) == "true");
 
 if (parentNode != null) {
     var values = parentNode.getChildren();
 
     for each(var item in values) {
-		if (item.isSubType(selectableType) && (!item.hasAspect("lecm-dic:aspect_active") || item.properties["lecm-dic:active"])) {
+		if (item.isSubType(selectableType) && (!item.hasAspect("lecm-dic:aspect_active") || item.properties["lecm-dic:active"])
+            && orgstructure.hasAccessToOrgElement(item, useStrictFilterByOrg)) {
 	        branch.push({
 	            label: substitude.formatNodeTitle(item, nodeSubstituteString),
 	            title: substitude.formatNodeTitle(item, nodeTitleSubstituteString),
