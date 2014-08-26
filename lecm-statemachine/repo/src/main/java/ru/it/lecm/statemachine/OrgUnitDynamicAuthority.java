e723bfcf67f7827b7726b87ec3fa0bfc3324f85c
Зиновин Александр <AZinovin@it.ru>
2014-08-26 13:41:12 +0000

    ALF-3181 - оптимизировать работу левой области АРМ передвинул проверку authorityService.getAuthoritiesForUser(userName) внутрь проверки типа документа - стала вызываться намного реже (случай наличия глобального доступа к документам более редкий, чем проверка доступа к недокументам
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@9361 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-statemachine/repo/src/main/java/ru/it/lecm/statemachine/OrgUnitDynamicAuthority.java
+++ lecm-statemachine/repo/src/main/java/ru/it/lecm/statemachine/OrgUnitDynamicAuthority.java
@@ -1,28 +1,25 @@
 package ru.it.lecm.statemachine;
 
 
-import java.util.Set;
-
-import ru.it.lecm.documents.beans.DocumentService;
-import ru.it.lecm.orgstructure.beans.OrgstructureAspectsModel;
-
 import org.alfresco.repo.cache.SimpleCache;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
 import org.alfresco.repo.security.permissions.DynamicAuthority;
 import org.alfresco.repo.security.permissions.PermissionReference;
+import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.repository.AssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.security.AuthorityService;
 import org.alfresco.service.namespace.QName;
-import org.alfresco.util.EqualsHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import ru.it.lecm.documents.beans.DocumentService;
+import ru.it.lecm.orgstructure.beans.OrgstructureAspectsModel;
 
 import java.util.List;
+import java.util.Set;
 
 public class OrgUnitDynamicAuthority implements DynamicAuthority, InitializingBean
 {
@@ -64,43 +61,43 @@ public class OrgUnitDynamicAuthority implements DynamicAuthority, InitializingBe
     	final long startTime = System.nanoTime();
         
     	Boolean res = AuthenticationUtil.runAs(new RunAsWork<Boolean>(){
-    		
-    		public Boolean doWork() throws Exception
-    		{
-    			boolean result = true;
-    			if (userName.equalsIgnoreCase("System") || userName.equalsIgnoreCase("workflow")) {
-    				result = false;
-    			} else {
-	    			Set<String> auth = authorityService.getAuthoritiesForUser(userName);
-		            if (auth.contains("GROUP_LECM_GLOBAL_ORGANIZATIONS_ACCESS")) {//пользователь в группе LECM_GLOBAL_ORGANIZATIONS_ACCESS
-		            	result = false;
-				    } else {
-		    			QName refType = nodeService.getType(nodeRef);
-		    			Boolean isDocument = (refType != null && dictionaryService.isSubClass(refType, DocumentService.TYPE_BASE_DOCUMENT));
-		    			if(isDocument) {
-						   	NodeRef organisation = null;
-						   	NodeRef org = null;
-						   	if (userOrganizationsCache.contains(userName)) {
-						    	org = userOrganizationsCache.get(userName);
-							    if (nodeService.hasAspect(nodeRef, OrgstructureAspectsModel.ASPECT_HAS_LINKED_ORGANIZATION)) {
-							    	List<AssociationRef> contractorAssoc = nodeService.getTargetAssocs(nodeRef, OrgstructureAspectsModel.ASSOC_LINKED_ORGANIZATION);
-							    	if (!contractorAssoc.isEmpty()) {
-							    		organisation = contractorAssoc.get(0).getTargetRef();
-							    	}
-							    }
-						   	}
-						    if(org!=null&&organisation!=null) {
-						    	if(org.equals(organisation)) result = false;
-						    }
-		                } else {
-		                	result = false;
-		                }
-				    }
-    			}
-    			long endTime = System.nanoTime();
+
+            public Boolean doWork() throws Exception {
+                boolean result = true;
+                if (userName.equalsIgnoreCase("System") || userName.equalsIgnoreCase("workflow")) {
+                    result = false;
+                } else {
+
+                    QName refType = nodeService.getType(nodeRef);
+                    boolean isDocument = (refType != null && dictionaryService.isSubClass(refType, DocumentService.TYPE_BASE_DOCUMENT));
+                    if (isDocument) {
+                        Set<String> auth = authorityService.getAuthoritiesForUser(userName);
+                        if (auth.contains("GROUP_LECM_GLOBAL_ORGANIZATIONS_ACCESS")) {//пользователь в группе LECM_GLOBAL_ORGANIZATIONS_ACCESS
+                            result = false;
+                        } else {
+                            NodeRef organisation = null;
+                            NodeRef org = null;
+                            if (userOrganizationsCache.contains(userName)) {
+                                org = userOrganizationsCache.get(userName);
+                                if (nodeService.hasAspect(nodeRef, OrgstructureAspectsModel.ASPECT_HAS_LINKED_ORGANIZATION)) {
+                                    List<AssociationRef> contractorAssoc = nodeService.getTargetAssocs(nodeRef, OrgstructureAspectsModel.ASSOC_LINKED_ORGANIZATION);
+                                    if (!contractorAssoc.isEmpty()) {
+                                        organisation = contractorAssoc.get(0).getTargetRef();
+                                    }
+                                }
+                            }
+                            if (org != null && organisation != null) {
+                                if (org.equals(organisation)) result = false;
+                            }
+                        }
+                    } else {
+                        result = false;
+                    }
+                }
+                long endTime = System.nanoTime();
     			logger.debug("!!!!!!!!!!!!!!! Проверка прав пользователя "+userName+" на документ "+nodeRef + " длительность " + (endTime - startTime)/1000000 + " ms");
                 return result;
-                
+
             }
         }, AuthenticationUtil.getSystemUserName());
 		
