3df903cd0d5ea3fff3ec841a72abe62b1bb2c76a
Ивкин Алексей <AIvkin@it.ru>
2014-09-17 12:36:01 +0000

    Base64.encode падал при наличии русских символов
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@9779 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-base/share/src/main/amp/web/scripts/lecm-base/components/base64.js
+++ lecm-base/share/src/main/amp/web/scripts/lecm-base/components/base64.js
@@ -1,176 +1,136 @@
 /*
- * Copyright (c) 2010 Nick Galbreath
- * http://code.google.com/p/stringencoders/source/browse/#svn/trunk/javascript
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+ Copyright Vassilis Petroulias [DRDigit]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
-
-/* base64 encode/decode compatible with window.btoa/atob
- *
- * window.atob/btoa is a Firefox extension to convert binary data (the "b")
- * to base64 (ascii, the "a").
- *
- * It is also found in Safari and Chrome.  It is not available in IE.
- *
- * if (!window.btoa) window.btoa = base64.encode
- * if (!window.atob) window.atob = base64.decode
- *
- * The original spec's for atob/btoa are a bit lacking
- * https://developer.mozilla.org/en/DOM/window.atob
- * https://developer.mozilla.org/en/DOM/window.btoa
- *
- * window.btoa and base64.encode takes a string where charCodeAt is [0,255]
- * If any character is not [0,255], then an DOMException(5) is thrown.
- *
- * window.atob and base64.decode take a base64-encoded string
- * If the input length is not a multiple of 4, or contains invalid characters
- *   then an DOMException(5) is thrown.
- */
-var base64 = {};
-base64.PADCHAR = '=';
-base64.ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-
-base64.makeDOMException = function() {
-	// sadly in FF,Safari,Chrome you can't make a DOMException
-	var e, tmp;
-
-	try {
-		return new DOMException(DOMException.INVALID_CHARACTER_ERR);
-	} catch (tmp) {
-		// not available, just passback a duck-typed equiv
-		// https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Global_Objects/Error
-		// https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Global_Objects/Error/prototype
-		var ex = new Error("DOM Exception 5");
-
-		// ex.number and ex.description is IE-specific.
-		ex.code = ex.number = 5;
-		ex.name = ex.description = "INVALID_CHARACTER_ERR";
-
-		// Safari/Chrome output format
-		ex.toString = function() { return 'Error: ' + ex.name + ': ' + ex.message; };
-		return ex;
-	}
-}
-
-base64.getbyte64 = function(s,i) {
-	// This is oddly fast, except on Chrome/V8.
-	//  Minimal or no improvement in performance by using a
-	//   object with properties mapping chars to value (eg. 'A': 0)
-	var idx = base64.ALPHA.indexOf(s.charAt(i));
-	if (idx === -1) {
-		throw base64.makeDOMException();
-	}
-	return idx;
-}
-
-base64.decode = function(s) {
-	// convert to string
-	s = '' + s;
-	var getbyte64 = base64.getbyte64;
-	var pads, i, b10;
-	var imax = s.length
-	if (imax === 0) {
-		return s;
-	}
-
-	if (imax % 4 !== 0) {
-		throw base64.makeDOMException();
-	}
-
-	pads = 0
-	if (s.charAt(imax - 1) === base64.PADCHAR) {
-		pads = 1;
-		if (s.charAt(imax - 2) === base64.PADCHAR) {
-			pads = 2;
+var B64 = {
+	alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+	lookup: null,
+	ie: /MSIE /.test(navigator.userAgent),
+	ieo: /MSIE [67]/.test(navigator.userAgent),
+	encode: function (s) {
+		var buffer = B64.toUtf8(s),
+			position = -1,
+			len = buffer.length,
+			nan0, nan1, nan2, enc = [, , , ];
+		if (B64.ie) {
+			var result = [];
+			while (++position < len) {
+				nan0 = buffer[position];
+				nan1 = buffer[++position];
+				enc[0] = nan0 >> 2;
+				enc[1] = ((nan0 & 3) << 4) | (nan1 >> 4);
+				if (isNaN(nan1))
+					enc[2] = enc[3] = 64;
+				else {
+					nan2 = buffer[++position];
+					enc[2] = ((nan1 & 15) << 2) | (nan2 >> 6);
+					enc[3] = (isNaN(nan2)) ? 64 : nan2 & 63;
+				}
+				result.push(B64.alphabet.charAt(enc[0]), B64.alphabet.charAt(enc[1]), B64.alphabet.charAt(enc[2]), B64.alphabet.charAt(enc[3]));
+			}
+			return result.join('');
+		} else {
+			var result = '';
+			while (++position < len) {
+				nan0 = buffer[position];
+				nan1 = buffer[++position];
+				enc[0] = nan0 >> 2;
+				enc[1] = ((nan0 & 3) << 4) | (nan1 >> 4);
+				if (isNaN(nan1))
+					enc[2] = enc[3] = 64;
+				else {
+					nan2 = buffer[++position];
+					enc[2] = ((nan1 & 15) << 2) | (nan2 >> 6);
+					enc[3] = (isNaN(nan2)) ? 64 : nan2 & 63;
+				}
+				result += B64.alphabet[enc[0]] + B64.alphabet[enc[1]] + B64.alphabet[enc[2]] + B64.alphabet[enc[3]];
+			}
+			return result;
 		}
-		// either way, we want to ignore this last block
-		imax -= 4;
+	},
+	decode: function (s) {
+		if (s.length % 4)
+			throw new Error("InvalidCharacterError: 'B64.decode' failed: The string to be decoded is not correctly encoded.");
+		var buffer = B64.fromUtf8(s),
+			position = 0,
+			len = buffer.length;
+		if (B64.ieo) {
+			var result = [];
+			while (position < len) {
+				if (buffer[position] < 128)
+					result.push(String.fromCharCode(buffer[position++]));
+				else if (buffer[position] > 191 && buffer[position] < 224)
+					result.push(String.fromCharCode(((buffer[position++] & 31) << 6) | (buffer[position++] & 63)));
+				else
+					result.push(String.fromCharCode(((buffer[position++] & 15) << 12) | ((buffer[position++] & 63) << 6) | (buffer[position++] & 63)));
+			}
+			return result.join('');
+		} else {
+			var result = '';
+			while (position < len) {
+				if (buffer[position] < 128)
+					result += String.fromCharCode(buffer[position++]);
+				else if (buffer[position] > 191 && buffer[position] < 224)
+					result += String.fromCharCode(((buffer[position++] & 31) << 6) | (buffer[position++] & 63));
+				else
+					result += String.fromCharCode(((buffer[position++] & 15) << 12) | ((buffer[position++] & 63) << 6) | (buffer[position++] & 63));
+			}
+			return result;
+		}
+	},
+	toUtf8: function (s) {
+		var position = -1,
+			len = s.length,
+			chr, buffer = [];
+		if (/^[\x00-\x7f]*$/.test(s)) while (++position < len)
+			buffer.push(s.charCodeAt(position));
+		else while (++position < len) {
+			chr = s.charCodeAt(position);
+			if (chr < 128)
+				buffer.push(chr);
+			else if (chr < 2048)
+				buffer.push((chr >> 6) | 192, (chr & 63) | 128);
+			else
+				buffer.push((chr >> 12) | 224, ((chr >> 6) & 63) | 128, (chr & 63) | 128);
+		}
+		return buffer;
+	},
+	fromUtf8: function (s) {
+		var position = -1,
+			len, buffer = [],
+			enc = [, , , ];
+		if (!B64.lookup) {
+			len = B64.alphabet.length;
+			B64.lookup = {};
+			while (++position < len)
+				B64.lookup[B64.alphabet.charAt(position)] = position;
+			position = -1;
+		}
+		len = s.length;
+		while (++position < len) {
+			enc[0] = B64.lookup[s.charAt(position)];
+			enc[1] = B64.lookup[s.charAt(++position)];
+			buffer.push((enc[0] << 2) | (enc[1] >> 4));
+			enc[2] = B64.lookup[s.charAt(++position)];
+			if (enc[2] == 64)
+				break;
+			buffer.push(((enc[1] & 15) << 4) | (enc[2] >> 2));
+			enc[3] = B64.lookup[s.charAt(++position)];
+			if (enc[3] == 64)
+				break;
+			buffer.push(((enc[2] & 3) << 6) | enc[3]);
+		}
+		return buffer;
 	}
-
-	var x = [];
-	for (i = 0; i < imax; i += 4) {
-		b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12) |
-			(getbyte64(s,i+2) << 6) | getbyte64(s,i+3);
-		x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff, b10 & 0xff));
-	}
-
-	switch (pads) {
-		case 1:
-			b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12) | (getbyte64(s,i+2) << 6);
-			x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff));
-			break;
-		case 2:
-			b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12);
-			x.push(String.fromCharCode(b10 >> 16));
-			break;
-	}
-	return x.join('');
-}
-
-base64.getbyte = function(s,i) {
-	var x = s.charCodeAt(i);
-	if (x > 255) {
-		throw base64.makeDOMException();
-	}
-	return x;
-}
-
-base64.encode = function(s) {
-	if (arguments.length !== 1) {
-		throw new SyntaxError("Not enough arguments");
-	}
-	var padchar = base64.PADCHAR;
-	var alpha   = base64.ALPHA;
-	var getbyte = base64.getbyte;
-
-	var i, b10;
-	var x = [];
-
-	// convert to string
-	s = '' + s;
-
-	var imax = s.length - s.length % 3;
-
-	if (s.length === 0) {
-		return s;
-	}
-	for (i = 0; i < imax; i += 3) {
-		b10 = (getbyte(s,i) << 16) | (getbyte(s,i+1) << 8) | getbyte(s,i+2);
-		x.push(alpha.charAt(b10 >> 18));
-		x.push(alpha.charAt((b10 >> 12) & 0x3F));
-		x.push(alpha.charAt((b10 >> 6) & 0x3f));
-		x.push(alpha.charAt(b10 & 0x3f));
-	}
-	switch (s.length - imax) {
-		case 1:
-			b10 = getbyte(s,i) << 16;
-			x.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
-				padchar + padchar);
-			break;
-		case 2:
-			b10 = (getbyte(s,i) << 16) | (getbyte(s,i+1) << 8);
-			x.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
-				alpha.charAt((b10 >> 6) & 0x3f) + padchar);
-			break;
-	}
-	return x.join('');
-}
+};
