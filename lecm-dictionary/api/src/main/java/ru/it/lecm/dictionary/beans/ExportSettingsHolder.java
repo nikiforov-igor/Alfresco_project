f6ff350d63ef582e3d4928cadba3ab406e30cc40
Зиновин Александр <AZinovin@it.ru>
2014-10-30 08:01:09 +0000

    ALF-3512 - Упрощение процедуры экспорта вновь описанного справочника временно так - если настройки не заданы, то выгружать атрибуты типа
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@10613 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-dictionary/api/src/main/java/ru/it/lecm/dictionary/beans/ExportSettingsHolder.java
+++ lecm-dictionary/api/src/main/java/ru/it/lecm/dictionary/beans/ExportSettingsHolder.java
@@ -1,5 +1,9 @@
 package ru.it.lecm.dictionary.beans;
 
+import org.alfresco.service.cmr.dictionary.DictionaryService;
+import org.alfresco.service.cmr.dictionary.PropertyDefinition;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
 import ru.it.lecm.dictionary.ExportSettings;
 
 import java.util.ArrayList;
@@ -14,16 +18,43 @@ import java.util.Map;
  * Time: 9:09
  */
 public class ExportSettingsHolder implements ExportSettings {
+    private DictionaryService dictionaryService;
+    private NamespaceService namespaceService;
+
+    public void setDictionaryService(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
+    }
+
+    public void setNamespaceService(NamespaceService namespaceService) {
+        this.namespaceService = namespaceService;
+    }
+
     private Map<String, List<String>> typeFields = new HashMap<String, List<String>>();
 
     @Override
     public List<String> getFieldsForType(String typeName) {
         List<String> fields = typeFields.get(typeName);
         if (fields == null) {
-            fields = new ArrayList<String>();
+            fields = new ArrayList<>();
         }
         if (fields.isEmpty()) {
-            fields.add("cm:name");
+
+            Map<QName, PropertyDefinition> allProperties = dictionaryService.getPropertyDefs(QName.createQName(typeName, namespaceService));
+
+            for (QName property : allProperties.keySet()) {
+                String string = property.toPrefixString();
+                if (!string.equals("cm:content")) {
+                    fields.add(string);
+                }
+            }
+
+            if (!fields.contains("cm:name")) {
+                fields.add("cm:name");
+            }
+            if (!fields.contains("cm:title")) {
+                fields.add("cm:title");
+            }
+
             typeFields.put(typeName, fields);
         }
         return fields;
