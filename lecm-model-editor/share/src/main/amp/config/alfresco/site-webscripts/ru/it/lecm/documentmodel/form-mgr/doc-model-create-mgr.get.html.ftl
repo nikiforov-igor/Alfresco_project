d4c4fbb8f3f864af0fe6ee65e3d4e9153cad7422
Малыгин Владимир <VMalygin@it.ru>
2015-01-13 11:48:57 +0000

    ALF-3774 изменен способ формирования заголовка страницы
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@11581 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-model-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/documentmodel/form-mgr/doc-model-create-mgr.get.html.ftl
+++ lecm-model-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/documentmodel/form-mgr/doc-model-create-mgr.get.html.ftl
@@ -2,9 +2,6 @@
 <script type="text/javascript">//<![CDATA[
    new Alfresco.component.FormManager("${args.htmlid}").setOptions(
    {
-      
+
    }).setMessages(${messages});
 //]]></script>
-<div class="form-manager">
-   <h1>Создать модель документа</h1>
-</div>
--- lecm-statemachine-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/statemachine/editor/header/share-header.get.js
+++ lecm-statemachine-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/statemachine/editor/header/share-header.get.js
@@ -1,15 +1,11 @@
 (function() {
 	var json,
-		title = 'Жизненный цикл',
 		url = '/lecm/statemachine/editor/title?' + page.url.queryString,
 		headerTitle = widgetUtils.findObject(model.jsonModel, 'id', 'HEADER_TITLE');
-		if (headerTitle) {
-			if (page.url.args && 'statemachine' == page.id) {
-				json = remote.connect('alfresco').get(encodeURI(url));
-				if (200 == json.status) {
-					title = json + ' – ' + title;
-				}
-				headerTitle.config.label = title;
+		if (headerTitle && page.url.args && 'statemachine' == page.id) {
+			json = remote.connect('alfresco').get(encodeURI(url));
+			if (200 == json.status) {
+				headerTitle.config.label = json + ' – ' + page.title;
 			}
 		}
 })();
