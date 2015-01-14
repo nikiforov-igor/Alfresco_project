ccd080906b5d48b4b92776cb23fe35e6322da31f
Мельников Петр <PMelnikov@it.ru>
2015-01-14 11:37:29 +0000

    ALF-3814. Отчет не формируется, если длина URI с параметрами больше 10000 символов
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@11608 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/amp/web/scripts/lecm-documents/documents-reports.js
+++ lecm-documents/share/src/main/amp/web/scripts/lecm-documents/documents-reports.js
@@ -6,88 +6,103 @@ LogicECM.module.Documents = LogicECM.module.Documents|| {};
 LogicECM.module.Documents.Reports = LogicECM.module.Documents.Reports || {};
 
 (function() {
-    LogicECM.module.Documents.Reports.reportLinkClicked = function(element, param) {
-        var reportCode = param.reportCode;
-        var documentRef = param.nodeRef;
 
-        var actionUrl = null;
-        var doBeforeDialogShow = function (p_form, p_dialog) {
-            var defaultMsg = Alfresco.component.Base.prototype.msg("documents.report." + reportCode + ".title");
-            if (defaultMsg == "documents.report." + reportCode + ".title"){
-                defaultMsg = Alfresco.component.Base.prototype.msg("document.report.default.title");
-            }
-            Alfresco.util.populateHTML(
-                [ p_dialog.id + "-form-container_h", defaultMsg ]
-            );
+    LogicECM.module.Documents.Reports.openReport = function (actionUrl, params) {
+        var newWindow = window.open("about:blank", "report", "toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no");
+        var newDocument = newWindow.document;
+        var body = newDocument.body;
+        var newform = newDocument.createElement("form");
+        newform.action = actionUrl;
+        newform.method = "POST";
+        body.appendChild(newform);
+        var paramsField = newDocument.createElement("input");
+        paramsField.type = "hidden";
+        paramsField.name = "json_form_parameters";
+        paramsField.value = params != null ? JSON.stringify(params) : "";
+        newform.appendChild(paramsField);
+        newform.submit();
+    },
 
-            var formElement = Dom.get(p_form.formId);
-            if (formElement) {
-                actionUrl = formElement.action;
-                if (actionUrl && actionUrl.indexOf("autoSubmit") > 0) {
-                    var data = {};
-                    if (documentRef) { // добавляем к параметрам ID - если задано
-                        data["ID"] = documentRef;
-                    }
-                    p_dialog.dialog.subscribe('show', LogicECM.module.Base.Util.formDestructor, {moduleId: p_dialog.id}, this);
-                    var paramsStr = Alfresco.util.Ajax.jsonToParamString(data, true);
-                    window.open(actionUrl + (actionUrl.indexOf("?") == -1 ? "?" : "&") + paramsStr, "report", "toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no");
-                } else {
-                    p_dialog.dialog.subscribe('destroy', LogicECM.module.Base.Util.formDestructor, {moduleId: p_dialog.id}, this);
+        LogicECM.module.Documents.Reports.reportLinkClicked = function(element, param) {
+            var reportCode = param.reportCode;
+            var documentRef = param.nodeRef;
+
+            var actionUrl = null;
+            var doBeforeDialogShow = function (p_form, p_dialog) {
+                var defaultMsg = Alfresco.component.Base.prototype.msg("documents.report." + reportCode + ".title");
+                if (defaultMsg == "documents.report." + reportCode + ".title"){
+                    defaultMsg = Alfresco.component.Base.prototype.msg("document.report.default.title");
                 }
-            }
+                Alfresco.util.populateHTML(
+                    [ p_dialog.id + "-form-container_h", defaultMsg ]
+                );
+
+                var formElement = Dom.get(p_form.formId);
+                if (formElement) {
+                    actionUrl = formElement.action;
+                    if (actionUrl && actionUrl.indexOf("autoSubmit") > 0) {
+                        var data = {};
+                        if (documentRef) { // добавляем к параметрам ID - если задано
+                            data["ID"] = documentRef;
+                        }
+                        p_dialog.dialog.subscribe('show', LogicECM.module.Base.Util.formDestructor, {moduleId: p_dialog.id}, this);
+                        LogicECM.module.Documents.Reports.openReport(actionUrl, data);
+                    } else {
+                        p_dialog.dialog.subscribe('destroy', LogicECM.module.Base.Util.formDestructor, {moduleId: p_dialog.id}, this);
+                    }
+                }
+            };
+
+            var templateUrl = Alfresco.constants.URL_SERVICECONTEXT + "/lecm/components/form/report";
+            var templateRequestParams = {
+                itemKind: "type",
+                itemId: reportCode,
+                formId: "printReportForm",
+                mode: "create",
+                submitType: "json",
+                showCancelButton: true
+            };
+
+            var printReportForm = new Alfresco.module.SimpleDialog(reportCode + "-reportForm");
+
+            printReportForm.setOptions({
+                width: "50em",
+                templateUrl: templateUrl,
+                templateRequestParams: templateRequestParams,
+                destroyOnHide: true,
+                doBeforeDialogShow: {
+                    fn: doBeforeDialogShow,
+                    scope: this
+                },
+                ajaxSubmitMethod: "GET",
+                doBeforeAjaxRequest: {
+                    fn: function (form) {
+                        var renameProperty = function (dataObj, name) {
+                            if (name.indexOf("_removed") > 0 ||
+                                name.indexOf("-selectedItems") > 0 ||
+                                name.indexOf("-autocomplete-input") > 0) { //не может быть удаленных значений, да и они не нужны
+                                delete dataObj[name];
+                            } else if (name.indexOf("_added") > 0) { // что-то выбрано для ассоциаций
+                                var newName = name.replace("_added", "");
+                                dataObj[newName] = dataObj[name];
+                                delete dataObj[name];
+                            }
+                        };
+                        for (var property in form.dataObj) {
+                            if (form.dataObj.hasOwnProperty(property)) {
+                                renameProperty(form.dataObj, property);
+                            }
+                        }
+                        if (documentRef) { // добавляем к параметрам ID - если задано
+                            form.dataObj["ID"] = documentRef;
+                        }
+                        form.method = "GET";
+                        LogicECM.module.Documents.Reports.openReport(actionUrl, form.dataObj);
+                        printReportForm.hide();
+                        return false;
+                    }
+                }
+            });
+            printReportForm.show();
         };
-
-        var templateUrl = Alfresco.constants.URL_SERVICECONTEXT + "/lecm/components/form/report";
-	    var templateRequestParams = {
-            itemKind: "type",
-            itemId: reportCode,
-            formId: "printReportForm",
-            mode: "create",
-		    submitType: "json",
-		    showCancelButton: true
-	    };
-
-        var printReportForm = new Alfresco.module.SimpleDialog(reportCode + "-reportForm");
-
-        printReportForm.setOptions({
-            width: "50em",
-            templateUrl: templateUrl,
-	        templateRequestParams: templateRequestParams,
-            destroyOnHide: true,
-            doBeforeDialogShow: {
-                fn: doBeforeDialogShow,
-                scope: this
-            },
-            ajaxSubmitMethod: "GET",
-            doBeforeAjaxRequest: {
-                fn: function (form) {
-                    var renameProperty = function (dataObj, name) {
-                        if (name.indexOf("_removed") > 0 ||
-                            name.indexOf("-selectedItems") > 0 ||
-                            name.indexOf("-autocomplete-input") > 0) { //не может быть удаленных значений, да и они не нужны
-                            delete dataObj[name];
-                        } else if (name.indexOf("_added") > 0) { // что-то выбрано для ассоциаций
-                            var newName = name.replace("_added", "");
-                            dataObj[newName] = dataObj[name];
-                            delete dataObj[name];
-                        }
-                    };
-                    for (var property in form.dataObj) {
-                        if (form.dataObj.hasOwnProperty(property)) {
-                            renameProperty(form.dataObj, property);
-                        }
-                    }
-                    if (documentRef) { // добавляем к параметрам ID - если задано
-                        form.dataObj["ID"] = documentRef;
-                    }
-                    form.method = "GET";
-                    var paramsStr = Alfresco.util.Ajax.jsonToParamString(form.dataObj, true);
-                    window.open(actionUrl + (actionUrl.indexOf("?") == -1 ? "?" : "&") + paramsStr, "report", "toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no");
-                    printReportForm.hide();
-                    return false;
-                }
-            }
-        });
-        printReportForm.show();
-    };
 })();
--- lecm-jasperreports-forms/jar/src/main/java/ru/it/lecm/reports/forms/ReportMainProducer.java
+++ lecm-jasperreports-forms/jar/src/main/java/ru/it/lecm/reports/forms/ReportMainProducer.java
@@ -1,7 +1,10 @@
 package ru.it.lecm.reports.forms;
 
-import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.util.PropertyCheck;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.extensions.webscripts.AbstractWebScript;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
@@ -12,8 +15,8 @@ import ru.it.lecm.reports.utils.Utils;
 import javax.mail.internet.MimeUtility;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -33,6 +36,8 @@ public class ReportMainProducer extends AbstractWebScript {
 
     final static String PARAM_EXEC = "exec";
     final static String CONTENT_TEXT_HTML_CHARSET_UTF_8 = "text/html;charset=UTF-8";
+    private final static String JSON_PARAMETERS_KEY = "json_form_parameters";
+    private static final transient Logger logger = LoggerFactory.getLogger(ReportMainProducer.class);
 
     private ReportsManager reportsManager;
 
@@ -54,9 +59,28 @@ public class ReportMainProducer extends AbstractWebScript {
      */
     public static Map<String, String> getRequestParameters(WebScriptRequest webScriptRequest) {
         final Map<String, String> result = new HashMap<String, String>();
+        //Get json params
+        String json = webScriptRequest.getParameter(JSON_PARAMETERS_KEY);
+        if (json != null) {
+            try {
+                JSONObject jsonParameters = new JSONObject(json);
+                Iterator<String> keys = jsonParameters.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    String value = jsonParameters.getString(key);
+                    result.put(key, value);
+                }
+            } catch (JSONException e) {
+                logger.error("Error while parsing JSON parameters", e);
+            }
+        }
+
+        //Get query parameters
         for (String paramName : webScriptRequest.getParameterNames()) {
-            String value = webScriptRequest.getParameter(paramName);
-            result.put(paramName, value);
+            if (!JSON_PARAMETERS_KEY.equals(paramName)) {
+                String value = webScriptRequest.getParameter(paramName);
+                result.put(paramName, value);
+            }
         }
         return result;
     }
@@ -66,17 +90,17 @@ public class ReportMainProducer extends AbstractWebScript {
         PropertyCheck.mandatory(this, "reportsManager", getReportsManager());
         PropertyCheck.mandatory(this, "reportGenerators", getReportsManager().getReportGenerators());
 
+        final Map<String, String> requestParameters = getRequestParameters(webScriptRequest);
         final Map<String, String> templateParams = webScriptRequest.getServiceMatch().getTemplateVars();
         final String reportName = Utils.coalesce(templateParams.get("report"), templateParams.get("reportCode"));
-        final String templateCode = webScriptRequest.getParameter("templateCode");
-        final Map<String, String> requestParameters = getRequestParameters(webScriptRequest);
+        final String templateCode = requestParameters.get("templateCode");
 
         final ReportFileData result = getReportsManager().generateReport(reportName, templateCode, requestParameters);
 
         if (result != null) {
-	        webScriptResponse.setContentType(result.getMimeType());
-	        webScriptResponse.setContentEncoding(result.getEncoding());
-	        webScriptResponse.addHeader("Content-Disposition", "filename=\"" + MimeUtility.encodeWord(result.getFilename(), "utf-8", "Q") + "\"");
+            webScriptResponse.setContentType(result.getMimeType());
+            webScriptResponse.setContentEncoding(result.getEncoding());
+            webScriptResponse.addHeader("Content-Disposition", "filename=\"" + MimeUtility.encodeWord(result.getFilename(), "utf-8", "Q") + "\"");
 
             if (result.getData() != null) {
                 final OutputStream out = webScriptResponse.getOutputStream();
