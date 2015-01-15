1239c993dd25c8b9dada214a6e442700dda78dd1
Всеволод Левин <VLevin@it.ru>
2015-01-15 05:31:04 +0000

    ALF-3816 Контрольно-диагностическая утилита не компилируется.
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@11614 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-diagnostic-utility/src/main/java/ru/it/lecm/platform/DiagnosticUtility.java
+++ lecm-diagnostic-utility/src/main/java/ru/it/lecm/platform/DiagnosticUtility.java
@@ -10,7 +10,7 @@ import org.apache.commons.httpclient.params.HttpConnectionParams;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.management.ManagementFactory;
+import java.lang.management.ManagementFactory;
 
 import javax.net.ssl.SSLHandshakeException;
 import java.io.*;
