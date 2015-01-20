2b87d0f0013d153c74bf1647c1dc7a97c3337e83
Малыгин Владимир <VMalygin@it.ru>
2015-01-20 07:19:36 +0000

    избавил InternalCreateAnswerDocumentPolicy от зависимости на deprecated.signing
    для того чтобы изгнать остатки deprecated.signing с работающих стендов или с локальной сборки надо сделать следующее
    1) проверить наличие моделей lecm-signing-model.xml (namespace http://www.it.ru/logicECM/model/signing/wokflow/1.0) и lecm-signing-result-model.xml (namespace http://www.it.ru/logicECM/model/signing/workflow/result/1.0)
       если их нет, то загрузить их в репозиторий, в папку Data Dictionary/Models
    2) авторизоваться на странице /alfresco/faces/jsp/login.jsp и перейти на страницу /alfresco/faces/jsp/login.jsp
    3) выполнить команду show definitions all
    4) в полученном списке сделать поиск по строке "name: activiti$lecmSign ,"
    5) для каждого найденного элемента выполнить команду undeploy definition activiti$lecmSign:x:y, где x и y это номера конкретной версии workflow definition
    6) если элементов слишком много, то можно воспользоваться скриптом
       var applicationContext = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
       var workflowService = applicationContext.getBean('WorkflowService');
       var i, definitions = [
          //список workflowDefitions которые надо раздеплоить
       ];
       for (i in definitions) {
          try {
             workflowService.undeployDefinition(definitions[i]);
             logger.log(definitions[i] + ' successfully undeployed');
          } catch(e) {
             if (e.rhinoException) {
                logger.log(e.rhinoException.getMessage());
             }
             if (e.javaException) {
                logger.log(e.javaException.getMessage());
             }
          }
       }
    
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@11688 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-internal/repo/src/main/java/ru/it/lecm/internal/policy/InternalCreateAnswerDocumentPolicy.java
+++ lecm-internal/repo/src/main/java/ru/it/lecm/internal/policy/InternalCreateAnswerDocumentPolicy.java
@@ -15,79 +15,135 @@ import ru.it.lecm.documents.beans.DocumentMembersService;
 import ru.it.lecm.documents.beans.DocumentService;
 import ru.it.lecm.internal.api.InternalService;
 import ru.it.lecm.workflow.api.WorkflowResultModel;
-import ru.it.lecm.workflow.signing.api.deprecated.SigningWorkflowService;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import ru.it.lecm.base.beans.WriteTransactionNeededException;
+import ru.it.lecm.workflow.signing.api.SigningAspectsModel;
 
 /**
- * User: dbashmakov
- * Date: 11.03.14
- * Time: 16:02
+ * User: dbashmakov Date: 11.03.14 Time: 16:02
  */
-public class InternalCreateAnswerDocumentPolicy implements NodeServicePolicies.OnCreateAssociationPolicy {
-    private static final Logger logger = LoggerFactory.getLogger(InternalCreateAnswerDocumentPolicy.class);
+public class InternalCreateAnswerDocumentPolicy implements ApplicationContextAware, NodeServicePolicies.OnCreateAssociationPolicy {
 
-    private PolicyComponent policyComponent;
-    private NodeService nodeService;
-    private DocumentMembersService documentMembersService;
-    private SigningWorkflowService signingWorkflowService;
+	private static final Logger logger = LoggerFactory.getLogger(InternalCreateAnswerDocumentPolicy.class);
 
-    final public void init() {
-        PropertyCheck.mandatory(this, "policyComponent", policyComponent);
+	private ApplicationContext context;
+	private PolicyComponent policyComponent;
+	private NodeService nodeService;
+	private DocumentMembersService documentMembersService;
+	@Deprecated
+	private Object deprecatedSigningWorkflowService;
+	@Deprecated
+	private Method getOrCreateSigningFolderContainer;
 
-        policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME,
-                InternalService.TYPE_INTERNAL, DocumentService.ASSOC_RESPONSE_TO, new JavaBehaviour(this, "onCreateAssociation"));
-    }
+	final public void init() {
+		initDeprecatedSigningWorkflowService();
 
-    @Override
-    public void onCreateAssociation(AssociationRef nodeAssocRef) {
-        NodeRef targetRef = nodeAssocRef.getTargetRef();
-        NodeRef internalRef = nodeAssocRef.getSourceRef();
+		PropertyCheck.mandatory(this, "policyComponent", policyComponent);
+		policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME, InternalService.TYPE_INTERNAL, DocumentService.ASSOC_RESPONSE_TO, new JavaBehaviour(this, "onCreateAssociation"));
+	}
 
-        NodeRef signingContainer = signingWorkflowService.getOrCreateSigningFolderContainer(targetRef);
-        List<ChildAssociationRef> childAssocs = nodeService.getChildAssocs(signingContainer);
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.context = applicationContext;
+	}
 
-        HashSet<NodeRef> members = new HashSet<NodeRef>();
-        if (!childAssocs.isEmpty()) {
-            for (ChildAssociationRef list : childAssocs) {
-                List<ChildAssociationRef> users = nodeService.getChildAssocs(list.getChildRef());
-                for (ChildAssociationRef user : users) {
-                    List<AssociationRef> employees = nodeService.getTargetAssocs(user.getChildRef(), WorkflowResultModel.ASSOC_WORKFLOW_RESULT_ITEM_EMPLOYEE);
-                    for (AssociationRef employee : employees) {
-                        members.add(employee.getTargetRef());
-                    }
-                }
+	@Deprecated
+	private void initDeprecatedSigningWorkflowService() {
+		try {
+			deprecatedSigningWorkflowService = context.getBean("deprecated.signingWorkflowService");
+			if (deprecatedSigningWorkflowService != null) {
+				try {
+					Class<?> deprecatedSigningWorkflowServiceClass = deprecatedSigningWorkflowService.getClass();
+					getOrCreateSigningFolderContainer = deprecatedSigningWorkflowServiceClass.getDeclaredMethod("getOrCreateSigningFolderContainer", NodeRef.class);
+				} catch (NoSuchMethodException | SecurityException ex) {
+					logger.warn("Can't obtain deprecated method 'getOrCreateSigningFolderContainer' from bean 'deprecated.signingWorkflowService'. Caused by: {}", ex.getMessage());
+					getOrCreateSigningFolderContainer = null;
+				}
+			}
+		} catch (BeansException ex) {
+			logger.warn("Can't obtain bean named 'deprecated.signingWorkflowService' from applicationContext. Caused by: {}", ex.getMessage());
+			deprecatedSigningWorkflowService = null;
+		}
+	}
 
-            }
-        }
+	@Deprecated
+	private HashSet<NodeRef> getSignersUsingDepractedBean(final NodeRef targetRef) {
 
-        for (NodeRef member : members) {
-            try {
-                //транзакция должна быть: policy onCreateAssociation
-                documentMembersService.addMemberWithoutCheckPermission(internalRef, member, new HashMap<QName, Serializable>());
-            } catch (WriteTransactionNeededException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-    }
+		HashSet<NodeRef> members;
+		try {
+			Object result = getOrCreateSigningFolderContainer.invoke(deprecatedSigningWorkflowService, targetRef);
+			NodeRef signingContainer = (NodeRef) result;
+			List<ChildAssociationRef> childAssocs = nodeService.getChildAssocs(signingContainer);
 
-    public void setPolicyComponent(PolicyComponent policyComponent) {
-        this.policyComponent = policyComponent;
-    }
+			members = new HashSet<>();
+			for (ChildAssociationRef list : childAssocs) {
+				List<ChildAssociationRef> users = nodeService.getChildAssocs(list.getChildRef());
+				for (ChildAssociationRef user : users) {
+					List<AssociationRef> employees = nodeService.getTargetAssocs(user.getChildRef(), WorkflowResultModel.ASSOC_WORKFLOW_RESULT_ITEM_EMPLOYEE);
+					for (AssociationRef employee : employees) {
+						members.add(employee.getTargetRef());
+					}
+				}
+			}
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+			logger.error(ex.getMessage());
+			logger.debug(ex.getMessage(), ex);
+			members = new HashSet<>();
+		}
+		return members;
+	}
 
-    public void setNodeService(NodeService nodeService) {
-        this.nodeService = nodeService;
-    }
+	private HashSet<NodeRef> getSigners(final NodeRef targetRef) {
+		HashSet<NodeRef> members = new HashSet<>();
+		if (nodeService.hasAspect(targetRef, SigningAspectsModel.ASPECT_SIGNING_DETAILS)) {
+			List<AssociationRef> assocs = nodeService.getTargetAssocs(targetRef, SigningAspectsModel.ASSOC_SIGNER_EMPLOYEE_ASSOC);
+			for (AssociationRef assoc : assocs) {
+				members.add(assoc.getTargetRef());
+			}
+		}
+		return members;
+	}
 
-    public void setDocumentMembersService(DocumentMembersService documentMembersService) {
-        this.documentMembersService = documentMembersService;
-    }
+	@Override
+	public void onCreateAssociation(AssociationRef nodeAssocRef) {
+		NodeRef targetRef = nodeAssocRef.getTargetRef();
+		NodeRef internalRef = nodeAssocRef.getSourceRef();
 
-    public void setSigningWorkflowService(SigningWorkflowService signingWorkflowService) {
-        this.signingWorkflowService = signingWorkflowService;
-    }
+		HashSet<NodeRef> members;
+		if (deprecatedSigningWorkflowService != null && getOrCreateSigningFolderContainer != null) {
+			members = getSignersUsingDepractedBean(targetRef);
+		} else {
+			members = getSigners(targetRef);
+		}
+
+		for (NodeRef member : members) {
+			try {
+				//транзакция должна быть: policy onCreateAssociation
+				documentMembersService.addMemberWithoutCheckPermission(internalRef, member, new HashMap<QName, Serializable>());
+			} catch (WriteTransactionNeededException ex) {
+				throw new RuntimeException(ex);
+			}
+		}
+	}
+
+	public void setPolicyComponent(PolicyComponent policyComponent) {
+		this.policyComponent = policyComponent;
+	}
+
+	public void setNodeService(NodeService nodeService) {
+		this.nodeService = nodeService;
+	}
+
+	public void setDocumentMembersService(DocumentMembersService documentMembersService) {
+		this.documentMembersService = documentMembersService;
+	}
 }
