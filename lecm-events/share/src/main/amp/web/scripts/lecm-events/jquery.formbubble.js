aa2e807ac6acbdca3173d89b79cdcf874e448306
Мельников Петр <PMelnikov@it.ru>
2015-04-23 12:58:28 +0000

    ALF-3995. Дополнительные мероприятия за день
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@12849 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-events/share/src/main/amp/web/scripts/lecm-events/jquery.formbubble.js
+++ lecm-events/share/src/main/amp/web/scripts/lecm-events/jquery.formbubble.js
@@ -18,7 +18,7 @@
 (function($) {
     $.fn.formBubble = function(params) {
         var self = arguments.callee;
-        
+
         self.p = $.extend({
             alignment: {
                 bubble: 'right',
@@ -41,8 +41,8 @@
             content: '',
             dataType: 'none',
             graphics: {
-              close: true,
-              pointer: true
+                close: true,
+                pointer: true
             },
             offset: {
                 x: 13,
@@ -59,16 +59,16 @@
             self.open(this);
         });
     };
-    
+
     $.extend($.fn.formBubble, {
         align: function(bubbleObject, bubbleTarget, alignment){
             var p = this.p;
-            
+
             bubbleObject = $(bubbleObject);
             bubbleTarget = $(bubbleTarget);
-            
+
             if (!$.fn.formBubble.bubbleObject.parents('body').length) return false; //no bubble exists in page yet
-            
+
             var position = bubbleTarget.offset(),
                 top = position.top,
                 left = position.left,
@@ -77,16 +77,16 @@
                 offset = bubbleObject.data('offset') || p.offset,
                 hOffset = offset.x,
                 vOffset = offset.y;
-            
+
             if (!alignment) alignment = bubbleObject.data('alignment');
-            
+
             if (alignment.bubble == 'top'){
                 hOffset = hOffset + bubbleObject.outerWidth()/-4;
                 vOffset = vOffset + bubbleObject.outerHeight();
             }else if (alignment.bubble === 'right'){
                 hOffset = hOffset + bubbleTarget.outerWidth();
                 bubbleObject[0].style.right = '';
-                 bubbleObject.find('.form-bubble-pointer')
+                bubbleObject.find('.form-bubble-pointer')
                     .removeClass('form-bubble-pointer-top-right')
                     .addClass('form-bubble-pointer-top-left');
             }else if (alignment.bubble == 'left'){
@@ -96,15 +96,15 @@
                     .removeClass('form-bubble-pointer-top-left')
                     .addClass('form-bubble-pointer-top-right');
             }
-            
+
             top = top - vOffset;
-            left = left + hOffset;            
+            left = left + hOffset;
 
             if ($.browser.msie && parseInt($.browser.version) <= 7) $.fn.formBubble.browser = 'lte ie7';
             else if (bubbleObject.css("display") != "none") bubbleObject.stop().fadeTo(0, 1);
 
             positionCSS = (right) ? {'right' : right, 'top' : top} : {'left' : left, 'top' : top};
-            
+
             if (p.animation.slide && bubbleObject.css("display") != "none") bubbleObject.stop().animate(positionCSS, p.animation.speed);
             else bubbleObject.css(positionCSS);
 
@@ -118,16 +118,16 @@
             var align = 'right',
                 rightSpace = $(window).width() - ($(bubbleTarget).offset().left + $(bubbleTarget).width()),
                 leftSpace = $(bubbleTarget).offset().left;
-    
+
             if (leftSpace > rightSpace) align = 'left';
-    
+
             $.fn.formBubble.align($.fn.formBubble.bubbleObject, bubbleTarget, {
                 bubble: align
             });
         },
         ajax: function(){
             var p = this.p;
-            
+
             $.ajax({
                 beforeSend: function() { $.fn.formBubble.beforeSend(); },
                 cache: p.cache,
@@ -145,7 +145,7 @@
         },
         bindings: function(bubbleObject){
             var p = this.p;
-            
+
             bubbleObject //close button click and hover state
                 .find('.form-bubble-close')
                 .hover(function(){
@@ -157,25 +157,31 @@
                 .click(function(){
                     $.fn.formBubble.close(bubbleObject);
                 });
-            
+
+            bubbleObject.mouseout(function(ev) {
+                if (bubbleObject.find(ev.target).length == 0) {
+                    $.fn.formBubble.close(bubbleObject);
+                }
+            });
+
             if (!$.fn.formBubble.isBound){ //ensures document-wide events are only bound once
                 if (p.bindings.realignOnWindowResize){
                     $(window).resize(function(){
-                       $('.form-bubble').each(function(){
+                        $('.form-bubble').each(function(){
                             var bubble = $(this),
                                 target = bubble.data('target');
-                            
+
                             $.fn.formBubble.align(bubble, target);
                             if (p.alignment.bubble === 'auto') $.fn.formBubble.alignAuto(bubble, target);
-                       });
+                        });
                     });
                 }
-                
+
                 $(document).click(function(event){
                     if (event.button === 0 && p.bindings.fadeOnBlur){
                         var len=p.bindings.fadeOnBlurExceptions.length,
                             doClose = false;
-                            
+
                         for (var i=0; i<len; ++i){ //loop through close exceptions, determine if click causes bubble to close
                             if ($(event.target).parents(p.bindings.fadeOnBlurExceptions[i]).length || $(event.target).is(p.bindings.fadeOnBlurExceptions[i])){
                                 doClose = false;
@@ -184,24 +190,24 @@
                                 doClose = true; //set it to true... for now
                             }
                         }
-                        
+
                         if (doClose) $.fn.formBubble.close();
                     }
                 });
             }
-            
+
             $.fn.formBubble.isBound = true;
         },
         browser: '',
         close: function(bubbleObject){
             var p = this.p;
-            
+
             if (!bubbleObject) bubbleObject = '.form-bubble';
             bubbleObject = $(bubbleObject);
 
             function remove(){
                 bubbleObject.remove();
-                p.callbacks.onClose();                
+                p.callbacks.onClose();
             }
 
             if (bubbleObject.is(':visible') && $.fn.formBubble.browser === 'lte ie7') remove();
@@ -215,7 +221,7 @@
         },
         init: function(bubbleTarget){
             var p = this.p;
-            
+
             bubbleTarget = $(bubbleTarget);
 
             var bubbleObject = $('<div class="form-bubble"><div class="form-bubble-content"></div></div>')
@@ -225,17 +231,17 @@
                     alignment: p.alignment,
                     offset: p.offset
                 });
-            
+
             $.fn.formBubble.bubbleObject = bubbleObject;
-            
+
             if (p.unique){
                 $('.form-bubble.unique').remove(); //close ALL other uniques
                 bubbleObject.addClass('unique'); //add class unique to current object
             }
-            
+
             if (p.graphics.close) bubbleObject.prepend('<div class="form-bubble-close"></div>');
             if (p.graphics.pointer) bubbleObject.append('<div class="form-bubble-pointer form-bubble-pointer-' + p.alignment.pointer + '"></div>');
-            
+
             $.fn.formBubble.bindings(bubbleObject);
         },
         open: function(bubbleTarget){
@@ -243,22 +249,22 @@
                 bubbleObject = $($.fn.formBubble.bubbleObject);
 
             if (typeof p.content === 'function') p.content = p.content();
-            
+
             if (p.dataType == 'image'){
                 if (bubbleObject.find('.form-bubble-content img').length === 0){
                     bubbleObject.find('.form-bubble-content').append('<div class="image"><img src="' + p.url + '" /></div>');
                 }
-                
+
                 $.fn.formBubble.beforeSend();
                 bubbleObject.find('.form-bubble-content').append('<div class="image"><img src="' + p.url + '" /></div>');
                 $.fn.formBubble.complete();
             }else if (p.content.length){
                 $.fn.formBubble.content(p.content, bubbleTarget);
             }
-            
+
             //with auto align, contents must be loaded FIRST so we can measure width to calculate whether or not it will fit
             if (bubbleObject.data('alignment').bubble == 'auto') $.fn.formBubble.alignAuto(bubbleObject, bubbleTarget);
-            
+
             if (bubbleObject.css("display") === "none"){
                 bubbleObject.stop().fadeIn(p.animation.speed, function(){
                     p.callbacks.onOpen();
@@ -267,19 +273,19 @@
         },
         success: function(data) {
             var p = this.p;
-            
+
             var dataValue;
-            
+
             if (p.dataType == 'json') dataValue = data.html;
             if (p.dataType == 'html') dataValue = data;
-            
+
             $($.fn.formBubble.bubbleObject).find('.form-bubble-content').append(dataValue);
         },
         content: function(data, bubbleTarget){
             var bubbleObject = $($.fn.formBubble.bubbleObject);
 
             if (data == 'targetText') data = $(bubbleTarget).text();
-            
+
             bubbleObject.find('.form-bubble-content').remove();
             bubbleObject.append('<div class="form-bubble-content">' + data + '</div>');
         }
