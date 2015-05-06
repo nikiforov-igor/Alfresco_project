838a7238699947187adb7915f81d65c8416532e9
Ивкин Алексей <AIvkin@it.ru>
2015-05-06 12:02:02 +0000

    ALF-3947: Страница создания мероприятия - редирект со страницы document-create на страницу event-create
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@12930 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-documents/share/src/main/amp/config/alfresco/templates/ru/it/lecm/documents/document-create.js
+++ lecm-documents/share/src/main/amp/config/alfresco/templates/ru/it/lecm/documents/document-create.js
@@ -1,5 +1,14 @@
 function main() {
-
+	if (page.url.args.documentType == "lecm-events:document") {
+		var response;
+		var requestContext = context.getRequestContext();
+		if (requestContext.isPassiveMode()) {
+			response = requestContext.getResponse().getResponse();
+		} else {
+			response = requestContext.getResponse();
+		}
+		response.sendRedirect(page.url.servletContext + '/event-create?documentType=' + page.url.args.documentType + "&p1=" + page.url.args.p1 + "&p2=" + page.url.args.p2);
+	}
 }
 
 main();
