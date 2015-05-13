26076cf2c108f5f06fd0f576ab14cd438060986b
Мельников Петр <PMelnikov@it.ru>
2015-05-13 09:20:26 +0000

    ALF-4120. Атрибуты Дата завершения поручения, Автор, Дата исполнения и Отчет об исполнении редактируемы в статусе поручения Черновик
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@12986 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-statemachine/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/statemachine/state.fields.get.js
+++ lecm-statemachine/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/statemachine/state.fields.get.js
@@ -7,12 +7,13 @@ var helper = ctx.getBean("stateMachineHelper");
 var stateFields = helper.getStateFields(node.nodeRef);
 var items = [];
 var fields = stateFields.getFields().toArray();
+var isDraft = helper.isDraft(node.nodeRef);
 var writeAllFieldsPermission = lecmPermission.hasPermission(node, "_lecmPerm_WriteAllFields");
 
 for each (var field in fields) {
     items.push({
         name: field.getName(),
-        editable: writeAllFieldsPermission ? true : field.isEditable()
+        editable: writeAllFieldsPermission && !isDraft ? true : field.isEditable()
     });
 }
 
