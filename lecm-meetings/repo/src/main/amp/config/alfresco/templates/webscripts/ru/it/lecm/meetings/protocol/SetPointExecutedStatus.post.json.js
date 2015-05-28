31b48eb859aff49e977b3e2a3fde30064f8d6dbb
Сергей Новиков <SNovikov@it.ru>
2015-05-28 10:22:52 +0000

    ALF-4269 При исполнении пункта с формы "Пункт протокола" в бизнес-журнале не формируется соответствующая запись
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@13224 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-meetings/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/meetings/protocol/SetPointExecutedStatus.post.json.js
+++ lecm-meetings/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/meetings/protocol/SetPointExecutedStatus.post.json.js
@@ -6,6 +6,15 @@ function main() {
     node = search.findNode(pointRef);
 	node.properties["lecm-protocol-ts:execution-date-real"] = new Date();
 	node.save();
+	
+	//получим протокол
+	var protocol = node.parent.parent.parent;
+	
+	var pointNumber = node.properties["lecm-document:indexTableRow"];
+	var wrapPoint = documentScript.wrapperLink(node, "Пункт №" + pointNumber);
+	var logText = wrapPoint + " документа #mainobject перешел в статус Исполнен";
+	businessJournal.log(protocol.nodeRef.toString(), "PROTOCOL_POINT_STATUS_CHANGE", logText, [node.nodeRef.toString()]);
+	
     var ctx = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
     model.item = Evaluator.run(node, [], null, ctx);
 }
