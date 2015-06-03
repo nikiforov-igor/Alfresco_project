ac4b119b92747fff8013add20b1b96cd5de5504a
Ивкин Алексей <AIvkin@it.ru>
2015-06-03 08:16:29 +0000

    ALF-3965: Дашлет "Согласование"
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@13294 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-workflow/lecm-approval/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/workflow/approval/info/getApprovalInfoForDocument.get.js
+++ lecm-workflow/lecm-approval/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/workflow/approval/info/getApprovalInfoForDocument.get.js
@@ -6,50 +6,52 @@
 	var stages = [];
 	if (documentNode != null) {
 		var currentIterationNode = routesService.getDocumentCurrentIteration(documentNode);
-		var children = currentIterationNode.getChildren();
-		if (children != null) {
-			var ctx = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
-			var dictionaryService = ctx.getBean("dictionaryService");
-			var namespaceService = ctx.getBean("namespaceService");
+		if (currentIterationNode != null) {
+			var children = currentIterationNode.getChildren();
+			if (children != null) {
+				var ctx = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
+				var dictionaryService = ctx.getBean("dictionaryService");
+				var namespaceService = ctx.getBean("namespaceService");
 
-			var decisionPropDefinition = getFieldDefinition("lecmApproveAspects:approvalDecision", dictionaryService, namespaceService);
+				var decisionPropDefinition = getFieldDefinition("lecmApproveAspects:approvalDecision", dictionaryService, namespaceService);
 
-			for (var i = 0; i < children.length; i++) {
-				var stage = children[i];
-				if (stage.typeShort == "lecmWorkflowRoutes:stage" && !stage.hasAspect("sys:temporary") && !stage.hasAspect("lecm-workflow:temp")) {
-					var items = [];
-					var stageChildren = stage.getChildren();
-					if (stageChildren != null) {
-						for (var j = 0; j < stageChildren.length; j++) {
-							var item = stageChildren[j];
-							if (item.typeShort == "lecmWorkflowRoutes:stageItem") {
-								var employeeName = null;
-								var employeeAssoc = item.associations["lecmWorkflowRoutes:stageItemEmployeeAssoc"];
-								if (employeeAssoc != null && employeeAssoc.length > 0) {
-									employeeName = employeeAssoc[0].properties["lecm-orgstr:employee-short-name"];
+				for (var i = 0; i < children.length; i++) {
+					var stage = children[i];
+					if (stage.typeShort == "lecmWorkflowRoutes:stage" && !stage.hasAspect("sys:temporary") && !stage.hasAspect("lecm-workflow:temp")) {
+						var items = [];
+						var stageChildren = stage.getChildren();
+						if (stageChildren != null) {
+							for (var j = 0; j < stageChildren.length; j++) {
+								var item = stageChildren[j];
+								if (item.typeShort == "lecmWorkflowRoutes:stageItem") {
+									var employeeName = null;
+									var employeeAssoc = item.associations["lecmWorkflowRoutes:stageItemEmployeeAssoc"];
+									if (employeeAssoc != null && employeeAssoc.length > 0) {
+										employeeName = employeeAssoc[0].properties["lecm-orgstr:employee-short-name"];
+									}
+
+									items.push({
+										employee: employeeName,
+										dueDate: item.properties["lecmWorkflowRoutes:stageItemDueDate"],
+										state: Evaluator.translateField(decisionPropDefinition, item.properties["lecmApproveAspects:approvalDecision"], dictionaryService)
+									});
 								}
-
-								items.push({
-									employee: employeeName,
-									dueDate: item.properties["lecmWorkflowRoutes:stageItemDueDate"],
-									state: Evaluator.translateField(decisionPropDefinition, item.properties["lecmApproveAspects:approvalDecision"], dictionaryService)
-								});
 							}
 						}
-					}
-					stages.push({
-						node: {
-							nodeRef: stage.nodeRef.toString(),
-							title: stage.properties["cm:title"],
-							type: stage.properties["lecmWorkflowRoutes:stageWorkflowType"],
-							decision: {
-								value: stage.properties["lecmApproveAspects:approvalDecision"],
-								displayValue: Evaluator.translateField(decisionPropDefinition, stage.properties["lecmApproveAspects:approvalDecision"], dictionaryService)
+						stages.push({
+							node: {
+								nodeRef: stage.nodeRef.toString(),
+								title: stage.properties["cm:title"],
+								type: stage.properties["lecmWorkflowRoutes:stageWorkflowType"],
+								decision: {
+									value: stage.properties["lecmApproveAspects:approvalDecision"],
+									displayValue: Evaluator.translateField(decisionPropDefinition, stage.properties["lecmApproveAspects:approvalDecision"], dictionaryService)
+								},
+								term: stage.properties["lecmWorkflowRoutes:stageWorkflowTerm"]
 							},
-							term: stage.properties["lecmWorkflowRoutes:stageWorkflowTerm"]
-						},
-						items: items
-					});
+							items: items
+						});
+					}
 				}
 			}
 		}
