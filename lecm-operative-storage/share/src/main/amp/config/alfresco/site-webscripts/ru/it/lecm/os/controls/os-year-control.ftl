fb55ed746db1853a60d8ab87cce8df5404576c8c
Халиков Искандер <IKhalikov@it.ru>
2015-06-18 17:26:10 +0000

    [ALF-4505, ALF-4416] - Валидатор разделён на две части (простая валидация года и на уникальность)
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@13573 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-operative-storage/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/os/controls/os-year-control.ftl
+++ lecm-operative-storage/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/os/controls/os-year-control.ftl
@@ -17,8 +17,13 @@
 			var control = new LogicECM.module.OS.YearSectionControl("${fieldHtmlId}");
 			control.setOptions({
 				<#if field.value?? && field.value?is_number>
-				currentValue: '${field.value?c}'
+				currentValue: '${field.value?c}',
 				</#if>
+				<#if field.control.params.orgFieldId??>
+				orgFieldId: '${field.control.params.orgFieldId?replace(":", "_")}',
+				</#if>
+				yearFieldId: '${field.id}',
+				formId: '${args.htmlid}'
 			});
 
 			control.registerValidator();
--- lecm-operative-storage/share/src/main/amp/web/scripts/lecm-os/controls/year-section-control.js
+++ lecm-operative-storage/share/src/main/amp/web/scripts/lecm-os/controls/year-section-control.js
@@ -21,7 +21,10 @@ LogicECM.module.OS = LogicECM.module.OS || {};
 	YAHOO.extend(LogicECM.module.OS.YearSectionControl, Alfresco.component.Base, {
 
 		options: {
-			currentValue: null
+			currentValue: null,
+			yearFieldId: null,
+			orgFieldId: null,
+			formId: null,
 		},
 
 		controlId: null,
@@ -32,23 +35,32 @@ LogicECM.module.OS = LogicECM.module.OS || {};
 				if(LogicECM.Nomenclature.isCentralized) {
 					return 'Неверное значение';
 				}
-				var orgValue = this.getFormData()['assoc_lecm-os_nomenclature-organization-assoc_added'];
-				if(!orgValue) {
-					return 'Необходимо выбрать организацию';
-				}
 
 				return 'Выбранный год уже существует';
 			}
 
 			function validationHandler(field, args, event, form, silent, message) {
-				var valid = false;
-				var orgValue = form.getFormData()['assoc_lecm-os_nomenclature-organization-assoc_added'];
 
 				if(this.options.currentValue && field.value == this.options.currentValue) {
 					return true;
 				}
 
-				if(field.value.length != 4 || (!orgValue && !LogicECM.Nomenclature.isCentralized)) {
+				return field.value.length == 4;
+			}
+
+			function validateUniqness(field, args, event, form, silent, message) {
+				var valid = false;
+				var orgValue = form.getFormData()[this.options.orgFieldId];
+
+				if(this.options.currentValue && field.value == this.options.currentValue) {
+					return true;
+				}
+
+				if(!orgValue && !LogicECM.Nomenclature.isCentralized) {
+					return true; //ОЧЕНЬ неправильно, но за заполненность поля отвечает другой валидатор
+				}
+
+				if(field.value.length != 4 || isNaN(field.value)) {
 					return false;
 				}
 
@@ -67,13 +79,28 @@ LogicECM.module.OS = LogicECM.module.OS || {};
 				return valid;
 			}
 
-
 			YAHOO.Bubbling.fire('registerValidationHandler', {
-				message: messageHandler,
-				fieldId: this.controlId,
+				message: 'Неверное значение',
+				fieldId: this.options.formId + '_' + this.options.yearFieldId,
 				handler: validationHandler.bind(this),
 				when: 'keyup'
 			});
+
+			YAHOO.Bubbling.fire('registerValidationHandler', {
+				message: 'Выбранный год уже существует',
+				fieldId: this.options.formId + '_' + this.options.yearFieldId,
+				handler: validateUniqness.bind(this),
+				when: 'keyup'
+			});
+
+			if(!LogicECM.Nomenclature.isCentralized) {
+				YAHOO.Bubbling.fire('registerValidationHandler', {
+				message: 'Необходимо выбрать раздел',
+				fieldId: this.options.formId + '_' + this.options.orgFieldId,
+				handler: Alfresco.forms.validation.mandatory,
+				when: 'keyup'
+			});
+			}
 		}
 
 	});
