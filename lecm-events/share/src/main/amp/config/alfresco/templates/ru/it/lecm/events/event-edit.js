2c1038be9d94751e8845f2e42a4a119b4d9ebe84
Ивкин Алексей <AIvkin@it.ru>
2015-06-19 07:48:11 +0000

    ALF-4508: Сотрудник, приглашенный на мероприятие, может отредактировать его с помощью формы event-edit
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@13583 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-events/share/src/main/amp/config/alfresco/templates/ru/it/lecm/events/event-edit.js
+++ lecm-events/share/src/main/amp/config/alfresco/templates/ru/it/lecm/events/event-edit.js
@@ -1,13 +1,7 @@
+<import resource="classpath:/alfresco/site-webscripts/ru/it/lecm/documents/utils/permission-utils.js">
+
 function main() {
-	var url = '/lecm/document/api/getPermissions?nodeRef=' + page.url.args.nodeRef;
-	var result = remote.connect("alfresco").get(url);
-	model.hasPermission = false;
-	if (result.status == 200) {
-		var access = eval('(' + result + ')');
-		if (access && (access.writeAccess)) {
-			model.hasPermission = (("" + access.writeAccess) == "true");
-		}
-	}
+	model.hasPermission = hasPermission(page.url.args.nodeRef, PERM_ATTR_EDIT);
 }
 
 main();
