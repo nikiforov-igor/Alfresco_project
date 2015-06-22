538bb2d23b9886b5318bac3e7bded76602903998
Халиков Искандер <IKhalikov@it.ru>
2015-06-22 18:39:15 +0000

    [ALF-4540] - Пункт меню "АРМ Архивариуса" теперь доступен только взору Архивариусов + даже если пользователь окажется достаточно хитрым, чтобы перейти по ссылке, то и в самом АРМе все секции видны только Архивариусу
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@13616 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-operative-storage/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/os/header/share-header.get.js
+++ lecm-operative-storage/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/os/header/share-header.get.js
@@ -3,18 +3,20 @@
 
 (function(){
 
-	//if (hasRole("DA_ARCHIVISTS")) {
-		var operativeStorageWidgets = {
-			id: "NOMENCLATURE_MENU_ITEM",
-			name: "alfresco/header/AlfMenuItem",
-			config: {
-				id: "NOMENCLATURE_MENU_ITEM",
-				label: msg.get("lecm.os.lbl.archivist.workplace"),
-				targetUrl: "arm?code=archive"
-			}
-		};
-	//}
+	if (!hasRole("DA_ARCHIVISTS")) {
+		return;
+	}
 
+	var operativeStorageWidgets = {
+		id: "NOMENCLATURE_MENU_ITEM",
+		name: "alfresco/header/AlfMenuItem",
+		config: {
+			id: "NOMENCLATURE_MENU_ITEM",
+			label: msg.get("lecm.os.lbl.archivist.workplace"),
+			targetUrl: "arm?code=archive"
+		}
+	};
+	
 	var menuBar = widgetUtils.findObject(model.jsonModel, "id", "LOGIC_ECM_WIDGETS");
 	if (menuBar != null) {
 		menuBar.config.widgets.push(operativeStorageWidgets);
