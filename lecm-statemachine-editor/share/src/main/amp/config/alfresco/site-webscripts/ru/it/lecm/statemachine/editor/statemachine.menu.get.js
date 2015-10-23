cd3c7386695d853f08d5487951c4194f95c09d3d
Зиновин Александр <AZinovin@it.ru>
2015-10-23 14:08:14 +0000

    ALF-4813 - Возможность редактировать ЖЦ документов не только пользователю admin
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@14911 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-statemachine-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/statemachine/editor/statemachine.menu.get.js
+++ lecm-statemachine-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/statemachine/editor/statemachine.menu.get.js
@@ -1,5 +1,9 @@
 model.hasRole = false;
-
-if (user.id == "admin") {
-    model.hasRole = true;
+// Get the user name of the person to get
+var login = user.id;
+var url = '/lecm/security/api/isAdmin?login=' + encodeURI(login);
+var isAdminResponse = remote.connect("alfresco").get(url);
+if (isAdminResponse.status == 200) {
+    var result = eval('(' + isAdminResponse + ')');
+    model.hasRole = result.isAdmin;
 }
