7b7329be8d25d6497303b63b2ad8e6184f7da162
Кучуркин Дмитрий <DKuchurkin@it.ru>
2016-04-01 14:57:31 +0000

    ALF-5111 При попытке исполнить пункт из карточки пункта появляется сообщение Сбой
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@17539 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-meetings/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/meetings/protocol/CheckPointExecutedStatus.get.js
+++ lecm-meetings/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/meetings/protocol/CheckPointExecutedStatus.get.js
@@ -2,6 +2,16 @@ function main() {
 	var pointRef = args["pointRef"];
 	var isExecuted = protocolService.checkPointExecutedStatus(pointRef);
 	model.isExecuted = isExecuted;
+
+	if ("true" !== isExecuted) {
+		var node = search.findNode(pointRef);
+		var protocol = node.parent.parent.parent;
+		var registrar = protocol.assocs['lecm-document-aspects:registrator-assoc'];
+
+		if (registrar && registrar.length) {
+			model.isExecuted = !orgstructure.getCurrentEmployee().equals(registrar[0]);
+		}
+	}
 }
 
 main();
