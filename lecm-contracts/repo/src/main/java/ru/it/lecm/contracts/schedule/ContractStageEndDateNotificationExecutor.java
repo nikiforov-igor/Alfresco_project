1d42a5495c6e25bde618a39fa54ee16fb57c0eae
Ивкин Алексей <AIvkin@it.ru>
2016-08-25 14:08:29 +0000

    ALF-6009: Рассылка уведомлений о наступающих датах окончания этапов договора
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@19222 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

---
+++ lecm-contracts/repo/src/main/java/ru/it/lecm/contracts/schedule/ContractStageEndDateNotificationExecutor.java
@@ -0,0 +1,98 @@
+package ru.it.lecm.contracts.schedule;
+
+import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
+import ru.it.lecm.documents.beans.DocumentMembersService;
+import ru.it.lecm.documents.beans.DocumentService;
+import ru.it.lecm.documents.beans.DocumentTableService;
+import ru.it.lecm.notifications.beans.NotificationsService;
+import ru.it.lecm.orgstructure.beans.OrgstructureBean;
+
+import java.util.*;
+
+/**
+ * User: AZinovin
+ * Date: 12.05.16
+ * Time: 10:51
+ */
+public class ContractStageEndDateNotificationExecutor extends ActionExecuterAbstractBase {
+
+    private NotificationsService notificationsService;
+    private DocumentTableService documentTableService;
+    private DocumentMembersService documentMembersService;
+    private DocumentService documentService;
+    private OrgstructureBean orgstructureService;
+    private NodeService nodeService;
+    private NamespaceService namespaceService;
+    private String templateCode = "CONTRACT_STAGE_END";
+
+    public void setNodeService(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    public void setNamespaceService(NamespaceService namespaceService) {
+        this.namespaceService = namespaceService;
+    }
+
+    public void setNotificationsService(NotificationsService notificationsService) {
+        this.notificationsService = notificationsService;
+    }
+
+    public void setDocumentTableService(DocumentTableService documentTableService) {
+        this.documentTableService = documentTableService;
+    }
+
+    public void setOrgstructureService(OrgstructureBean orgstructureService) {
+        this.orgstructureService = orgstructureService;
+    }
+
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    public void setTemplateCode(String templateCode) {
+        this.templateCode = templateCode;
+    }
+
+    public void setDocumentMembersService(DocumentMembersService documentMembersService) {
+        this.documentMembersService = documentMembersService;
+    }
+
+    @Override
+    protected void executeImpl(Action action, NodeRef stageRef) {
+        NodeRef contractRef = documentTableService.getDocumentByTableDataRow(stageRef);
+        final String contractStatus = (String) nodeService.getProperty(contractRef, QName.createQName("lecm-statemachine:status", namespaceService));
+        if (!"Действует".equals(contractStatus)) {
+            return; //Рассылать уведомления необходимо только по действующим договорам
+        }
+
+        final Date contractStatusDate = (Date) nodeService.getProperty(contractRef, QName.createQName("lecm-document:status-changed-date", namespaceService));
+        final Date stageEndDate = (Date) nodeService.getProperty(stageRef, QName.createQName("lecm-contract-table-structure:end-date", namespaceService));
+        if ((stageEndDate.getTime() - contractStatusDate.getTime()) < 1000 * 3600 * 24 * notificationsService.getSettingsNDays()) {
+            return; //Если с момента регистрации (возобновления действия) договора и до ближайшей даты завершения этапа по договору осталось менее указанного количества дней, уведомление не рассылается
+        }
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("mainObject", contractRef);
+        config.put("stage", stageRef);
+
+        final NodeRef author = documentService.getDocumentAuthor(contractRef);
+
+        List<NodeRef> recipients = new ArrayList<>();
+        recipients.add(author);
+        recipients.addAll(orgstructureService.getEmployeesByBusinessRole("CONTRACT_EXECUTOR"));
+        recipients.addAll(orgstructureService.getEmployeesByBusinessRole("CONTRACT_CURATOR"));
+
+        notificationsService.sendNotification(AuthenticationUtil.getSystemUserName(), null, recipients, templateCode, config, false);
+    }
+
+    @Override
+    protected void addParameterDefinitions(List<ParameterDefinition> parameterDefinitions) {
+    }
+}
---
+++ lecm-contracts/repo/src/main/java/ru/it/lecm/contracts/schedule/ContractStageEndDateNotificationSchedule.java
@@ -0,0 +1,166 @@
+package ru.it.lecm.contracts.schedule;
+
+import org.alfresco.repo.action.scheduled.AbstractScheduledAction;
+import org.alfresco.repo.action.scheduled.InvalidCronExpression;
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+import org.quartz.CronTrigger;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import ru.it.lecm.documents.beans.DocumentService;
+import ru.it.lecm.notifications.beans.NotificationsService;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * User: AZinovin
+ * Date: 12.05.16
+ * Time: 10:51
+ */
+public class ContractStageEndDateNotificationSchedule extends AbstractScheduledAction {
+
+    public static final QName TYPE_CONTRACT_STAGE = QName.createQName("http://www.it.ru/logicECM/contract/table-structure/1.0", "stage");
+    private String cronExpression = "";
+
+    private String jobName = "";
+    private String jobGroup = "";
+
+    private String triggerName = "";
+    private String triggerGroup = "";
+    private Scheduler scheduler;
+
+    private DocumentService documentService;
+
+    private NotificationsService notificationsService;
+
+    DateFormat DateFormatISO8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+    public ContractStageEndDateNotificationSchedule() {
+        super();
+    }
+
+    public Scheduler getScheduler() {
+        return scheduler;
+    }
+
+    public void setScheduler(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    @Override
+    public Action getAction(NodeRef nodeRef) {
+        return getActionService().createAction("contractStageEndDateNotificationExecutor");
+    }
+
+    @Override
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    @Override
+    public String getJobName() {
+        return jobName;
+    }
+
+    @Override
+    public void setJobGroup(String jobGroup) {
+        this.jobGroup = jobGroup;
+    }
+
+    @Override
+    public String getJobGroup() {
+        return this.jobGroup;
+    }
+
+    @Override
+    public void setTriggerName(String triggerName) {
+        this.triggerName = triggerName;
+    }
+
+    @Override
+    public String getTriggerName() {
+        return this.triggerName;
+    }
+
+    @Override
+    public void setTriggerGroup(String triggerGroup) {
+        this.triggerGroup = triggerGroup;
+    }
+
+    @Override
+    public String getTriggerGroup() {
+        return this.triggerGroup;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        register(getScheduler());
+    }
+
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    public void setNotificationsService(NotificationsService notificationsService) {
+        this.notificationsService = notificationsService;
+    }
+
+    @Override
+    public Trigger getTrigger() {
+        try {
+            CronTrigger trigger = new CronTrigger(getTriggerName(), getTriggerGroup(), cronExpression);
+            trigger.setJobName(getJobName());
+            trigger.setJobGroup(getJobGroup());
+            return trigger;
+        } catch (final ParseException e) {
+            throw new InvalidCronExpression("Invalid cron expression: " + cronExpression);
+        }
+    }
+
+    @Override
+    public List<NodeRef> getNodes() {
+        Date now = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        final String MIN = DateFormatISO8601.format(calendar.getTime());
+
+        calendar.setTime(now);
+        calendar.add(Calendar.DAY_OF_YEAR, notificationsService.getSettingsNDays());
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        final String MAX = DateFormatISO8601.format(calendar.getTime());
+
+        String filters;
+        List<QName> types = new ArrayList<>();
+        List<String> paths = new ArrayList<>();
+
+        types.add(TYPE_CONTRACT_STAGE);
+
+        paths.add(documentService.getDocumentsFolderPath());
+
+        filters = "@lecm\\-contract\\-table\\-structure\\:end\\-date: [\"" + MIN + "\" to \"" + MAX + "\"]  AND NOT lecm\\-contract\\-table\\-structure\\:stage\\-status:\"Закрыт\"";
+
+        return documentService.getDocumentsByFilter(types, paths, null, filters, null);
+    }
+
+}
