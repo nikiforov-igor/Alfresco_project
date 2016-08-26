56549cf269a23f779de73fe4cad0b1d39e0047b1
Кучуркин Дмитрий <DKuchurkin@it.ru>
2016-08-26 13:43:19 +0000

    ALF-6071 На форме создания маршрутов согласования неверно выводится список подразделений
    git-svn-id: http://svn.datateh.ru/alfrescoecm/Business%20Platform/trunk@19239 697f8a5b-bb4b-4f4c-ab90-ff1ad96ece97

--- lecm-orgstructure/repo/src/main/java/ru/it/lecm/orgstructure/beans/OrgstructureBeanImpl.java
+++ lecm-orgstructure/repo/src/main/java/ru/it/lecm/orgstructure/beans/OrgstructureBeanImpl.java
@@ -41,6 +41,7 @@ public class OrgstructureBeanImpl extends BaseBean implements OrgstructureBean {
 	private Repository repositoryHelper;
 	private AuthorityService authorityService;
     private SimpleCache<String, NodeRef> userOrganizationsCache;
+	private NamespaceService namespaceService;
 
     public void setPersonService(PersonService personService) {
 		this.personService = personService;
@@ -58,8 +59,11 @@ public class OrgstructureBeanImpl extends BaseBean implements OrgstructureBean {
         this.userOrganizationsCache = userOrganizationsCache;
     }
 
+	public void setNamespaceService(NamespaceService namespaceService) {
+		this.namespaceService = namespaceService;
+	}
 
-    public void setAuthorityService(AuthorityService authorityService) {
+	public void setAuthorityService(AuthorityService authorityService) {
         this.authorityService = authorityService;
     }
     @Override
@@ -2196,7 +2200,7 @@ public class OrgstructureBeanImpl extends BaseBean implements OrgstructureBean {
 		SearchParameters sp = new SearchParameters();
 		sp.addStore(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
 		sp.setLanguage(SearchService.LANGUAGE_FTS_ALFRESCO);
-		String path = "/app:company_home/cm:Business_x0020_platform/cm:LECM/cm:Сервис_x0020_Структура_x0020_организации_x0020_и_x0020_сотрудников/cm:Организация/cm:Структура//*";
+		String path = nodeService.getPath(getHolding()).toPrefixString(namespaceService) + "/*";
 		String type = "lecm-orgstr:organization-unit";
 		sp.setQuery(String.format("+PATH:\"%s\" AND TYPE:\"%s\" AND @lecm\\-orgstr\\-aspects\\:linked\\-organization\\-assoc\\-ref:\"%s\" AND NOT @lecm\\-dic\\:active:false", path, type, organization.toString()));
 		ResultSet results = null;
