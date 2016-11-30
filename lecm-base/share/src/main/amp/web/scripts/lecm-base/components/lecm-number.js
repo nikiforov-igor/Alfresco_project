7a84352cf66ee452eed18ff1c187d930746ecfac
Малыгин Владимир <VMalygin@it.ru>
2016-11-30 14:49:35 +0500

    ALF-5763 режим readonly для LogicECM.module.Number

--- lecm-base/share/src/main/amp/web/scripts/lecm-base/components/lecm-number.js
+++ lecm-base/share/src/main/amp/web/scripts/lecm-base/components/lecm-number.js
@@ -45,8 +45,9 @@ LogicECM.module = LogicECM.module || {};
 	LogicECM.module.Number = function (htmlId)
 	{
 		LogicECM.module.Number.superclass.constructor.call(this, "LogicECM.module.Number", htmlId);
-		
+
 		this.controlId = htmlId;
+		YAHOO.Bubbling.on("readonlyControl", this.onReadonlyControl, this);
 		YAHOO.Bubbling.on("disableControl", this.onDisableControl, this);
 	    YAHOO.Bubbling.on("enableControl", this.onEnableControl, this);
 		return this;
@@ -55,27 +56,20 @@ LogicECM.module = LogicECM.module || {};
 	YAHOO.extend(LogicECM.module.Number, Alfresco.component.Base,
 			{
 				controlId:null,
+				readonly: false,
 				options: {
 					formId: null,
 					fieldId: null,
                     disabled: false
 				},
-				onDisableControl: function (layer, args) {
+				onReadonlyControl: function (layer, args) {
+					var input, fn;
 					if (this.options.formId === args[1].formId && this.options.fieldId === args[1].fieldId) {
-						var input = Dom.get(this.controlId);
-						if (input !== null) {
-							input.disabled = true;
-							input.value = "";
-						}
-					}
-				},
-				onEnableControl: function (layer, args) {
-					if (this.options.formId === args[1].formId && this.options.fieldId === args[1].fieldId) {
-						if (!this.options.disabled) {
-							var input = Dom.get(this.controlId);
-							if (input !== null) {
-								input.disabled = false;
-							}
+						this.readonly = args[1].readonly;
+						input = Dom.get(this.controlId);
+						if (input) {
+							fn = args[1].readonly ? input.setAttribute : input.removeAttribute;
+							fn.call(input, "readonly", "");
 						}
 					}
 				},
