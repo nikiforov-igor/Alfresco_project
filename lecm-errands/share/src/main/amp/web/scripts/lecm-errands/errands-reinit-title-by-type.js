0677ca7850a58794f1265f38766e3683c5da5d3a
Панюков Антон <APanyukov@it.ru>
2016-12-22 11:07:31 +0300

    ALF-6707 дополнен справочник типы поручений. заполнение полей на форме создание значениями из справочника

--- lecm-errands/share/src/main/amp/web/scripts/lecm-errands/errands-reinit-title-by-type.js
+++ lecm-errands/share/src/main/amp/web/scripts/lecm-errands/errands-reinit-title-by-type.js
@@ -1,5 +1,5 @@
 (function() {
-    var errandsTypesForTitles = [];
+    var errandsTypes = [];
     Alfresco.util.Ajax.jsonGet({
         url: Alfresco.constants.PROXY_URI + "lecm/errands/api/getTypes",
         successCallback: {
@@ -8,7 +8,7 @@
                 if (oResults && oResults.length) {
                     var i;
                     for (i = 0; i < oResults.length; i++) {
-                        errandsTypesForTitles[oResults[i].nodeRef] = oResults[i].defaultTitle;
+                        errandsTypes[oResults[i].nodeRef] = oResults[i];
                     }
                 }
             },
@@ -33,12 +33,46 @@
 		if(nodeRef) {
             var titleElement = Dom.get(obj.formId + "_prop_lecm-errands_title");
             var contentElement = Dom.get(obj.formId + "_prop_lecm-errands_content");
-            if (errandsTypesForTitles[nodeRef]) {
+            var reportRequiredElement = Dom.get(obj.formId + "_prop_lecm-errands_report-required");
+            var limitationDateRadio = Dom.get(obj.formId + "_prop_lecm-errands_limitation-date-radio");
+            if (errandsTypes[nodeRef]) {
                 if (titleElement) {
-                    titleElement.value = errandsTypesForTitles[nodeRef];
+                    titleElement.value = errandsTypes[nodeRef].defaultTitle;
                 }
                 if (contentElement && !contentElement.value) {
-                    contentElement.value = errandsTypesForTitles[nodeRef];
+                    contentElement.value = errandsTypes[nodeRef].defaultTitle;
+                }
+                if (limitationDateRadio) {
+                    var checkedRadioButton = YAHOO.util.Selector.query("input[checked]", limitationDateRadio.parentElement, true);
+                    checkedRadioButton.checked = false;
+                    if (errandsTypes[nodeRef].limitless) {
+                        limitationDateRadio.value = "LIMITLESS";
+                        var limitlessRadioButton = YAHOO.util.Selector.query("input[type=radio][value='LIMITLESS']", limitationDateRadio.parentElement, true);
+                        limitlessRadioButton.checked = true;
+                    } else {
+                        limitationDateRadio.value = "DAYS";
+                        var daysRadioButton = YAHOO.util.Selector.query("input[type=radio][value='DAYS']", limitationDateRadio.parentElement, true);
+                        daysRadioButton.checked = true;
+                    }
+                    YAHOO.Bubbling.fire("changeLimitationDateRadio", {
+                        value: limitationDateRadio.value,
+                        formId: obj.formId,
+                        fieldId: "lecm-errands:limitation-date-radio"
+                    });
+                }
+                if (reportRequiredElement) {
+                    var reportRequiredCheckBox = Dom.get(reportRequiredElement.id + "-entry");
+                    if (errandsTypes[nodeRef]["report-required"]) {
+                        reportRequiredElement.value = true;
+                        reportRequiredCheckBox.checked = true;
+                    } else {
+                        reportRequiredElement.value = false;
+                        reportRequiredCheckBox.checked = false;
+                    }
+                    YAHOO.Bubbling.fire("errandReportRequiredChanged", {
+                        formId: obj.formId,
+                        fieldId: "lecm-errands:report-required"
+                    });
                 }
             }
 
