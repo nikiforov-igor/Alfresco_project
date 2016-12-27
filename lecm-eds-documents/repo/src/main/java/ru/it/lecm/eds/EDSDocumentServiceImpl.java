ea63880d89e7d99cd43ca66c7a14877bfa1f7ea0
Ивкин Алексей <AIvkin@it.ru>
2016-12-27 11:25:55 +0300

    ALFSED-76: 11.16. Завершение работ по резолюции - выделен общий аспект изменения дочернего документа

--- lecm-eds-documents/repo/src/main/java/ru/it/lecm/eds/EDSDocumentServiceImpl.java
+++ lecm-eds-documents/repo/src/main/java/ru/it/lecm/eds/EDSDocumentServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.it.lecm.eds;
 
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.alfresco.service.namespace.QName;
 import ru.it.lecm.base.beans.BaseBean;
 import ru.it.lecm.eds.api.EDSDocumentService;
 
@@ -12,9 +11,20 @@ import ru.it.lecm.eds.api.EDSDocumentService;
  */
 public class EDSDocumentServiceImpl extends BaseBean implements EDSDocumentService {
 
-	@Override
-	public NodeRef getServiceRootFolder() {
-		return null;
-	}
+    @Override
+    public NodeRef getServiceRootFolder() {
+        return null;
+    }
 
+    @Override
+    public void sendChildChangeSignal(NodeRef baseDoc) {
+        Integer currentCount = (Integer) nodeService.getProperty(baseDoc, PROP_CHILD_CHANGE_SIGNAL_COUNT);
+        if (currentCount != null) {
+            currentCount++;
+        } else {
+            currentCount = 1;
+        }
+
+        nodeService.setProperty(baseDoc, PROP_CHILD_CHANGE_SIGNAL_COUNT, currentCount);
+    }
 }
