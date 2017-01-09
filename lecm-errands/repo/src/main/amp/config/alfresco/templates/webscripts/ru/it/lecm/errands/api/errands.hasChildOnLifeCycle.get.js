5f1f2767524cc0fe8143367edae2091d631498a4
Панюков Антон <APanyukov@it.ru>
2017-01-09 15:46:54 +0300

    ALFSED-118 правкa

--- lecm-errands/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/errands/api/errands.hasChildOnLifeCycle.get.js
+++ lecm-errands/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/errands/api/errands.hasChildOnLifeCycle.get.js
@@ -5,16 +5,11 @@ var childrenErrands = errands.getChildErrands(errand.nodeRef.toString());
 var childrenResolutions = errands.getChildResolutions(errand.nodeRef.toString());
 
 var hasChildOnLifeCycle = false;
-if (childrenErrands || childrenResolutions) {
-    if (childrenErrands) {
-        children = childrenErrands;
-    }
-    if (childrenResolutions) {
-        children = childrenResolutions;
-    }
-    if (childrenErrands && childrenResolutions) {
-        children = childrenErrands.concat(childrenResolutions);
-    }
+if (childrenErrands) {
+    children = children.concat(childrenErrands);
+}
+if (childrenResolutions) {
+    children = children.concat(childrenResolutions);
 }
 if (children && children.length) {
     hasChildOnLifeCycle = children.some(function (child) {
