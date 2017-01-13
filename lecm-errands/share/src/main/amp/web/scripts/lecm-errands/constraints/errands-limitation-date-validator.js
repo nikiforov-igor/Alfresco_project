03014094eef04d8f83779eec52876f287e500cea
Пальм Александр <apalmkurdenkov@it.ru>
2017-01-13 18:31:32 +0300

    ALF-6908 Дочернее поручение может иметь значение атрибута "Срок исполнения" большее чем у поручения-основания при заполнении вручную

---
+++ lecm-errands/share/src/main/amp/web/scripts/lecm-errands/constraints/errands-limitation-date-validator.js
@@ -0,0 +1,29 @@
+if (typeof LogicECM == "undefined" || !LogicECM) {
+    LogicECM = {};
+}
+
+LogicECM.module = LogicECM.module || {};
+LogicECM.module.Errands = LogicECM.module.Errands || {};
+
+LogicECM.module.Errands.limitationDateValidation =
+    function (field, args,  event, form, silent, message) {
+        var getDate = function(argString) {
+            var dateString = argString ? argString.split('T')[0] : '';
+            return new Date(dateString);
+        };
+
+        if (field.value && field.value.length > 0) {
+            var foundDatePickers = Alfresco.util.ComponentManager.find({id : this.fieldId + '-cntrl'});
+            if (foundDatePickers && foundDatePickers.length == 1) {
+                var datePicker = foundDatePickers[0];
+                if (datePicker.options && datePicker.options.maxLimit && datePicker.options.maxLimit != '') {
+                    var curSelectedDate = getDate(field.value);
+                    var maxLimitDate = getDate(datePicker.options.maxLimit);
+                    if (curSelectedDate && maxLimitDate) {
+                        return (curSelectedDate > maxLimitDate) ? false : true;
+                    }
+                }
+            }
+        }
+        return true;
+    };
