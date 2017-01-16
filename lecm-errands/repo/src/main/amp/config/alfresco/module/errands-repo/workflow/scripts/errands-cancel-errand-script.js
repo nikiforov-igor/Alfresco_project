066c704f035f5a62fb7d10cc7a356a87fc79ccb0
Панюков Антон <APanyukov@it.ru>
2017-01-16 13:16:40 +0300

    ALFSED-147 - представление для списка, изменение limitation-date-radio на date при вычислении даты. ALFSED-116 - отмена поручения ALFSED-117 - обработка запроса на отмену поручения

---
+++ lecm-errands/repo/src/main/amp/config/alfresco/module/errands-repo/workflow/scripts/errands-cancel-errand-script.js
@@ -0,0 +1,44 @@
+function processCancelErrand(cancelChildren, reason) {
+    if (cancelChildren) {
+        var childrenErrands = errands.getChildErrands(document.nodeRef.toString());
+        var childrenResolutions = errands.getChildResolutions(document.nodeRef.toString());
+        childrenErrands.forEach(function (childErrand) {
+            if (!statemachine.isFinal(childErrand.nodeRef.toString()) && !statemachine.isDraft(childErrand)) {
+                childErrand.properties["lecm-errands:cancellation-signal"] = true;
+            }
+        });
+        childrenResolutions.forEach(function (childResolution) {
+            if (!statemachine.isFinal(childErrand.nodeRef.toString()) && !statemachine.isDraft(childErrand)) {
+                childResolution.properties["lecm-errands:annul-signal"] = true;
+                childResolution.properties["lecm-errands:annul-signal-reason"] = reason;
+            }
+        });
+    }
+    var soExecutors = document.assocs["lecm-errands:coexecutors-assoc"];
+    if (soExecutors != null) {
+        soExecutors.forEach(function (coexecutor) {
+            recipients.push(coexecutor);
+        });
+    }
+
+    var controllerAssoc = document.assocs["lecm-errands:controller-assoc"];
+    if (controllerAssoc != null && controllerAssoc.length == 1) {
+        recipients.push(controllerAssoc[0]);
+    }
+
+    notifications.sendNotificationFromCurrentUser({
+        recipients: recipients,
+        templateCode: 'ERRANDS_CANCEL',
+        templateConfig: {
+            mainObject: document,
+            eventExecutor: currentUser,
+            comment: reason
+        },
+        dontCheckAccessToObject: true
+    });
+
+    var logText = "#initiator";
+    logText += documentScript.wrapperTitle("отменил", reason);
+    logText += "поручение: #mainobject.";
+    businessJournal.log(document.nodeRef.toString(), "CANCEL_ERRAND", logText, []);
+}
