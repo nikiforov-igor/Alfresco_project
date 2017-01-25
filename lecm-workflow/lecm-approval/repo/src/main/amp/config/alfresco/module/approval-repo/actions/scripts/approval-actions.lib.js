59a6ce78bcbd58eea38cafcc411d755b6e09524f
Пальм Александр <apalmkurdenkov@it.ru>
2017-01-25 14:51:57 +0300

    ALF-7019 Перенос логики ГО согласования, разделение их по модулям СЭД. Очет "История согласования". Убран хардкод documents-repo из файла контекста.

---
+++ lecm-workflow/lecm-approval/repo/src/main/amp/config/alfresco/module/approval-repo/actions/scripts/approval-actions.lib.js
@@ -0,0 +1,45 @@
+function forceApproval() {
+    /* получение nodeRef-ы на текущую итерацию */
+    var currentIteration = routesService.getDocumentCurrentIteration(document);
+
+    if (currentIteration) {
+        var stages = currentIteration.getChildAssocsByType('lecmWorkflowRoutes:stage');
+        for (i = 0, size = stages.length; i < size; ++i) {
+            state = '' + stages[i].properties['lecmApproveAspects:approvalState'];
+            if ('NEW' == state || 'ACTIVE' == state) {
+                var items = stages[i].getChildAssocsByType('lecmWorkflowRoutes:stageItem');
+                var hasItems = (items && items.length);
+                if (hasItems) { /*"Отменен" - только если айтемы, иначе - "Пропущен"*/
+                    stages[i].properties['lecmApproveAspects:approvalComment'] = reason;
+                    stages[i].properties['lecmApproveAspects:hasComment'] = (reason !== null && reason.length > 0);
+                    stages[i].save();
+                }
+            }
+        }
+
+        statemachine.terminateWorkflowsByDefinition(document, 'lecmApprovementWorkflow', 'forcedDecision', 'APPROVED_FORCED');
+    }
+}
+
+function rejectApproval() {
+    /* получение nodeRef-ы на текущую итерацию */
+    var currentIteration = routesService.getDocumentCurrentIteration(document);
+
+    if (currentIteration) {
+        var stages = currentIteration.getChildAssocsByType('lecmWorkflowRoutes:stage');
+        for (i = 0, size = stages.length; i < size; ++i) {
+            state = '' + stages[i].properties['lecmApproveAspects:approvalState'];
+            if ('NEW' == state || 'ACTIVE' == state) {
+                var items = stages[i].getChildAssocsByType('lecmWorkflowRoutes:stageItem');
+                var hasItems = (items && items.length);
+                if (hasItems) { /*"Отменен" - только если айтемы, иначе - "Пропущен"*/
+                    stages[i].properties['lecmApproveAspects:approvalComment'] = reason;
+                    stages[i].properties['lecmApproveAspects:hasComment'] = (reason !== null && reason.length > 0);
+                    stages[i].save();
+                }
+            }
+        }
+
+        statemachine.terminateWorkflowsByDefinition(document, 'lecmApprovementWorkflow', 'forcedDecision', 'REJECTED_FORCED');
+    }
+}
