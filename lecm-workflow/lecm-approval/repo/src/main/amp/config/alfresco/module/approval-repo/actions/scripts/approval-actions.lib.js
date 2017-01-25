c806ddb32f215445d4440bcede4190ac95d91c21
Пальм Александр <apalmkurdenkov@it.ru>
2017-01-25 16:02:45 +0300

    ALF-7019 Исправления по замечаниям.

--- lecm-workflow/lecm-approval/repo/src/main/amp/config/alfresco/module/approval-repo/actions/scripts/approval-actions.lib.js
+++ lecm-workflow/lecm-approval/repo/src/main/amp/config/alfresco/module/approval-repo/actions/scripts/approval-actions.lib.js
@@ -1,45 +1,47 @@
-function forceApproval(document, reason) {
-    /* получение nodeRef-ы на текущую итерацию */
-    var currentIteration = routesService.getDocumentCurrentIteration(document);
+var LECMApprovalActions = {
+    forceApproval: function forceApproval(document, reason) {
+        /* получение nodeRef-ы на текущую итерацию */
+        var currentIteration = routesService.getDocumentCurrentIteration(document);
 
-    if (currentIteration) {
-        var stages = currentIteration.getChildAssocsByType('lecmWorkflowRoutes:stage');
-        for (i = 0, size = stages.length; i < size; ++i) {
-            state = '' + stages[i].properties['lecmApproveAspects:approvalState'];
-            if ('NEW' == state || 'ACTIVE' == state) {
-                var items = stages[i].getChildAssocsByType('lecmWorkflowRoutes:stageItem');
-                var hasItems = (items && items.length);
-                if (hasItems) { /*"Отменен" - только если айтемы, иначе - "Пропущен"*/
-                    stages[i].properties['lecmApproveAspects:approvalComment'] = reason;
-                    stages[i].properties['lecmApproveAspects:hasComment'] = (reason !== null && reason.length > 0);
-                    stages[i].save();
+        if (currentIteration) {
+            var stages = currentIteration.getChildAssocsByType('lecmWorkflowRoutes:stage');
+            for (i = 0, size = stages.length; i < size; ++i) {
+                state = '' + stages[i].properties['lecmApproveAspects:approvalState'];
+                if ('NEW' == state || 'ACTIVE' == state) {
+                    var items = stages[i].getChildAssocsByType('lecmWorkflowRoutes:stageItem');
+                    var hasItems = (items && items.length);
+                    if (hasItems) { /*"Отменен" - только если айтемы, иначе - "Пропущен"*/
+                        stages[i].properties['lecmApproveAspects:approvalComment'] = reason;
+                        stages[i].properties['lecmApproveAspects:hasComment'] = (reason !== null && reason.length > 0);
+                        stages[i].save();
+                    }
                 }
             }
+
+            statemachine.terminateWorkflowsByDefinition(document, 'lecmApprovementWorkflow', 'forcedDecision', 'APPROVED_FORCED');
         }
+    },
 
-        statemachine.terminateWorkflowsByDefinition(document, 'lecmApprovementWorkflow', 'forcedDecision', 'APPROVED_FORCED');
-    }
-}
+    rejectApproval: function rejectApproval(document, reason) {
+        /* получение nodeRef-ы на текущую итерацию */
+        var currentIteration = routesService.getDocumentCurrentIteration(document);
 
-function rejectApproval(document, reason) {
-    /* получение nodeRef-ы на текущую итерацию */
-    var currentIteration = routesService.getDocumentCurrentIteration(document);
-
-    if (currentIteration) {
-        var stages = currentIteration.getChildAssocsByType('lecmWorkflowRoutes:stage');
-        for (i = 0, size = stages.length; i < size; ++i) {
-            state = '' + stages[i].properties['lecmApproveAspects:approvalState'];
-            if ('NEW' == state || 'ACTIVE' == state) {
-                var items = stages[i].getChildAssocsByType('lecmWorkflowRoutes:stageItem');
-                var hasItems = (items && items.length);
-                if (hasItems) { /*"Отменен" - только если айтемы, иначе - "Пропущен"*/
-                    stages[i].properties['lecmApproveAspects:approvalComment'] = reason;
-                    stages[i].properties['lecmApproveAspects:hasComment'] = (reason !== null && reason.length > 0);
-                    stages[i].save();
+        if (currentIteration) {
+            var stages = currentIteration.getChildAssocsByType('lecmWorkflowRoutes:stage');
+            for (i = 0, size = stages.length; i < size; ++i) {
+                state = '' + stages[i].properties['lecmApproveAspects:approvalState'];
+                if ('NEW' == state || 'ACTIVE' == state) {
+                    var items = stages[i].getChildAssocsByType('lecmWorkflowRoutes:stageItem');
+                    var hasItems = (items && items.length);
+                    if (hasItems) { /*"Отменен" - только если айтемы, иначе - "Пропущен"*/
+                        stages[i].properties['lecmApproveAspects:approvalComment'] = reason;
+                        stages[i].properties['lecmApproveAspects:hasComment'] = (reason !== null && reason.length > 0);
+                        stages[i].save();
+                    }
                 }
             }
-        }
 
-        statemachine.terminateWorkflowsByDefinition(document, 'lecmApprovementWorkflow', 'forcedDecision', 'REJECTED_FORCED');
+            statemachine.terminateWorkflowsByDefinition(document, 'lecmApprovementWorkflow', 'forcedDecision', 'REJECTED_FORCED');
+        }
     }
-}
+};
