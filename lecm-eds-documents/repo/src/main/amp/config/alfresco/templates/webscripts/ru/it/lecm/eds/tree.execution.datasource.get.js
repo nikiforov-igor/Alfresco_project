92dc886db347207ae09c9aaf14c5754495f52655
Ивкин Алексей <AIvkin@it.ru>
2017-01-26 16:18:55 +0300

    ALFSED-179: 12.14. Обработка сигнала об изменении срока исполнения резолюции - сортирвока. отображение документов, к которым нет доступа

--- lecm-eds-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/eds/tree.execution.datasource.get.js
+++ lecm-eds-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/eds/tree.execution.datasource.get.js
@@ -3,34 +3,18 @@ var viewLinksMode = '' + edsGlobalSettings.getLinksViewMode();
 var isMlSupported = lecmMessages.isMlSupported();
 
 var documentNodeRef = args['documentNodeRef'];
-
-/*filtering*/
-var isFirstLayer = 'true' == args['isFirstLayer'];
 var substituteTitle = isMlSupported ? "{lecm-document:ml-ext-present-string}" : "{lecm-document:ext-present-string}";
 
 var items = [];
 var item = search.findNode(documentNodeRef);
-if (isFirstLayer) {
-    var firstItem = evaluateItem(item, substituteTitle);
-    if (firstItem.numberOfChildElements > 0) {
-        items.push(firstItem);
-    }
-} else {
-    var children = [];
-    var childrenErrands = errands.getChildErrands(item.nodeRef.toString());
-    var childrenResolutions = errands.getChildResolutions(item.nodeRef.toString());
-    if (childrenErrands) {
-        children = children.concat(childrenErrands);
-    }
-    if (childrenResolutions) {
-        children = children.concat(childrenResolutions);
-    }
-    children.forEach(function (child) {
-        if (lecmPermission.hasReadAccess(child) || 'VIEW_NO' !== viewLinksMode) {
-            items.push(evaluateItem(child, substituteTitle));
-        }
-    });
-}
+
+var children = getChildren(item);
+children.sort(function(a, b) {
+    return (a.properties["cm:created"] < b.properties["cm:created"]) ? -1 : (a.properties["cm:created"] > b.properties["cm:created"]) ? 1 : 0;
+});
+children.forEach(function (child) {
+    items.push(evaluateItem(child, substituteTitle));
+});
 
 model.items = items;
 model.documentRef = documentNodeRef;
@@ -45,17 +29,39 @@ function evaluateItem(document, substituteTitle) {
         itemObj.status = document.properties["lecm-statemachine:status"];
         itemObj.hasAccess = lecmPermission.hasReadAccess(document);
 
-        var childrenErrands = errands.getChildErrands(document.nodeRef.toString());
-        var childrenResolutions = errands.getChildResolutions(document.nodeRef.toString());
+        var children = getChildren(document);
+        var numberOfChildren = children === null ? 0 : children.length;
 
-        var numberOfChildErrands = childrenErrands === null ? 0 : childrenErrands.length;
-        var numberOfChildResolutions = childrenResolutions === null ? 0 : childrenResolutions.length;
-
-        itemObj.numberOfChildErrands = numberOfChildErrands + numberOfChildResolutions;
-        itemObj.numberOfChildElements = numberOfChildErrands + numberOfChildResolutions;
+        itemObj.numberOfChildErrands = numberOfChildren;
+        itemObj.numberOfChildElements = numberOfChildren;
 
         return itemObj;
     }
     return null;
+}
+
+function getChildren(item) {
+    var result = [];
+    var children = [];
+
+    lecmPermission.pushAuthentication();
+    lecmPermission.setRunAsUserSystem();
+    var childrenErrands = errands.getChildErrands(item.nodeRef.toString());
+    var childrenResolutions = errands.getChildResolutions(item.nodeRef.toString());
+    lecmPermission.popAuthentication();
+
+    if (childrenErrands) {
+        children = children.concat(childrenErrands);
+    }
+    if (childrenResolutions) {
+        children = children.concat(childrenResolutions);
+    }
+    children.forEach(function (child) {
+        if (lecmPermission.hasReadAccess(child) || 'VIEW_NO' !== viewLinksMode) {
+            result.push(child);
+        }
+    });
+
+    return result;
 }
 
