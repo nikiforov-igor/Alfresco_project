873292e85d127f44a1245eb84d2ae4f09582a32a
Ивкин Алексей <AIvkin@it.ru>
2017-02-01 15:54:12 +0300

    ALFSED-166: Работа с деревом поручений и резолюций на карточке документа

--- lecm-errands/api/src/main/java/ru/it/lecm/errands/ErrandsService.java
+++ lecm-errands/api/src/main/java/ru/it/lecm/errands/ErrandsService.java
@@ -79,8 +79,6 @@ public interface ErrandsService {
     QName ASSOC_ERRANDS_TS_COEXECUTOR_ATTACHMENT = QName.createQName(ERRANDS_TS_NAMESPACE_URI,"coexecutor-report-attachment-assoc");
     QName ASSOC_ERRANDS_TS_COEXECUTOR_CONNECTED_DOCUMENT = QName.createQName(ERRANDS_TS_NAMESPACE_URI,"coexecutor-report-connected-document-assoc");
 
-    QName TYPE_ERRANDS_TS_EXECUTOR_REPORT = QName.createQName(ERRANDS_TS_NAMESPACE_URI,"execution-reports-table");
-    QName PROP_ERRANDS_TS_EXECUTOR_REPORT_STATUS = QName.createQName(ERRANDS_TS_NAMESPACE_URI,"execution-report-status");
     QName ASSOC_ERRANDS_TS_EXECUTOR_ATTACHMENT = QName.createQName(ERRANDS_TS_NAMESPACE_URI,"execution-report-attachment-assoc");
     QName ASSOC_ERRANDS_TS_EXECUTOR_CONNECTED_DOCUMENT = QName.createQName(ERRANDS_TS_NAMESPACE_URI,"execution-report-connected-document-assoc");
 
