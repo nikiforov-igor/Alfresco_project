c0d4adcf9dcc528deb7611d7118831a94441594e
Панюков Антон <APanyukov@it.ru>
2017-02-07 17:45:03 +0300

    ALFSED-236  Обработка сигнала о необходимости завершения

---
+++ lecm-errands/repo/src/main/amp/config/alfresco/module/errands-repo/workflow/scripts/errands-completion-signal-script.js
@@ -0,0 +1,84 @@
+
+var ErrandsCompletionSignalScript = {
+    processCompletionSignal: function() {
+        var doc = bpm_package.children[0];
+        if (doc) {
+            var signalReason = doc.properties["lecm-eds-aspect:completion-signal-reason"];
+            var closeChild = doc.properties["lecm-eds-aspect:completion-signal-close-child"];
+            if (closeChild) {
+                var childrenErrands = errands.getChildErrands(document.nodeRef.toString());
+                var childrenResolutions = errands.getChildResolutions(document.nodeRef.toString());
+                childrenErrands.forEach(function (childErrand) {
+                    if (!statemachine.isFinal(childErrand.nodeRef.toString()) && !statemachine.isDraft(childErrand)) {
+                        edsDocument.sendCompletionSignal(childErrand, reason, currentUser);
+                    }
+                });
+                childrenResolutions.forEach(function (childResolution) {
+                    if (!statemachine.isFinal(childResolution.nodeRef.toString()) && !statemachine.isDraft(childResolution)) {
+                        edsDocument.sendCompletionSignal(childResolution, reason, currentUser);
+                    }
+                });
+            }
+            var signalSender = null;
+            var signalSenderAssoc = doc.assocs["lecm-eds-aspect:completion-signal-sender-assoc"];
+            if (signalSenderAssoc && signalSenderAssoc.length) {
+                signalSender = signalSenderAssoc[0];
+            }
+            if (signalReason) {
+                if (doc.properties["lecm-errands:execution-report-status"] == "PROJECT") {
+                    doc.properties["lecm-errands:execution-report"] += '<p>' + signalReason + '</p>';
+                } else if (!doc.properties["lecm-errands:execution-report-status"]) {
+                    doc.properties["lecm-errands:execution-report"] = signalReason;
+                }
+            } else {
+                var reportText = "Завершено по инициативе ";
+                reportText += documentScript.wrapperLink(signalSender, signalSender.properties["lecm-orgstr:employee-short-name"]);
+                if (doc.properties["lecm-errands:execution-report-status"] == "PROJECT") {
+                    doc.properties["lecm-errands:execution-report"] += '<p>' + reportText + '</p>';
+                } else if (!doc.properties["lecm-errands:execution-report-status"]) {
+                    doc.properties["lecm-errands:execution-report"] = reportText;
+                }
+            }
+            doc.properties["lecm-errands:execution-report-status"] = "ACCEPT";
+            doc.save();
+
+            var authorAssoc = document.assocs["lecm-errands:initiator-assoc"];
+            if (authorAssoc && authorAssoc.length) {
+                recipients.push(authorAssoc[0]);
+            }
+            var executorAssoc = document.assocs["lecm-errands:executor-assoc"];
+            if (executorAssoc && executorAssoc.length) {
+                recipients.push(executorAssoc[0]);
+            }
+
+            var soExecutors = document.assocs["lecm-errands:coexecutors-assoc"];
+            if (soExecutors) {
+                soExecutors.forEach(function (coexecutor) {
+                    recipients.push(coexecutor);
+                });
+            }
+
+            var controllerAssoc = document.assocs["lecm-errands:controller-assoc"];
+            if (controllerAssoc && controllerAssoc.length == 1) {
+                recipients.push(controllerAssoc[0]);
+            }
+            notifications.sendNotificationFromCurrentUser({
+                recipients: recipients,
+                templateCode: 'ERRANDS_EXECUTED_WITHOUT_REPORT',
+                templateConfig: {
+                    mainObject: document,
+                    eventExecutor: signalSender
+                },
+                dontCheckAccessToObject: true
+            });
+            var logObjects = [];
+            logObjects.push("" + signalSender.nodeRef);
+            var logText = "#object1 ";
+            logText += documentScript.wrapperTitle("завершил", signalReason ? signalReason : "");
+            logText += "исполнение поручения #mainobject.";
+            businessJournal.log(document.nodeRef.toString(), "ERRAND_EXECUTION", logText, logObjects);
+
+            edsDocument.resetCompletionSignal(document);
+        }
+    }
+};
