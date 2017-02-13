21505d6656735ba7dda463f266ce3987eb98d156
Башмаков Денис <DBashmakov@it.ru>
2017-02-13 15:20:34 +0300

    ALF-6631 - При смене в поле "Ответ на" документа, не отрабатывает разрыв связи

--- lecm-documents/repo/src/main/java/ru/it/lecm/documents/policy/DocumentResponseToPolicy.java
+++ lecm-documents/repo/src/main/java/ru/it/lecm/documents/policy/DocumentResponseToPolicy.java
@@ -5,21 +5,27 @@ import org.alfresco.repo.policy.Behaviour;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
 import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.it.lecm.documents.beans.DocumentConnectionService;
 import ru.it.lecm.documents.beans.DocumentService;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * User: AIvkin
  * Date: 15.07.13
  * Time: 10:16
  */
-public class DocumentResponseToPolicy implements NodeServicePolicies.OnCreateAssociationPolicy {
+public class DocumentResponseToPolicy implements NodeServicePolicies.OnCreateAssociationPolicy, NodeServicePolicies.OnDeleteAssociationPolicy {
 	final static protected Logger logger = LoggerFactory.getLogger(DocumentResponseToPolicy.class);
 
 	private PolicyComponent policyComponent;
 	private DocumentConnectionService documentConnectionService;
+	private NodeService nodeService;
 
 	public void setPolicyComponent(PolicyComponent policyComponent) {
 		this.policyComponent = policyComponent;
@@ -29,7 +35,13 @@ public class DocumentResponseToPolicy implements NodeServicePolicies.OnCreateAss
 		this.documentConnectionService = documentConnectionService;
 	}
 
+	public void setNodeService(NodeService nodeService) {
+		this.nodeService = nodeService;
+	}
+
 	final public void init() {
+		policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnDeleteAssociationPolicy.QNAME,
+				DocumentService.TYPE_BASE_DOCUMENT, DocumentService.ASSOC_RESPONSE_TO, new JavaBehaviour(this, "onDeleteAssociation", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
 		policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME,
 				DocumentService.TYPE_BASE_DOCUMENT, DocumentService.ASSOC_RESPONSE_TO, new JavaBehaviour(this, "onCreateAssociation", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
 	}
@@ -40,5 +52,22 @@ public class DocumentResponseToPolicy implements NodeServicePolicies.OnCreateAss
 	@Override
 	public void onCreateAssociation(AssociationRef associationRef) {
 		documentConnectionService.createConnection(associationRef.getSourceRef(), associationRef.getTargetRef(), "inResponseTo", true);
+	}
+
+	@Override
+	public void onDeleteAssociation(AssociationRef nodeAssocRef) {
+		NodeRef parent = nodeAssocRef.getSourceRef();
+		NodeRef connected = nodeAssocRef.getTargetRef();
+
+		List<NodeRef> connections = documentConnectionService.getConnectionsWithDocument(connected, "inResponseTo", false);
+		for (NodeRef connection : connections) {
+			NodeRef targetDoc = nodeService.getTargetAssocs(connection, DocumentConnectionService.ASSOC_PRIMARY_DOCUMENT).get(0).getTargetRef();
+			if (Objects.equals(targetDoc, parent)) {
+				Boolean isSystem = Boolean.TRUE.equals(nodeService.getProperties(connection).get(DocumentConnectionService.PROP_IS_SYSTEM));
+				if (isSystem) {
+					documentConnectionService.deleteConnection(connection);
+				}
+			}
+		}
 	}
 }
