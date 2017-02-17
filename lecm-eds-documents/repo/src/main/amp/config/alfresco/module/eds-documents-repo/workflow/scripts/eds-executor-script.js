f95cececba82493149eff88f0863e6943ff7af46
Панюков Антон <APanyukov@it.ru>
2017-02-17 17:30:30 +0300

    ALFSED-358 правка

--- lecm-eds-documents/repo/src/main/amp/config/alfresco/module/eds-documents-repo/workflow/scripts/eds-executor-script.js
+++ lecm-eds-documents/repo/src/main/amp/config/alfresco/module/eds-documents-repo/workflow/scripts/eds-executor-script.js
@@ -1,23 +1,20 @@
 var EDSExecutorScript = {
-    notificateExecutor: function(){
-        var doc = bpm_package.children[0];
-        if (doc) {
-            var docCompiler = doc.assocs["lecm-document:author-assoc"][0];
-            var docExecutorAssoc = doc.assocs["lecm-eds-document:executor-assoc"];
-            var docExecutor = null;
-            if (docExecutorAssoc && docExecutorAssoc.length) {
-                docExecutor = docExecutorAssoc[0];
-            }
-            if (docExecutor && docCompiler && !docExecutor.nodeRef.equals(docCompiler.nodeRef)) {
-                statemachine.grandDynamicRoleForEmployee(doc, docExecutor, "BR_INITIATOR", task);
-                notifications.sendNotificationFromCurrentUser({
-                    recipients: [docExecutor],
-                    templateCode: 'EDS_EXECUTOR_NEW',
-                    templateConfig: {
-                        mainObject: doc
-                    }
-                });
-            }
+    notificateExecutor: function(doc) {
+        var docCompiler = doc.assocs["lecm-document:author-assoc"][0];
+        var docExecutorAssoc = doc.assocs["lecm-eds-document:executor-assoc"];
+        var docExecutor = null;
+        if (docExecutorAssoc && docExecutorAssoc.length) {
+            docExecutor = docExecutorAssoc[0];
+        }
+        if (docExecutor && docCompiler && !docExecutor.nodeRef.equals(docCompiler.nodeRef)) {
+            statemachine.grandDynamicRoleForEmployee(doc, docExecutor, "BR_INITIATOR", task);
+            notifications.sendNotificationFromCurrentUser({
+                recipients: [docExecutor],
+                templateCode: 'EDS_EXECUTOR_NEW',
+                templateConfig: {
+                    mainObject: doc
+                }
+            });
         }
     }
 };
