2c00ca15ccafafacdc8bfb8761faed1c4f9793a8
Бурлаков Алексей <aburlakov@it.ru>
2017-02-20 19:11:15 +0300

    ALFSED-338-1.8 Фильтр "По документу-основанию"
    Внес исправления

--- lecm-eds-documents/api/src/main/java/ru/it/lecm/eds/policy/EDSBaseDocumentTypePolicy.java
+++ lecm-eds-documents/api/src/main/java/ru/it/lecm/eds/policy/EDSBaseDocumentTypePolicy.java
@@ -11,40 +11,22 @@ import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import ru.it.lecm.documents.beans.DocumentService;
 import ru.it.lecm.eds.api.EDSDocumentService;
+import ru.it.lecm.eds.BaseDocumentTypeBean;
 
 /**
  * Created by ABurlakov on 20.02.2017.
  */
-public class EDSBaseDocumentTypePolicy implements NodeServicePolicies.OnCreateAssociationPolicy,
+public class EDSBaseDocumentTypePolicy extends BaseDocumentTypeBean
+        implements NodeServicePolicies.OnCreateAssociationPolicy,
         NodeServicePolicies.OnDeleteAssociationPolicy {
 
-    private NodeService nodeService;
     private PolicyComponent policyComponent;
-    private NamespaceService namespaceService;
-    private EDSDocumentService edsDocumentService;
-    private DocumentService documentService;
 
     private String type;
     private String assocBaseDocumentType;
 
-    public void setNodeService(NodeService nodeService) {
-        this.nodeService = nodeService;
-    }
-
     public void setPolicyComponent(PolicyComponent policyComponent) {
         this.policyComponent = policyComponent;
-    }
-
-    public void setNamespaceService(NamespaceService namespaceService) {
-        this.namespaceService = namespaceService;
-    }
-
-    public void setEdsDocumentService(EDSDocumentService edsDocumentService) {
-        this.edsDocumentService = edsDocumentService;
-    }
-
-    public void setDocumentService(DocumentService documentService) {
-        this.documentService = documentService;
     }
 
     public void setType(String type) {
@@ -56,35 +38,22 @@ public class EDSBaseDocumentTypePolicy implements NodeServicePolicies.OnCreateAs
     }
 
     final public void init() {
+        QName typeQName = QName.createQName(type, namespaceService);
+        QName assocQName = QName.createQName(assocBaseDocumentType, namespaceService);
         policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME,
-                QName.createQName(type, namespaceService),
-                QName.createQName(assocBaseDocumentType, namespaceService),
-                new JavaBehaviour(this, "onCreateAssociation", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
-
+                typeQName, assocQName, new JavaBehaviour(this, "onCreateAssociation", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
         policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnDeleteAssociationPolicy.QNAME,
-                QName.createQName(type, namespaceService),
-                QName.createQName(assocBaseDocumentType, namespaceService),
-                new JavaBehaviour(this, "onDeleteAssociation", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
+                typeQName, assocQName, new JavaBehaviour(this, "onDeleteAssociation", Behaviour.NotificationFrequency.TRANSACTION_COMMIT));
     }
 
     @Override
     public void onCreateAssociation(AssociationRef associationRef) {
-        setBaseDocumentType(associationRef.getSourceRef(), associationRef.getTargetRef(), true);
+        String docType = (String) nodeService.getProperty(associationRef.getTargetRef(), DocumentService.PROP_DOCUMENT_TYPE);
+        nodeService.setProperty(associationRef.getSourceRef(), EDSDocumentService.PROP_BASE_DOCUMENT_TYPE, docType);
     }
 
     @Override
     public void onDeleteAssociation(AssociationRef associationRef) {
-        setBaseDocumentType(associationRef.getSourceRef(), associationRef.getTargetRef(), false);
-    }
-
-    private void setBaseDocumentType(NodeRef documentRef, NodeRef baseDocumentRef, boolean isCreate) {
-        String docType = new String();
-        if (isCreate) {
-            docType = (String) nodeService.getProperty(baseDocumentRef, DocumentService.PROP_DOCUMENT_TYPE);
-        } else {
-            docType = "Без документа-основания";
-        }
-
-        nodeService.setProperty(documentRef, EDSDocumentService.PROP_BASE_DOCUMENT_TYPE, docType);
+        nodeService.setProperty(associationRef.getSourceRef(), EDSDocumentService.PROP_BASE_DOCUMENT_TYPE, "Без документа-основания");
     }
 }
