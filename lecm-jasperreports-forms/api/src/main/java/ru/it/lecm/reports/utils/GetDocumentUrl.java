f8d15b630904dcc9f0e63eaffbea14a2507cfe05
Башмаков Денис <DBashmakov@it.ru>
2017-02-28 17:14:09 +0300

    ALF-7226 - Возможность формирования ссылок на документы для использования в отчетах (учел констрейнт)

--- lecm-jasperreports-forms/api/src/main/java/ru/it/lecm/reports/utils/GetDocumentUrl.java
+++ lecm-jasperreports-forms/api/src/main/java/ru/it/lecm/reports/utils/GetDocumentUrl.java
@@ -1,7 +1,11 @@
 package ru.it.lecm.reports.utils;
 
 import org.alfresco.repo.admin.SysAdminParams;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.QName;
 import org.alfresco.util.UrlUtil;
+import ru.it.lecm.documents.beans.DocumentService;
 
 /**
  * User: dbashmakov
@@ -10,11 +14,26 @@ import org.alfresco.util.UrlUtil;
  */
 public class GetDocumentUrl {
     private static SysAdminParams sysAdminParams;
+    private static DocumentService documentService;
+    private static NodeService nodeService;
+
+    public void setNodeService(NodeService nodeService) {
+        GetDocumentUrl.nodeService = nodeService;
+    }
 
     public void setSysAdminParams(SysAdminParams sysAdminParams) {
         GetDocumentUrl.sysAdminParams = sysAdminParams;
     }
+
+    public void setDocumentService(DocumentService documentService) {
+        GetDocumentUrl.documentService = documentService;
+    }
+
     public static String getDocumentLink(String nodeRef) {
-        return UrlUtil.getShareUrl(sysAdminParams) + "/page/document?nodeRef=" + nodeRef;
+        QName docType = DocumentService.TYPE_BASE_DOCUMENT;
+        if (NodeRef.isNodeRef(nodeRef)) {
+            docType = nodeService.getType(new NodeRef(nodeRef));
+        }
+        return UrlUtil.getShareUrl(sysAdminParams) + "/page/" + documentService.getViewUrl(docType) + "?nodeRef=" + nodeRef;
     }
 }
