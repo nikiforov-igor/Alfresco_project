361a4535e9e0512687ac5be4ddbbd7911d60423f
Пальм Александр <apalmkurdenkov@it.ru>
2017-03-02 14:43:58 +0300

    ALF-6969 Изменить поведение полей с выбором из двух источников. Убран лишний отступ

--- lecm-base/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/autocomplete.complex.pickerchildren.post.json.js
+++ lecm-base/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/autocomplete.complex.pickerchildren.post.json.js
@@ -1,23 +1,23 @@
 <import resource="classpath:/alfresco/templates/webscripts/ru/it/lecm/pickerchildren.lib.js">
 
-	(function () {
-		var obj = eval('(' + json.toString() + ')'),
-			data = {
-				parent: null,
-				rootNode: null,
-				results: [],
-				additionalProperties: null
-			};
-		if (obj.elementsParams) {
-			obj.elementsParams.forEach(function(elementParams) {
-				data.results = data.results.concat(getPickerChildrenItems('ISNOTNULL:"sys:node-dbid"', null, true, elementParams).results);
-			});
-		} else {
-			data = getPickerChildrenItems('ISNOTNULL:"sys:node-dbid"');
-		}
+(function () {
+	var obj = eval('(' + json.toString() + ')'),
+		data = {
+			parent: null,
+			rootNode: null,
+			results: [],
+			additionalProperties: null
+		};
+	if (obj.elementsParams) {
+		obj.elementsParams.forEach(function(elementParams) {
+			data.results = data.results.concat(getPickerChildrenItems('ISNOTNULL:"sys:node-dbid"', null, true, elementParams).results);
+		});
+	} else {
+		data = getPickerChildrenItems('ISNOTNULL:"sys:node-dbid"');
+	}
 
-		model.parent = data.parent;
-		model.rootNode = data.rootNode;
-		model.results = data.results;
-		model.additionalProperties = data.additionalProperties;
-	})();
+	model.parent = data.parent;
+	model.rootNode = data.rootNode;
+	model.results = data.results;
+	model.additionalProperties = data.additionalProperties;
+})();
