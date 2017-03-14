12e754ae5711aad44328acb13a8a5bc35531ffcf
Логинов Андрей <ALoginov@it.ru>
2017-03-14 12:16:23 +0300

    ALFSED-367: Внес исправление в процессор: обавил логгер, изменил ключ входного параметра, добавил проверку на наличие следующего элемента в масиве. Поправил запрос для фильтра в отчете и изменил порядок сортировки для корректного отображения.

--- lecm-resolutions/repo/src/main/java/ru/it/lecm/resolutions/processors/FilterByResolutionsCloserProcessor.java
+++ lecm-resolutions/repo/src/main/java/ru/it/lecm/resolutions/processors/FilterByResolutionsCloserProcessor.java
@@ -2,44 +2,46 @@ package ru.it.lecm.resolutions.processors;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.it.lecm.base.beans.SearchQueryProcessor;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * Created by ALoginov on 13.03.2017.
  */
 public class FilterByResolutionsCloserProcessor extends SearchQueryProcessor {
+    private final static Logger logger = LoggerFactory.getLogger(FilterByResolutionsCloserProcessor.class);
 
     /*
-     * Usage example: {{FILTER_BY_RESOLUTIONS_CLOSER({signers:[#value]})}}
+     * Usage example: {{FILTER_BY_RESOLUTIONS_CLOSER({closers:[#value]})}}
 	 */
 
     @Override
-    public String getQuery(Map<String, Object> params){
-        String closer="";
+    public String getQuery(Map<String, Object> params) {
+        String closer = "";
         StringBuilder sbQuery = new StringBuilder();
-        JSONArray signers = params != null ? (JSONArray)params.get("signers") : null;
-        if (signers != null) {
-            for (int i=0;i<signers.length();i++) {
+        JSONArray closers = params != null ? (JSONArray) params.get("closers") : null;
+        if (closers != null) {
+            for (int i = 0; i < closers.length(); i++) {
                 try {
-                    closer=(String) signers.get(i);
+                    closer = (String) closers.get(i);
                 } catch (JSONException e) {
-                    e.printStackTrace();
+                    logger.error("JSONException:", e);
                 }
-                sbQuery.append("((@lecm\\-resolutions\\:closers:\"AUTHOR_AND_CONTROLLER\" OR @lecm\\-resolutions\\:closers:\"AUTHOR\") " +
-                        "AND @lecm\\-resolutions\\:author\\-assoc\\-ref:\"");
+                sbQuery.append("((@lecm\\-resolutions\\:closers:\"AUTHOR_AND_CONTROLLER\" OR @lecm\\-resolutions\\:closers:\"AUTHOR\") AND @lecm\\-resolutions\\:author\\-assoc\\-ref:\"");
                 sbQuery.append(closer);
-                sbQuery.append("\") OR ((@lecm\\-resolutions\\:closers:\"AUTHOR_AND_CONTROLLER\" OR @lecm\\-resolutions\\:closers:\"CONTROLLER\") " +
-                        "AND @lecm\\-resolutions\\:controller\\-assoc\\-ref:\"");
+                sbQuery.append("\") OR ((@lecm\\-resolutions\\:closers:\"AUTHOR_AND_CONTROLLER\" OR @lecm\\-resolutions\\:closers:\"CONTROLLER\") AND @lecm\\-resolutions\\:controller\\-assoc\\-ref:\"");
                 sbQuery.append(closer);
-                sbQuery.append("\") OR ");
+                sbQuery.append("\")");
+                if (i != closers.length() - 1) {
+                    sbQuery.append(" OR ");
+                }
             }
         } else {
             sbQuery.append("\"NOT_REF\"");
         }
-        sbQuery.delete(sbQuery.length() - 3, sbQuery.length());
         return sbQuery.toString();
     }
 }
