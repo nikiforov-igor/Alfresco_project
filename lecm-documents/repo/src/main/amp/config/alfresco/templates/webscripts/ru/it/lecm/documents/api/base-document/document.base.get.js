5aaf40c1623d04aa3ef3848abf8ebbad5da02115
Айтжан Бисембаев <abisembaev@it.ru>
2017-03-15 16:46:38 +0500

    ALFFIVE-174  Исправление ошибки при отсутствии документа-основания

--- lecm-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/documents/api/base-document/document.base.get.js
+++ lecm-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/documents/api/base-document/document.base.get.js
@@ -2,13 +2,9 @@ var nodeRef = args['nodeRef'];
 var document = utils.getNodeFromString(nodeRef);
 var baseDocAssocName = args['baseDocAssocName'];
 var baseDocAssoc = document.assocs[baseDocAssocName];
-if (baseDocAssoc) {
-    if (baseDocAssoc && baseDocAssoc.length > 0) {
-        var baseDoc = baseDocAssoc[0];
-        model.baseDocNodeRef = baseDoc.getNodeRef().toString();
-    } else {
-        model.baseDocNodeRef = "";
-    }
+if (baseDocAssoc && baseDocAssoc.length > 0) {
+    var baseDoc = baseDocAssoc[0];
+    model.baseDocNodeRef = baseDoc.getNodeRef().toString();
 } else {
-    logger.error("ERROR: Base document association with name \"" + baseDocAssocName + "\" not found on " + nodeRef);
+    model.baseDocNodeRef = "";
 }
