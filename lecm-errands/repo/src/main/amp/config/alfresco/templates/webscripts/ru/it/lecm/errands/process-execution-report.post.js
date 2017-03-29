00880986af7897c7d8b2f47a6b3183888a9d86e5
Панюков Антон <APanyukov@it.ru>
2017-03-29 13:48:34 +0300

    ALFSED-870 правка

--- lecm-errands/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/errands/process-execution-report.post.js
+++ lecm-errands/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/errands/process-execution-report.post.js
@@ -21,7 +21,7 @@ if (requestContent) {
         allConnectedDocuments = requestContent["assoc_lecm-errands-ts_execution-report-connected-document-assoc"];
         lecmPermission.pushAuthentication();
         lecmPermission.setRunAsUserSystem();
-        if (allConnectedDocuments && !allConnectedDocuments.equals("")) {
+        if (allConnectedDocuments) {
             connectedDocuments = allConnectedDocuments.split(",").map(function (ref) {
                 return search.findNode(ref);
             });
@@ -30,7 +30,7 @@ if (requestContent) {
     }
     if (requestContent.hasOwnProperty("assoc_lecm-errands-ts_execution-report-attachment-assoc")) {
         allAttachments =requestContent["assoc_lecm-errands-ts_execution-report-attachment-assoc"];
-        if (allAttachments && !allAttachments.equals("")) {
+        if (allAttachments) {
             attachments = allAttachments.split(",").map(function (ref) {
                 return search.findNode(ref);
             });
