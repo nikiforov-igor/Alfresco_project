9c9291bd89dab682d51a74e1ba98484b8c7e9e97
Боруроев Павел <PBoruroev@it.ru>
2017-04-05 17:58:06 +0300

    ALFFIVE-204 уменьшил кол-во проверок перед бутстрапом

--- lecm-eds-documents/repo/src/main/java/ru/it/lecm/eds/policy/EDSExecutorPolicy.java
+++ lecm-eds-documents/repo/src/main/java/ru/it/lecm/eds/policy/EDSExecutorPolicy.java
@@ -10,6 +10,7 @@ import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.security.AuthenticationService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.util.PropertyCheck;
+import ru.it.lecm.base.beans.LecmBasePropertiesService;
 import ru.it.lecm.base.policies.LogicECMAssociationPolicy;
 import ru.it.lecm.documents.beans.DocumentService;
 import ru.it.lecm.eds.api.EDSDocumentService;
@@ -36,6 +37,7 @@ public class EDSExecutorPolicy implements NodeServicePolicies.OnCreateAssociatio
     private OrgstructureBean orgstructureService;
     private DocumentService documentService;
     private StateMachineServiceBean stateMachineService;
+    private LecmBasePropertiesService propertiesService;
 
     public DocumentService getDocumentService() {
         return documentService;
@@ -102,6 +104,10 @@ public class EDSExecutorPolicy implements NodeServicePolicies.OnCreateAssociatio
         this.stateMachineService = stateMachineService;
     }
 
+    public void setPropertiesService(LecmBasePropertiesService propertiesService) {
+        this.propertiesService = propertiesService;
+    }
+
     public final void init() {
         PropertyCheck.mandatory(this, "nodeService", nodeService);
         PropertyCheck.mandatory(this, "policyComponent", policyComponent);
@@ -109,6 +115,7 @@ public class EDSExecutorPolicy implements NodeServicePolicies.OnCreateAssociatio
         PropertyCheck.mandatory(this, "stateMachineService", stateMachineService);
         PropertyCheck.mandatory(this, "lecmPermissionService", lecmPermissionService);
         PropertyCheck.mandatory(this, "notificationsService", notificationsService);
+        PropertyCheck.mandatory(this, "propertiesService", propertiesService);
 
         policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateAssociationPolicy.QNAME,
                 EDSDocumentService.TYPE_EDS_DOCUMENT, EDSDocumentService.ASSOC_EXECUTOR, new JavaBehaviour(this, "onCreateAssociation"));
@@ -119,30 +126,55 @@ public class EDSExecutorPolicy implements NodeServicePolicies.OnCreateAssociatio
 
     @Override
     public void onCreateAssociation(AssociationRef associationRef) {
-        NodeRef documentRef = associationRef.getSourceRef();
-        NodeRef docExecutorRef = associationRef.getTargetRef();
-        String author = authenticationService.getCurrentUserName();
-        NodeRef initiator = orgstructureService.getCurrentEmployee();
-        Map<String, Object> templateConfig = new HashMap<>();
-        templateConfig.put("mainObject", documentRef);
-        stateMachineService.grandDynamicRoleForEmployee(documentRef, docExecutorRef, GRAND_DYNAMIC_ROLE_CODE_INITIATOR);
-        if (nodeService.getProperty(documentRef, StatemachineModel.PROP_STATUS) != null) {
-            notificationsService.sendNotification(author, initiator, Collections.singletonList(docExecutorRef), "EDS_EXECUTOR_NEW", templateConfig, true);
+        Object editorEnabled = propertiesService.getProperty("ru.it.lecm.properties.bj.enabled");
+        boolean enabled;
+        if (editorEnabled == null) {
+            enabled = true;
+        } else {
+            enabled = Boolean.valueOf((String) editorEnabled);
+        }
+        if (enabled) {
+            NodeRef documentRef = associationRef.getSourceRef();
+            NodeRef docExecutorRef = associationRef.getTargetRef();
+            String author = authenticationService.getCurrentUserName();
+            NodeRef initiator = orgstructureService.getCurrentEmployee();
+            Map<String, Object> templateConfig = new HashMap<>();
+            templateConfig.put("mainObject", documentRef);
+            stateMachineService.grandDynamicRoleForEmployee(documentRef, docExecutorRef, GRAND_DYNAMIC_ROLE_CODE_INITIATOR);
+            if (nodeService.getProperty(documentRef, StatemachineModel.PROP_STATUS) != null) {
+                notificationsService.sendNotification(author, initiator, Collections.singletonList(docExecutorRef), "EDS_EXECUTOR_NEW", templateConfig, true);
+            }
+
+        }
+        else {
+            throw new RuntimeException("Property ru.it.lecm.properties.bj.enabled not found");
         }
     }
 
     @Override
     public void onDeleteAssociation(AssociationRef associationRef) {
-        NodeRef documentRef = associationRef.getSourceRef();
-        NodeRef docExecutorRef = associationRef.getTargetRef();
-        NodeRef docCompilerRef = documentService.getDocumentAuthor(documentRef);
-        String author = authenticationService.getCurrentUserName();
-        NodeRef initiator = orgstructureService.getCurrentEmployee();
-        Map<String, Object> templateConfig = new HashMap<>();
-        templateConfig.put("mainObject", documentRef);
-        if (!docCompilerRef.equals(docExecutorRef)) {
-            lecmPermissionService.revokeDynamicRole(GRAND_DYNAMIC_ROLE_CODE_INITIATOR, documentRef, docExecutorRef.getId());
+        Object editorEnabled = propertiesService.getProperty("ru.it.lecm.properties.bj.enabled");
+        boolean enabled;
+        if (editorEnabled == null) {
+            enabled = true;
+        } else {
+            enabled = Boolean.valueOf((String) editorEnabled);
         }
-        notificationsService.sendNotification(author, initiator, Collections.singletonList(docExecutorRef), "EDS_EXECUTOR_OLD", templateConfig, true);
+        if (enabled) {
+            NodeRef documentRef = associationRef.getSourceRef();
+            NodeRef docExecutorRef = associationRef.getTargetRef();
+            NodeRef docCompilerRef = documentService.getDocumentAuthor(documentRef);
+            String author = authenticationService.getCurrentUserName();
+            NodeRef initiator = orgstructureService.getCurrentEmployee();
+            Map<String, Object> templateConfig = new HashMap<>();
+            templateConfig.put("mainObject", documentRef);
+            if (!docCompilerRef.equals(docExecutorRef)) {
+                lecmPermissionService.revokeDynamicRole(GRAND_DYNAMIC_ROLE_CODE_INITIATOR, documentRef, docExecutorRef.getId());
+            }
+            notificationsService.sendNotification(author, initiator, Collections.singletonList(docExecutorRef), "EDS_EXECUTOR_OLD", templateConfig, true);
+        }
+        else {
+            throw new RuntimeException("Property ru.it.lecm.properties.bj.enabled not found");
+        }
     }
 }
