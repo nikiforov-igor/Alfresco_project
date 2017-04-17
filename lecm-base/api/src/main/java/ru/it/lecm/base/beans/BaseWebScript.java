54d95e43f33d86cad1dff0b0c317952512f4616e
Панюков Антон <APanyukov@it.ru>
2017-04-17 09:46:54 +0300

    ALFSED-7589 - точечное экранирование недопустимых сиволов для тайтла.

--- lecm-base/api/src/main/java/ru/it/lecm/base/beans/BaseWebScript.java
+++ lecm-base/api/src/main/java/ru/it/lecm/base/beans/BaseWebScript.java
@@ -8,7 +8,6 @@ import org.alfresco.repo.workflow.WorkflowNodeConverter;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.version.Version;
-import org.apache.commons.lang.StringEscapeUtils;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.NativeJavaObject;
 import org.mozilla.javascript.Scriptable;
@@ -145,9 +144,160 @@ public abstract class BaseWebScript extends BaseScopableProcessorExtension {
 	 * @return
 	 */
 	public String wrapperTitle(String text, String title) {
-		return  "<span class=\"wrapper-title\" title=\"" + StringEscapeUtils.escapeHtml(title) + "\">" + text + "</span>";
+		return  "<span class=\"wrapper-title\" title=\"" + escapeHtml(title) + "\">" + text + "</span>";
 	}
 
+	private String escapeHtml(String string){
+		StringBuilder sb = new StringBuilder();
+		int n = string.length();
+		boolean lastWasBlankChar = false;
+		for (int i = 0; i < n; i++) {
+			char c = string.charAt(i);
+			if (c == ' ') {
+				if (lastWasBlankChar) {
+					lastWasBlankChar = false;
+					sb.append("&nbsp;");
+				} else {
+					lastWasBlankChar = true;
+					sb.append(' ');
+				}
+			} else {
+				lastWasBlankChar = false;
+				switch (c) {
+					case '<':
+						sb.append("&lt;");
+						break;
+					case '>':
+						sb.append("&gt;");
+						break;
+					case '&':
+						sb.append("&amp;");
+						break;
+					case '"':
+						sb.append("&quot;");
+						break;
+					case 'à':
+						sb.append("&agrave;");
+						break;
+					case 'À':
+						sb.append("&Agrave;");
+						break;
+					case 'â':
+						sb.append("&acirc;");
+						break;
+					case 'Â':
+						sb.append("&Acirc;");
+						break;
+					case 'ä':
+						sb.append("&auml;");
+						break;
+					case 'Ä':
+						sb.append("&Auml;");
+						break;
+					case 'å':
+						sb.append("&aring;");
+						break;
+					case 'Å':
+						sb.append("&Aring;");
+						break;
+					case 'æ':
+						sb.append("&aelig;");
+						break;
+					case 'Æ':
+						sb.append("&AElig;");
+						break;
+					case 'ç':
+						sb.append("&ccedil;");
+						break;
+					case 'Ç':
+						sb.append("&Ccedil;");
+						break;
+					case 'é':
+						sb.append("&eacute;");
+						break;
+					case 'É':
+						sb.append("&Eacute;");
+						break;
+					case 'è':
+						sb.append("&egrave;");
+						break;
+					case 'È':
+						sb.append("&Egrave;");
+						break;
+					case 'ê':
+						sb.append("&ecirc;");
+						break;
+					case 'Ê':
+						sb.append("&Ecirc;");
+						break;
+					case 'ë':
+						sb.append("&euml;");
+						break;
+					case 'Ë':
+						sb.append("&Euml;");
+						break;
+					case 'ï':
+						sb.append("&iuml;");
+						break;
+					case 'Ï':
+						sb.append("&Iuml;");
+						break;
+					case 'ô':
+						sb.append("&ocirc;");
+						break;
+					case 'Ô':
+						sb.append("&Ocirc;");
+						break;
+					case 'ö':
+						sb.append("&ouml;");
+						break;
+					case 'Ö':
+						sb.append("&Ouml;");
+						break;
+					case 'ø':
+						sb.append("&oslash;");
+						break;
+					case 'Ø':
+						sb.append("&Oslash;");
+						break;
+					case 'ß':
+						sb.append("&szlig;");
+						break;
+					case 'ù':
+						sb.append("&ugrave;");
+						break;
+					case 'Ù':
+						sb.append("&Ugrave;");
+						break;
+					case 'û':
+						sb.append("&ucirc;");
+						break;
+					case 'Û':
+						sb.append("&Ucirc;");
+						break;
+					case 'ü':
+						sb.append("&uuml;");
+						break;
+					case 'Ü':
+						sb.append("&Uuml;");
+						break;
+					case '®':
+						sb.append("&reg;");
+						break;
+					case '©':
+						sb.append("&copy;");
+						break;
+					case '€':
+						sb.append("&euro;");
+						break;
+					default:
+						sb.append(c);
+						break;
+				}
+			}
+		}
+		return sb.toString();
+	}
 	/**
 	 * Получить значение свойства share.context из global.properties
 	 * @return
