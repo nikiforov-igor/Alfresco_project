3c4a2b9c6f45be29927be603faf3f5599960bcb8
Панюков Антон <APanyukov@it.ru>
2017-04-18 17:05:06 +0300

    ALFSED-7593 проверка узлов верхнего уровня

--- lecm-arm/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/arm/arm-accordeons.get.js
+++ lecm-arm/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/arm/arm-accordeons.get.js
@@ -1,48 +1,15 @@
 var value = armWrapper.getAccordeons(args["armRef"]);
 if (url.getMatch().indexOf("forDashlet") != -1) {
 
-    function isTypeOK(node) {
-        return node.typeShort != "lecm-arm:html-node" && node.typeShort != "lecm-arm:reports-node";
-    }
-
-    function concatNodes(childs, armNodes) {
-        if (childs && childs.length) {
-            var nodes = childs.filter(function (child) {
-                return child.typeShort == "lecm-arm:node";
-            });
-            nodes.forEach(function (node) {
-                var isFinalNode = node.children.every(function (item) {
-                    return item.typeShort != "lecm-arm:node";
-                });
-                if (isFinalNode) {
-                    armNodes.push(node);
-                } else {
-                    concatNodes(node.children, armNodes)
-                }
-            });
-        }
-    }
     value = value.filter(function (rootNode) {
-        var rootIsOk = rootNode.name != "Работа руководителя";
+        var rootIsOk = !rootNode.properties["lecm-arm:is-for-secretaries"];
         if (rootIsOk) {
             var children = rootNode.children;
-            var armNodes = [];
             if (children && children.length) {
-                //проверка вложенных узлов
-                concatNodes(children, armNodes);
-                if (armNodes && armNodes.length) {
-                    rootIsOk = armNodes.some(function (node) {
-                            if (node.children && node.children.length) {
-                                return node.children.every(function (child) {
-                                    return isTypeOK(child);
-                                });
-                            } else {
-                                return true;
-                            }
-                        })
-                } else {
-                    rootIsOk = false;
-                }
+                //проверка узлов первого уровня
+                rootIsOk = children.some(function (child) {
+                    return child.typeShort == "lecm-arm:node";
+                });
             }
         }
         return rootIsOk;
