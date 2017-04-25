9373b5412731854cf7b9168ab0fded33140cf7fe
Панюков Антон <APanyukov@it.ru>
2017-04-25 13:37:29 +0300

    ALF-7325 лишнее

--- lecm-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/documents/api/attachments/attachments.categories.get.js
+++ lecm-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/documents/api/attachments/attachments.categories.get.js
@@ -1,22 +1,16 @@
 var documentNodeRef = args['documentNodeRef'];
-var documentType = args['documentType'];
-//model.categories = documentAttachments.getCategories(documentNodeRef);
-var items = [];
-if (documentNodeRef) {
-	var categories = documentAttachments.getCategories(documentNodeRef);
-	if (categories) {
-		for (var i = 0; i < categories.length; i++) {
-			items.push({
-				category: categories[i],
-				isReadOnly: documentAttachments.isReadonlyCategory(categories[i].nodeRef)
-			});
-		}
-	}
-	model.isMlSupported = lecmMessages.isMlSupported();
-	model.kind = "node";
-} else if (documentType) {
-	items = documentAttachments.getCategoriesForType(documentType);
-	model.kind = "type";
-}
 
+//model.categories = documentAttachments.getCategories(documentNodeRef);
+
+var categories = documentAttachments.getCategories(documentNodeRef);
+var items = [];
+if (categories != null) {
+	for (var i = 0; i < categories.length; i++) {
+		items.push({
+			category: categories[i],
+			isReadOnly: documentAttachments.isReadonlyCategory(categories[i].nodeRef)
+		});
+	}
+}
+model.isMlSupported = lecmMessages.isMlSupported();
 model.items = items;
