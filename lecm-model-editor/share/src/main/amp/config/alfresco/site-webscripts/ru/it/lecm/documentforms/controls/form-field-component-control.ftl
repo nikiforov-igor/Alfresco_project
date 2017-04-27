e5d579afffad18d1298818ccc2921a4350a6c08c
Башмаков Денис <DBashmakov@it.ru>
2017-04-27 17:29:34 +0300

    ALF-7615 - ГО. Доработать механизм Данные скрипта.

--- lecm-model-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/documentforms/controls/form-field-component-control.ftl
+++ lecm-model-editor/share/src/main/amp/config/alfresco/site-webscripts/ru/it/lecm/documentforms/controls/form-field-component-control.ftl
@@ -1,64 +1,94 @@
 <#assign disabled = form.mode == "view" || (field.disabled && !(field.control.params.forceEditable?? && field.control.params.forceEditable == "true"))>
 
-<script type="text/javascript">//<![CDATA[
-(function()
-{
-
-	function init() {
-		LogicECM.module.Base.Util.loadScripts([
-			'scripts/lecm-forms-editor/lecm-form-field-component.js'
-		], createControl);
-	}
-
-	function createControl() {
-		var control = new LogicECM.module.FormsEditor.FieldComponent("${fieldHtmlId}").setMessages(${messages});
-		control.setOptions({
-		<#if disabled>
-			disabled: true,
-		</#if>
-
-		<#if field.mandatory??>
-			mandatory: ${field.mandatory?string},
-		<#elseif field.endpointMandatory??>
-			mandatory: ${field.endpointMandatory?string},
-		</#if>
-
-			itemNodeRef: "${form.arguments.itemId!''}",
-		<#if (field.value?? && field.value?length > 0)>
-			value: ${field.value}
-		</#if>
-		});
-	}
-
-	YAHOO.util.Event.onDOMReady(init);
-})();
-//]]></script>
-
-<div class="form-field">
 <#if form.mode == "view">
-	<div class="viewmode-field">
-		<#if field.mandatory && !(field.value?is_number) && field.value == "">
-		<span class="incomplete-warning"><img src="${url.context}/res/components/form/images/warning-16.png" title="${msg("form.field.incomplete")}" /><span>
-		</#if>
-		<span class="viewmode-label">${field.label?html}:</span>
-		<#if field.control.params.activateLinks?? && field.control.params.activateLinks == "true">
-			<#assign fieldValue=field.value?html?replace("((http|ftp|https):\\/\\/[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?\\^=%&:\\/~\\+#]*[\\w\\-\\@?\\^=%&\\/~\\+#])?)", "<a href=\"$1\" target=\"_blank\">$1</a>", "r")>
+    <div id="${fieldHtmlId}-parent" class="control selectone viewmode">
+        <div class="label-div">
+			<#if field.mandatory && !(field.value?is_number) && field.value == "">
+            <span class="incomplete-warning"><img src="${url.context}/res/components/form/images/warning-16.png"
+                                                  title="${msg("form.field.incomplete")}"/><span>
+			</#if>
+            <label>${field.label?html}:</label>
+        </div>
+		<#if fieldValue?string == "">
+			<#assign valueToShow=msg("form.control.novalue")>
 		<#else>
-			<#if field.value?is_number>
-				<#assign fieldValue=field.value?c>
-			<#else>
-				<#assign fieldValue=field.value?html>
+			<#assign valueToShow=fieldValue>
+			<#if field.control.params.options?? && field.control.params.options != "">
+				<#list field.control.params.options?split(optionSeparator) as nameValue>
+					<#if nameValue?index_of(labelSeparator) == -1>
+						<#if nameValue == fieldValue?string || (fieldValue?is_number && fieldValue?c == nameValue)>
+							<#assign valueToShow=nameValue>
+							<#break>
+						</#if>
+					<#else>
+						<#assign choice=nameValue?split(labelSeparator)>
+						<#if choice[0] == fieldValue?string || (fieldValue?is_number && fieldValue?c == choice[0])>
+							<#assign valueToShow=msgValue(choice[1])>
+							<#break>
+						</#if>
+					</#if>
+				</#list>
 			</#if>
 		</#if>
-		<span class="viewmode-value"><#if fieldValue == "">${msg("form.control.novalue")}<#else>${fieldValue}</#if></span>
-	</div>
+        <div class="container">
+            <div class="value-div">
+			${valueToShow?html}
+            </div>
+        </div>
+    </div>
 <#else>
-	<label for="${fieldHtmlId}">${field.label?html}:<#if field.mandatory><span class="mandatory-indicator">${msg("form.required.fields.marker")}</span></#if></label>
-	<input id="${fieldHtmlId}" type="hidden" name="${field.name}" <#if disabled>disabled="true"</#if>/>
-	<select id="${fieldHtmlId}-select" <#if disabled>disabled="true"</#if>>
-		<option>${msg("label.control.default")}</option>
-	</select>
-	<table class="formFieldControlParamsTable"><tbody id="${fieldHtmlId}-params"></tbody></table>
-	<div id="${fieldHtmlId}-hidden-params"></div>
+    <div id="${fieldHtmlId}-parent" class="control selectone editmode">
+        <div class="label-div">
+            <label for="${fieldHtmlId}">${field.label?html}:
+				<#if field.mandatory>
+                    <span class="mandatory-indicator">${msg("form.required.fields.marker")}</span>
+				</#if>
+            </label>
+        </div>
+        <div class="container">
+            <div class="buttons-div"><@formLib.renderFieldHelp field=field /></div>
+            <div class="value-div">
+                <input id="${fieldHtmlId}" type="hidden" name="${field.name}" <#if disabled>disabled="true"</#if>/>
+                <select id="${fieldHtmlId}-select" <#if disabled>disabled="true"</#if>>
+                    <option>${msg("label.control.default")}</option>
+                </select>
+            </div>
+            <table class="formFieldControlParamsTable"><tbody id="${fieldHtmlId}-params"></tbody></table>
+            <div id="${fieldHtmlId}-hidden-params"></div>
+        </div>
+    </div>
 </#if>
-</div>
+<div class="clear"></div>
+
+<script type="text/javascript">//<![CDATA[
+(function () {
+    function init() {
+        LogicECM.module.Base.Util.loadScripts([
+            'scripts/lecm-forms-editor/lecm-form-field-component.js'
+        ], createControl);
+    }
+
+    function createControl() {
+        var control = new LogicECM.module.FormsEditor.FieldComponent("${fieldHtmlId}").setMessages(${messages});
+        control.setOptions({
+		<#if disabled>
+            disabled: true,
+		</#if>
+        defaultOption: "${msg("label.control.default")}",
+		<#if field.mandatory??>
+            mandatory: ${field.mandatory?string},
+		<#elseif field.endpointMandatory??>
+            mandatory: ${field.endpointMandatory?string},
+		</#if>
+			fromForm: ${(field.control.params.fromForm!"true")?string},
+            updateOnAction: "${field.control.params.updateOnAction!""}",
+            itemNodeRef: "${form.arguments.itemId!''}",
+		<#if (field.value?? && field.value?length > 0)>
+            value: ${field.value}
+		</#if>
+        });
+    }
+
+    YAHOO.util.Event.onDOMReady(init);
+})();
+//]]></script>
--- lecm-model-editor/share/src/main/amp/web/scripts/lecm-forms-editor/lecm-form-field-component.js
+++ lecm-model-editor/share/src/main/amp/web/scripts/lecm-forms-editor/lecm-form-field-component.js
@@ -21,9 +21,8 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 (function() {
 	var Dom = YAHOO.util.Dom;
 
-	LogicECM.module.FormsEditor.FieldComponent = function (fieldHtmlId)
-	{
-		LogicECM.module.FormsEditor.FieldComponent.superclass.constructor.call(this, "LogicECM.module.FormsEditor.FieldComponent", fieldHtmlId, [ "container"]);
+	LogicECM.module.FormsEditor.FieldComponent = function (fieldHtmlId) {
+		LogicECM.module.FormsEditor.FieldComponent.superclass.constructor.call(this, "LogicECM.module.FormsEditor.FieldComponent", fieldHtmlId, ["container"]);
 		this.formIds = [];
 		return this;
 	};
@@ -33,7 +32,10 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 			disabled: false,
 			mandatory: false,
 			itemNodeRef: null,
-			value: null
+			value: null,
+			fromForm: true,
+			updateOnAction: null,
+			defaultOption: null
 		},
 
 		targetType: null,
@@ -41,8 +43,21 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 
 		onReady: function() {
 			YAHOO.util.Event.on(this.id + "-select", "change", this.onSelectChange, this, true);
+			if (this.options.fromForm && this.options.itemNodeRef) {
+				this.getTargetType();
+			} else {
+				if (this.options.updateOnAction) {
+					YAHOO.Bubbling.unsubscribe(this.options.updateOnAction, this.getTargetTypeFromEvent, this);
+					YAHOO.Bubbling.on(this.options.updateOnAction, this.getTargetTypeFromEvent, this);
+				}
+			}
+		},
 
-			this.getTargetType();
+		getTargetTypeFromEvent: function(layer, args) {
+			var type = args[1].selectedItem;
+			if (type) {
+				this.loadConfig(type);
+			}
 		},
 
 		getTargetType: function() {
@@ -52,8 +67,7 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 					fn: function (response) {
 						var oResults = response.json;
 						if (oResults && oResults.targetType) {
-							this.targetType = oResults.targetType;
-							this.loadConfig()
+							this.loadConfig(oResults.targetType)
 						}
 					},
 					scope: this
@@ -61,15 +75,15 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 			});
 		},
 
-		loadConfig: function() {
+		loadConfig: function(type) {
+			this.targetType = type;
 			Alfresco.util.Ajax.jsonGet({
-				url: Alfresco.constants.URL_SERVICECONTEXT + "/lecm/forms/getConfig?action=getControlsById&typeId=" + encodeURIComponent(this.targetType),
+				url: Alfresco.constants.URL_SERVICECONTEXT + "/lecm/forms/getConfig?action=getControlsById&typeId=" + encodeURIComponent(type),
 				successCallback: {
 					fn: function (response) {
 						var oResults = response.json;
-						if (oResults != null) {
-							this.config = oResults;
-							this.applyConfig();
+						if (oResults) {
+							this.applyConfig(oResults);
 						}
 					},
 					scope: this
@@ -77,66 +91,78 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 			});
 		},
 
-		applyConfig: function() {
-			if (this.config != null) {
+		applyConfig: function(configObj) {
+			this.config = configObj;
+			if (this.config) {
 				var select = Dom.get(this.id + "-select");
+				if (select) {
+					while (select.firstChild) {
+						select.removeChild(select.firstChild);
+					}
 
-				for (var i = 0; i < this.config.length; i++) {
-					var conf = this.config[i];
+					if (this.options.defaultOption) {
+						option = document.createElement("option");
+						option.innerHTML = this.options.defaultOption;
+						select.appendChild(option);
+					}
 
-					var option = document.createElement("option");
-					option.innerHTML = conf.displayName;
-					if (this.options.value != null && this.options.value.id == conf.id) {
-						option.selected = true;
+					for (var i = 0; i < this.config.length; i++) {
+						var conf = this.config[i];
 
-						var valueParams = this.options.value.params;
-						var confParams = conf.params;
-						if (valueParams != null && confParams != null) {
-							for (var j = 0; j < confParams.length; j++) {
-								for (var k = 0; k < valueParams.length; k++) {
-									if (valueParams[k].name == confParams[j].id) {
-										confParams[j].value = valueParams[k].value;
+						var option = document.createElement("option");
+						option.innerHTML = conf.displayName;
+						if (this.options.value && this.options.value.id == conf.id) {
+							option.selected = true;
+
+							var valueParams = this.options.value.params;
+							var confParams = conf.params;
+							if (valueParams && confParams) {
+								for (var j = 0; j < confParams.length; j++) {
+									for (var k = 0; k < valueParams.length; k++) {
+										if (valueParams[k].name == confParams[j].id) {
+											confParams[j].value = valueParams[k].value;
+										}
 									}
 								}
 							}
 						}
+						select.appendChild(option);
 					}
-					select.appendChild(option);
+					this.onSelectChange();
 				}
-				this.onSelectChange();
 			}
 		},
 
 		onSelectChange: function() {
 			var select = Dom.get(this.id + "-select");
-			if (select != null) {
+			if (select) {
 				var tableParams = Dom.get(this.id + "-params");
 				var hiddenParams = Dom.get(this.id + "-hidden-params");
-				if (tableParams != null && hiddenParams != null) {
+				if (tableParams && hiddenParams) {
 					tableParams.innerHTML = "";
 					hiddenParams.innerHTML = "";
 
 					var conf = this.config[select.selectedIndex -1];
-					if (conf != null && conf.params != null) {
+					if (conf && conf.params) {
 						for (var i = 0; i < conf.params.length; i++) {
 							var param = conf.params[i];
 							if (param.visible){
-								var tr = document.createElement("tr");
+								var paramRow = document.createElement("tr");
 
-								var td1 = document.createElement("td");
-								td1.innerHTML = param.localName;
+								var parNameTd = document.createElement("td");
+								parNameTd.innerHTML = param.localName;
 								if (param.mandatory) {
-									td1.innerHTML += "<span class='mandatory-indicator'>*</span>";
+									parNameTd.innerHTML += "<span class='mandatory-indicator'>*</span>";
 								}
-								tr.appendChild(td1);
-
-								var td2 = document.createElement("td");
-								td2.innerHTML = "<input type='text' id='" + param.id + "' value='" + param.value + "' class='formFieldControlParams'/>";
-								tr.appendChild(td2);
-
-								tableParams.appendChild(tr);
+								paramRow.appendChild(parNameTd);
 
 								YAHOO.util.Event.onAvailable(param.id, this.attachChangeParamsListener, param.id, this);
+
+								var parValueTd = document.createElement("td");
+								parValueTd.innerHTML = "<input type='text' id='" + param.id + "' value='" + param.value + "' class='formFieldControlParams'/>";
+								paramRow.appendChild(parValueTd);
+
+								tableParams.appendChild(paramRow);
 							} else {
 								hiddenParams.innerHTML += "<input type='hidden' id='" + param.id + "' value='" + param.value + "' class='formFieldControlParams'>";
 							}
@@ -148,19 +174,18 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 			this.updateComponentValue();
 		},
 
-		attachChangeParamsListener: function(id)
-		{
+		attachChangeParamsListener: function (id) {
 			YAHOO.util.Event.on(id, 'change', this.updateComponentValue, {}, this);
 		},
 
 		updateComponentValue: function() {
 			var el = Dom.get(this.id);
-			if (el != null) {
+			if (el) {
 				var value = "";
 				var select = Dom.get(this.id + "-select");
-				if (select != null) {
+				if (select) {
 					var conf = this.config[select.selectedIndex -1];
-					if (conf != null) {
+					if (conf) {
 						var obj = {
 							template: conf.templatePath,
 							displayName: conf.displayName,
@@ -168,11 +193,11 @@ LogicECM.module.FormsEditor = LogicECM.module.FormsEditor || {};
 							params: []
 						};
 
-						if (conf.params != null) {
+						if (conf.params) {
 							for (var i = 0; i < conf.params.length; i++) {
 								var paramEl = Dom.get(conf.params[i].id);
 
-								if (paramEl != null) {
+								if (paramEl) {
 									obj.params.push({
 										name: conf.params[i].id,
 										value: paramEl.value
