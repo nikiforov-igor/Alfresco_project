18272dfb0d0483488aa2ecb331e15eafa368a5ab
Панюков Антон <APanyukov@it.ru>
2017-04-28 14:30:53 +0300

    ALFSED-1025 - правки

--- lecm-workflow/lecm-reservation/repo/src/main/java/ru/it/lecm/workflow/reservation/ReservationWorkflowServiceImpl.java
+++ lecm-workflow/lecm-reservation/repo/src/main/java/ru/it/lecm/workflow/reservation/ReservationWorkflowServiceImpl.java
@@ -207,8 +207,7 @@ public class ReservationWorkflowServiceImpl extends WorkflowServiceAbstract impl
 	public boolean isReservationRunning(NodeRef document) {
 		ParameterCheck.mandatory("document", document);
 		if (nodeService.exists(document)) {
-			Boolean isReservationRunning = (Boolean) nodeService.getProperty(document, PROP_IS_RESERVATION_RUNNING);
-			return isReservationRunning != null ? isReservationRunning : false;
+			return Boolean.valueOf((Boolean) nodeService.getProperty(document, PROP_IS_RESERVATION_RUNNING));
 		}
 		return false;
 	}
