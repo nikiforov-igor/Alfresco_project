40445ee0f4cec293acb45682cc361646169171f4
Мельников Петр <PMelnikov@it.ru>
2017-06-09 11:43:07 +0300

    ARCHLITE-163. Модуль Документы (Большие вложения)

--- lecm-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/documents/api/attachments/complex/complex-attachments.page.get.js
+++ lecm-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/documents/api/attachments/complex/complex-attachments.page.get.js
@@ -2,24 +2,26 @@ var nodeRef = args["nodeRef"];
 
 var node = search.findNode(nodeRef);
 if (node) {
-    var folder = node.assocs["lecm-document-aspects:complex-attachment-folder"][0];
-    var nodes = folder.getChildren();
-    nodes.sort(function (a, b) {
-        if (a.properties['cm:name'] > b.properties['cm:name']) {
-            return 1;
-        }
-        if (a.properties['cm:name'] < b.properties['cm:name']) {
-            return -1;
-        }
-        return 0;
-    });
+    var folderAssocs = node.assocs["lecm-document-aspects:complex-attachment-folder"];
+    if (folderAssocs && folderAssocs.length) {
+        var nodes = folder.getChildren();
+        nodes.sort(function (a, b) {
+            if (a.properties['cm:name'] > b.properties['cm:name']) {
+                return 1;
+            }
+            if (a.properties['cm:name'] < b.properties['cm:name']) {
+                return -1;
+            }
+            return 0;
+        });
 
-    var jsonNodes = nodes.map(function (item) {
-        return item.toJSON(true);
-    });
+        var jsonNodes = nodes.map(function (item) {
+            return item.toJSON(true);
+        });
 
-    model.nodes = jsonNodes;
-    model.totalPageNum = folder.getChildren().length;
-    model.folderNodeRef = folder.nodeRef.toString();
+        model.nodes = jsonNodes;
+        model.totalPageNum = folder.getChildren().length;
+        model.folderNodeRef = folder.nodeRef.toString();
+    }
 }
 
