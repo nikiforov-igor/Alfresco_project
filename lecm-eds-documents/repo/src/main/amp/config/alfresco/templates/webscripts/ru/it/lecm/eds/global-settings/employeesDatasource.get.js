b2b6da7d5d3c4cc603908d9af4cbadd830e832b4
Курец Константин <KKurets@it.ru>
2017-07-20 16:39:36 +0300

    ALFSED-1177: изменены ключи лейблов

--- lecm-eds-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/eds/global-settings/employeesDatasource.get.js
+++ lecm-eds-documents/repo/src/main/amp/config/alfresco/templates/webscripts/ru/it/lecm/eds/global-settings/employeesDatasource.get.js
@@ -6,16 +6,6 @@ var businessRoleId = args["businessRoleId"],
 	employees = orgstructure.getEmployeesByBusinessRoleId(businessRoleId, withDelegation && withDelegation == "true"),
 	results = new Array();
 
-if (!employees || !employees.length) {
-	var dutyRegistrar = edsGlobalSettings.getDutyRegistrar();
-    results.push(
-        {
-            item: dutyRegistrar,
-            selectable: true,
-            visibleName: argsNameSubstituteString ? substitude.formatNodeTitle(dutyRegistrar, argsNameSubstituteString) : null,
-            selectedVisibleName: argsSelectedItemsNameSubstituteString ? substitude.formatNodeTitle(dutyRegistrar, argsSelectedItemsNameSubstituteString) : null
-        });
-} else {
     for each(var employee in employees) {
         results.push(
             {
@@ -25,6 +15,5 @@ if (!employees || !employees.length) {
                 selectedVisibleName: argsSelectedItemsNameSubstituteString ? substitude.formatNodeTitle(employee, argsSelectedItemsNameSubstituteString) : null
             });
     }
-}
 
 model.results = results;
