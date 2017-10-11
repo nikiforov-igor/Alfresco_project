953184700dbfee51c4fd0ed5a41810095b7fd577
Башмаков Денис <DBashmakov@it.ru>
2017-10-11 15:10:11 +0300

    ALFSED-1822 - При автоматическом завершении согласования, у этапа в статусе "Отменен" отсутствует дата завершения

--- lecm-eds-documents/repo/src/main/amp/config/alfresco/module/eds-documents-repo/workflow/scripts/eds-auto-approval.js
+++ lecm-eds-documents/repo/src/main/amp/config/alfresco/module/eds-documents-repo/workflow/scripts/eds-auto-approval.js
@@ -3,41 +3,19 @@ var AutoApprovalScript = {
         var currentIteration = approvalRoutes.getDocumentCurrentIteration(doc);
         if (currentIteration && currentIteration.properties['lecm-routes-v2:status'] == "ACTIVE") {
             var stages = currentIteration.getChildAssocsByType('lecm-approval-route:stage');
-
-            var stage, i, j, size, length, approvalState, items, item, itemState, isApproved, approver, approvers = [];
+            var stage, i, j, size, length, items, item, approver, approvers = [];
 
             for (i = 0, size = stages.length; i < size; ++i) {
                 stage = stages[i];
-                approvalState = '' + stage.properties['lecm-routes-v2:status'];
-
                 items = stage.getChildAssocsByType('lecm-approval-route:stageItem');
-                isApproved = false;
-
                 for (j = 0, length = items.length; j < length; ++j) {
                     item = items[j];
-                    itemState = '' + item.properties['lecm-routes-v2:status'];
-
                     approver = item.assocs["lecm-routes-v2:stageItemMemberAssoc"] && item.assocs["lecm-routes-v2:stageItemMemberAssoc"].length ?
                         item.assocs["lecm-routes-v2:stageItemMemberAssoc"][0] : null;
                     if (approver) {
                         approvers.push(approver);
                     }
-
-                    if ('NEW' == itemState || 'ACTIVE' == itemState) {
-                        item.properties['lecm-routes-v2:status'] = 'CANCELLED';
-                        item.properties['lecm-routes-v2:stageItemCompleteDate'] = base.getValueConverter().convertValueForJava(new Date());
-                        item.save();
-                    } else if ('APPROVED_EARLY' != itemState) {
-                        isApproved = true;
-                    }
                 }
-
-                if ("NEW" == approvalState) {
-                    stage.properties['lecm-routes-v2:status'] = 'CANCELLED';
-                } else {
-                    stage.properties['lecm-routes-v2:status'] = isApproved ? 'FINISHED' : 'CANCELLED';
-                }
-                stage.save();
             }
 
             businessJournal.log(doc.nodeRef.toString(), 'APPROVAL', substitude.formatNodeTitle(doc.nodeRef.toString(), logMessage.toString()), []);
