44c4a15aa9e9072cc4c9bf339f96a1f090baf7b2
Novikov Victor <VNovikov@it.ru>
2017-12-13 10:02:51 +0300

    ALFSED-2114 IE 11 не поддреживает скрытие элемента option в select. Поэтому option приходится хранить в виде span при скрытии.

--- lecm-errands/share/src/main/amp/web/scripts/lecm-errands/errands-report-required-handler.js
+++ lecm-errands/share/src/main/amp/web/scripts/lecm-errands/errands-report-required-handler.js
@@ -36,15 +36,49 @@
             Dom.setStyle(recipientField.parentNode.parentNode.parentNode, "display", !requiredReport ? "none" : "block");
             if(propName == "lecm-errands_report-recipient-type") {
                 if (!requiredReport) {
-                    recipientField.options[3].hidden = false;
+                    showOption(recipientField);
                     Dom.get(args[1].formId + '_prop_' + propName).selectedIndex = 3;
                 } else {
-                    recipientField.options[3].hidden = true;
+                    hideOption(recipientField.options[3]);
                     if (Dom.get(args[1].formId + '_prop_' + propName).selectedIndex == 3) {
                         Dom.get(args[1].formId + '_prop_' + propName).selectedIndex = 0;
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * IE 11 is not support hidden or display:none option in select. This function allow hide option in select.
+     * Option will be replaced on span with id = select.id + "_hidden_opt".
+     *
+     * @param opt html element option in select for hide
+     */
+    function hideOption(opt) {
+        var select = opt.parentElement;
+        var span = document.createElement("span");
+        span.hidden = true;
+        span.title = opt.value;
+        span.textContent = opt.textContent;
+        span.id = select.id + "_hidden_opt";
+        select.appendChild(span);
+        select.removeChild(opt);
+    }
+
+    /**
+     * IE 11 is not support hidden or display:none option in select. This function show hidden option.
+     * Get hidden option with id select.id + "_hidden_opt" and add it to current select
+     *
+     * @param select html element select for option
+     */
+    function showOption(select) {
+        var hiddenOpt = Dom.get(select.id + "_hidden_opt");
+        if (hiddenOpt) {
+            var option = document.createElement("option");
+            option.value = hiddenOpt.title;
+            option.textContent = hiddenOpt.textContent;
+            select.appendChild(option);
+            select.removeChild(hiddenOpt);
         }
     }
 })();
