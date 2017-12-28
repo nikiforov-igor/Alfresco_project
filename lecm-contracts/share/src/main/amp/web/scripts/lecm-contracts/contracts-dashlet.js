9c421271538f3d05c41f4f0e52abca37ef14d2ae
Novikov Victor <VNovikov@it.ru>
2017-12-28 13:45:22 +0300

    ALFSED-2188 Создания Договора из дашлета "Договары" теперь происходит в полной странице создания, а не в диалоговом окне

--- lecm-contracts/share/src/main/amp/web/scripts/lecm-contracts/contracts-dashlet.js
+++ lecm-contracts/share/src/main/amp/web/scripts/lecm-contracts/contracts-dashlet.js
@@ -236,70 +236,9 @@ LogicECM.module.Contracts.dashlet = LogicECM.module.Contracts.dashlet || {};
                 }
             },
 
-            onAddContractClick: function Contracts_onAddContractClick(){
-                if (this.doubleClickLock) return;
-                this.doubleClickLock = true;
-                var destination = this.options.destination,
-                    itemType = this.options.itemType;
-
-                // Intercept before dialog show
-                var doBeforeDialogShow = function (p_form, p_dialog) {
-                    Alfresco.util.populateHTML(
-                        [ p_dialog.id + "-form-container_h", this.msg("label.create-row.title") ]
-                    );
-                    Dom.addClass(p_dialog.id + "-form-container", "metadata-form-edit");
-                    this.doubleClickLock = false;
-	                p_dialog.dialog.subscribe('destroy', LogicECM.module.Base.Util.formDestructor, {moduleId: p_dialog.id}, this);
-                };
-
-                var templateUrl = Alfresco.constants.URL_SERVICECONTEXT + "lecm/components/form";
-	            var templateRequestParams = {
-                        itemKind: "type",
-                        itemId: itemType,
-                        destination: destination,
-                        mode: "create",
-                        formId: "",
-		            submitType: "json",
-		            showCancelButton: true,
-					showCaption: false
-	            };
-
-                // Using Forms Service, so always create new instance
-                var createDetails = new Alfresco.module.SimpleDialog(this.id + "-createContractDetails");
-                createDetails.setOptions(
-                    {
-                        width: "84em",
-                        templateUrl: templateUrl,
-	                    templateRequestParams: templateRequestParams,
-                        actionUrl: null,
-                        destroyOnHide: true,
-                        doBeforeDialogShow: {
-                            fn: doBeforeDialogShow,
-                            scope: this
-                        },
-                        onSuccess: {
-                            fn: function DataGrid_onActionCreate_success(response) {
-                                Alfresco.util.PopupManager.displayMessage(
-                                    {
-                                        text: this.msg("message.save.success")
-                                    });
-                                window.location.href = window.location.protocol + "//" + window.location.host +
-                                    Alfresco.constants.URL_PAGECONTEXT + "document?nodeRef=" + response.json.persistedObject;
-                                this.doubleClickLock = false;
-                            },
-                            scope: this
-                        },
-                        onFailure: {
-                            fn: function DataGrid_onActionCreate_failure(response) {
-                                Alfresco.util.PopupManager.displayMessage(
-                                    {
-                                        text: this.msg("message.save.failure")
-                                    });
-                                this.doubleClickLock = false;
-                            },
-                            scope: this
-                        }
-                    }).show();
+            onAddContractClick: function Contracts_onAddContractClick() {
+                var url = Alfresco.constants.URL_PAGECONTEXT + "document-create?documentType=" + this.options.itemType;
+                window.location.href = url + "&" + LogicECM.module.Base.Util.encodeUrlParams("documentType=" + this.options.itemType);
             }
         });
 })();
