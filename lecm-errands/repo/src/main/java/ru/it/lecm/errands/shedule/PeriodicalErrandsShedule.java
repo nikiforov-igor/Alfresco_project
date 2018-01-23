f7cbd6a1fe0e47b7a56aaacf2036bbec34266d9a
Панюков Антон <APanyukov@it.ru>
2018-01-23 17:43:42 +0300

    ALFSED-13 унаследовал шедулеры от baseTransactionalSchedule

--- lecm-errands/repo/src/main/java/ru/it/lecm/errands/shedule/PeriodicalErrandsShedule.java
+++ lecm-errands/repo/src/main/java/ru/it/lecm/errands/shedule/PeriodicalErrandsShedule.java
@@ -1,24 +1,16 @@
 package ru.it.lecm.errands.shedule;
 
-import org.alfresco.repo.action.scheduled.AbstractScheduledAction;
-import org.alfresco.repo.action.scheduled.InvalidCronExpression;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
-import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.lang3.time.DateFormatUtils;
-import org.quartz.CronTrigger;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.it.lecm.base.beans.BaseBean;
+import ru.it.lecm.base.beans.BaseTransactionalSchedule;
 import ru.it.lecm.documents.beans.DocumentService;
 import ru.it.lecm.errands.ErrandsService;
 
-import java.text.ParseException;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -28,104 +20,19 @@ import java.util.stream.Collectors;
  * Date: 05.08.14
  * Time: 15:24
  */
-public class PeriodicalErrandsShedule extends AbstractScheduledAction {
+public class PeriodicalErrandsShedule extends BaseTransactionalSchedule {
 
     private final static Logger logger = LoggerFactory.getLogger(PeriodicalErrandsShedule.class);
 
-    private String cronExpression = "0 0 3 */1 * ?"; // каждый день в 3 часа ночи
-                                                     // "0 59 * * * ? *" - каждый час в xx:59
-                                                     // "0 0/5 * * * ? *"; - каждые 5 минут
-    private String firstStartExpression = "0 */15 * * * ?"; // через 15 минут после старта
-
-    private boolean onServerStart = false;
-
-    private String jobName = "periodical-errands-sys-agent";
-    private String jobGroup = "periodical-errands";
-
-    private String triggerName = "periodical-errands-sys-agent-trigger";
-    private String triggerGroup = "periodical-errands-sys-agent-trigger-group";
-
-    private Scheduler scheduler;
-
     private DocumentService documentService;
     private ErrandsService errandsService;
-    private NodeService nodeService;
 
     public PeriodicalErrandsShedule() {
         super();
     }
 
-    public Scheduler getScheduler() {
-        return scheduler;
-    }
-
-    public void setScheduler(Scheduler scheduler) {
-        this.scheduler = scheduler;
-    }
-
     public void setErrandsService(ErrandsService errandsService) {
         this.errandsService = errandsService;
-    }
-
-    public void setNodeService(NodeService nodeService) {
-        this.nodeService = nodeService;
-    }
-
-    @Override
-    public void setJobName(String jobName) {
-        this.jobName = jobName;
-    }
-
-    @Override
-    public String getJobName() {
-        return jobName;
-    }
-
-    @Override
-    public void setJobGroup(String jobGroup) {
-        this.jobGroup = jobGroup;
-    }
-
-    @Override
-    public String getJobGroup() {
-        return this.jobGroup;
-    }
-
-    @Override
-    public void setTriggerName(String triggerName) {
-        this.triggerName = triggerName;
-    }
-
-    @Override
-    public String getTriggerName() {
-        return this.triggerName;
-    }
-
-    @Override
-    public void setTriggerGroup(String triggerGroup) {
-        this.triggerGroup = triggerGroup;
-    }
-
-    @Override
-    public String getTriggerGroup() {
-        return this.triggerGroup;
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        register(getScheduler());
-    }
-
-    public void setCronExpression(String cronExpression) {
-        this.cronExpression = cronExpression;
-    }
-
-    public void setFirstStartExpression(String firstStartExpression) {
-        this.firstStartExpression = firstStartExpression;
-    }
-
-    public void setOnServerStart(boolean onServerStart) {
-        this.onServerStart = onServerStart;
     }
 
     public void setDocumentService(DocumentService documentService) {
@@ -133,35 +40,8 @@ public class PeriodicalErrandsShedule extends AbstractScheduledAction {
     }
 
     @Override
-    public Trigger getTrigger() {
-        try {
-            CronTrigger trigger = new CronTrigger(getTriggerName(), getTriggerGroup(), onServerStart ? firstStartExpression : cronExpression);
-            trigger.setJobName(getJobName());
-            trigger.setJobGroup(getJobGroup());
-            return trigger;
-        } catch (final ParseException e) {
-            throw new InvalidCronExpression("Invalid chron expression: n" + (onServerStart ? firstStartExpression : cronExpression));
-        }
-    }
-
-    @Override
-    public List<NodeRef> getNodes() {
-        if (onServerStart) { // если был запуск на старте - подменяем триггер на основной
-            CronTrigger trigger = (CronTrigger) getTrigger();
-            try {
-                trigger.setCronExpression(cronExpression);
-                getScheduler().rescheduleJob(getTriggerName(), getTriggerGroup(), trigger);
-                onServerStart = false; // включаем основной триггер
-            } catch (final ParseException | SchedulerException ignored) {
-            }
-        }
-
+    public List<NodeRef> getNodesInTx() {
         return getErrandsOnExecution();
-    }
-
-    @Override
-    public Action getAction(NodeRef nodeRef) {
-        return getActionService().createAction("periodicalErrandsExecutor");
     }
 
     private List<NodeRef> getErrandsOnExecution() {
--- lecm-errands/repo/src/main/java/ru/it/lecm/errands/shedule/PeriodicalErrandsToExecutedShedule.java
+++ lecm-errands/repo/src/main/java/ru/it/lecm/errands/shedule/PeriodicalErrandsToExecutedShedule.java
@@ -1,22 +1,14 @@
 package ru.it.lecm.errands.shedule;
 
-import org.alfresco.repo.action.scheduled.AbstractScheduledAction;
-import org.alfresco.repo.action.scheduled.InvalidCronExpression;
-import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.QName;
-import org.quartz.CronTrigger;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.it.lecm.base.beans.BaseBean;
+import ru.it.lecm.base.beans.BaseTransactionalSchedule;
 import ru.it.lecm.documents.beans.DocumentService;
 import ru.it.lecm.errands.ErrandsService;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -29,96 +21,15 @@ import java.util.stream.Collectors;
  * Date: 09.11.2017
  * Time: 14:37
  */
-public class PeriodicalErrandsToExecutedShedule extends AbstractScheduledAction {
+public class PeriodicalErrandsToExecutedShedule extends BaseTransactionalSchedule {
 
     private final static Logger logger = LoggerFactory.getLogger(PeriodicalErrandsToExecutedShedule.class);
 
-    private String cronExpression = "0 0 3 */1 * ?"; // каждый день в 3 часа ночи
-    // "0 59 * * * ? *" - каждый час в xx:59
-    // "0 0/5 * * * ? *"; - каждые 5 минут
-    private String firstStartExpression = "0 */15 * * * ?"; // через 15 минут после старта
-
-    private boolean onServerStart = false;
-
-    private String jobName = "periodical-errands-to-executed--sys-agent";
-    private String jobGroup = "periodical-errands-to-executed";
-
-    private String triggerName = "periodical-errands-to-executed-sys-agent-trigger";
-    private String triggerGroup = "periodical-errands-to-executed-sys-agent-trigger-group";
-
-    private Scheduler scheduler;
-
     private DocumentService documentService;
     private ErrandsService errandsService;
-    private NodeService nodeService;
 
     public PeriodicalErrandsToExecutedShedule() {
         super();
-    }
-
-    public Scheduler getScheduler() {
-        return scheduler;
-    }
-
-    public void setScheduler(Scheduler scheduler) {
-        this.scheduler = scheduler;
-    }
-
-    @Override
-    public void setJobName(String jobName) {
-        this.jobName = jobName;
-    }
-
-    @Override
-    public String getJobName() {
-        return jobName;
-    }
-
-    @Override
-    public void setJobGroup(String jobGroup) {
-        this.jobGroup = jobGroup;
-    }
-
-    @Override
-    public String getJobGroup() {
-        return this.jobGroup;
-    }
-
-    @Override
-    public void setTriggerName(String triggerName) {
-        this.triggerName = triggerName;
-    }
-
-    @Override
-    public String getTriggerName() {
-        return this.triggerName;
-    }
-
-    @Override
-    public void setTriggerGroup(String triggerGroup) {
-        this.triggerGroup = triggerGroup;
-    }
-
-    @Override
-    public String getTriggerGroup() {
-        return this.triggerGroup;
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        register(getScheduler());
-    }
-
-    public void setCronExpression(String cronExpression) {
-        this.cronExpression = cronExpression;
-    }
-
-    public void setFirstStartExpression(String firstStartExpression) {
-        this.firstStartExpression = firstStartExpression;
-    }
-
-    public void setOnServerStart(boolean onServerStart) {
-        this.onServerStart = onServerStart;
     }
 
     public void setDocumentService(DocumentService documentService) {
@@ -129,40 +40,9 @@ public class PeriodicalErrandsToExecutedShedule extends AbstractScheduledAction 
         this.errandsService = errandsService;
     }
 
-    public void setNodeService(NodeService nodeService) {
-        this.nodeService = nodeService;
-    }
-
     @Override
-    public Trigger getTrigger() {
-        try {
-            CronTrigger trigger = new CronTrigger(getTriggerName(), getTriggerGroup(), onServerStart ? firstStartExpression : cronExpression);
-            trigger.setJobName(getJobName());
-            trigger.setJobGroup(getJobGroup());
-            return trigger;
-        } catch (final ParseException e) {
-            throw new InvalidCronExpression("Invalid chron expression: n" + (onServerStart ? firstStartExpression : cronExpression));
-        }
-    }
-
-    @Override
-    public List<NodeRef> getNodes() {
-        if (onServerStart) { // если был запуск на старте - подменяем триггер на основной
-            CronTrigger trigger = (CronTrigger) getTrigger();
-            try {
-                trigger.setCronExpression(cronExpression);
-                getScheduler().rescheduleJob(getTriggerName(), getTriggerGroup(), trigger);
-                onServerStart = false; // включаем основной триггер
-            } catch (final ParseException | SchedulerException ignored) {
-            }
-        }
-
+    public List<NodeRef> getNodesInTx() {
         return getErrandsOnExecution();
-    }
-
-    @Override
-    public Action getAction(NodeRef nodeRef) {
-        return getActionService().createAction("periodicalErrandsToExecutedExecutor");
     }
 
     private List<NodeRef> getErrandsOnExecution() {
