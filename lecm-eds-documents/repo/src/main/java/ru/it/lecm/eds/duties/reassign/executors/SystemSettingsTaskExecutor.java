8dc7403e521ec47ed0099cae71cd5d93e9aa2cd0
Башмаков Денис <DBashmakov@it.ru>
2018-03-02 10:12:41 +0300

    ALFSED-2346 - Передача дел. Некорректный поиск настройки "Автор по умолчанию"

--- lecm-eds-documents/repo/src/main/java/ru/it/lecm/eds/duties/reassign/executors/SystemSettingsTaskExecutor.java
+++ lecm-eds-documents/repo/src/main/java/ru/it/lecm/eds/duties/reassign/executors/SystemSettingsTaskExecutor.java
@@ -1,5 +1,7 @@
 package ru.it.lecm.eds.duties.reassign.executors;
 
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.cmr.repository.AssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
@@ -29,44 +31,46 @@ public class SystemSettingsTaskExecutor extends TaskExecutor {
     private static final String SETTINGS = "settings";
     private static final String TYPE = "type";
 
-    private static final String SUCCEESS_LOG_TEMPLATE = "Сотрудник #object3 изменен на сотрудника #object2 в настройке %s";
-    private static final String FAIL_LOG_TEMPLATE = "Не удалось изменить сотрудника #object3 на сотрудника #object2 в настройке %s";
+    private static final String SUCCEESS_LOG_TEMPLATE = "Сотрудник #object2 изменен на сотрудника #object3 в настройке %s";
+    private static final String FAIL_LOG_TEMPLATE = "Не удалось изменить сотрудника #object2 на сотрудника #object3 в настройке %s";
 
     @Override
     public boolean execute(NodeRef task, JSONObject element) {
-        boolean result = false;
+        AuthenticationUtil.RunAsWork<Boolean> raw = () -> {
+            boolean result = false;
+            try {
+                NodeRef fromAsssoc = nodeService.getTargetAssocs(task, DutiesReassignService.ASSOC_FROM).get(0).getTargetRef();
+                NodeRef toAsssoc = nodeService.getTargetAssocs(task, DutiesReassignService.ASSOC_TO).get(0).getTargetRef();
 
-        try {
-            NodeRef fromAsssoc = nodeService.getTargetAssocs(task, DutiesReassignService.ASSOC_FROM).get(0).getTargetRef();
-            NodeRef toAsssoc = nodeService.getTargetAssocs(task, DutiesReassignService.ASSOC_TO).get(0).getTargetRef();
+                String settingsItemRef = element.has(NODE_REF) ? element.getString(NODE_REF) : null;
+                NodeRef settingItem = (settingsItemRef != null && NodeRef.isNodeRef(settingsItemRef)) ? new NodeRef(settingsItemRef) : null;
+                if (settingItem != null) {
+                    if (element.getString(TYPE).equals(USER_SETTINGS_ASSOC_DEFAULT_INITIATOR.toString())) {
+                        nodeService.removeAssociation(settingItem, fromAsssoc, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR);
+                        nodeService.createAssociation(settingItem, toAsssoc, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR);
+                    } else if (element.getString(TYPE).equals(EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR.toString())) {
+                        nodeService.removeAssociation(settingItem, fromAsssoc, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR);
+                        nodeService.createAssociation(settingItem, toAsssoc, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR);
+                    }
 
-            String settingsItemRef = element.has(NODE_REF) ? element.getString(NODE_REF) : null;
-            NodeRef settingItem = (settingsItemRef != null && NodeRef.isNodeRef(settingsItemRef)) ? new NodeRef(settingsItemRef) : null;
-            if (settingItem != null) {
-                if (element.getString(TYPE).equals(USER_SETTINGS_ASSOC_DEFAULT_INITIATOR.toString())) {
-                    nodeService.removeAssociation(settingItem, fromAsssoc, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR);
-                    nodeService.createAssociation(settingItem, toAsssoc, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR);
-                } else if (element.getString(TYPE).equals(EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR.toString())) {
-                    nodeService.removeAssociation(settingItem, fromAsssoc, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR);
-                    nodeService.createAssociation(settingItem, toAsssoc, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR);
+                } else {
+                    throw new IllegalStateException("Cannot find field \"nodeRef\" in elementData");
                 }
 
-            } else {
-                throw new IllegalStateException("Cannot find field \"nodeRef\" in elementData");
+                result = true;
+            } catch (Exception ex) {
+                logger.error(ex.getMessage(), ex);
             }
 
-            result = true;
-        } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
-        }
-
-        return result;
+            return result;
+        };
+        return AuthenticationUtil.runAsSystem(raw);
     }
 
     @Override
     public List<JSONObject> calculateElements(NodeRef task) {
         NodeRef fromAsssoc = nodeService.getTargetAssocs(task, DutiesReassignService.ASSOC_FROM).get(0).getTargetRef();
-        return handleElements(fromAsssoc.toString());
+        return handleElements(fromAsssoc.toString(), null);
     }
 
     @Override
@@ -75,7 +79,8 @@ public class SystemSettingsTaskExecutor extends TaskExecutor {
         try {
             String fromAssocRef = taskData.has("assoc_lecm-duties-reassign_from-assoc") ?
                     taskData.getString("assoc_lecm-duties-reassign_from-assoc") : null;
-            elements = handleElements(fromAssocRef);
+            final String searchTerm = taskData.has(SEARCH_TERM) && !taskData.getString(SEARCH_TERM).isEmpty() ? taskData.getString(SEARCH_TERM).toLowerCase() : null;
+            elements = handleElements(fromAssocRef, searchTerm);
         } catch (JSONException e) {
             logger.error(e.getMessage(), e);
         }
@@ -83,49 +88,69 @@ public class SystemSettingsTaskExecutor extends TaskExecutor {
         return elements;
     }
 
-    private List<JSONObject> handleElements(String fromAssocRef) {
-        List<JSONObject> result = new ArrayList<>();
-        if (fromAssocRef != null && !fromAssocRef.isEmpty()) {
-            NodeRef fromAssoc = new NodeRef(fromAssocRef);
-            //Дежурный регистратор
-            List<AssociationRef> registarSettingList = nodeService.getSourceAssocs(fromAssoc, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR);
-            if (!registarSettingList.isEmpty()) {
-                NodeRef registarSetting = registarSettingList.get(0).getSourceRef();
-                JSONObject element = new JSONObject();
-                try {
-                    element.put(NAME, registarSetting.toString());
-                    element.put(NODE_REF, registarSetting.toString());
-                    element.put(ID, registarSetting.toString());
-                    element.put(TYPE, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR.toString());
-                    element.put(ATTRIBUTE, I18NUtil.getMessage("lecm-eds-globset_model.association.lecm-eds-globset_duty-registrar-assoc.title"));
-                    element.put(SETTINGS, I18NUtil.getMessage("system-settings-task-executor.assoc-duty-registrar-path"));
-                    result.add(element);
-                } catch (JSONException e) {
-                    logger.error(e.getMessage(), e);
+    private List<JSONObject> handleElements(String fromAssocRef, String searchTerm) {
+        AuthenticationUtil.RunAsWork<List<JSONObject>> raw = () -> {
+            List<JSONObject> elementsInWork = new ArrayList<>();
+            if (fromAssocRef != null && !fromAssocRef.isEmpty()) {
+                NodeRef fromAssoc = new NodeRef(fromAssocRef);
+                //Дежурный регистратор
+                List<AssociationRef> registarSettingList = nodeService.getSourceAssocs(fromAssoc, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR);
+                if (!registarSettingList.isEmpty()) {
+                    NodeRef registarSetting = registarSettingList.get(0).getSourceRef();
+                    String attribute = I18NUtil.getMessage("lecm-eds-globset_model.association.lecm-eds-globset_duty-registrar-assoc.title");
+                    String settings = I18NUtil.getMessage("system-settings-task-executor.assoc-duty-registrar-path");
+                    if (searchTerm == null ||
+                            attribute.toLowerCase().contains(searchTerm) ||
+                            settings.toLowerCase().contains(searchTerm)
+                            ) {
+                        JSONObject element = new JSONObject();
+                        try {
+                            element.put(NAME, registarSetting.toString());
+                            element.put(NODE_REF, registarSetting.toString());
+                            element.put(ID, registarSetting.toString());
+                            element.put(TYPE, EDSGlobalSettingsService.ASSOC_DUTY_REGISTRAR.toString());
+                            element.put(ATTRIBUTE, attribute);
+                            element.put(SETTINGS, settings);
+                            elementsInWork.add(element);
+                        } catch (JSONException e) {
+                            logger.error(e.getMessage(), e);
+                        }
+                    }
                 }
-            }
 
-            //Автор по умолчанию
-            List<AssociationRef> authorSettingList = nodeService.getSourceAssocs(fromAssoc, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR);
-            if (!authorSettingList.isEmpty()) {
-                NodeRef authorSetting = authorSettingList.get(0).getSourceRef();
-                JSONObject element = new JSONObject();
-                try {
-                    element.put(NAME, authorSetting.toString());
-                    element.put(NODE_REF, authorSetting.toString());
-                    element.put(ID, authorSetting.toString());
-                    element.put(TYPE, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR.toString());
-                    element.put(ATTRIBUTE, I18NUtil.getMessage("lecm-errands_model.association.lecm-errands_user-settings-default-initiator-assoc.title"));
-                    String employeeName = (String) nodeService.getProperty(fromAssoc, OrgstructureBean.PROP_EMPLOYEE_SHORT_NAME);
-                    String settingsPath = String.format(I18NUtil.getMessage("system-settings-task-executor.user-settings-assoc-default-initiator-path"), employeeName);
-                    element.put(SETTINGS, settingsPath);
-                    result.add(element);
-                } catch (JSONException e) {
-                    logger.error(e.getMessage(), e);
+                //Автор по умолчанию
+                List<AssociationRef> authorSettingList = nodeService.getSourceAssocs(fromAssoc, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR);
+                for (AssociationRef authorSettingAssoc : authorSettingList) {
+                    NodeRef authorSetting = authorSettingAssoc.getSourceRef();
+                    String attribute = I18NUtil.getMessage("lecm-errands_model.association.lecm-errands_user-settings-default-initiator-assoc.title");
+                    String userSettingsName = ((String) nodeService.getProperty(authorSetting, ContentModel.PROP_NAME)).replace("_Settings", "");
+                    NodeRef employee = orgstructureBean.getEmployeeByPerson(userSettingsName);
+                    if (employee != null) {
+                        String employeeName = (String) nodeService.getProperty(employee, OrgstructureBean.PROP_EMPLOYEE_SHORT_NAME);
+                        String settingsPath = String.format(I18NUtil.getMessage("system-settings-task-executor.user-settings-assoc-default-initiator-path"), employeeName);
+                        if (searchTerm == null ||
+                                attribute.toLowerCase().contains(searchTerm) ||
+                                settingsPath.toLowerCase().contains(searchTerm)
+                                ) {
+                            JSONObject element = new JSONObject();
+                            try {
+                                element.put(NAME, authorSetting.toString());
+                                element.put(NODE_REF, authorSetting.toString());
+                                element.put(ID, authorSetting.toString());
+                                element.put(TYPE, USER_SETTINGS_ASSOC_DEFAULT_INITIATOR.toString());
+                                element.put(ATTRIBUTE, attribute);
+                                element.put(SETTINGS, settingsPath);
+                                elementsInWork.add(element);
+                            } catch (JSONException e) {
+                                logger.error(e.getMessage(), e);
+                            }
+                        }
+                    }
                 }
             }
-        }
-        return result;
+            return elementsInWork;
+        };
+        return AuthenticationUtil.runAsSystem(raw);
     }
 
     @Override
