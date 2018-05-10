0f055db47fccc07bc7fcc9c83b8124c98b8e215c
Novikov Victor <VNovikov@it.ru>
2018-05-10 13:33:40 +0300

    [ALFSED-2484] 1) С форм создания этапов договоров и пунтков ОРД убрал indexTableRow. 2) Убрал из скрипта для формы создания пунктов ОРД метод, в котором заполнялся indexTableRow

--- lecm-ord/share/src/main/amp/web/scripts/lecm-ord/ord-item-create-form-script.js
+++ lecm-ord/share/src/main/amp/web/scripts/lecm-ord/ord-item-create-form-script.js
@@ -8,54 +8,8 @@
         'css/lecm-ord/ord-date-set-with-icon.css'
     ]);
 
-    if (Bubbling.addLayer("ordItemCreateFormScriptLoaded")) {
-        Bubbling.on('ordItemCreateFormScriptLoaded', processForm);
-    }
-    if (Bubbling.addLayer("ordItemEditFormScriptLoaded")) {
-        Bubbling.on('ordItemEditFormScriptLoaded', processForm);
-    }
     if (Bubbling.addLayer("ordItemReportRequiredChanged")) {
         Bubbling.on('ordItemReportRequiredChanged', processController);
-    }
-
-    function processForm(layer, args) {
-        var formId = args[1].formId;
-        var form = Alfresco.util.ComponentManager.get(formId);
-        var documentFormId = formId.substring(0, formId.indexOf("_assoc_"));
-        var formMode = form.options.templateRequestParams.mode;
-        var tableRef = formMode == "create" ? form.options.templateRequestParams.destination : form.options.templateRequestParams.itemId;
-        if (layer == "ordItemCreateFormScriptLoaded") {
-            var itemNumberField = Dom.get(formId + "_prop_lecm-document_indexTableRow");
-            if (!itemNumberField.value) {
-                var itemNumber = 1;
-                var itemsTempTable = Dom.get(documentFormId + "_assoc_lecm-ord_temp-items-assoc");
-                if (itemsTempTable) {
-                    if (itemsTempTable.value) {
-                        var valueArr = itemsTempTable.value.split(",");
-                        itemNumber = valueArr.length + 1;
-                    }
-                    itemNumberField.value = itemNumber;
-                } else {
-                    Alfresco.util.Ajax.jsonGet({
-                        url: Alfresco.constants.PROXY_URI + "lecm/ord/items/getNumber",
-                        dataObj: {
-                            tableDataRef: tableRef
-                        },
-                        successCallback: {
-                            fn: function (response) {
-                                if (response && response.json.number) {
-                                    itemNumber = parseInt(response.json.number);
-                                    itemNumberField.value = itemNumber;
-                                }
-                            },
-                            scope: this
-                        },
-                        failureMessage: Alfresco.util.message("message.details.failure"),
-                        scope: this
-                    });
-                }
-            }
-        }
     }
 
     function processController(layer, args) {
