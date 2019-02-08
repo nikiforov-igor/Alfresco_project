b655d0b391e6819292dd20c679fdda3b72b6a64c
Павел Бармёнков <pbarmenkov@blogic.ru>
2019-02-08 09:41:32 +0300

    ALFSED-2731 Секретарю недоступен выбор соисполнителей и неограничен выбор Исполнителя в рамкаx подразделения 1. На форму редактрирования резолюции добавлен обработчик изменения автора резолюции. 2. При добавлении нового поручения в резлюции, добавлен обработчик инициализирующий "исполнителя" и "соисполнителя" в поручениях, исходя из автора резолюции.

--- lecm-resolutions/share/src/main/amp/web/scripts/lecm-resolution/resolution-change-author.js
+++ lecm-resolutions/share/src/main/amp/web/scripts/lecm-resolution/resolution-change-author.js
@@ -1,11 +1,14 @@
 (function() {
+    var Dom = YAHOO.util.Dom;
+    var formId;
 
     YAHOO.Bubbling.on('authorChanged', reInit);
+    YAHOO.Bubbling.on('afterFormRuntimeInit', onAfterFormRuntimeInit);
 
     function reInit(layer, args) {
         var obj = args[1];
         var nodeRef;
-        var formId = obj.formId;
+        formId = obj.formId;
         if (obj.selectedItems) {
             for (var prop in obj.selectedItems) {
                 nodeRef = prop;
@@ -40,5 +43,14 @@
                 resetValue: true
             }
         });
+    }
+
+    function onAfterFormRuntimeInit(layer, args) {
+        if (formId) {
+            var authorResolutionRef = Dom.get(formId + "_assoc_lecm-resolutions_author-assoc").value;
+            if (args[1] && args[1].runtime && args[1].runtime.formId.indexOf(formId + "_prop_lecm-resolutions_errands-json-line-") == 0 && authorResolutionRef) {
+                doReInit(formId, authorResolutionRef)
+            }
+        }
     }
 })();
