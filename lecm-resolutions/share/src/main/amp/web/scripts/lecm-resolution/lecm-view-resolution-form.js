b297d2c8033d59f4e6311a7950df245359477c3d
Павел Бармёнков <pbarmenkov@blogic.ru>
2019-04-17 13:22:50 +0300

    ALFSED-2791 Резолюция. Отображается секция Ознакомление, если документ-основание резолюция без ознакомления 1) Добавлена проверка на наличие у документа аспекта lecm-review-ts:review-aspect

--- lecm-resolutions/share/src/main/amp/web/scripts/lecm-resolution/lecm-view-resolution-form.js
+++ lecm-resolutions/share/src/main/amp/web/scripts/lecm-resolution/lecm-view-resolution-form.js
@@ -11,38 +11,51 @@
         var nodeRef = args[1].nodeRef;
 
         var baseDocument = Dom.get(formId + "_assoc_lecm-resolutions_base-document-assoc");
-        if (baseDocument && baseDocument.value) {
-            showSet(formId, "reviewers-hidden");
-
-            if (nodeRef) {
-                Alfresco.util.Ajax.jsonPost({
+        var value = (baseDocument && baseDocument.value);
+        if (value && nodeRef) {
+            Alfresco.util.Ajax.jsonPost(
+                {
                     url: Alfresco.constants.PROXY_URI + "lecm/substitude/format/node",
                     dataObj: {
-                        nodeRef: nodeRef,
-                        substituteString: "{lecm-statemachine:status}"
+                        nodeRef: value,
+                        substituteString: "{@doc.hasAspect('lecm-review-ts:review-aspect')}"
                     },
                     successCallback: {
                         fn: function (response) {
-                            if (response && response.json.formatString) {
-                                var status = response.json.formatString;
-                                if (status == "Черновик" || status == "На доработке" || status == "На утверждении" ||
-                                    status == Alfresco.util.message("lecm.resolutions.statemachine-status.draft") ||
-                                    status == Alfresco.util.message("lecm.resolutions.statemachine-status.on-rework") ||
-                                    status == Alfresco.util.message("lecm.resolutions.statemachine-status.on-approvment")) {
-                                    showSet(formId, "reviewers-list-hidden");
-                                } else if (status == "На исполнении" || status == "Завершено" ||
-                                    status == Alfresco.util.message("lecm.resolutions.statemachine-status.on-execution") ||
-                                    status == Alfresco.util.message("lecm.resolutions.statemachine-status.completed")) {
-                                    showSet(formId, "reviewers-table-hidden");
-                                }
+                            if (response.json && response.json.formatString && response.json.formatString == "true") {
+                                showSet(formId, "reviewers-hidden");
+                                    Alfresco.util.Ajax.jsonPost({
+                                        url: Alfresco.constants.PROXY_URI + "lecm/substitude/format/node",
+                                        dataObj: {
+                                            nodeRef: nodeRef,
+                                            substituteString: "{lecm-statemachine:status}"
+                                        },
+                                        successCallback: {
+                                            fn: function (response) {
+                                                if (response && response.json.formatString) {
+                                                    var status = response.json.formatString;
+                                                    if (status == "Черновик" || status == "На доработке" || status == "На утверждении" ||
+                                                        status == Alfresco.util.message("lecm.resolutions.statemachine-status.draft") ||
+                                                        status == Alfresco.util.message("lecm.resolutions.statemachine-status.on-rework") ||
+                                                        status == Alfresco.util.message("lecm.resolutions.statemachine-status.on-approvment")) {
+                                                        showSet(formId, "reviewers-list-hidden");
+                                                    } else if (status == "На исполнении" || status == "Завершено" ||
+                                                        status == Alfresco.util.message("lecm.resolutions.statemachine-status.on-execution") ||
+                                                        status == Alfresco.util.message("lecm.resolutions.statemachine-status.completed")) {
+                                                        showSet(formId, "reviewers-table-hidden");
+                                                    }
+                                                }
+                                            },
+                                            scope: this
+                                        },
+                                        failureMessage: Alfresco.util.message("message.details.failure"),
+                                        scope: this
+                                    });
                             }
                         },
                         scope: this
-                    },
-                    failureMessage: Alfresco.util.message("message.details.failure"),
-                    scope: this
+                    }
                 });
-            }
         }
     }
 
