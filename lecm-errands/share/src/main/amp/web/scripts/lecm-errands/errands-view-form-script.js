0aaccc309b9914db6229820f6d0d376c31d427bc
Башмаков Денис <DBashmakov@it.ru>
2019-07-18 09:38:38 +0300

    ALF-11027 - Типовые формы контента. Согласование. ALF-11038 - Документ. Конфликты и синхронизация панелей. ALF-11021 - Документ. Область действий.

--- lecm-errands/share/src/main/amp/web/scripts/lecm-errands/errands-view-form-script.js
+++ lecm-errands/share/src/main/amp/web/scripts/lecm-errands/errands-view-form-script.js
@@ -3,42 +3,12 @@
         'css/lecm-errands/errands-view-form.css'
     ]);
     var Dom = YAHOO.util.Dom,
-        Event = YAHOO.util.Event,
-        Selector = YAHOO.util.Selector,
-        Bubbling = YAHOO.Bubbling,
-        Substitute = YAHOO.lang.substitute;
+        Bubbling = YAHOO.Bubbling;
 
     Bubbling.on("errandsTabsFormScriptLoaded", processForm);
 
     function processForm(layer, args){
         var formId = args[1].formId;
-        var nodeRef = args[1].nodeRef;
-
-        if (nodeRef) {
-            Alfresco.util.Ajax.jsonGet({
-                url: Alfresco.constants.PROXY_URI + "/lecm/eds/tree/execution/datasource",
-                dataObj: {
-                    documentNodeRef: nodeRef
-                },
-                successCallback: {
-                    fn: function (response) {
-                        if (response && response.json) {
-                            var items = response.json.items;
-                            if (!items || !items.length) {
-                                var formTabs = Dom.get(formId + "-form-tabs");
-                                var executionTab = Selector.query("ul li", formTabs)[2];
-                                var executionTabContents = Selector.query('.yui-content .tab', formTabs)[2];
-                                executionTab.parentNode.removeChild(executionTab);
-                                executionTabContents.parentNode.removeChild(executionTabContents);
-                            }
-                        }
-                    },
-                    scope: this
-                },
-                failureMessage: Alfresco.util.message("message.details.failure"),
-                scope: this
-            });
-        }
         if (formId) {
             var periodicallyFieldId = "lecm-errands:periodically";
             var field = YAHOO.util.Dom.get(formId + "_prop_" + periodicallyFieldId.replace(":", "_"));
--- lecm-meetings/share/src/main/amp/web/scripts/lecm-meetings/components/meetings-agenda-info.js
+++ lecm-meetings/share/src/main/amp/web/scripts/lecm-meetings/components/meetings-agenda-info.js
@@ -1,25 +1,11 @@
-/**
- * LogicECM root namespace.
- *
- * @namespace LogicECM
- */
-// Ensure LogicECM root object exists
 if (typeof LogicECM == "undefined" || !LogicECM) {
     LogicECM = {};
 }
-
-/**
- * MeetingAgenda
- *
- * @namespace LogicECM
- * @class LogicECM.MeetingAgenda
- */
 (function () {
     /**
      * YUI Library aliases
      */
-    var Dom = YAHOO.util.Dom,
-        Event = YAHOO.util.Event;
+    var Dom = YAHOO.util.Dom;
 
     /**
      * MeetingAgenda constructor.
@@ -30,57 +16,34 @@ if (typeof LogicECM == "undefined" || !LogicECM) {
      */
     LogicECM.MeetingAgenda = function MeetingAgenda_constructor(htmlId) {
         LogicECM.MeetingAgenda.superclass.constructor.call(this, htmlId);
-
-	    YAHOO.Bubbling.on("formValueChanged", this.onUpdate, this);
         return this;
     };
 
-    YAHOO.extend(LogicECM.MeetingAgenda, LogicECM.DocumentComponentBase);
+    YAHOO.extend(LogicECM.MeetingAgenda, Alfresco.component.Base);
 
     YAHOO.lang.augmentObject(LogicECM.MeetingAgenda.prototype,
         {
-	        newId: null,
             options: {
-		        showAfterReady: false
+		        nodeRef: null,
+                componentHtmlId: null
 	        },
 
-            /**
-             * Fired by YUI when parent element is available for scripting.
-             * Template initialisation, including instantiation of YUI widgets and event listener binding.
-             *
-             * @method onReady
-             */
-            onReady: function MeetingAgenda_onReady() {
-                var id = this.newId ? this.newId : this.id;
-
-				var expandButton = Dom.getElementsByClassName('agenda-expand');
-				Event.addListener(expandButton, 'click', this.onExpand, this, true);
-
-                Alfresco.util.createTwister(id  + "-heading", "MeetingAgenda");
-
-				var lastCustomPanelViewTitle = this.getLastCustomPanelView();
-				if (lastCustomPanelViewTitle == this.getTitle() && this.isSplitPanel()) {
-					this.onExpand();
-				}
-
-				if (this.options.showAfterReady) {
-		            this.onExpand();
-	            }
-            },
-
-	        onExpand: function MeetingAgenda_onLinkClick() {
-                // Load the form
+            onReady: function () {
                 Alfresco.util.Ajax.request(
                     {
                         url: Alfresco.constants.URL_SERVICECONTEXT + "lecm/components/meetings/agenda-list",
                         dataObj: {
                             nodeRef: this.options.nodeRef,
-                            htmlid: (this.id + Alfresco.util.generateDomId()).replace(/\//g,"_")
+                            htmlid: this.id + "-" + Alfresco.util.generateDomId()
                         },
                         successCallback: {
                             fn:function(response){
-                                var text = response.serverResponse.responseText;
-                                this.expandView(text);
+                                var html = response.serverResponse.responseText;
+                                var formEl = Dom.get(this.id);
+                                if (formEl) {
+                                    formEl.innerHTML = "";
+                                    formEl.innerHTML = html;
+                                }
                             },
                             scope: this
                         },
@@ -88,51 +51,6 @@ if (typeof LogicECM == "undefined" || !LogicECM) {
                         scope: this,
                         execScripts: true
                     });
-            },
-
-	        onUpdate: function MeetingAgenda_onUpdate(layer, args) {
-		        Alfresco.util.Ajax.jsonGet(
-			        {
-				        url: Alfresco.constants.PROXY_URI + "/lecm/meetings/getAgendaInfo",
-				        dataObj: {
-					        nodeRef: this.options.nodeRef
-				        },
-				        successCallback: {
-					        fn:function(response){
-						        var container = Dom.get(this.id+"-formContainer");
-						        if (container != null) {
-							    	container.innerHTML = "";
-									var json = response.json;
-									if (json && json.size!=null) {
-										var sizeDiv =  document.createElement('div');
-										var span = document.createElement('span');
-										sizeDiv.appendChild(span);
-										container.appendChild(sizeDiv);
-										span.innerHTML = Alfresco.util.message('title.agenda_size', this.name)+": " + json.size;
-										
-										var statusDiv =  document.createElement('div');
-										span = document.createElement('span');
-										statusDiv.appendChild(span);
-										container.appendChild(statusDiv);
-										span.innerHTML = Alfresco.util.message("agenda_status."+json.status, this.name);
-										
-									} else {
-										var div = document.createElement('div');
-										div.className = "block-empty-body";
-										var span = document.createElement('span');
-										span.className = "block-empty faded";
-										div.appendChild(span);
-										container.appendChild(div);
-										span.innerHTML = Alfresco.util.message("message.block.empty");
-									}
-                                }
-					        },
-					        scope: this
-				        },
-				        failureMessage: this.msg("message.failure"),
-				        scope: this,
-				        execScripts: true
-			        });
-	        }
+            }
         }, true);
 })();
